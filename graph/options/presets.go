b89efc2069d934e86580386c180bb7016a748a6b
ZalgoNoise <zalgo.noise@gmail.com>
2022-08-21 14:25:23 +0200

    fix: typo

--- graph/options/presets.go
+++ graph/options/presets.go
@@ -3,7 +3,7 @@ package options
 type intID int
 
 var (
-	CfgAdjencyList Setting = MultiOption(
+	CfgAdjacencyList Setting = MultiOption(
 		GraphList,  // init type
 		Unweighted, // adjancy lists do not hold weights
 	)
