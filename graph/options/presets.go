e5df87be8335287241f982f339eb18c5d0591829
ZalgoNoise <zalgo.noise@gmail.com>
2022-08-21 20:59:35 +0200

    new: linked list preset config

--- graph/options/presets.go
+++ graph/options/presets.go
@@ -7,4 +7,11 @@ var (
 		GraphList,  // init type
 		Unweighted, // adjancy lists do not hold weights
 	)
+	CfgLinkedList Setting = MultiOption(
+		GraphList,      // init type
+		Unweighted,     // adjancy lists do not hold weights
+		NonCyclical,    // linked lists have no edges
+		NonDirectional, // linked lists have no edges
+		MaxNodes(1),    // linked lists only have one node
+	)
 )
