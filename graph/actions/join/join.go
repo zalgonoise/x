044e4d27883e4a325d46775cd6f626c6629155b1
ZalgoNoise <zalgo.noise@gmail.com>
2022-08-28 17:02:13 +0200

    fix: corrected logic gate filters

--- graph/actions/join/join.go
+++ graph/actions/join/join.go
@@ -2,14 +2,13 @@ package join
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/zalgonoise/x/graph"
 	"github.com/zalgonoise/x/graph/errs"
 	"github.com/zalgonoise/x/graph/model"
 )
 
-type FilterFunc[T model.ID, I model.Num] func(from, with model.Graph[T, I], node T, edge []T) bool
+type FilterFunc[T model.ID, I model.Num] func(from, with model.Graph[T, I], node T, edge T) bool
 
 func Join[T model.ID, I model.Num](from, with model.Graph[T, I], filter FilterFunc[T, I]) (model.Graph[T, I], error) {
 	out := graph.New[T, I](from.ID(), from.Value(), from.Config())
@@ -23,146 +22,102 @@ func Join[T model.ID, I model.Num](from, with model.Graph[T, I], filter FilterFu
 		return nil, err
 	}
 
-	var fromEdgeMap = map[T][]T{}
-	var withEdgeMap = map[T][]T{}
+	var fromEdgeMap = map[model.Graph[T, I]][]model.Graph[T, I]{}
+	var withEdgeMap = map[model.Graph[T, I]][]model.Graph[T, I]{}
 
 	for _, fromNode := range nodesFrom {
-		if filter(from, with, fromNode.ID(), nil) { //&& doesNotExist(out, fromNode.ID(), nil) {
-			err = out.Add(fromNode)
-			if err != nil {
-				return nil, err
-			}
-		}
-
 		fromEdges, err := from.Edges(fromNode.ID())
 		if err != nil {
 			return nil, err
 		}
 
-		fromEdgeMap[fromNode.ID()] = getIDs(fromEdges)
+		fromEdgeMap[fromNode] = append(fromEdgeMap[fromNode], fromEdges...)
 
 	}
 	for _, withNode := range nodesWith {
-		if filter(from, with, withNode.ID(), nil) { // && doesNotExist(out, withNode.ID(), nil) {
-			err = out.Add(withNode)
-			if err != nil {
-				return nil, err
-			}
-		}
 		withEdges, err := with.Edges(withNode.ID())
 		if err != nil {
 			return nil, err
 		}
 
-		withEdgeMap[withNode.ID()] = getIDs(withEdges)
+		withEdgeMap[withNode] = append(withEdgeMap[withNode], withEdges...)
 	}
 
 	for k, v := range fromEdgeMap {
 		for _, edge := range v {
-			w, err := from.Weight(k, edge)
+			w, err := from.Weight(k.ID(), edge.ID())
 			if err != nil {
 				return nil, err
 			}
 
-			if filter(from, with, k, []T{edge}) { //&& doesNotExist(out, k, []T{edge}) {
-				out.Connect(k, edge, w)
+			if filter(from, with, k.ID(), edge.ID()) {
+				if _, err := out.Get(k.ID()); errors.Is(err, errs.DoesNotExist) {
+					out.Add(k)
+				}
+				if _, err := out.Get(edge.ID()); errors.Is(err, errs.DoesNotExist) {
+					out.Add(edge)
+				}
+
+				out.Connect(k.ID(), edge.ID(), w)
 			}
 		}
 	}
 
 	for k, v := range withEdgeMap {
 		for _, edge := range v {
-			w, err := with.Weight(k, edge)
+			w, err := with.Weight(k.ID(), edge.ID())
 			if err != nil {
 				return nil, err
 			}
 
-			if filter(from, with, k, []T{edge}) { //&& doesNotExist(out, k, []T{edge}) {
-				out.Connect(k, edge, w)
+			if filter(from, with, k.ID(), edge.ID()) {
+				if _, err := out.Get(k.ID()); errors.Is(err, errs.DoesNotExist) {
+					out.Add(k)
+				}
+				if _, err := out.Get(edge.ID()); errors.Is(err, errs.DoesNotExist) {
+					out.Add(edge)
+				}
+
+				out.Connect(k.ID(), edge.ID(), w)
 			}
 		}
 	}
 
-	l, _ := out.List()
-	fmt.Println(l[0].ID(), l[1].ID())
-
 	return out, nil
 }
 
-func OR[T model.ID, I model.Num](from, with model.Graph[T, I], node T, edge []T) bool {
+func OR[T model.ID, I model.Num](from, with model.Graph[T, I], node, edge T) bool {
 	return true
 }
 
-func AND[T model.ID, I model.Num](from, with model.Graph[T, I], node T, edge []T) bool {
-	if edge == nil {
-		_, ferr := from.Get(node)
-		fromOK := errors.Is(ferr, errs.DoesNotExist)
-
-		_, werr := with.Get(node)
-		withOK := errors.Is(werr, errs.DoesNotExist)
-
-		if !fromOK && !withOK {
-			return true
-		}
-		return false
-	}
-	_, ferr := from.Weight(node, edge[0])
+func AND[T model.ID, I model.Num](from, with model.Graph[T, I], node, edge T) bool {
+	fw, ferr := from.Weight(node, edge)
 	fromOK := errors.Is(ferr, errs.DoesNotExist)
 
-	_, werr := with.Weight(node, edge[0])
+	ww, werr := with.Weight(node, edge)
 	withOK := errors.Is(werr, errs.DoesNotExist)
 
-	if !fromOK && !withOK {
+	if !fromOK && !withOK && fw != 0 && ww != 0 {
 		return true
 	}
 	return false
 }
 
-func XOR[T model.ID, I model.Num](from, with model.Graph[T, I], node T, edge []T) bool {
-	if edge == nil {
-		_, ferr := from.Get(node)
-		fromOK := errors.Is(ferr, errs.DoesNotExist)
+func XOR[T model.ID, I model.Num](from, with model.Graph[T, I], node, edge T) bool {
+	_, ferr := from.Weight(node, edge)
+	_, werr := with.Weight(node, edge)
 
-		_, werr := with.Get(node)
-		withOK := errors.Is(werr, errs.DoesNotExist)
-
-		if (!fromOK && withOK) || (fromOK && !withOK) {
-			return true
-		}
-		return false
-	}
-	_, ferr := from.Weight(node, edge[0])
-	fromOK := errors.Is(ferr, errs.DoesNotExist)
-
-	_, werr := with.Weight(node, edge[0])
-	withOK := errors.Is(werr, errs.DoesNotExist)
-
-	if (!fromOK && withOK) || (fromOK && !withOK) {
+	if (ferr == nil && werr != nil) || (ferr != nil && werr == nil) {
 		return true
 	}
 	return false
 }
 
-func NOT[T model.ID, I model.Num](from, with model.Graph[T, I], node T, edge []T) bool {
-	if edge == nil {
-		_, ferr := from.Get(node)
-		fromOK := errors.Is(ferr, errs.DoesNotExist)
+func NOT[T model.ID, I model.Num](from, with model.Graph[T, I], node, edge T) bool {
+	fw, ferr := from.Weight(node, edge)
+	ww, werr := with.Weight(node, edge)
 
-		_, werr := with.Get(node)
-		withOK := errors.Is(werr, errs.DoesNotExist)
-
-		if fromOK && withOK {
-			return false
-		}
-		return true
-	}
-	_, ferr := from.Weight(node, edge[0])
-	fromOK := errors.Is(ferr, errs.DoesNotExist)
-
-	_, werr := with.Weight(node, edge[0])
-	withOK := errors.Is(werr, errs.DoesNotExist)
-
-	if fromOK && withOK {
+	if ferr == nil && werr == nil && fw == ww {
 		return false
 	}
 	return true
