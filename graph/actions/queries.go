19d14773a0e5e889157a6a927764743050cb2f33
zalgonoise <zalgo.noise@gmail.com>
2022-09-07 19:33:54 +0200

    new: added multigraph lookups

--- graph/actions/queries.go
+++ graph/actions/queries.go
@@ -1,6 +1,9 @@
 package actions
 
 import (
+	"errors"
+
+	"github.com/zalgonoise/x/graph/errs"
 	"github.com/zalgonoise/x/graph/model"
 )
 
@@ -85,6 +88,62 @@ func DepthFirstSearch[T model.ID, I model.Num](g model.Graph[T, I], fn func(from
 		}
 	}
 	return true, nil
+}
+
+func LeafLookup[T model.ID, I model.Num](from model.Graph[T, I], to T) (parent model.Graph[T, I], err error) {
+	var (
+		g       model.Graph[T, I] = from
+		visited                   = map[model.Graph[T, I]]struct{}{from: {}}
+	)
+	if g == nil {
+		return nil, errs.DoesNotExist
+	}
+	for g.Parent() != nil {
+		g = g.Parent()
+
+		// short circuit out - lookup in this graph
+		toNode, err := g.Get(to)
+		if err == nil && toNode != nil {
+			return g, nil
+		}
+		// continue until it gets to root
+		visited[g] = struct{}{}
+	}
+
+	return leafLookup(g, to, visited)
+
+}
+
+func leafLookup[T model.ID, I model.Num](root model.Graph[T, I], to T, visited map[model.Graph[T, I]]struct{}) (parent model.Graph[T, I], err error) {
+	nodes, err := root.List()
+	if err != nil {
+		return nil, err
+	}
+
+	if _, ok := visited[root]; !ok {
+		visited[root] = struct{}{}
+	}
+
+	for _, node := range nodes {
+		if _, ok := visited[node]; ok {
+			continue
+		}
+
+		// short circuit out
+		if n, err := node.Get(to); err == nil && n != nil {
+			return node, nil
+		}
+
+		p, err := leafLookup(node, to, visited)
+		if err != nil && !errors.Is(err, errs.DoesNotExist) {
+			return nil, err
+		}
+		if p != nil && err == nil {
+			return p, nil
+		}
+	}
+
+	return nil, errs.DoesNotExist
 }
 
 func VerifyCycles[T model.ID, I model.Num](from, to model.Graph[T, I]) func(target, edge model.Graph[T, I], weight I) bool {
