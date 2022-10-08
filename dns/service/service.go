1f6429b9b2b71c5292d0bf14eb09f18075e96a57
ZalgoNoise <zalgo.noise@gmail.com>
2022-10-08 19:34:37 +0200

    fix: slight refactor, reorganizing

--- dns/service/service.go
+++ dns/service/service.go
@@ -1,31 +1,70 @@
 package service
 
 import (
-	"github.com/zalgonoise/x/dns/service/dns"
-	"github.com/zalgonoise/x/dns/service/store"
+	"context"
+
+	dnsr "github.com/miekg/dns"
+	"github.com/zalgonoise/x/dns/dns"
+	"github.com/zalgonoise/x/dns/store"
 )
 
 type Service interface {
-	DNS() *dns.DNSRepository
-	Store() *store.StoreRepository
+	dns.DNSRepository
+	store.StoreRepository
 }
 
 type service struct {
-	dns   *dns.DNSRepository
-	store *store.StoreRepository
+	dns   dns.DNSRepository
+	store store.StoreRepository
 }
 
-func New(dns *dns.DNSRepository, store *store.StoreRepository) Service {
+func New(dns dns.DNSRepository, store store.StoreRepository) Service {
 	return &service{
 		dns:   dns,
 		store: store,
 	}
 }
 
-func (s *service) DNS() *dns.DNSRepository {
-	return s.dns
+func (s *service) ParseQuery(m *dnsr.Msg) {
+	s.dns.ParseQuery(m)
 }
 
-func (s *service) Store() *store.StoreRepository {
-	return s.store
+func (s *service) HandleRequest(w dnsr.ResponseWriter, r *dnsr.Msg) {
+	s.dns.HandleRequest(w, r)
+}
+
+func (s *service) Start() error {
+	return s.dns.Start()
+}
+
+func (s *service) Stop() error {
+	return s.dns.Stop()
+}
+
+func (s *service) Reload() error {
+	return s.dns.Reload()
+}
+
+func (s *service) Add(ctx context.Context, r store.Record) error {
+	return s.store.Add(ctx, r)
+}
+
+func (s *service) List(ctx context.Context) ([]store.Record, error) {
+	return s.store.List(ctx)
+}
+
+func (s *service) GetByAddr(ctx context.Context, addr string) (store.Record, error) {
+	return s.store.GetByAddr(ctx, addr)
+}
+
+func (s *service) GetByDest(ctx context.Context, addr string) ([]store.Record, error) {
+	return s.store.GetByDest(ctx, addr)
+}
+
+func (s *service) Update(ctx context.Context, addr string, r store.Record) error {
+	return s.store.Update(ctx, addr, r)
+}
+
+func (s *service) Delete(ctx context.Context, addr string) error {
+	return s.store.Delete(ctx, addr)
 }
---
+++ dns/store/store.go
@@ -0,0 +1,44 @@
+package store
+
+import "context"
+
+// Record defines the elements of a DNS Record
+//
+// TODO: add the elements necessary to comprehend the most common
+// DNS records' elements
+type Record struct{}
+
+// StoreRepository defines the behavior that a record store should have
+//
+// This will consist of basic CRUD operations against a key-value store,
+// to add, list, get, update or delete DNS Records from the key-value store.
+//
+// Additionally, it is exposing both GetByAddr and GetByDest methods to
+// fetch items in the records list interchangeably
+type StoreRepository interface {
+	// Add will create a new entry in they key-value store to include a
+	// new Record, returning an error
+	Add(ctx context.Context, r Record) error
+
+	// List will fetch all records in the key-value store
+	//
+	// TODO: implement listing filters
+	List(ctx context.Context) ([]Record, error)
+
+	// GetByAddr will fetch an address based on its address string
+	//
+	// GetByAddr(ctx, "service.mydomain") -> { "127.0.0.1", nil }
+	GetByAddr(ctx context.Context, addr string) (Record, error)
+
+	// GetByDest will fetch an address based on its target IP
+	//
+	// GetByDest(ctx, "127.0.0.1") -> { ["service.mydomain"], nil }
+	GetByDest(ctx context.Context, addr string) ([]Record, error)
+
+	// Update will modify an existing record by targetting its address string,
+	// and by supplying a new version of the Record to update. Returns an error
+	Update(ctx context.Context, addr string, r Record) error
+
+	// Delete will remove a DNS Record by targetting its address string
+	Delete(ctx context.Context, addr string) error
+}
