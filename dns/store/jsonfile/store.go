013a8352ab3c37d6094aefc889a7db205b1263fc
ZalgoNoise <zalgo.noise@gmail.com>
2022-10-23 21:40:28 +0200

    fix: updated json / yaml store logic

--- dns/store/jsonfile/store.go
+++ dns/store/jsonfile/store.go
@@ -2,6 +2,7 @@ package jsonfile
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/zalgonoise/x/dns/store"
 )
@@ -12,33 +13,50 @@ import (
 // a `Sync()` call to ensure the records file is up-to-date
 func (f *FileStore) Create(ctx context.Context, rs ...*store.Record) error {
 	f.mtx.Lock()
-	defer func() {
-		_ = f.sync()
-	}()
 	defer f.mtx.Unlock()
 
-	return f.store.Create(ctx, rs...)
+	err := f.store.Create(ctx, rs...)
+	if err != nil {
+		return fmt.Errorf("failed to create record: %w", err)
+	}
+	err = f.sync()
+	if err != nil {
+		return fmt.Errorf("failed to sync store to file: %w", err)
+	}
+	return nil
 }
 
 // List implements the store.Repository interface
 //
 // It will call the in-memory store's method of the same signature
 func (f *FileStore) List(ctx context.Context) ([]*store.Record, error) {
-	return f.store.List(ctx)
+	rs, err := f.store.List(ctx)
+	if err != nil {
+		return rs, fmt.Errorf("failed to list records: %w", err)
+	}
+	return rs, nil
 }
 
 // FilterByDomain implements the store.Repository interface
 //
 // It will call the in-memory store's method of the same signature
 func (f *FileStore) FilterByDomain(ctx context.Context, r *store.Record) (*store.Record, error) {
-	return f.store.FilterByDomain(ctx, r)
+	r, err := f.store.FilterByDomain(ctx, r)
+	if err != nil {
+		return r, fmt.Errorf("failed to get record by domain: %w", err)
+	}
+	return r, nil
 }
 
 // FilterByDest implements the store.Repository interface
 //
 // It will call the in-memory store's method of the same signature
 func (f *FileStore) FilterByDest(ctx context.Context, r *store.Record) ([]*store.Record, error) {
-	return f.store.FilterByDest(ctx, r)
+	rs, err := f.store.FilterByDest(ctx, r)
+	if err != nil {
+		return rs, fmt.Errorf("failed to get record by address: %w", err)
+	}
+	return rs, nil
 }
 
 // Update implements the store.Repository interface
@@ -47,12 +65,17 @@ func (f *FileStore) FilterByDest(ctx context.Context, r *store.Record) ([]*store
 // a `Sync()` call to ensure the records file is up-to-date
 func (f *FileStore) Update(ctx context.Context, domain string, r *store.Record) error {
 	f.mtx.Lock()
-	defer func() {
-		_ = f.sync()
-	}()
 	defer f.mtx.Unlock()
 
-	return f.store.Update(ctx, domain, r)
+	err := f.store.Update(ctx, domain, r)
+	if err != nil {
+		return fmt.Errorf("failed to update record: %w", err)
+	}
+	err = f.sync()
+	if err != nil {
+		return fmt.Errorf("failed to sync store to file: %w", err)
+	}
+	return nil
 }
 
 // Delete implements the store.Repository interface
@@ -61,10 +84,15 @@ func (f *FileStore) Update(ctx context.Context, domain string, r *store.Record) 
 // a `Sync()` call to ensure the records file is up-to-date
 func (f *FileStore) Delete(ctx context.Context, r *store.Record) error {
 	f.mtx.Lock()
-	defer func() {
-		_ = f.sync()
-	}()
 	defer f.mtx.Unlock()
 
-	return f.store.Delete(ctx, r)
+	err := f.store.Delete(ctx, r)
+	if err != nil {
+		return fmt.Errorf("failed to delete record: %w", err)
+	}
+	err = f.sync()
+	if err != nil {
+		return fmt.Errorf("failed to sync store to file: %w", err)
+	}
+	return nil
 }
