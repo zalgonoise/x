5843fa99ca0638c7c6a229f03723726209f1530d
zalgonoise <zalgo.noise@gmail.com>
2022-10-23 18:33:36 +0200

    new: memmap tests

---
+++ dns/store/memmap/store_test.go
@@ -0,0 +1,348 @@
+package memmap
+
+import (
+	"context"
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/zalgonoise/x/dns/store"
+)
+
+var (
+	test1 = store.New().Name("not.a.dom.ain").Type("A").Addr("192.168.0.10").Build()
+	test2 = store.New().Name("really.not.a.dom.ain").Type("A").Addr("192.168.0.15").Build()
+	test3 = store.New().Name("really.not.a.dom.ain").Type("CNAME").Addr("am.i.not.a.dom.ain.").Build()
+	test4 = store.New().Name("am.i.not.a.dom.ain").Type("A").Addr("192.168.0.15").Build()
+)
+
+func TestCreate(t *testing.T) {
+	t.Run("OneRecord", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+
+		err := s.Create(ctx, test1)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		records, ok := s.(*MemoryStore).Records[test1.Name]
+		if !ok {
+			t.Errorf("expected entry for %v to be present in the store, but domain is not assigned", test1)
+		}
+		addr, ok := records[test1.Type]
+		if !ok {
+			t.Errorf("expected entry for %v to be present in the store, but record type is not assigned", test1)
+		}
+		if addr != test1.Addr {
+			t.Errorf("stored address %s is incompatible with %v", addr, test1)
+		}
+	})
+	t.Run("ManyRecords", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+
+		err := s.Create(ctx, test1, test2)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		records, ok := s.(*MemoryStore).Records[test1.Name]
+		if !ok {
+			t.Errorf("expected entry for %v to be present in the store, but domain is not assigned", test1)
+		}
+		addr, ok := records[test1.Type]
+		if !ok {
+			t.Errorf("expected entry for %v to be present in the store, but record type is not assigned", test1)
+		}
+		if addr != test1.Addr {
+			t.Errorf("stored address %s is incompatible with %v", addr, test1)
+		}
+
+		records, ok = s.(*MemoryStore).Records[test2.Name]
+		if !ok {
+			t.Errorf("expected entry for %v to be present in the store, but domain is not assigned", test2)
+		}
+		addr, ok = records[test2.Type]
+		if !ok {
+			t.Errorf("expected entry for %v to be present in the store, but record type is not assigned", test2)
+		}
+		if addr != test2.Addr {
+			t.Errorf("stored address %s is incompatible with %v", addr, test2)
+		}
+	})
+}
+
+func TestList(t *testing.T) {
+	t.Run("EmptyStore", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+
+		rs, err := s.List(ctx)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		if len(rs) != 0 {
+			t.Errorf("expected store to have zero records; got %v", len(rs))
+		}
+
+	})
+
+	t.Run("PopulatedStore", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+
+		err := s.Create(ctx, test1, test2)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		rs, err := s.List(ctx)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		if len(rs) != 2 {
+			t.Errorf("expected store to have %v records; got %v", 2, len(rs))
+		}
+	})
+}
+
+func TestFilterByDomain(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+
+		err := s.Create(ctx, test1, test2)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		query := store.New().Name(test2.Name).Type(test2.Type).Build()
+
+		r, err := s.FilterByDomain(ctx, query)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		if r == nil {
+			t.Errorf("expected a succesful query, returned store.Record is nil")
+		}
+
+		if r.Addr != test2.Addr {
+			t.Errorf("unexpected output IP address: wanted %s ; got %s", test2.Addr, r.Addr)
+		}
+	})
+	t.Run("FailDoesNotExistDomain", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+
+		err := s.Create(ctx, test1)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		query := store.New().Name(test2.Name).Type(test2.Type).Build()
+
+		_, err = s.FilterByDomain(ctx, query)
+		if !errors.Is(store.ErrDoesNotExist, err) {
+			t.Errorf("unexpected error: %v", err)
+		}
+	})
+	t.Run("FailDoesNotExistType", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+
+		err := s.Create(ctx, test1, test3)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		query := store.New().Name(test2.Name).Type(test2.Type).Build()
+
+		_, err = s.FilterByDomain(ctx, query)
+		if !errors.Is(store.ErrDoesNotExist, err) {
+			t.Errorf("unexpected error: %v", err)
+		}
+	})
+}
+
+func TestFilterByDest(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+		wants := []*store.Record{test2, test4}
+
+		err := s.Create(ctx, test1, test2, test3, test4)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		query := store.New().Addr(test2.Addr).Build()
+
+		rs, err := s.FilterByDest(ctx, query)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		if len(rs) != 2 {
+			t.Errorf("unexpected results length: wanted %v ; got %v", 2, len(rs))
+		}
+
+		for idx, record := range rs {
+			var passes bool
+			for _, want := range wants {
+				if reflect.DeepEqual(want, record) {
+					passes = true
+					break
+				}
+			}
+
+			if !passes {
+				t.Errorf("output mismatch error: record #%v doesn't match any of %v: record: %v", idx, wants, record)
+			}
+		}
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+		updated := store.New().Name(test1.Name).Type(test1.Type).Addr("192.168.0.9").Build()
+
+		err := s.Create(ctx, test1)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		err = s.Update(ctx, updated.Name, updated)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		records, ok := s.(*MemoryStore).Records[updated.Name]
+		if !ok {
+			t.Errorf("expected entry for %v to be present in the store, but domain is not assigned", updated)
+		}
+		addr, ok := records[updated.Type]
+		if !ok {
+			t.Errorf("expected entry for %v to be present in the store, but record type is not assigned", updated)
+		}
+		if addr != updated.Addr {
+			t.Errorf("stored address %s is incompatible with %v", addr, updated)
+		}
+	})
+
+	t.Run("FailDoesNotExistDomain", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+
+		err := s.Create(ctx, test1)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		query := store.New().Name(test2.Name).Type(test2.Type).Build()
+
+		err = s.Update(ctx, query.Name, query)
+		if !errors.Is(store.ErrDoesNotExist, err) {
+			t.Errorf("unexpected error: %v", err)
+		}
+	})
+	t.Run("FailDoesNotExistType", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+
+		err := s.Create(ctx, test1, test3)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		query := store.New().Name(test2.Name).Type(test2.Type).Build()
+
+		err = s.Update(ctx, query.Name, query)
+		if !errors.Is(store.ErrDoesNotExist, err) {
+			t.Errorf("unexpected error: %v", err)
+		}
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("SuccessByDomain", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+
+		err := s.Create(ctx, test1, test2, test3, test4)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		query := store.New().Name(test2.Name).Build()
+
+		err = s.Delete(ctx, query)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		rs, err := s.List(ctx)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		if len(rs) != 2 {
+			t.Errorf("unexpected output length: wanted %v records ; got %v", 2, len(rs))
+		}
+	})
+	t.Run("SuccessByDomainAndType", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+
+		err := s.Create(ctx, test1, test2, test3, test4)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		query := store.New().Name(test2.Name).Type(test2.Type).Build()
+
+		err = s.Delete(ctx, query)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		rs, err := s.List(ctx)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		if len(rs) != 3 {
+			t.Errorf("unexpected output length: wanted %v records ; got %v", 3, len(rs))
+		}
+	})
+	t.Run("SuccessByAddress", func(t *testing.T) {
+		ctx := context.Background()
+		s := New()
+
+		err := s.Create(ctx, test1, test2, test3, test4)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		query := store.New().Addr(test2.Addr).Build()
+
+		err = s.Delete(ctx, query)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		rs, err := s.List(ctx)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		if len(rs) != 2 {
+			t.Errorf("unexpected output length: wanted %v records ; got %v", 2, len(rs))
+		}
+	})
+}
