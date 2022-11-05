b2534299474ea87a391bfab00fb602f1c2f0afde
ZalgoNoise <zalgo.noise@gmail.com>
2022-11-05 01:48:03 +0100

    new: finished transport tests

--- dns/transport/e2e/transport_test.go
+++ dns/transport/e2e/transport_test.go
@@ -8,20 +8,20 @@ package e2e
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"regexp"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/miekg/dns"
+	"github.com/zalgonoise/x/dns/health"
 	"github.com/zalgonoise/x/dns/store"
-)
-
-var (
-	record1 = store.New().Type("A").Name("not.a.dom.ain").Addr("192.168.0.10").Build()
-	record2 = store.New().Type("A").Name("really.not.a.dom.ain").Addr("192.168.0.10").Build()
+	"github.com/zalgonoise/x/dns/transport/httpapi/endpoints"
 )
 
 func httpReq(endpoint string, data []byte) ([]byte, int, error) {
@@ -102,6 +102,8 @@ func TestTransport(t *testing.T) {
 		})
 		t.Run("Store", func(t *testing.T) {
 			t.Run("AddRecord", func(t *testing.T) {
+				wants := `{"success":true,"message":"added record successfully","record":{"type":"A","name":"not.a.dom.ain","address":"192.168.0.10"}}`
+
 				b, status, err := httpReq("/records/add", []byte(`{"name":"not.a.dom.ain","type":"A","address":"192.168.0.10"}`))
 				if err != nil {
 					t.Errorf("unexpected error: %v", err)
@@ -110,6 +112,12 @@ func TestTransport(t *testing.T) {
 
 				if status != 200 {
 					t.Errorf("unexpected status code: wanted %v ; got %v -- body: %s", 200, status, string(b))
+					return
+				}
+
+				if string(b) != wants {
+					t.Errorf("unexpected response: wanted %s ; got %s", wants, string(b))
+					return
 				}
 			})
 			t.Run("ListRecords", func(t *testing.T) {
@@ -150,15 +158,97 @@ func TestTransport(t *testing.T) {
 					return
 				}
 			})
+			t.Run("GetRecordByAddress", func(t *testing.T) {
+				wants := `{"success":true,"message":"listing all records for IP address 192.168.0.10","records":[{"type":"A","name":"not.a.dom.ain","address":"192.168.0.10"}]}`
+
+				b, status, err := httpReq("/records/getDomains", []byte(`{"address":"192.168.0.10"}`))
+				if err != nil {
+					t.Errorf("unexpected error: %v", err)
+					return
+				}
+
+				if status != 200 {
+					t.Errorf("unexpected status code: wanted %v ; got %v -- body: %s", 200, status, string(b))
+					return
+				}
+
+				if string(b) != wants {
+					t.Errorf("unexpected response: wanted %s ; got %s", wants, string(b))
+					return
+				}
+			})
+			t.Run("UpdateRecord", func(t *testing.T) {
+				wants := `{"success":true,"message":"updated record successfully","record":{"type":"A","name":"really.not.a.dom.ain","address":"192.168.0.10"}}`
+
+				b, status, err := httpReq("/records/update", []byte(`{"target":"not.a.dom.ain","record":{"name":"really.not.a.dom.ain","type":"A","address":"192.168.0.10"}}`))
+				if err != nil {
+					t.Errorf("unexpected error: %v", err)
+					return
+				}
+
+				if status != 200 {
+					t.Errorf("unexpected status code: wanted %v ; got %v -- body: %s", 200, status, string(b))
+					return
+				}
+
+				if string(b) != wants {
+					t.Errorf("unexpected response: wanted %s ; got %s", wants, string(b))
+					return
+				}
+			})
 		})
 	})
 	t.Run("UDP", func(t *testing.T) {
+		t.Run("LocalQuery", func(t *testing.T) {
+			m := dnsQuestion("A", "really.not.a.dom.ain")
 
+			if m == nil {
+				t.Errorf("unexpected nil DNS message")
+				return
+			}
+			if len(m.Answer) != 1 {
+				t.Errorf("unexpected answer length: wanted %v ; got %v", 1, len(m.Answer))
+				return
+			}
+
+			if !strings.Contains(m.Answer[0].String(), "really.not.a.dom.ain") {
+				t.Errorf("missing expected domain name: %s", m.Answer[0].String())
+			}
+			if !strings.Contains(m.Answer[0].String(), "A") {
+				t.Errorf("missing expected record type: %s", m.Answer[0].String())
+			}
+			if !strings.Contains(m.Answer[0].String(), "192.168.0.10") {
+				t.Errorf("missing expected IP address: %s", m.Answer[0].String())
+			}
+		})
+		t.Run("ExternalQuery", func(t *testing.T) {
+			addrRgx := regexp.MustCompile(`([\d]+?\.){3}[\d]+`)
+			m := dnsQuestion("A", "google.com")
+
+			if m == nil {
+				t.Errorf("unexpected nil DNS message")
+				return
+			}
+			if len(m.Answer) != 1 {
+				t.Errorf("unexpected answer length: wanted %v ; got %v", 1, len(m.Answer))
+				return
+			}
+
+			if !strings.Contains(m.Answer[0].String(), "google.com") {
+				t.Errorf("missing expected domain name: %s", m.Answer[0].String())
+			}
+			if !strings.Contains(m.Answer[0].String(), "A") {
+				t.Errorf("missing expected record type: %s", m.Answer[0].String())
+			}
+			if !addrRgx.MatchString(m.Answer[0].String()) {
+				t.Errorf("missing expected IP address: %s", m.Answer[0].String())
+			}
+		})
 	})
 
 	t.Run("HTTP", func(t *testing.T) {
-		t.Run("StopDNS", func(t *testing.T) {
-			b, status, err := httpReq("/dns/stop", nil)
+		t.Run("Health", func(t *testing.T) {
+			b, status, err := httpReq("/health", nil)
 			if err != nil {
 				t.Errorf("unexpected error: %v", err)
 				return
@@ -166,7 +256,52 @@ func TestTransport(t *testing.T) {
 
 			if status != 200 {
 				t.Errorf("unexpected status code: wanted %v ; got %v -- body: %s", 200, status, string(b))
+				return
 			}
+
+			res := &endpoints.HealthResponse{}
+			_ = json.Unmarshal(b, res)
+
+			if res.Report.Status != health.Healthy {
+				t.Errorf("service status is not as expected: wanted %v ; got %v", health.Healthy, res.Report.Status)
+				return
+			}
+
+		})
+		t.Run("Store", func(t *testing.T) {
+			t.Run("DeleteRecord", func(t *testing.T) {
+				wants := `{"success":true,"message":"record deleted successfully"}`
+
+				b, status, err := httpReq("/records/delete", []byte(`{"name":"really.not.a.dom.ain","type":"A"}`))
+				if err != nil {
+					t.Errorf("unexpected error: %v", err)
+					return
+				}
+
+				if status != 200 {
+					t.Errorf("unexpected status code: wanted %v ; got %v -- body: %s", 200, status, string(b))
+					return
+				}
+
+				if string(b) != wants {
+					t.Errorf("unexpected response: wanted %s ; got %s", wants, string(b))
+					return
+				}
+
+			})
+		})
+		t.Run("DNS", func(t *testing.T) {
+			t.Run("StopDNS", func(t *testing.T) {
+				b, status, err := httpReq("/dns/stop", nil)
+				if err != nil {
+					t.Errorf("unexpected error: %v", err)
+					return
+				}
+
+				if status != 200 {
+					t.Errorf("unexpected status code: wanted %v ; got %v -- body: %s", 200, status, string(b))
+				}
+			})
 		})
 	})
 }
