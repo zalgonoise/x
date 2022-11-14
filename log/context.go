f4de5d57ce3fb4ccdf3229b62b38f10961d126ca
ZalgoNoise <zalgo.noise@gmail.com>
2022-11-14 22:40:49 +0100

    new: added context logger

--- log/context.go
+++ log/context.go
@@ -2,9 +2,17 @@ package log
 
 import "context"
 
+type CtxLoggerKey string
+
+const StandardCtxKey CtxLoggerKey = "logger"
+
 func InContext(ctx context.Context, logger Logger) context.Context {
-	return nil
+	return context.WithValue(ctx, StandardCtxKey, logger)
 }
 func From(ctx context.Context) Logger {
-	return nil
+	logger := ctx.Value(StandardCtxKey)
+	if logger == nil {
+		return std
+	}
+	return (logger).(Logger)
 }
