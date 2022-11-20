455b6fff98b4dea5fe975f1f5de8bd1924c98c4b
ZalgoNoise <zalgo.noise@gmail.com>
2022-11-20 20:14:01 +0100

    new: added code docs

--- log/level/level.go
+++ log/level/level.go
@@ -1,7 +1,12 @@
 package level
 
+// Level interface describes the behavior that a log level should have
+//
+// It must provide methods to be casted as a string or as an int
 type Level interface {
+	// String returns the level as a string
 	String() string
+	// Int returns the level as an int
 	Int() int
 }
 
@@ -17,22 +22,32 @@ const (
 )
 
 var (
+	// Trace represents log level 0
 	Trace Level = lTrace
+	// Debug represents log level 1
 	Debug Level = lDebug
-	Info  Level = lInfo
-	Warn  Level = lWarn
+	// Info represents log level 2
+	Info Level = lInfo
+	// Warn represents log level 3
+	Warn Level = lWarn
+	// Error represents log level 4
 	Error Level = lError
+	// Fatal represents log level 5
 	Fatal Level = lFatal
 )
 
+// String returns the level as a string
 func (l lv) String() string {
 	return lvValues[l]
 }
 
+// Int returns the level as an int
 func (l lv) Int() int {
 	return (int)(l)
 }
 
+// AsLevel converts an input string to a Level, returning nil if
+// invalid
 func AsLevel(level string) Level {
 	if l, ok := lvKeys[level]; ok {
 		return l
--- log/records/record.go
+++ log/records/record.go
@@ -7,15 +7,36 @@ import (
 	"github.com/zalgonoise/x/log/level"
 )
 
+// Record interface describes the behavior that a Record should have
+//
+// It expose getter methods for its elements, as well as two helper methods:
+//   - `AddAttr()` will return a copy of this Record with the input Attr appended
+//     to the existing ones
+//   - `AttrLen()` will return the length of the attributes in the record
 type Record interface {
+	// AddAttr returns a copy of this Record with the input Attr appended to the
+	// existing ones
 	AddAttr(a ...attr.Attr) Record
+	// Attrs returns the slice of Attr associated to this Record
 	Attrs() []attr.Attr
+	// AttrLen returns the length of the slice of Attr in the Record
 	AttrLen() int
+	// Message returns the string Message associated to this Record
 	Message() string
+	// Time returns the time.Time timestamp associated to this Record
 	Time() time.Time
+	// Level returns the level.Level level associated to this Record
 	Level() level.Level
 }
 
+// New will return a Record based on the input time.Time `t`, level.Level `lv`,
+// message string `msg` and attributes `attrs`.
+//
+// If the input time is zero or Unix Time zero, it will set the Records time as now.
+// If the level is nil, level.Info is set instead
+// If there are nil attr.Attr values, they are dismissed
+//
+// A Record is based on an immutable data structure
 func New(t time.Time, lv level.Level, msg string, attrs ...attr.Attr) Record {
 	if t.IsZero() || t == time.Unix(0, 0) {
 		t = time.Now()
@@ -45,6 +66,8 @@ type record struct {
 	attrs     []attr.Attr
 }
 
+// AddAttr returns a copy of this Record with the input Attr appended to the
+// existing ones
 func (r record) AddAttr(attrs ...attr.Attr) Record {
 	as := r.attrs
 	for _, a := range attrs {
@@ -60,22 +83,27 @@ func (r record) AddAttr(attrs ...attr.Attr) Record {
 	}
 }
 
+// Attrs returns the slice of Attr associated to this Record
 func (r record) Attrs() []attr.Attr {
 	return r.attrs
 }
 
+// AttrLen returns the length of the slice of Attr in the Record
 func (r record) AttrLen() int {
 	return len(r.attrs)
 }
 
+// Message returns the string Message associated to this Record
 func (r record) Message() string {
 	return r.message
 }
 
+// Time returns the time.Time timestamp associated to this Record
 func (r record) Time() time.Time {
 	return r.timestamp
 }
 
+// Level returns the level.Level level associated to this Record
 func (r record) Level() level.Level {
 	return r.level
 }
