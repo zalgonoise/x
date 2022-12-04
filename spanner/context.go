543fd4775b8b5f144d81a1801e85060847400904
zalgonoise <zalgo.noise@gmail.com>
2022-12-04 17:50:38 +0100

    new: added Span in context funcs

--- spanner/context.go
+++ spanner/context.go
@@ -4,11 +4,18 @@ import (
 	"context"
 )
 
-// SpannerContextKey is a unique type to use as key when storing traces in context
-type SpannerContextKey string
+// TraceContextKey is a unique type to use as key when storing traces in context
+type TraceContextKey string
 
-// ContextKey is the package's default context key for storing traces
-var ContextKey SpannerContextKey = "spanner"
+// ContextKey is the package's default context key for storing traces in context
+var ContextKey TraceContextKey = "spanner"
+
+// SpanIDContextKey is a unique type to use as key when storing span IDs in context,
+// which are referenced as parent Span IDs in new Spans
+type SpanIDContextKey string
+
+// SpanContextKey is the package's default context key for storing Spans in context
+var SpanContextKey SpanIDContextKey = "span"
 
 // GetTrace returns the Trace from the input context `ctx`, or nil if it doesn't have one
 func GetTrace(ctx context.Context) Trace {
@@ -45,4 +52,21 @@ func WithNewTrace(ctx context.Context) (context.Context, Trace) {
 // WithTrace wraps the input context `ctx` with the Trace `t`, returning the context with value
 func WithTrace(ctx context.Context, t Trace) context.Context {
 	return context.WithValue(ctx, ContextKey, t)
+}
+
+// GetSpan returns the Span from the input context `ctx`, or nil if it doesn't have one
+func GetSpan(ctx context.Context) Span {
+	v := ctx.Value(SpanContextKey)
+	if v == nil {
+		return nil
+	}
+	if s, ok := v.(Span); ok {
+		return s
+	}
+	return nil
+}
+
+// WithSpan wraps the input context `ctx` with the Span `s`, returning the context with value
+func WithSpan(ctx context.Context, s Span) context.Context {
+	return context.WithValue(ctx, SpanContextKey, s)
 }
