7f999cbf66aae2d8aa0af0393d1db19da118b266
zalgonoise <zalgo.noise@gmail.com>
2022-12-12 22:03:16 +0100

    fix: trace no longer holds spans

--- spanner/trace.go
+++ spanner/trace.go
@@ -1,53 +1,34 @@
 package spanner
 
+import "sync/atomic"
+
 // Trace records and stores the set of Spans, as actions in a transaction
 //
 // It exposes methods for retrieving the slice of Spans, adding a new Span,
 // retrieving the TraceID, and retrieving the next span's parent SpanID if set
 type Trace interface {
-	// Add appends a Span to the Trace
-	Add(s Span)
-	// Get returns the slice of Spans in the Trace
-	Get() []Span
 	// Register sets the input Span `s`'s SpanID as this Trace's reference parent_id
-	Register(s Span)
+	Register(s *SpanID)
 	// ID returns the TraceID
 	ID() TraceID
 	// Parent returns the parent SpanID, or nil if unset
-	Parent() Span
-	// Extract returns the SpanData from the Trace, if any
-	Extract() []SpanData
-	// Export pushes the current slice of SpanData to the configured Exporter
-	Export()
+	Parent() *SpanID
+	// Tracer returns the configured Tracer in the Trace
+	Processor() SpanProcessor
 }
 
 type trace struct {
-	exporter Exporter
-	trace    TraceID
-	ref      Span
-	spans    []Span
+	processor atomic.Value
+	trace     TraceID
+	ref       *SpanID
 }
 
-func newTrace(e Exporter) Trace {
-	if e == nil {
-		e = noOpExporter{}
+func newTrace(processor atomic.Value) Trace {
+	newTr := &trace{
+		processor: processor,
+		trace:     NewTraceID(),
 	}
-	t := &trace{
-		exporter: e,
-		trace:    NewTraceID(),
-		spans:    []Span{},
-	}
-	return t
-}
-
-// Add appends a Span to the Trace
-func (t *trace) Add(s Span) {
-	t.spans = append(t.spans, s)
-}
-
-// Get returns the slice of Spans in the Trace
-func (t trace) Get() []Span {
-	return t.spans
+	return newTr
 }
 
 // Add takes in two Spans, the first one which is appended to the list of Spans
@@ -55,7 +36,7 @@ func (t trace) Get() []Span {
 // next Trace
 //
 // Traces are immutable, and adding a Span returns a new Trace
-func (t *trace) Register(s Span) {
+func (t *trace) Register(s *SpanID) {
 	t.ref = s
 }
 
@@ -65,22 +46,10 @@ func (t trace) ID() TraceID {
 }
 
 // PID returns the parent SpanID, or nil if unset
-func (t trace) Parent() Span {
+func (t trace) Parent() *SpanID {
 	return t.ref
 }
 
-// Extract returns the SpanData from the Trace, if any
-func (t trace) Extract() []SpanData {
-	if len(t.spans) == 0 {
-		return nil
-	}
-	data := make([]SpanData, 0, len(t.spans))
-	for _, s := range t.spans {
-		data = append(data, s.Extract())
-	}
-	return data
-}
-
-func (t *trace) Export() {
-	t.exporter.Export(t)
+func (t *trace) Processor() SpanProcessor {
+	return t.processor.Load().(SpanProcessor)
 }
