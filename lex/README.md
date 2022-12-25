dbf4636f6a60c9aa36b753dd009adcdfa4367ba6
zalgonoise <zalgo.noise@gmail.com>
2022-12-26 00:20:25 +0100

    fix: code docs

--- lex/README.md
+++ lex/README.md
@@ -6,7 +6,7 @@ _______________
 
 ## Overview
 
-`lex` is a lexer for Go, based on the concept of the [`text/template`](https://pkg.go.dev/text/template) lexer, as a generic implementation. The logic behind this lexer is mostly based off of Rob Pike's talk about [Lexical Scanning in Go](https://www.youtube.com/watch?v=HxaD_trXwRE), which is also seen in the standard library (in [`text/template/parse/lex.go`](https://cs.opensource.google/go/go/+/refs/tags/go1.19.4:src/text/template/parse/lex.go)).
+`lex` is a lexer for Go, based on the concept of the [`text/template`](https://pkg.go.dev/text/template) lexer, as a generic implementation. The logic behind this lexer is mostly based off of [Rob Pike](https://github.com/robpike)'s talk about [Lexical Scanning in Go](https://www.youtube.com/watch?v=HxaD_trXwRE), which is also seen in the standard library (in [`text/template/parse/lex.go`](https://cs.opensource.google/go/go/+/refs/tags/go1.19.4:src/text/template/parse/lex.go)).
 
 
 The idea behind implementing a generic algorithm for a lexer came from trying to build a graph (data structure) representing the logic blocks in a Go file. Watching the talk above was a breath of fresh air when it came to the design of the lexer and its simple approach. So, it would be nice to leverage this algorithm for the Go code graph idea from before. By making the logic generic, one could implement an `Item` type to hold a defined token type, and a set of (any type of) values and `StateFn` state-functions to tokenize input data. In concept this works for any given type, as the point is to label elements of a slice with identifying tokens, that will be processed into a parse tree (with a specific parser implementation).
@@ -26,6 +26,113 @@ go get github.com/zalgonoise/lex
 ```
 
 ## Features
+
+### Entities
+
+#### Item
+
+An Item is an object holding a token and a set of values (lexemes) corresponding to that token. It is a key-value data structure, where the value-half is a slice of any type -- which could be populated with any number of items.
+
+Items are created as the lexer tokenizes symbols / units from the input data, and returned to the caller on each `Lexer.NextItem()` call. 
+
+```go
+// Item represents a set of any type of tokens identified by a comparable type
+type Item[T comparable, V any] struct {
+	Type  T
+	Value []V
+}
+```
+
+#### StateFn
+
+A StateFn (state-function) describes a recursive function that will consume each unit of the input data, and either take action, emit tokens, or to finally pass along the data analysis action to a different StateFn. These functions will be implemented by the consumer of the library, describing the steps (their) lexer will take when consuming data. Examples further below.
+
+The point to the StateFn is to keep the Lexer in control of the state, and the StateFn controlling the cursor and actions the Lexer needs to take. This will allow the biggest flexibility possible, as different Lexer StateFn will derive different flavors for the lexer. Thinking of it as a Markdown lexer, the StateFn would define which and how Markdown tokens are classified, emitted, or even ignored.
+
+
+```go
+// StateFn is a recursive function that updates the Lexer state according to a specific
+// input token along the lexing / parsing action.
+//
+// It returns another StateFn or nil, as it consumes each token with a certain logic applied,
+// passing along the lexing / parsing to the next StateFn
+type StateFn[C comparable, T any, I Item[C, T]] func(l Lexer[C, T, I]) StateFn[C, T, I]
+```
+
+#### Lexer
+
+The Lexer is a state-machine that keeps track of the generated Items as the input data is consumed and labled with tokens. It's part cursor, part controller/verifier (within the `StateFn`s), but its main job is keep the state-functions running as the items are consumed, returning lexical items to the caller as they are generated.
+
+The Lexer should be accompanied with a Parser, that consumes the tokenized Items to build a parse tree.
+
+This Lexer exposes methods that should be perceived as utilities for the caller when building `StateFn`s. In reality, when actually *running* the Lexer, the caller will loop through its `NextItem()` method until it hits an EOF token.
+
+The methods in the Lexer will be covered individually below, as well as the design decisions when writing it this way.
+
+```go
+// Lexer describes the behavior of a lexer / parser with cursor capabilities
+//
+// Once spawned, it will consume all tokens as its `NextItem()` method is called,
+// returning processed `Item[C, T]` as it goes
+//
+// Its `Emit()` method pushes items into the stack to be returned, and its `Accept()`,
+// `Check()` and `AcceptRun()` methods act as verifiers for a (set of) token(s)
+type Lexer[C comparable, T any, I Item[C, T]] interface {
+
+	// Cursor navigates through a slice in a controlled manner, allowing the
+	// caller to move forward, backwards, and jump around the slice as they need
+	cur.Cursor[T]
+
+	// NextItem processes the tokens sequentially, through the corresponding StateFn
+	//
+	// As each item is processed, it is returned to the Lexer by `Emit()`, and
+	// finally returned to the caller.
+	//
+	// Note that multiple calls to `NextItem()` should be made when tokenizing input data;
+	// usually in a for-loop while the output item is not EOF.
+	NextItem() I
+
+	// Emit pushes the set of units identified by token `itemType` to the items channel,
+	// that returns it in the NextItem() method.
+	//
+	// The emitted item will be a subsection of the input data slice, from the lexer's
+	// starting index to the current position index.
+	//
+	// It also sets the lexer's starting index to the current position index.
+	Emit(itemType C)
+
+	// Ignore will set the starting point as the current position, ignoring any preceeding units
+	Ignore()
+
+	// Backup will rewind the index for the width of the current item
+	Backup()
+
+	// Width returns the size of the set of units ready to be emitted with a token
+	Width() int
+
+	// Start returns the current starting-point index for when an item is emitted
+	Start() int
+
+	// Check passes the current token through the input `verifFn` function as a validator, returning
+	// its result
+	Check(verifFn func(item T) bool) bool
+
+	// Accept passes the current token through the input `verifFn` function as a validator, returning
+	// its result
+	//
+	// If the validation passes, the cursor has moved one step forward (the unit was consumed)
+	//
+	// If the validation fails, the cursor rolls back one step
+	Accept(verifFn func(item T) bool) bool
+
+	// AcceptRun iterates through all following tokens, passing them through the input `verifFn`
+	// function as a validator
+	//
+	// Once it fails the verification, the cursor is rolledback once, leaving the caller at the unit
+	// that failed the verifFn
+	AcceptRun(verifFn func(item T) bool)
+}
+```
 
 ## Usage
 
--- lex/lexer.go
+++ lex/lexer.go
@@ -9,36 +9,61 @@ import (
 // Once spawned, it will consume all tokens as its `NextItem()` method is called,
 // returning processed `Item[C, T]` as it goes
 //
-// Its `Emit()` method pushes items into the stack to be returned, and its `Accept()`
-// and `AcceptRun()` methods act as verifiers for a (set of) token(s)
+// Its `Emit()` method pushes items into the stack to be returned, and its `Accept()`,
+// `Check()` and `AcceptRun()` methods act as verifiers for a (set of) token(s)
 type Lexer[C comparable, T any, I Item[C, T]] interface {
+
 	// Cursor navigates through a slice in a controlled manner, allowing the
 	// caller to move forward, backwards, and jump around the slice as they need
 	cur.Cursor[T]
+
 	// NextItem processes the tokens sequentially, through the corresponding StateFn
 	//
-	// As each item is processed, it is returned to the Lexer by `NextItem()`, which is
-	// finally returned to the caller when finished
+	// As each item is processed, it is returned to the Lexer by `Emit()`, and
+	// finally returned to the caller.
+	//
+	// Note that multiple calls to `NextItem()` should be made when tokenizing input data;
+	// usually in a for-loop while the output item is not EOF.
 	NextItem() I
-	// Ignore will scroll the starting point to the current position
+
+	// Emit pushes the set of units identified by token `itemType` to the items channel,
+	// that returns it in the NextItem() method.
+	//
+	// The emitted item will be a subsection of the input data slice, from the lexer's
+	// starting index to the current position index.
+	//
+	// It also sets the lexer's starting index to the current position index.
+	Emit(itemType C)
+
+	// Ignore will set the starting point as the current position, ignoring any preceeding units
 	Ignore()
+
 	// Backup will rewind the index for the width of the current item
 	Backup()
-	// Width returns the size for the set of tokens
+
+	// Width returns the size of the set of units ready to be emitted with a token
 	Width() int
+
 	// Start returns the current starting-point index for when an item is emitted
 	Start() int
-	// Emit pushes the set of tokens from the starting point to the current position to the
-	// receiver, identified by C `itemType`
-	Emit(itemType C)
+
 	// Check passes the current token through the input `verifFn` function as a validator, returning
 	// its result
 	Check(verifFn func(item T) bool) bool
-	// Accept passes the next token through the input `verifFn` function as a validator, returning
-	// its result while also rewinding to the current position
+
+	// Accept passes the current token through the input `verifFn` function as a validator, returning
+	// its result
+	//
+	// If the validation passes, the cursor has moved one step forward (the unit was consumed)
+	//
+	// If the validation fails, the cursor rolls back one step
 	Accept(verifFn func(item T) bool) bool
+
 	// AcceptRun iterates through all following tokens, passing them through the input `verifFn`
-	// function as a validator, returning its result while also rewinding to the current position
+	// function as a validator
+	//
+	// Once it fails the verification, the cursor is rolledback once, leaving the caller at the unit
+	// that failed the verifFn
 	AcceptRun(verifFn func(item T) bool)
 }
 
@@ -69,8 +94,11 @@ func New[C comparable, T any, I Item[C, T]](
 
 // NextItem processes the tokens sequentially, through the corresponding StateFn
 //
-// As each item is processed, it is returned to the Lexer by `NextItem()`, which is
-// finally returned to the caller when finished
+// As each item is processed, it is returned to the Lexer by `Emit()`, and
+// finally returned to the caller.
+//
+// Note that multiple calls to `NextItem()` should be made when tokenizing input data;
+// usually in a for-loop while the output item is not EOF.
 func (l *lexer[C, T, I]) NextItem() I {
 	for {
 		select {
@@ -88,7 +116,22 @@ func (l *lexer[C, T, I]) NextItem() I {
 	}
 }
 
-// Ignore will scroll the starting point to the current position
+// Emit pushes the set of units identified by token `itemType` to the items channel,
+// that returns it in the NextItem() method.
+//
+// The emitted item will be a subsection of the input data slice, from the lexer's
+// starting index to the current position index.
+//
+// It also sets the lexer's starting index to the current position index.
+func (l *lexer[C, T, I]) Emit(itemType C) {
+	l.items <- I{
+		Type:  itemType,
+		Value: l.input[l.start:l.pos],
+	}
+	l.start = l.pos
+}
+
+// Ignore will set the starting point as the current position, ignoring any preceeding units
 func (l *lexer[C, T, I]) Ignore() {
 	l.start = l.pos
 }
@@ -98,29 +141,14 @@ func (l *lexer[C, T, I]) Backup() {
 	l.pos = l.start
 }
 
-// Width returns the size for the set of tokens
+// Width returns the size of the set of units ready to be emitted with a token
 func (l *lexer[C, T, I]) Width() int {
 	return l.pos - l.start
 }
 
-// Emit pushes the set of tokens from the starting point to the current position to the
-// receiver, identified by C `itemType`
-func (l *lexer[C, T, I]) Emit(itemType C) {
-	l.items <- I{
-		Type:  itemType,
-		Value: l.input[l.start:l.pos],
-	}
-	l.start = l.pos
-}
-
-// Accept passes the next token through the input `verifFn` function as a validator, returning
-// its result while also rewinding to the current position
-func (l *lexer[C, T, I]) Accept(verifFn func(item T) bool) bool {
-	if ok := verifFn(l.Next()); ok {
-		return true
-	}
-	l.Prev()
-	return false
+// Start returns the current starting-point index for when an item is emitted
+func (l *lexer[C, T, I]) Start() int {
+	return l.start
 }
 
 // Check passes the current token through the input `verifFn` function as a validator, returning
@@ -129,8 +157,25 @@ func (l *lexer[C, T, I]) Check(verifFn func(item T) bool) bool {
 	return verifFn(l.Cur())
 }
 
+// Accept passes the current token through the input `verifFn` function as a validator, returning
+// its result
+//
+// If the validation passes, the cursor has moved one step forward (the unit was consumed)
+//
+// If the validation fails, the cursor rolls back one step
+func (l *lexer[C, T, I]) Accept(verifFn func(item T) bool) bool {
+	if ok := verifFn(l.Next()); ok {
+		return true
+	}
+	l.Prev()
+	return false
+}
+
 // AcceptRun iterates through all following tokens, passing them through the input `verifFn`
-// function as a validator, returning its result while also rewinding to the current position
+// function as a validator
+//
+// Once it fails the verification, the cursor is rolledback once, leaving the caller at the unit
+// that failed the verifFn
 func (l *lexer[C, T, I]) AcceptRun(verifFn func(item T) bool) {
 	for verifFn(l.Next()) {
 	}
@@ -139,6 +184,8 @@ func (l *lexer[C, T, I]) AcceptRun(verifFn func(item T) bool) {
 }
 
 // Cur returns the same indexed item in the slice
+//
+// If the position is already over the size of the input, the zero-value EOF token is returned
 func (l *lexer[C, T, I]) Cur() T {
 	if l.pos >= len(l.input) {
 		var eof T
@@ -157,13 +204,11 @@ func (l *lexer[C, T, I]) Len() int {
 	return len(l.input)
 }
 
-// Start returns the current starting-point index for when an item is emitted
-func (l *lexer[C, T, I]) Start() int {
-	return l.start
-}
-
-// Next returns the next item in the slice, or the last index if already
-// at the tail
+// Next returns the current item in the slice as per the lexer's position, and
+// increments the position by one.
+//
+// If the position is bigger or equal to the size of the input data, the position
+// value is NOT incremented and the zero-value EOF token is returned
 func (l *lexer[C, T, I]) Next() T {
 	if l.pos >= len(l.input) {
 		var eof T
@@ -173,8 +218,11 @@ func (l *lexer[C, T, I]) Next() T {
 	return l.input[l.pos-1]
 }
 
-// Prev returns the previous item in the slice, or the first index if
-// already at the head
+// Prev returns the previous item in the slice, while also decrementing the
+// lexer's position.
+//
+// If the new position is less than zero, the position value is NOT decremented
+// and the zero-value EOF token is returned
 func (l *lexer[C, T, I]) Prev() T {
 	if l.pos-1 < 0 {
 		var eof T
@@ -185,6 +233,8 @@ func (l *lexer[C, T, I]) Prev() T {
 }
 
 // Peek returns the next indexed item without advancing the cursor
+//
+// If the next token overflows the input's index, the zero-value EOF token is returned
 func (l *lexer[C, T, I]) Peek() T {
 	if l.pos+1 >= len(l.input) {
 		var eof T
@@ -193,14 +243,16 @@ func (l *lexer[C, T, I]) Peek() T {
 	return l.input[l.pos+1]
 }
 
-// Head returns to the beginning of the slice
+// Head returns to the beginning of the slice, setting both lexer's start and position
+// values to zero
 func (l *lexer[C, T, I]) Head() T {
 	l.pos = 0
 	l.start = 0
 	return l.input[l.pos]
 }
 
-// Tail jumps to the end of the slice
+// Tail jumps to the end of the slice, setting both lexer's start and position values to
+// the last item in the input
 func (l *lexer[C, T, I]) Tail() T {
 	l.pos = len(l.input) - 1
 	l.start = len(l.input) - 1
@@ -209,9 +261,9 @@ func (l *lexer[C, T, I]) Tail() T {
 
 // Idx jumps to the specific index `idx` in the slice
 //
-// If the input index is below 0, the head of the slice is returned;
-// If the input index is greater than the size of the slice, the tail of the slice
-// is returned
+// If the input index is below 0, the zero-value EOF token is returned
+// If the input index is greater than the size of the input, the
+// zero-value EOF token is returned
 func (l *lexer[C, T, I]) Idx(idx int) T {
 	if idx < 0 {
 		var eof T
@@ -231,9 +283,9 @@ func (l *lexer[C, T, I]) Idx(idx int) T {
 // Offset advances or rewinds `amount` steps in the slice, be it a positive or negative
 // input.
 //
-// If the result offset is below 0, the head of the slice is returned;
-// If the result offset is greater than the size of the slice, the tail of the slice
-// is returned
+// If the result offset is below 0, the zero-value EOF token is returned
+// If the result offset is greater than the size of the slice, the
+// zero-value EOF token is returned
 func (l *lexer[C, T, I]) Offset(amount int) T {
 	if l.pos+amount < 0 {
 		var eof T
@@ -252,6 +304,10 @@ func (l *lexer[C, T, I]) Offset(amount int) T {
 
 // PeekIdx returns the next indexed item without advancing the cursor,
 // with the index `idx`
+//
+// If the input index is below 0, the zero-value EOF token is returned
+// If the input index is greater than the size of the input, the
+// zero-value EOF token is returned
 func (l *lexer[C, T, I]) PeekIdx(idx int) T {
 	if idx >= len(l.input) {
 		var eof T
@@ -266,6 +322,10 @@ func (l *lexer[C, T, I]) PeekIdx(idx int) T {
 
 // PeekOffset returns the next indexed item without advancing the cursor,
 // with offset `amount`
+//
+// If the result offset is below 0, the zero-value EOF token is returned
+// If the result offset is greater than the size of the slice, the
+// zero-value EOF token is returned
 func (l *lexer[C, T, I]) PeekOffset(amount int) T {
 	if l.pos+amount >= len(l.input) || l.pos+amount < 0 {
 		var eof T
@@ -275,6 +335,10 @@ func (l *lexer[C, T, I]) PeekOffset(amount int) T {
 }
 
 // Extract returns a slice from index `start` to index `end`
+//
+// If the input start index is below 0, the starting point will be set to zero
+// If the input end index is greater than the size of the input, the
+// ending point will be set to the size of the input.
 func (l *lexer[C, T, I]) Extract(start, end int) []T {
 	if start < 0 {
 		start = 0
