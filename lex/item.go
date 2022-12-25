fc2a316676ba5c3233a80372a024b6f73915c71a
zalgonoise <zalgo.noise@gmail.com>
2022-12-25 18:42:47 +0100

    fix: updated logic ; passing tests

--- lex/item.go
+++ lex/item.go
@@ -2,14 +2,14 @@ package lex
 
 // Item represents a set of any type of tokens identified by a comparable type
 type Item[T comparable, V any] struct {
-	Typ T
-	Val []V
+	Type  T
+	Value []V
 }
 
 // NewItem creates an Item with type `T` and values `[]V`
 func NewItem[T comparable, V any](itemType T, value ...V) Item[T, V] {
 	return Item[T, V]{
-		Typ: itemType,
-		Val: value,
+		Type:  itemType,
+		Value: value,
 	}
 }
--- lex/lexer.go
+++ lex/lexer.go
@@ -26,9 +26,14 @@ type Lexer[C comparable, T any, I Item[C, T]] interface {
 	Backup()
 	// Width returns the size for the set of tokens
 	Width() int
+	// Start returns the current starting-point index for when an item is emitted
+	Start() int
 	// Emit pushes the set of tokens from the starting point to the current position to the
 	// receiver, identified by C `itemType`
 	Emit(itemType C)
+	// Check passes the current token through the input `verifFn` function as a validator, returning
+	// its result
+	Check(verifFn func(item T) bool) bool
 	// Accept passes the next token through the input `verifFn` function as a validator, returning
 	// its result while also rewinding to the current position
 	Accept(verifFn func(item T) bool) bool
@@ -41,15 +46,18 @@ type lexer[C comparable, T any, I Item[C, T]] struct {
 	input []T
 	start int
 	pos   int
-	width int
 	state StateFn[C, T, I]
 	items chan I
 }
 
-func NewLexer[C comparable, T any, I Item[C, T]](
+func New[C comparable, T any, I Item[C, T]](
 	initFn StateFn[C, T, I],
 	input []T,
 ) Lexer[C, T, I] {
+	if len(input) == 0 {
+		return nil
+	}
+
 	l := &lexer[C, T, I]{
 		input: input,
 		state: initFn,
@@ -73,7 +81,8 @@ func (l *lexer[C, T, I]) NextItem() I {
 				continue
 			}
 			close(l.items)
-			return I{}
+			var eof I
+			return eof
 		}
 	}
 }
@@ -85,20 +94,20 @@ func (l *lexer[C, T, I]) Ignore() {
 
 // Backup will rewind the index for the width of the current item
 func (l *lexer[C, T, I]) Backup() {
-	l.pos -= l.width
+	l.pos = l.start
 }
 
 // Width returns the size for the set of tokens
 func (l *lexer[C, T, I]) Width() int {
-	return l.width
+	return l.pos - l.start
 }
 
 // Emit pushes the set of tokens from the starting point to the current position to the
 // receiver, identified by C `itemType`
 func (l *lexer[C, T, I]) Emit(itemType C) {
 	l.items <- I{
-		Typ: itemType,
-		Val: l.input[l.start : l.pos+1],
+		Type:  itemType,
+		Value: l.input[l.start:l.pos],
 	}
 	l.start = l.pos
 }
@@ -109,26 +118,33 @@ func (l *lexer[C, T, I]) Accept(verifFn func(item T) bool) bool {
 	if ok := verifFn(l.Next()); ok {
 		return true
 	}
-	l.Prev()
+	return false
+}
+
+// Check passes the current token through the input `verifFn` function as a validator, returning
+// its result
+func (l *lexer[C, T, I]) Check(verifFn func(item T) bool) bool {
+	if ok := verifFn(l.Cur()); ok {
+		return true
+	}
 	return false
 }
 
 // AcceptRun iterates through all following tokens, passing them through the input `verifFn`
 // function as a validator, returning its result while also rewinding to the current position
 func (l *lexer[C, T, I]) AcceptRun(verifFn func(item T) bool) {
-	var ok bool
-	for l.Pos()+1 < l.Len() {
-		ok = verifFn(l.Peek())
-		if !ok {
-			return
-		}
-		l.Next()
+	for verifFn(l.Next()) {
 	}
+	l.Prev()
 	return
 }
 
 // Cur returns the same indexed item in the slice
 func (l *lexer[C, T, I]) Cur() T {
+	if l.pos >= len(l.input) {
+		var eof T
+		return eof
+	}
 	return l.input[l.pos]
 }
 
@@ -142,31 +158,37 @@ func (l *lexer[C, T, I]) Len() int {
 	return len(l.input)
 }
 
+// Start returns the current starting-point index for when an item is emitted
+func (l *lexer[C, T, I]) Start() int {
+	return l.start
+}
+
 // Next returns the next item in the slice, or the last index if already
 // at the tail
 func (l *lexer[C, T, I]) Next() T {
-	if l.pos+1 >= len(l.input) {
-		l.width = 0
+	if l.pos+1 > len(l.input) {
+		l.start = l.pos // zero width on eof
 		var eof T
 		return eof
 	}
 	l.pos++
-	l.width++
-	return l.input[l.pos]
+	return l.input[l.pos-1]
 }
 
 // Prev returns the previous item in the slice, or the first index if
 // already at the head
 func (l *lexer[C, T, I]) Prev() T {
+	if l.pos-1 < 0 {
+		var eof T
+		return eof
+	}
 	l.pos--
-	l.width--
 	return l.input[l.pos]
 }
 
 // Peek returns the next indexed item without advancing the cursor
 func (l *lexer[C, T, I]) Peek() T {
 	if l.pos+1 >= len(l.input) {
-		l.width = 0
 		var eof T
 		return eof
 	}
@@ -194,12 +216,10 @@ func (l *lexer[C, T, I]) Tail() T {
 // is returned
 func (l *lexer[C, T, I]) Idx(idx int) T {
 	if idx < 0 {
-		l.width = 0
 		var eof T
 		return eof
 	}
 	if idx >= len(l.input) {
-		l.width = 0
 		var eof T
 		return eof
 	}
@@ -218,17 +238,15 @@ func (l *lexer[C, T, I]) Idx(idx int) T {
 // is returned
 func (l *lexer[C, T, I]) Offset(amount int) T {
 	if l.pos+amount < 0 {
-		l.width = 0
 		var eof T
 		return eof
 	}
 	if l.pos+amount >= len(l.input) {
-		l.width = 0
 		var eof T
 		return eof
 	}
 	l.pos += amount
-	if l.start-l.pos < 0 {
+	if l.pos-l.start < 0 {
 		l.start += amount
 	}
 	return l.input[l.pos]
@@ -238,12 +256,10 @@ func (l *lexer[C, T, I]) Offset(amount int) T {
 // with the index `idx`
 func (l *lexer[C, T, I]) PeekIdx(idx int) T {
 	if idx >= len(l.input) {
-		l.width = 0
 		var eof T
 		return eof
 	}
 	if idx < 0 {
-		l.width = 0
 		var eof T
 		return eof
 	}
@@ -254,7 +270,6 @@ func (l *lexer[C, T, I]) PeekIdx(idx int) T {
 // with offset `amount`
 func (l *lexer[C, T, I]) PeekOffset(amount int) T {
 	if l.pos+amount >= len(l.input) || l.pos+amount < 0 {
-		l.width = 0
 		var eof T
 		return eof
 	}
