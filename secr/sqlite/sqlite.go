7e25ebb0864f2b7c97025844f6a5452ffe864016
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-09 20:50:37 +0100

    doc: documented repositories

--- secr/sqlite/sqlite.go
+++ secr/sqlite/sqlite.go
@@ -11,6 +11,10 @@ import (
 //go:embed migrations/1672703190_initial_up.sql
 var initialMigration string
 
+// Open will initialize a SQLite DB based on the `.sql` file in `path`,
+// returning a pointer to a sql.DB and an error
+//
+// It executes the initial migration, as well.
 func Open(path string) (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", path)
 	if err != nil {
--- secr/user/repository.go
+++ secr/user/repository.go
@@ -2,10 +2,16 @@ package user
 
 import "context"
 
+// Repository describes the actions exposed by the users store
 type Repository interface {
+	// Create will create a user `u`, returning its ID and an error
 	Create(ctx context.Context, u *User) (uint64, error)
+	// Update will update the user `username` with its updated version `updated`. Returns an error
 	Update(ctx context.Context, username string, updated *User) error
+	// Get returns the user identified by `username`, and an error
 	Get(ctx context.Context, username string) (*User, error)
+	// List returns all the users, and an error
 	List(ctx context.Context) ([]*User, error)
+	// Delete removes the user identified by `username`, returning an error
 	Delete(ctx context.Context, username string) error
 }
