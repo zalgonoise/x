c84f2d3bad1938c7850d816b8dd96f0a27247214
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-09 19:32:10 +0100

    doc: documented service interface

--- secr/service/service.go
+++ secr/service/service.go
@@ -9,24 +9,37 @@ import (
 	"github.com/zalgonoise/x/secr/user"
 )
 
+// Service defines all the exposed features and functionalities of the secrets store
 type Service interface {
-	// user sessions
+	// Login verifies the user's credentials and returns a session and an error
 	Login(ctx context.Context, username, password string) (*user.Session, error)
+	// Logout signs-out the user `username`
 	Logout(ctx context.Context, username string) error
+	// ChangePassword updates user `username`'s password after verifying the old one, returning an error
 	ChangePassword(ctx context.Context, username, password, newPassword string) error
+	// Refresh renews a user's JWT provided it is a valid one. Returns a session and an error
 	Refresh(ctx context.Context, username, token string) (*user.Session, error)
 
-	// user directory
+	// CreateUser creates the user under username `username`, with the provided password `password` and name `name`
+	// It returns a user and an error
 	CreateUser(ctx context.Context, username, password, name string) (*user.User, error)
+	// GetUser fetches the user with username `username`. Returns a user and an error
 	GetUser(ctx context.Context, username string) (*user.User, error)
+	// ListUsers returns all the users in the directory, and an error
 	ListUsers(ctx context.Context) ([]*user.User, error)
+	// UpdateUser updates the user `username`'s name, found in `updated` user. Returns an error
 	UpdateUser(ctx context.Context, username string, updated *user.User) error
+	// DeleteUser removes the user with username `username`. Returns an error
 	DeleteUser(ctx context.Context, username string) error
 
-	// secrets
+	// CreateSecret creates a secret with key `key` and value `value` (as a slice of bytes), for the
+	// user `username`. It returns an error
 	CreateSecret(ctx context.Context, username string, key string, value []byte) error
+	// GetSecret fetches the secret with key `key`, for user `username`. Returns a secret and an error
 	GetSecret(ctx context.Context, username string, key string) (*secret.Secret, error)
+	// ListSecrets retuns all secrets for user `username`. Returns a list of secrets and an error
 	ListSecrets(ctx context.Context, username string) ([]*secret.Secret, error)
+	// DeleteSecret removes a secret with key `key` from the user `username`. Returns an error
 	DeleteSecret(ctx context.Context, username string, key string) error
 }
 
