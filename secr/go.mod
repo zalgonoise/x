c81e2c4eac886cab313d553c99777b47144a09c8
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-10 21:01:30 +0100

    http: added http server implementation

--- secr/go.mod
+++ secr/go.mod
@@ -3,9 +3,16 @@ module github.com/zalgonoise/x/secr
 go 1.19
 
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/mattn/go-sqlite3 v1.14.16 // indirect
-	go.etcd.io/bbolt v1.3.6 // indirect
-	golang.org/x/exp v0.0.0-20221230185412-738e83a70c30 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/mattn/go-sqlite3 v1.14.16
+	github.com/zalgonoise/attr v0.0.0-20221218020548-25d0939ced5d
+	github.com/zalgonoise/spanner v0.0.0-20221218020729-6780005ee1f6
+	go.etcd.io/bbolt v1.3.6
+	golang.org/x/exp v0.0.0-20221230185412-738e83a70c30
+)
+
+require (
+	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/zalgonoise/logx v0.0.0-20221210214610-c003c0f931be // indirect
 	golang.org/x/sys v0.1.0 // indirect
 )
---
+++ secr/transport/http/server.go
@@ -0,0 +1,74 @@
+package http
+
+import (
+	"context"
+	"fmt"
+	stdhttp "net/http"
+
+	"github.com/zalgonoise/attr"
+	"github.com/zalgonoise/spanner"
+)
+
+type Server interface {
+	Start(ctx context.Context) error
+	Stop(ctx context.Context) error
+}
+
+type server struct {
+	ep   API
+	port int
+	srv  *stdhttp.Server
+}
+
+func NewServer(api API, port int) Server {
+	mux := stdhttp.NewServeMux()
+	httpSrv := &stdhttp.Server{
+		Addr:    fmt.Sprintf(":%v", port),
+		Handler: mux,
+	}
+	srv := &server{
+		ep:   api,
+		port: port,
+		srv:  httpSrv,
+	}
+	// handlers
+	// TODO: implement dynamic URL paths (e.g.: /users/me/secrets/github)
+	mux.HandleFunc("/login", srv.ep.Login)
+	mux.HandleFunc("/logout", srv.ep.Logout)
+	mux.HandleFunc("/recover", srv.ep.ChangePassword)
+	mux.HandleFunc("/refresh", srv.ep.Refresh)
+	mux.HandleFunc("/users/create", srv.ep.CreateUser)
+	mux.HandleFunc("/users/get", srv.ep.GetUser)
+	mux.HandleFunc("/users", srv.ep.ListUsers)
+	mux.HandleFunc("/users/update", srv.ep.UpdateUser)
+	mux.HandleFunc("/users/delete", srv.ep.DeleteUser)
+	mux.HandleFunc("/secrets/create", srv.ep.CreateSecret)
+	mux.HandleFunc("/secrets/get", srv.ep.GetSecret)
+	mux.HandleFunc("/secrets", srv.ep.ListSecrets)
+	mux.HandleFunc("/secrets/delete", srv.ep.DeleteSecret)
+	return srv
+}
+
+func (s *server) Start(ctx context.Context) error {
+	ctx, span := spanner.Start(ctx, "http.Start")
+	defer span.End()
+
+	err := s.srv.ListenAndServe()
+	if err != nil {
+		span.Event("failed to start HTTP server", attr.New("error", err.Error()))
+		return err
+	}
+	return nil
+}
+
+func (s *server) Stop(ctx context.Context) error {
+	ctx, span := spanner.Start(ctx, "http.Stop")
+	defer span.End()
+
+	err := s.srv.Shutdown(ctx)
+	if err != nil {
+		span.Event("failed to stop HTTP server", attr.New("error", err.Error()))
+		return err
+	}
+	return nil
+}
