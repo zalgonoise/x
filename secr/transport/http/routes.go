b70957f0dcbd4b94c5d635f457b709742a83dc1a
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-15 15:39:56 +0100

    http: added routes

---
+++ secr/transport/http/routes.go
@@ -0,0 +1,155 @@
+package http
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/zalgonoise/x/ghttp"
+)
+
+const userPath = "users"
+const secrPath = "secrets"
+
+func (s server) endpoints() ghttp.Endpoints {
+	e := ghttp.NewEndpoints()
+	e.Set(s.sessionsHandler()...)
+	e.Set(s.usersHandler()...)
+	return e
+}
+
+func (s server) sessionsHandler() []ghttp.Handler {
+	return []ghttp.Handler{
+		{
+			Method: http.MethodPost,
+			Path:   "/login",
+			Fn:     s.login(),
+		},
+		{
+			Method: http.MethodPost,
+			Path:   "/logout",
+			Fn:     s.logout(),
+		},
+		{
+			Method: http.MethodPost,
+			Path:   "/recover",
+			Fn:     s.changePassword(),
+		},
+		{
+			Method: http.MethodPost,
+			Path:   "/refresh",
+			Fn:     s.refresh(),
+		},
+	}
+}
+
+func (s server) usersHandler() []ghttp.Handler {
+	p := "/users/"
+
+	return []ghttp.Handler{
+		{
+			Method: http.MethodGet,
+			Path:   p,
+			Fn:     s.usersGetRoute(),
+			Middleware: []ghttp.MiddlewareFn{
+				s.WithAuth(),
+			},
+		},
+		{
+			Method: http.MethodPost,
+			Path:   p,
+			Fn:     s.usersPostRoute(),
+			Middleware: []ghttp.MiddlewareFn{
+				s.WithAuth(),
+			},
+		},
+		{
+			Method: http.MethodDelete,
+			Path:   p,
+			Fn:     s.usersDeleteRoute(),
+			Middleware: []ghttp.MiddlewareFn{
+				s.WithAuth(),
+			},
+		},
+		{
+			Method: http.MethodPut,
+			Path:   p,
+			Fn:     s.usersUpdate(),
+			Middleware: []ghttp.MiddlewareFn{
+				s.WithAuth(),
+			},
+		},
+	}
+}
+
+func (s server) usersGetRoute() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		splitPath := strings.Split(r.URL.Path, ",")
+
+		switch len(splitPath) {
+		case 1:
+			if splitPath[0] == userPath {
+				s.usersList()(w, r)
+				return
+			}
+		case 2:
+			if splitPath[0] == userPath {
+				s.usersGet()(w, r)
+				return
+			}
+		case 3:
+			if splitPath[0] == userPath && splitPath[2] == secrPath {
+				s.secretsList()(w, r)
+				return
+			}
+		case 4:
+			if splitPath[0] == userPath && splitPath[2] == secrPath {
+				s.secretsGet()(w, r)
+				return
+			}
+		default:
+			http.Error(w, "not found", http.StatusNotFound)
+		}
+	}
+}
+
+func (s server) usersPostRoute() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		splitPath := strings.Split(r.URL.Path, ",")
+
+		switch len(splitPath) {
+		case 1:
+			if splitPath[0] == userPath {
+				s.usersCreate()(w, r)
+				return
+			}
+		case 3:
+			if splitPath[0] == userPath && splitPath[2] == secrPath {
+				s.secretsCreate()(w, r)
+				return
+			}
+		default:
+			http.Error(w, "not found", http.StatusNotFound)
+		}
+	}
+}
+
+func (s server) usersDeleteRoute() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		splitPath := strings.Split(r.URL.Path, ",")
+
+		switch len(splitPath) {
+		case 1:
+			if splitPath[0] == userPath {
+				s.usersDelete()(w, r)
+				return
+			}
+		case 3:
+			if splitPath[0] == userPath && splitPath[2] == secrPath {
+				s.secretsDelete()(w, r)
+				return
+			}
+		default:
+			http.Error(w, "not found", http.StatusNotFound)
+		}
+	}
+}
