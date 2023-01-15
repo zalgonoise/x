62a0a91141b3a6a2ce2ccaa8fd97f60a590215de
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-15 15:40:37 +0100

    http: server updated to ghttp

--- secr/transport/http/server.go
+++ secr/transport/http/server.go
@@ -2,11 +2,11 @@ package http
 
 import (
 	"context"
-	"fmt"
-	stdhttp "net/http"
 
 	"github.com/zalgonoise/attr"
 	"github.com/zalgonoise/spanner"
+	"github.com/zalgonoise/x/ghttp"
+	"github.com/zalgonoise/x/secr/service"
 )
 
 type Server interface {
@@ -15,38 +15,13 @@ type Server interface {
 }
 
 type server struct {
-	ep   API
-	port int
-	srv  *stdhttp.Server
+	s    service.Service
+	HTTP *ghttp.Server
 }
 
-func NewServer(api API, port int) Server {
-	mux := stdhttp.NewServeMux()
-	httpSrv := &stdhttp.Server{
-		Addr:    fmt.Sprintf(":%v", port),
-		Handler: mux,
-	}
-	srv := server{
-		ep:   api,
-		port: port,
-		srv:  httpSrv,
-	}
-
-	// handlers
-	// TODO: implement dynamic URL paths (e.g.: /users/me/secrets/github)
-	mux.HandleFunc("/login", srv.ep.Login)
-	mux.HandleFunc("/logout", srv.ep.Logout)
-	mux.HandleFunc("/recover", srv.ep.ChangePassword)
-	mux.HandleFunc("/refresh", srv.ep.Refresh)
-	mux.HandleFunc("/users/create", srv.ep.CreateUser)
-	mux.HandleFunc("/users/get", srv.ep.GetUser)
-	mux.HandleFunc("/users", srv.ep.ListUsers)
-	mux.HandleFunc("/users/update", srv.ep.UpdateUser)
-	mux.HandleFunc("/users/delete", srv.ep.DeleteUser)
-	mux.HandleFunc("/secrets/create", srv.ep.CreateSecret)
-	mux.HandleFunc("/secrets/get", srv.ep.GetSecret)
-	mux.HandleFunc("/secrets", srv.ep.ListSecrets)
-	mux.HandleFunc("/secrets/delete", srv.ep.DeleteSecret)
+func NewServer(s service.Service, port int) Server {
+	srv := server{}
+	srv.HTTP = ghttp.NewServer(port, srv.endpoints())
 	return srv
 }
 
@@ -54,7 +29,7 @@ func (s server) Start(ctx context.Context) error {
 	_, span := spanner.Start(ctx, "http.Start")
 	defer span.End()
 
-	err := s.srv.ListenAndServe()
+	err := s.HTTP.ListenAndServe()
 	if err != nil {
 		span.Event("failed to start HTTP server", attr.New("error", err.Error()))
 		return err
@@ -66,7 +41,7 @@ func (s server) Stop(ctx context.Context) error {
 	ctx, span := spanner.Start(ctx, "http.Stop")
 	defer span.End()
 
-	err := s.srv.Shutdown(ctx)
+	err := s.HTTP.Shutdown(ctx)
 	if err != nil {
 		span.Event("failed to stop HTTP server", attr.New("error", err.Error()))
 		return err
