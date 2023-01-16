397c1cde28c532c278cc9466e8db2e440b277294
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-16 17:49:33 +0100

    validation: added validation funcs

---
+++ secr/secret/validate.go
@@ -0,0 +1,47 @@
+package secret
+
+import (
+	"errors"
+	"regexp"
+)
+
+var (
+	ErrEmptyKey   = errors.New("key cannot be empty")
+	ErrLongKey    = errors.New("key is too long")
+	ErrInvalidKey = errors.New("invalid key")
+
+	ErrEmptyValue = errors.New("value cannot be empty")
+	ErrLongValue  = errors.New("value is too long")
+)
+
+const (
+	keyMaxLength   = 20
+	valueMaxLength = 8192
+)
+
+var (
+	keyRegex = regexp.MustCompile(`[a-z0-9]+[a-z0-9\-_]+[a-z0-9]+`)
+)
+
+func ValidateKey(key string) error {
+	if key == "" {
+		return ErrEmptyKey
+	}
+	if len(key) > keyMaxLength {
+		return ErrLongKey
+	}
+	if match := keyRegex.FindString(key); match != key {
+		return ErrInvalidKey
+	}
+	return nil
+}
+
+func ValidateValue(value []byte) error {
+	if len(value) == 0 {
+		return ErrEmptyValue
+	}
+	if len(value) > valueMaxLength {
+		return ErrLongValue
+	}
+	return nil
+}
---
+++ secr/user/validate.go
@@ -0,0 +1,88 @@
+package user
+
+import (
+	"errors"
+	"regexp"
+)
+
+var (
+	ErrEmptyUsername   = errors.New("username cannot be empty")
+	ErrShortUsername   = errors.New("username is too short")
+	ErrLongUsername    = errors.New("username is too long")
+	ErrInvalidUsername = errors.New("invalid username")
+
+	ErrEmptyName   = errors.New("name cannot be empty")
+	ErrShortName   = errors.New("name is too short")
+	ErrLongName    = errors.New("name is too long")
+	ErrInvalidName = errors.New("invalid name")
+
+	ErrEmptyPassword   = errors.New("password cannot be empty")
+	ErrShortPassword   = errors.New("password is too short")
+	ErrLongPassword    = errors.New("password is too long")
+	ErrInvalidPassword = errors.New("invalid password")
+)
+
+const (
+	usernameMinLength = 3
+	usernameMaxLength = 25
+
+	nameMinLength = 2
+	nameMaxLength = 25
+
+	passwordMinLength = 7
+	passwordMaxLength = 30
+)
+
+var (
+	usernameRegex = regexp.MustCompile(`[a-z0-9]+[a-z0-9\-_]+[a-z0-9]+`)
+	nameRegex     = regexp.MustCompile(`[a-zA-Z]+[\s]?[a-zA-Z]+`)
+	passwordRegex = regexp.MustCompile(`[a-zA-Z\d\-\_\~\!\@\#\$\%\^\&\*\(\)\[\]\{\}\;\:\,\.\<\>\?]+`)
+)
+
+func ValidateUsername(username string) error {
+	if username == "" {
+		return ErrEmptyUsername
+	}
+	if len(username) < usernameMinLength {
+		return ErrShortUsername
+	}
+	if len(username) > usernameMaxLength {
+		return ErrLongUsername
+	}
+	if match := usernameRegex.FindString(username); match != username {
+		return ErrInvalidUsername
+	}
+	return nil
+}
+
+func ValidateName(name string) error {
+	if name == "" {
+		return ErrEmptyName
+	}
+	if len(name) < nameMinLength {
+		return ErrShortName
+	}
+	if len(name) > nameMaxLength {
+		return ErrLongName
+	}
+	if match := nameRegex.FindString(name); match != name {
+		return ErrInvalidName
+	}
+	return nil
+}
+
+func ValidatePassword(password string) error {
+	if password == "" {
+		return ErrEmptyPassword
+	}
+	if len(password) < passwordMinLength {
+		return ErrShortPassword
+	}
+	if len(password) > passwordMaxLength {
+		return ErrLongPassword
+	}
+	if match := passwordRegex.FindString(password); match != password {
+		return ErrInvalidPassword
+	}
+	return nil
+}
