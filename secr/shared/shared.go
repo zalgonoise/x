12fbfc194b07adbdf1c7e6648038a8c36c082e79
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-18 13:56:15 +0100

    shared: updated entity

--- secr/shared/shared.go
+++ secr/shared/shared.go
@@ -3,14 +3,17 @@ package shared
 import (
 	"time"
 
+	"github.com/zalgonoise/x/secr/secret"
 	"github.com/zalgonoise/x/secr/user"
 )
 
 // Shared is metadata for a secret that a user (the owner) shares with a set of users
 // optionally within a limited period of time
-type Shared struct {
-	Key    string
-	Owner  user.User
-	Shares []user.User
-	Until  time.Time
+type Share struct {
+	ID        uint64
+	Secret    secret.Secret
+	Owner     user.User
+	Target    []user.User
+	Until     time.Time
+	CreatedAt time.Time
 }
