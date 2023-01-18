fcf8306900eb7c5df7c01341b8f887c5a854b546
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-18 13:56:49 +0100

    secret: add ID to secret's entity

--- secr/secret/secret.go
+++ secr/secret/secret.go
@@ -8,6 +8,7 @@ import (
 // is a slice of bytes. Secrets are encrypted then stored with a user-scoped
 // private key
 type Secret struct {
+	ID        uint64
 	Key       string
 	Value     []byte
 	CreatedAt time.Time
