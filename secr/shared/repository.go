46eb16b6a7293166228446ea995615f1156c5793
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-18 19:05:49 +0100

    shared: expanded repository

--- secr/shared/repository.go
+++ secr/shared/repository.go
@@ -6,7 +6,9 @@ import (
 
 type Repository interface {
 	// Get fetches the secret's share metadata for a given username and secret key
-	Get(ctx context.Context, username, secretName string) (*Share, error)
+	Get(ctx context.Context, username, secretName string) ([]*Share, error)
+	// List fetches all shared secrets for a given username
+	List(ctx context.Context, username string) ([]*Share, error)
 	// Create shares the secret identified by `secretName`, owned by `owner`, with
 	// user `target`. Returns an error
 	Create(ctx context.Context, s *Share) error
