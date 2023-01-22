547dc1444e636411def9e3e4ccd1360946aeeb22
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-22 22:11:16 +0100

    shared: default duration for shared secrets

--- secr/shared/shared.go
+++ secr/shared/shared.go
@@ -4,13 +4,16 @@ import (
 	"time"
 )
 
+// DefaultShareDuration sets a maximum of 30 days for shared secrets with no defined time limit
+const DefaultShareDuration = time.Hour * 24 * 30
+
 // Shared is metadata for a secret that a user (the owner) shares with a set of users
 // optionally within a limited period of time
 type Share struct {
 	ID        uint64     `json:"id"`
 	SecretKey string     `json:"secret_key"`
 	Owner     string     `json:"owner"`
-	Target    []string   `json:"targets"`
+	Target    []string   `json:"shared_with"`
 	Until     *time.Time `json:"until,omitempty"`
 	CreatedAt time.Time  `json:"created_at"`
 }
