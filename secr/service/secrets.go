44329dc86a767cbad603a6e13fec4e237167763b
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-22 15:56:13 +0100

    service: updated secrets implementation

--- secr/service/secrets.go
+++ secr/service/secrets.go
@@ -4,17 +4,21 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/zalgonoise/x/secr/bolt"
 	"github.com/zalgonoise/x/secr/crypt"
 	"github.com/zalgonoise/x/secr/keys"
 	"github.com/zalgonoise/x/secr/secret"
+	"github.com/zalgonoise/x/secr/shared"
 	"github.com/zalgonoise/x/secr/user"
 )
 
 var (
 	ErrInvalidKey   = errors.New("error validating secret key")
 	ErrInvalidValue = errors.New("error validating secret value")
+	ErrZeroShares   = errors.New("no shared secrets found with the input key")
 )
 
 // CreateSecret creates a secret with key `key` and value `value` (as a slice of bytes), for the
@@ -23,7 +27,7 @@ func (s service) CreateSecret(ctx context.Context, username string, key string, 
 	if err := user.ValidateUsername(username); err != nil {
 		return fmt.Errorf("%w: %v", ErrInvalidUser, err)
 	}
-	if err := secret.ValidateKey(key); err != nil {
+	if isShared, err := secret.ValidateKey(key); err != nil || isShared {
 		return fmt.Errorf("%w: %v", ErrInvalidKey, err)
 	}
 	if err := secret.ValidateValue(value); err != nil {
@@ -80,10 +84,20 @@ func (s service) GetSecret(ctx context.Context, username string, key string) (*s
 	if err := user.ValidateUsername(username); err != nil {
 		return nil, fmt.Errorf("%w: %v", ErrInvalidUser, err)
 	}
-	if err := secret.ValidateKey(key); err != nil {
+	isShared, err := secret.ValidateKey(key)
+	if err != nil {
 		return nil, fmt.Errorf("%w: %v", ErrInvalidKey, err)
 	}
 
+	if isShared {
+		userAndKey := strings.Split(key, ":")
+		return s.getSharedSecret(ctx, userAndKey[0], userAndKey[1], username)
+	}
+
+	return s.getSecret(ctx, username, key)
+}
+
+func (s service) getSecret(ctx context.Context, username, key string) (*secret.Secret, error) {
 	u, err := s.GetUser(ctx, username)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch user: %v", err)
@@ -116,6 +130,44 @@ func (s service) GetSecret(ctx context.Context, username string, key string) (*s
 	cipher.Decrypt(decValue, encValue)
 
 	secr.Value = decValue
+	return secr, nil
+}
+
+func (s service) getSharedSecret(ctx context.Context, owner, key, target string) (*secret.Secret, error) {
+	sh, err := s.shares.Get(ctx, owner, key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch shared secret metadata: %v", err)
+	}
+
+	var validShare *shared.Share
+
+	for _, share := range sh {
+		if share.Until != nil {
+			if time.Now().After(*share.Until) {
+				err := s.shares.Delete(ctx, share)
+				if err != nil {
+					return nil, fmt.Errorf("failed to remove expired shared secret: %v", err)
+				}
+				continue
+			}
+		}
+		for _, t := range share.Target {
+			if t.Username == target {
+				validShare = share
+				break
+			}
+		}
+	}
+
+	if validShare == nil {
+		return nil, ErrZeroShares
+	}
+
+	secr, err := s.getSecret(ctx, owner, key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch shared secret: %v", err)
+	}
+	secr.CreatedAt = time.Time{}
 	return secr, nil
 }
 
@@ -161,6 +213,18 @@ func (s service) ListSecrets(ctx context.Context, username string) ([]*secret.Se
 		secr.Value = decValue
 	}
 
+	sharedSecrets, err := s.shares.ListTarget(ctx, username)
+	if err != nil {
+		return secrets, fmt.Errorf("failed to fetch secrets shared with %s: %v", username, err)
+	}
+	for _, sh := range sharedSecrets {
+		sharedSecr, err := s.getSharedSecret(ctx, sh.Owner.Username, sh.Secret.Key, username)
+		if err != nil {
+			return secrets, fmt.Errorf("failed to fetch secrets shared with %s: %v", username, err)
+		}
+		secrets = append(secrets, sharedSecr)
+	}
+
 	return secrets, nil
 }
 
@@ -169,7 +233,7 @@ func (s service) DeleteSecret(ctx context.Context, username string, key string) 
 	if err := user.ValidateUsername(username); err != nil {
 		return fmt.Errorf("%w: %v", ErrInvalidUser, err)
 	}
-	if err := secret.ValidateKey(key); err != nil {
+	if isShared, err := secret.ValidateKey(key); err != nil || isShared {
 		return fmt.Errorf("%w: %v", ErrInvalidKey, err)
 	}
 
