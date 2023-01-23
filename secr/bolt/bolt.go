dfac54aff25b80afb70cc1fd6753ddf97c212bf0
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-23 14:09:08 +0100

    bolt: refactored Open

--- secr/bolt/bolt.go
+++ secr/bolt/bolt.go
@@ -7,9 +7,5 @@ import (
 // Open will initialize a Bolt DB based on the `.db` file in `path`,
 // returning a pointer to a bbolt.DB and an error
 func Open(path string) (*bbolt.DB, error) {
-	db, err := bbolt.Open(path, 0600, nil)
-	if err != nil {
-		return nil, err
-	}
-	return db, nil
+	return bbolt.Open(path, 0600, nil)
 }
