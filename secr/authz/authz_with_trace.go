13ef5620bf2eac96ad08616a7e836d27a22bce48
ZalgoNoise <zalgo.noise@gmail.com>
2023-01-24 01:02:12 +0100

    authz: remove Refresh / Validate, polish

--- secr/authz/authz_with_trace.go
+++ secr/authz/authz_with_trace.go
@@ -34,39 +34,6 @@ func (t withTrace) NewToken(ctx context.Context, u *user.User) (string, error) {
 	return token, nil
 }
 
-// Refresh returns a new JWT for the user `u` based on token `token`, and an error
-func (t withTrace) Refresh(ctx context.Context, u *user.User, token string) (string, error) {
-	ctx, s := spanner.Start(ctx, "authz.Refresh")
-	defer s.End()
-	s.Add(
-		attr.String("username", u.Username),
-	)
-
-	token, err := t.r.Refresh(ctx, u, token)
-	if err != nil {
-		s.Event("error refreshing token for user", attr.New("error", err.Error()))
-		return token, err
-	}
-	return token, nil
-}
-
-// Validate verifies if the JWT `token` is valid for the user `u`, returning a
-// boolean and an error
-func (t withTrace) Validate(ctx context.Context, u *user.User, token string) (bool, error) {
-	ctx, s := spanner.Start(ctx, "authz.Validate")
-	defer s.End()
-	s.Add(
-		attr.String("username", u.Username),
-	)
-
-	ok, err := t.r.Validate(ctx, u, token)
-	if err != nil {
-		s.Event("error validating token for user", attr.New("error", err.Error()))
-		return ok, err
-	}
-	return ok, nil
-}
-
 // Parse returns the data from a valid JWT
 func (t withTrace) Parse(ctx context.Context, token string) (*user.User, error) {
 	ctx, s := spanner.Start(ctx, "authz.Parse")
--- secr/authz/jwt.go
+++ secr/authz/jwt.go
@@ -13,36 +13,34 @@ import (
 
 type usernameIdentifier string
 
-const ContextUsername usernameIdentifier = "secr:username"
+const (
+	contextUsername usernameIdentifier = "secr:username"
+
+	jwtExpiryTime = time.Hour * 1
+)
 
 var (
 	ErrMissingExpiry = errors.New("missing expiry claim")
 	ErrMissingUser   = errors.New("missing user claim")
 	ErrInvalidUser   = errors.New("invalid user claim")
 	ErrExpired       = errors.New("token has expired")
-	ErrInvalidToken  = errors.New("invalid token")
 )
 
 // Authorizer is responsible for generating, refreshing and validating JWT
 type Authorizer interface {
 	// NewToken returns a new JWT for the user `u`, and an error
 	NewToken(ctx context.Context, u *user.User) (string, error)
-	// Refresh returns a new JWT for the user `u` based on token `token`, and an error
-	Refresh(ctx context.Context, u *user.User, token string) (string, error)
-	// Validate verifies if the JWT `token` is valid for the user `u`, returning a
-	// boolean and an error
-	Validate(ctx context.Context, u *user.User, token string) (bool, error)
 	// Parse returns the data from a valid JWT
 	Parse(ctx context.Context, token string) (*user.User, error)
+}
+
+type authz struct {
+	signingKey []byte
 }
 
 // NewAuthorizer initializes an Authorizer with the signing key `signingKey`
 func NewAuthorizer(signingKey []byte) Authorizer {
 	return &authz{signingKey}
-}
-
-type authz struct {
-	signingKey []byte
 }
 
 type jwtUser struct {
@@ -55,7 +53,7 @@ type jwtUser struct {
 // NewToken returns a new JWT for the user `u`, and an error
 func (a *authz) NewToken(ctx context.Context, u *user.User) (string, error) {
 	tok := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"exp": time.Now().Add(time.Hour * 1).Unix(),
+		"exp": time.Now().Add(jwtExpiryTime).Unix(),
 		"user": jwtUser{
 			Username:  u.Username,
 			Name:      u.Name,
@@ -69,39 +67,6 @@ func (a *authz) NewToken(ctx context.Context, u *user.User) (string, error) {
 		return "", fmt.Errorf("failed to sign token: %v", err)
 	}
 	return token, nil
-}
-
-// Refresh returns a new JWT for the user `u` based on token `token`, and an error
-func (a *authz) Refresh(ctx context.Context, u *user.User, token string) (string, error) {
-	ok, err := a.Validate(ctx, u, token)
-	if err != nil {
-		return "", fmt.Errorf("failed to validate token: %v", err)
-	}
-	if !ok {
-		return "", ErrInvalidToken
-	}
-	return a.NewToken(ctx, u)
-}
-
-// Validate verifies if the JWT `token` is valid for the user `u`, returning a
-// boolean and an error
-func (a *authz) Validate(ctx context.Context, u *user.User, token string) (bool, error) {
-	jwtUser, err := a.Parse(ctx, token)
-	if err != nil {
-		return false, err
-	}
-
-	if jwtUser.Username != u.Username {
-		return false, ErrInvalidUser
-	}
-	return true, nil
-}
-
-func (a *authz) parseToken(token *jwt.Token) (interface{}, error) {
-	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-		return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-	}
-	return a.signingKey, nil
 }
 
 // Parse returns the data from a valid JWT
@@ -136,18 +101,25 @@ func (a *authz) Parse(ctx context.Context, token string) (*user.User, error) {
 	}, nil
 }
 
-// SignRequest sets the input username `u` as a ContextUsername context value for
+func (a *authz) parseToken(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+	}
+	return a.signingKey, nil
+}
+
+// SignRequest sets the input username `u` as a contextUsername context value for
 // the HTTP Request `r`'s context
 func SignRequest(u string, r *http.Request) *http.Request {
-	return r.WithContext(context.WithValue(r.Context(), ContextUsername, u))
+	return r.WithContext(context.WithValue(r.Context(), contextUsername, u))
 }
 
 // GetCaller returns the username associated with the HTTP Request `r`, as extracted
-// from the request's context, under its ContextUsername value (if existing).
+// from the request's context, under its contextUsername value (if existing).
 //
 // Returns the username and an OK-boolean.
 func GetCaller(r *http.Request) (string, bool) {
-	v := r.Context().Value(ContextUsername)
+	v := r.Context().Value(contextUsername)
 	if v == nil {
 		return "", false
 	}
