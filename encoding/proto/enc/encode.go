d0e93760d3c0e7a70cadec5a18f10089b909dc9e
ZalgoNoise <zalgo.noise@gmail.com>
2023-02-07 01:37:26 +0100

    enc: Encoder is not a pointer

--- encoding/proto/enc/encode.go
+++ encoding/proto/enc/encode.go
@@ -2,17 +2,19 @@ package types
 
 import "bytes"
 
+const minSize = 512
+
 type Encoder struct {
 	b *bytes.Buffer
 }
 
-func NewEncoder() *Encoder {
-	return &Encoder{
-		b: bytes.NewBuffer(make([]byte, 0, 512)),
+func NewEncoder() Encoder {
+	return Encoder{
+		b: bytes.NewBuffer(make([]byte, 0, minSize)),
 	}
 }
 
-func (w *Encoder) EncodeVarint(value uint64) int {
+func (w Encoder) EncodeVarint(value uint64) int {
 	for n := 0; ; n++ {
 		if value < 128 {
 			_ = w.b.WriteByte((byte)(value))
@@ -23,28 +25,28 @@ func (w *Encoder) EncodeVarint(value uint64) int {
 		value >>= 7
 	}
 }
-func (w *Encoder) EncodeInt64(value int64) int {
+func (w Encoder) EncodeInt64(value int64) int {
 	return w.EncodeVarint(uint64(value<<1) ^ uint64(value>>63))
 }
-func (w *Encoder) EncodeLengthDelimited(value []byte) int {
+func (w Encoder) EncodeLengthDelimited(value []byte) int {
 	n := w.EncodeVarint(uint64(len(value)))
 	_, _ = w.b.Write(value)
 	return n + len(value)
 
 }
-func (w *Encoder) EncodeField(fieldNumber int, wireType int, value []byte) int {
+func (w Encoder) EncodeField(fieldNumber int, wireType int, value []byte) int {
 	n := w.EncodeVarint(uint64((fieldNumber << 3) | wireType))
 	return n + w.EncodeLengthDelimited(value)
 }
-func (w *Encoder) EncodeVarintField(fieldNumber int, value uint64) int {
+func (w Encoder) EncodeVarintField(fieldNumber int, value uint64) int {
 	n := w.EncodeVarint(uint64((fieldNumber << 3)))
 	return n + w.EncodeVarint(value)
 }
 
-func (w *Encoder) String() string {
+func (w Encoder) String() string {
 	return w.b.String()
 }
 
-func (w *Encoder) Bytes() []byte {
+func (w Encoder) Bytes() []byte {
 	return w.b.Bytes()
 }
