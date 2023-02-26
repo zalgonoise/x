ba251753bc3a463cc815ce823f09740dd3602aa9
ZalgoNoise <zalgo.noise@gmail.com>
2023-02-26 17:56:18 +0100

    audio/wav: added subchunk validation

--- audio/wav/chunk.go
+++ audio/wav/chunk.go
@@ -5,6 +5,8 @@ import (
 	"encoding/binary"
 )
 
+const ErrInvalidSubChunkHeader err = "invalid subchunk header metadata"
+
 type SubChunk struct {
 	Subchunk2ID   [4]byte // 37-40
 	Subchunk2Size uint32  // 41-44
@@ -17,11 +19,24 @@ func SubChunkFrom(buf []byte) (*SubChunk, error) {
 	if err != nil {
 		return nil, err
 	}
-	return chunk, nil
+	switch string(chunk.Subchunk2ID[:]) {
+	case junkSubchunkIDString, dataSubchunkIDString:
+		return chunk, nil
+	default:
+		return nil, ErrInvalidSubChunkHeader
+	}
 }
 
 func (s *SubChunk) Bytes() []byte {
 	buf := bytes.NewBuffer(make([]byte, 0, 8))
 	_ = binary.Write(buf, binary.LittleEndian, s)
 	return buf.Bytes()
+}
+
+func NewDataSubChunk() *SubChunk {
+	return &SubChunk{Subchunk2ID: defaultSubchunk2ID}
+}
+
+func NewJunkSubChunk() *SubChunk {
+	return &SubChunk{Subchunk2ID: junkSubchunk2ID}
 }
