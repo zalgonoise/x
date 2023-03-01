885b92579d702ea613ad2b17e71c19d76964043c
zalgonoise <zalgo.noise@gmail.com>
2023-03-02 00:25:51 +0100

    audio/wav: swapped out "offset" for just "n"

--- audio/wav/decode.go
+++ audio/wav/decode.go
@@ -12,52 +12,51 @@ func (w *Wav) Write(buf []byte) (n int, err error) {
 	}
 
 	var (
-		offset int
 		header *WavHeader
 		end    int = headerLen
 	)
 
 	if header, err = HeaderFrom(buf[:end]); err == nil {
-		offset += end
+		n += end
 		w.Header = header
 	}
 	if err != nil && !errors.Is(err, ErrInvalidHeader) {
-		return offset, err
+		return n, err
 	}
 	if w.Header == nil {
-		return offset, ErrMissingHeader
+		return n, ErrMissingHeader
 	}
 
-	for offset < len(buf) {
+	for n < len(buf) {
 		// try to read subchunk headers
 		end = 8
-		if offset+end < len(buf) {
-			if subchunk, err := data.HeaderFrom(buf[offset : offset+end]); err == nil {
-				offset += end
+		if n+end < len(buf) {
+			if subchunk, err := data.HeaderFrom(buf[n : n+end]); err == nil {
+				n += end
 				chunk := NewChunk(w.Header.BitsPerSample, subchunk)
 				w.Data = chunk
 
 				end = int(subchunk.Subchunk2Size)
 				// grab remaining bytes if the byte slice isn't long enough
 				// for a subchunk read
-				if offset+end+8 > len(buf) {
-					end += len(buf) - (offset + end)
+				if n+end+8 > len(buf) {
+					end += len(buf) - (n + end)
 				}
 
-				chunk.Parse(buf[offset:offset+end], 0)
+				chunk.Parse(buf[n : n+end])
 				w.Chunks = append(w.Chunks, chunk)
-				offset += end
+				n += end
 				continue
 			}
 		}
 
 		if w.Data != nil {
-			w.Data.Parse(buf[offset:], offset)
-			return len(buf) - offset, nil
+			w.Data.Parse(buf[n:])
+			return len(buf) - n, nil
 		}
-		return offset, ErrMissingDataBuffer
+		return n, ErrMissingDataBuffer
 	}
-	return offset, nil
+	return n, nil
 }
 
 func Decode(buf []byte) (w *Wav, err error) {
