a23e4a6d010db15c59b1116dbb74a28f1b92a56b
ZalgoNoise <zalgo.noise@gmail.com>
2023-03-08 20:06:02 +0100

    audio/wav: documented Mode

--- audio/cmd/gsp/stream/mode.go
+++ audio/cmd/gsp/stream/mode.go
@@ -3,19 +3,24 @@ package stream
 type Mode int
 
 const (
-	Unset Mode = iota
-	Monitor
-	Record
-	Filter
+	Unset   Mode = iota // Unset is the default unset Mode
+	Monitor             // Monitor is the Mode that finds the max peak level of an input audio stream over time
+	Record              // Record is the Mode that records from an audio stream into a file
+	Filter              // Filter is the Mode that keeps recording an audio stream, every time it reaches the set peak
 )
 
-var modeKeys = map[Mode]string{
-	Monitor: "monitor",
-	Record:  "record",
-	Filter:  "filter",
-}
-var modeValues = map[string]Mode{
-	"monitor": Monitor,
-	"record":  Record,
-	"filter":  Filter,
-}
+var (
+	modeKeys = map[Mode]string{
+		Monitor: "monitor",
+		Record:  "record",
+		Filter:  "filter",
+	}
+	modeValues = map[string]Mode{
+		"monitor": Monitor,
+		"record":  Record,
+		"filter":  Filter,
+	}
+
+	_ = modeKeys   // skip any lint warnings for unused variable
+	_ = modeValues // skip any lint warnings for unused variable
+)
