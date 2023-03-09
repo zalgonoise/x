e8dbb1d5bd2924fd467d146e4d767a209dbfc899
ZalgoNoise <zalgo.noise@gmail.com>
2023-03-09 20:09:40 +0100

    audio/wav: remove unused errors

--- audio/wav/errors.go
+++ audio/wav/errors.go
@@ -10,9 +10,6 @@ const (
 	ErrInvalidBitDepth    err = "audio/wav: invalid bit depth"
 	ErrInvalidHeader      err = "audio/wav: invalid WAV header"
 	ErrShortDataBuffer    err = "audio/wav: data buffer is too short"
-	ErrShortHeaderBuffer  err = "audio/wav: header buffer is too short"
-	ErrZeroChunks         err = "audio/wav: no buffered chunks available"
 	ErrMissingHeader      err = "audio/wav: missing header metadata"
 	ErrMissingDataBuffer  err = "audio/wav: missing data buffer to write to, as no previous header was captured"
-	ErrNegativeRead       err = "audio/wav: reader returned negative count from Read"
 )
