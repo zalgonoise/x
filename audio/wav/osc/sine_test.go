f7a393077e452d47334e8fc0ce743e31017d8daf
ZalgoNoise <zalgo.noise@gmail.com>
2023-03-19 17:12:49 +0100

    audio/wav/forms: moved sine test file

--- audio/wav/osc/sine_test.go
+++ audio/wav/osc/sine_test.go
@@ -1,20 +1,22 @@
-package forms_test
+package osc_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/zalgonoise/x/audio/wav"
-	"github.com/zalgonoise/x/audio/wav/forms"
+	"github.com/zalgonoise/x/audio/wav/osc"
 )
 
 func TestSine(t *testing.T) {
 	t.Run(
 		"Success", func(t *testing.T) {
 			chunk := wav.NewChunk(16, nil)
-			chunk.Generate(forms.SineWave, 2000, 0.5, 44100)
+			chunk.Generate(osc.SineWave, 2000, 44100, time.Second/2)
 			if len(chunk.Value()) == 0 {
 				t.Errorf("expected chunk data to be generated")
 			}
+			t.Logf("%+v", chunk.Value()[:1024])
 		},
 	)
 }
