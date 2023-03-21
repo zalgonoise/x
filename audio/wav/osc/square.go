287e8d8baab0cd22da890cc1bb400127ad3f81a1
ZalgoNoise <zalgo.noise@gmail.com>
2023-03-21 19:21:06 +0100

    audio/wav/osc/square: improved oscillator performance

--- audio/wav/osc/square.go
+++ audio/wav/osc/square.go
@@ -1,12 +1,8 @@
 package osc
 
-import (
-	"math"
-)
-
 func Square[T bitDepths](buffer []T, freq, depth, sampleRate float64) {
-	halfPeriod := int(sampleRate / (2.0 * freq))
-	sampleInt := T(math.Pow(2.0, depth-1) - 1.0)
+	halfPeriod := int(sampleRate) / (2 * int(freq))
+	sampleInt := T(2<<int16(depth-2)) - 1
 
 	if len(buffer) > halfPeriod {
 		var wave = make([]T, halfPeriod)
@@ -21,8 +17,10 @@ func Square[T bitDepths](buffer []T, freq, depth, sampleRate float64) {
 }
 
 func square[T bitDepths](buffer []T, halfPeriod int, sampleInt T) {
+	quarterPeriod := halfPeriod / 2
+
 	for i := 0; i < len(buffer); i++ {
-		if i%halfPeriod < halfPeriod/2 {
+		if i%halfPeriod < quarterPeriod {
 			buffer[i] = sampleInt
 			continue
 		}
--- audio/wav/osc/square_test.go
+++ audio/wav/osc/square_test.go
@@ -1,6 +1,7 @@
 package osc_test
 
 import (
+	"math"
 	"testing"
 	"time"
 
@@ -19,6 +20,101 @@ func TestSquare(t *testing.T) {
 				t.Errorf("expected chunk data to be generated")
 			}
 			t.Logf("%+v", chunk.Value()[:1024])
+		},
+	)
+}
+
+func BenchmarkSquareCompare(b *testing.B) {
+	var (
+		sampleRate float64 = 44100
+		depth      float64 = 16
+		freq       float64 = 2000
+
+		before []int16
+		after  []int16
+	)
+
+	b.Run(
+		"squareImplementation", func(b *testing.B) {
+			// square function has been improved in performance by calculating the quarter period only once
+			// so that the multiplication isn't repeatedly computed; as well as bit-shifting a power of 2
+			// instead of calling math.Pow
+			//
+			// Before / After:
+			//
+			// goos: linux
+			// goarch: amd64
+			// cpu: AMD Ryzen 3 PRO 3300U w/ Radeon Vega Mobile Gfx
+			// BenchmarkSquareCompare/squareImplementation/square_21_3_23-4             7605760               199.8 ns/op            24 B/op          1 allocs/op
+			// BenchmarkSquareCompare/squareImplementation/square_replacement-4         9944721               126.9 ns/op            24 B/op          1 allocs/op
+			b.Run(
+				"square_21_3_23", func(b *testing.B) {
+					fn := func(buffer []int16, halfPeriod int, sampleInt int16) {
+						for i := 0; i < len(buffer); i++ {
+							if i%halfPeriod < halfPeriod/2 {
+								buffer[i] = sampleInt
+								continue
+							}
+							buffer[i] = -sampleInt
+						}
+					}
+					b.ResetTimer()
+					for i := 0; i < b.N; i++ {
+						var (
+							halfPeriod = int(sampleRate / (2.0 * freq))
+							sampleInt  = int16(math.Pow(2.0, depth-1) - 1.0)
+						)
+						before = make([]int16, halfPeriod)
+						fn(before, halfPeriod, sampleInt)
+					}
+					_ = before
+				},
+			)
+			b.Run(
+				"square_replacement", func(b *testing.B) {
+					fn := func(buffer []int16, halfPeriod int, sampleInt int16) {
+						// avoid calculating the quarter period over and over again
+						var quarterPeriod = halfPeriod / 2
+						for i := 0; i < len(buffer); i++ {
+							if i%halfPeriod < quarterPeriod {
+								buffer[i] = sampleInt
+								continue
+							}
+							buffer[i] = -sampleInt
+						}
+					}
+					b.ResetTimer()
+					for i := 0; i < b.N; i++ {
+						var (
+							halfPeriod = int(sampleRate) / (2 * int(freq))
+							sampleInt  = int16(2<<int16(depth-2)) - 1
+						)
+						after = make([]int16, halfPeriod)
+						fn(after, halfPeriod, sampleInt)
+					}
+					_ = after
+				},
+			)
+		},
+	)
+	b.Run(
+		"EnsureSimilarResults", func(b *testing.B) {
+			if len(before) == 0 || len(after) == 0 {
+				b.Error("expected both before and after slices to be populated")
+				return
+			}
+
+			if len(before) != len(after) {
+				b.Errorf("output length mismatch error: wanted %d ; got %d", len(before), len(after))
+				return
+			}
+
+			for i := range before {
+				if before[i] != after[i] {
+					b.Errorf("output mismatch error on index #%d -- wanted %d ; got %d", i, before[i], after[i])
+					return
+				}
+			}
 		},
 	)
 }
