0b48c7da21c7cd20ba357c1a459d4ca9c7c90266
ZalgoNoise <zalgo.noise@gmail.com>
2023-03-22 20:18:18 +0100

    audio/wav/osc: documented shapes.go ; exported BitDepths constraint

--- audio/wav/osc/shapes.go
+++ audio/wav/osc/shapes.go
@@ -4,12 +4,17 @@ import (
 	"math"
 )
 
+// Type is an enumeration for the supported oscillator types
 type Type uint8
 
 const (
+	// SineWave is the oscillator Type for a Sine Oscillator
 	SineWave Type = iota
+	// SquareWave is the oscillator Type for a Square Oscillator
 	SquareWave
+	// TriangleWave is the oscillator Type for a Triangle Oscillator
 	TriangleWave
+	// SawtoothWave is the oscillator Type for a Sawtooth Oscillator
 	SawtoothWave
 )
 
@@ -17,8 +22,12 @@ const (
 	tau float64 = math.Pi * 2
 )
 
-type bitDepths interface {
+// BitDepths describes the type constraint for Oscillator functions, for specific
+// bit depths
+type BitDepths interface {
 	int8 | int16 | int32
 }
 
-type Oscillator[T bitDepths] func(buffer []T, freq, depth, sampleRate float64)
+// Oscillator is a generic function that writes a wave of a certain shape into a buffer
+// of BitDepths type
+type Oscillator[T BitDepths] func(buffer []T, freq, depth, sampleRate float64)
