b3152d1b631f5b38e4ab42832876c8eca373ae49
ZalgoNoise <zalgo.noise@gmail.com>
2023-04-02 17:47:21 +0200

    audio/wav: updated Buffer tests with FFT filter

--- audio/wav/buffer_test.go
+++ audio/wav/buffer_test.go
@@ -8,66 +8,138 @@ import (
 	"testing"
 	"time"
 
+	"github.com/zalgonoise/x/audio/wav/fft"
 	"github.com/zalgonoise/x/audio/wav/osc"
 )
 
+func newSine(freq int) (*Wav, error) {
+	// create a sine wave 16 bit depth, 44.1kHz rate, mono,
+	// 5 second duration. Pass audio bytes into a new bytes.Buffer
+	sine, err := New(44100, 16, 1)
+	if err != nil {
+		return nil, err
+	}
+	sine.Data.Generate(osc.SineWave, freq, 44100, 5*time.Second)
+	return sine, nil
+}
+
 func TestWavBuffer(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
-		// expect test to be faster than the actual length of the generated audio
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
-		defer cancel()
+		t.Run("MaxValues", func(t *testing.T) {
+			// expect test to be faster than the actual length of the generated audio
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+			defer cancel()
 
-		// create a sine wave 16 bit depth, 44.1kHz rate, mono,
-		// 5 second duration. Pass audio bytes into a new bytes.Buffer
-		sine, err := New(44100, 16, 1)
-		if err != nil {
-			t.Error(err)
-			return
-		}
-		sine.Data.Generate(osc.SineWave, 2000, 44100, 5*time.Second)
-		buf := bytes.NewBuffer(sine.Bytes())
-
-		// create a channel to read the max values emitted by the filter
-		var maxCh = make(chan int, 2)
-		defer close(maxCh)
-		go func() {
-			for i := range maxCh {
-				t.Log(i)
-			}
-		}()
-
-		// create a new stream using the bytes.Buffer as an io.Reader
-		// half a second ratio (expect 11 entries), with a max values filter
-		w := NewStream(buf).
-			Ratio(0.5).
-			WithFilter(
-				MaxValues(maxCh),
-			)
-
-		// stream the audio using the context and an errors channel
-		errCh := make(chan error)
-		defer close(errCh)
-		go w.Stream(ctx, errCh)
-
-		// wait for the stream processing to end
-		// expect an error (io.EOF) when the stream is consumed
-		//
-		// in case the context is done before an error is received,
-		// it's surely a deadline reached error, as the test took too long
-		select {
-		case err := <-errCh:
-			if errors.Is(err, io.EOF) {
-				return
-			}
-			t.Errorf("unexpected error: wanted %v ; got %v", io.EOF, err)
-			return
-		case <-ctx.Done():
-			err := ctx.Err()
+			// sine wave stream for testing
+			sine, err := newSine(2000)
 			if err != nil {
 				t.Error(err)
 				return
 			}
-		}
+			buf := bytes.NewBuffer(sine.Bytes())
+
+			// create a channel to read the max values emitted by the filter
+			var maxCh = make(chan int, 2)
+			defer close(maxCh)
+			go func() {
+				for i := range maxCh {
+					t.Log(i)
+				}
+			}()
+
+			// create a new stream using the bytes.Buffer as an io.Reader
+			// half a second ratio (expect 11 entries), with a max values filter
+			w := NewStream(buf).
+				Ratio(0.5).
+				WithFilter(
+					MaxValues(maxCh),
+				)
+
+			// stream the audio using the context and an errors channel
+			errCh := make(chan error)
+			defer close(errCh)
+			go w.Stream(ctx, errCh)
+
+			// wait for the stream processing to end
+			// expect an error (io.EOF) when the stream is consumed
+			//
+			// in case the context is done before an error is received,
+			// it's surely a deadline reached error, as the test took too long
+			select {
+			case err := <-errCh:
+				if errors.Is(err, io.EOF) {
+					return
+				}
+				t.Errorf("unexpected error: wanted %v ; got %v", io.EOF, err)
+				return
+			case <-ctx.Done():
+				err := ctx.Err()
+				if err != nil {
+					t.Error(err)
+					return
+				}
+			}
+		})
+		t.Run("FFTOnThreshold", func(t *testing.T) {
+			// expect test to be faster than the actual length of the generated audio
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+			defer cancel()
+
+			// sine wave stream for testing
+			sine, err := newSine(1000)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			buf := bytes.NewBuffer(sine.Bytes())
+
+			var detectionCounter int
+
+			// create a channel to read the max values emitted by the filter
+			var maxCh = make(chan fft.FrequencyPower, 2)
+			defer close(maxCh)
+			go func() {
+				for i := range maxCh {
+					t.Log(i)
+					detectionCounter++
+				}
+			}()
+
+			// create a new stream using the bytes.Buffer as an io.Reader
+			// half a second ratio (expect 11 entries), with a max values filter
+			w := NewStream(buf).
+				WithFilter(
+					FFTOnThreshold(fft.Block128, 10, maxCh),
+				)
+
+			// stream the audio using the context and an errors channel
+			errCh := make(chan error)
+			defer close(errCh)
+			go w.Stream(ctx, errCh)
+
+			// wait for the stream processing to end
+			// expect an error (io.EOF) when the stream is consumed
+			//
+			// in case the context is done before an error is received,
+			// it's surely a deadline reached error, as the test took too long
+			select {
+			case err := <-errCh:
+				if !errors.Is(err, io.EOF) {
+					t.Errorf("unexpected error: wanted %v ; got %v", io.EOF, err)
+					return
+				}
+				if detectionCounter == 0 {
+					t.Errorf("expected detector to increase in value during the test")
+				}
+				return
+			case <-ctx.Done():
+				err := ctx.Err()
+				if err != nil {
+					t.Error(err)
+					return
+				}
+			}
+		})
 	})
 
 	t.Run("FailNoHeader", func(t *testing.T) {
@@ -75,15 +147,12 @@ func TestWavBuffer(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
 		defer cancel()
 
-		// create a sine wave 16 bit depth, 44.1kHz rate, mono,
-		// 5 second duration. Pass audio bytes into a new bytes.Buffer
-		// with no WAV header
-		sine, err := New(44100, 16, 1)
+		// sine wave stream for testing
+		sine, err := newSine(2000)
 		if err != nil {
 			t.Error(err)
 			return
 		}
-		sine.Data.Generate(osc.SineWave, 2000, 44100, 5*time.Second)
 		buf := bytes.NewBuffer(sine.Data.Bytes())
 
 		// create a channel to read the max values emitted by the filter
