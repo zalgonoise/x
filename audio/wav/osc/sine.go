2ddcb00f787136b7a1143205fb79d8baca35c6b2
ZalgoNoise <zalgo.noise@gmail.com>
2023-04-07 19:11:36 +0200

    audio/wav/osc: add fullCycle func to fix sine waves

--- audio/wav/osc/sine.go
+++ audio/wav/osc/sine.go
@@ -7,10 +7,10 @@ import (
 // Sine is an oscillator that writes a sine wave of frequency `freq`, bit depth `depth`,
 // and sample rate `sampleRate`, into the buffer of type T `buffer`
 func Sine[T BitDepths](buffer []T, freq, depth, sampleRate float64) {
-	halfPeriod := int(sampleRate / freq)
+	cycle := fullCycle(sampleRate, freq)
 
-	if len(buffer) > halfPeriod {
-		var wave = make([]T, halfPeriod)
+	if len(buffer) > cycle {
+		var wave = make([]T, cycle)
 		sine(wave, freq, depth, sampleRate)
 		for i := 0; i < len(buffer); i += len(wave) {
 			copy(buffer[i:], wave)
@@ -26,4 +26,23 @@ func sine[T BitDepths](buffer []T, freq, depth, sampleRate float64) {
 		sample := math.Sin(tau * freq * float64(i) / sampleRate)
 		buffer[i] = T(sample * float64(int(2)<<int(depth-1)/2-1))
 	}
+}
+
+// fullCycle provides a buffer length which allows a sine wave to complete a full cycle, until it reaches the same zero
+// point where it started -- allowing for more precise sine waves, as they are generated for one cycle and copied over
+//
+// Annotations in the code show an example of a 2000Hz sine wave in a 44100Hz sample rate. A half period for this
+// frequency is 22.05, and the function finds out that multiplying this value by 20 provides a rounded value of 441.
+func fullCycle(sampleRate, freq float64) int {
+	var (
+		halfPeriod        = sampleRate / freq              // 44100 / 2000 == 22.05
+		halfPeriodFloored = float64(int(halfPeriod))       // floored: 22
+		halfPeriodDecimal = halfPeriod - halfPeriodFloored // decimals: 22.05 - 22 = 0.05000000000000071 (float64 drift)
+	)
+
+	// fix floating point drift
+	halfPeriodDecimal = float64(int(halfPeriodDecimal*1000)) / 1000.0 // 0.05000000000000071 --> 0.05
+	var mul = 1.0 / halfPeriodDecimal                                 // 1.0 / 0.05 == 20
+
+	return int(halfPeriod * mul) // 22.05 * 20 == 441
 }
