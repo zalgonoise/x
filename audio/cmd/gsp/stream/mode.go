c3b521ad21d8e5bfc2548da091662e5ec04b9a5e
ZalgoNoise <zalgo.noise@gmail.com>
2023-04-08 02:21:23 +0200

    audio/cmd/gsp: add analyze mode

--- audio/cmd/gsp/stream/mode.go
+++ audio/cmd/gsp/stream/mode.go
@@ -6,7 +6,8 @@ const (
 	Unset   Mode = iota // Unset is the default unset Mode
 	Monitor             // Monitor is the Mode that finds the max peak level of an input audio stream over time
 	Record              // Record is the Mode that records from an audio stream into a file
-	Filter              // Filter is the Mode that keeps recording an audio stream, every time it reaches the set peak
+	Filter
+	Analyze // Filter is the Mode that keeps recording an audio stream, every time it reaches the set peak
 )
 
 var (
@@ -14,11 +15,13 @@ var (
 		Monitor: "monitor",
 		Record:  "record",
 		Filter:  "filter",
+		Analyze: "analyze",
 	}
 	modeValues = map[string]Mode{
 		"monitor": Monitor,
 		"record":  Record,
 		"filter":  Filter,
+		"analyze": Analyze,
 	}
 
 	_ = modeKeys   // skip any lint warnings for unused variable
