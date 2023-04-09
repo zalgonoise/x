82f586829df3d8b283c0b82a049b7bddb447c05c
ZalgoNoise <zalgo.noise@gmail.com>
2023-04-09 17:12:37 +0200

    audio/wav/fft: add AsBlock function

--- audio/wav/fft/fft.go
+++ audio/wav/fft/fft.go
@@ -34,6 +34,29 @@ const (
 	Block8192
 )
 
+var blockSizeMap = map[int]BlockSize{
+	8:    Block8,
+	16:   Block16,
+	32:   Block32,
+	64:   Block64,
+	128:  Block128,
+	256:  Block256,
+	512:  Block512,
+	1024: Block1024,
+	2048: Block2048,
+	4096: Block4096,
+	8192: Block8192,
+}
+
+// AsBlock returns a valid BlockSize for the input int `size`. If the input
+// size is not valid, a default BlockSize is returned (Block1024)
+func AsBlock(size int) BlockSize {
+	if bs, ok := blockSizeMap[size]; ok {
+		return bs
+	}
+	return Block1024
+}
+
 const (
 	tau    = math.Pi * 2
 	twoTau = math.Pi * 4
