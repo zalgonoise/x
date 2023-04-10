8473c7580a97f07ff42fb72c02a21de6222667e6
ZalgoNoise <zalgo.noise@gmail.com>
2023-04-10 14:59:46 +0200

    audio/wav/fft/window: reorganized Hamming

--- audio/wav/fft/window/hamming.go
+++ audio/wav/fft/window/hamming.go
@@ -1,10 +1,10 @@
 package window
 
 import (
-	"fmt"
+	"math"
 )
 
-var hammingMap = map[int]Block{
+var hammingMap = map[int]Window{
 	8:    Hamming8,
 	16:   Hamming16,
 	32:   Hamming32,
@@ -18,10 +18,33 @@ var hammingMap = map[int]Block{
 	8192: Hamming8192,
 }
 
-func Hamming(i int) (Block, error) {
+// Hamming returns an L-point symmetric Hamming window.
+// Reference: http://www.mathworks.com/help/signal/ref/hamming.html
+func Hamming(i int) Window {
 	w, ok := hammingMap[i]
 	if !ok {
-		return nil, fmt.Errorf("%w: size %d doesn't have a precomputed window", ErrInvalidBlockSize, i)
+		return newHamming(i)
 	}
-	return w, nil
+	return w
+}
+
+func newHamming(i int) Window {
+	switch i {
+	case 0:
+		return []float64{}
+	case 1:
+		return []float64{1}
+	default:
+		var (
+			r           = make([]float64, i, i)
+			idx         = float64(i - 1)
+			coefficient = tau / idx
+		)
+
+		for n := 0.0; n <= idx; n++ {
+			r[int(n)] = 0.54 - 0.46*math.Cos(coefficient*n)
+		}
+
+		return r
+	}
 }
