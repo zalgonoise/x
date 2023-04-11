f587b49852ef7ecc297b34afae6d743c50ab3deb
ZalgoNoise <zalgo.noise@gmail.com>
2023-04-11 18:04:12 +0200

    audio/wav/fft: documented code

--- audio/wav/fft/conv.go
+++ audio/wav/fft/conv.go
@@ -4,6 +4,8 @@ import (
 	"unsafe"
 )
 
+// ToComplex converts a slice of float64 into a slice of complex128, where the input
+// slice's values are set as the real part of the complex numbers
 func ToComplex(x []float64) []complex128 {
 	length := len(x)
 	out := make([]float64, length<<1)
