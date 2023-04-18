5a7ea7cb2c7b4991a597ccd129062e27474f9b3d
ZalgoNoise <zalgo.noise@gmail.com>
2023-04-18 18:54:34 +0200

    wav/fft/trig: add fast cos function

---
+++ audio/wav/fft/trig/cos.go
@@ -0,0 +1,51 @@
+package trig
+
+import (
+	"unsafe"
+
+	"github.com/zalgonoise/x/ptr"
+)
+
+func Cos(x float64) float64 {
+	// make argument positive
+	sign := false
+
+	// raw math.Abs(x)
+	x = *(*float64)(unsafe.Pointer(ptr.To(*(*uint64)(unsafe.Pointer(&x)) &^ (1 << 63))))
+
+	var j uint64
+	var y, z float64
+	if x >= reduceThreshold {
+		j, z = trigReduce(x)
+	} else {
+		j = uint64(x * (PI4)) // integer part of x/(Pi/4), as integer for tests on the phase angle
+		y = float64(j)        // integer part of x/(Pi/4), as float
+
+		// map zeros to origin
+		if j&1 == 1 {
+			j++
+			y++
+		}
+		j &= 7                               // octant modulo 2Pi radians (360 degrees)
+		z = ((x - y*PI4A) - y*PI4B) - y*PI4C // Extended precision modular arithmetic
+	}
+
+	if j > 3 {
+		j -= 4
+		sign = !sign
+	}
+	if j > 1 {
+		sign = !sign
+	}
+
+	zz := z * z
+	if j == 1 || j == 2 {
+		y = z + z*zz*((((((_sin[0]*zz)+_sin[1])*zz+_sin[2])*zz+_sin[3])*zz+_sin[4])*zz+_sin[5])
+	} else {
+		y = 1.0 - 0.5*zz + zz*zz*((((((_cos[0]*zz)+_cos[1])*zz+_cos[2])*zz+_cos[3])*zz+_cos[4])*zz+_cos[5])
+	}
+	if sign {
+		y = -y
+	}
+	return y
+}
---
+++ audio/wav/fft/trig/cos_test.go
@@ -0,0 +1,61 @@
+package trig
+
+import (
+	"math"
+	"testing"
+)
+
+// BenchmarkSin explores performance improvements in this Sin implementation
+// and the standar library's math package implementation
+//
+// goos: linux
+// goarch: amd64
+// pkg: github.com/zalgonoise/x/audio/wav/fft/trig
+// cpu: AMD Ryzen 3 PRO 3300U w/ Radeon Vega Mobile Gfx
+// BenchmarkCos/Self-4             539523524               11.12 ns/op            0 B/op          0 allocs/opp
+// BenchmarkCos/StdLib-4           504334958               11.61 ns/op            0 B/op          0 allocs/op
+// PASS
+func BenchmarkCos(b *testing.B) {
+	var (
+		input   = -1.0
+		resultA float64
+		resultB float64
+	)
+
+	b.Run("Self", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			resultA = Cos(input)
+		}
+		_ = resultA
+	})
+
+	b.Run("StdLib", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			resultB = math.Cos(input)
+		}
+		_ = resultB
+	})
+
+	if resultA != resultB {
+		b.Errorf("output mismatch error: A: %v ; B: %v", resultA, resultB)
+	}
+}
+
+func FuzzCos(f *testing.F) {
+
+	f.Add(1.2)
+	f.Add(0.3)
+	f.Add(8.0)
+	f.Add(123.7)
+	f.Add(-1.0)
+	f.Add(-0.3)
+	f.Add(-5.0)
+	f.Add(536870913.0)
+	f.Fuzz(func(t *testing.T, a float64) {
+		resA := Cos(a)
+		resB := math.Cos(a)
+		if resA != resB {
+			t.Errorf("output mismatch error: A: %v ; B: %v", resA, resB)
+		}
+	})
+}
