08b5ffdc9d91c1aa0b741224bdee36a97df3da2f
ZalgoNoise <zalgo.noise@gmail.com>
2023-04-20 21:58:43 +0200

    wav/fft: rev bits as over zero

--- audio/wav/fft/reverse_bits.go
+++ audio/wav/fft/reverse_bits.go
@@ -9,7 +9,7 @@ type RevBitsTypes interface {
 // ReverseFirstBits returns the first `size` bits of `value` in reverse order
 // from: http://graphics.stanford.edu/~seander/bithacks.html#BitReverseObvious
 func ReverseFirstBits[T RevBitsTypes](value, size T) (reverse T) {
-	for ; value != 0; value, size = value>>1, size-1 {
+	for ; value > 0; value, size = value>>1, size-1 {
 		reverse <<= 1        // left shift
 		reverse |= value & 1 // add next bit
 	}
@@ -20,7 +20,7 @@ func ReverseFirstBits[T RevBitsTypes](value, size T) (reverse T) {
 // ReverseBits returns the bits of `value` in reverse order
 // from: http://graphics.stanford.edu/~seander/bithacks.html#BitReverseObvious
 func ReverseBits[T RevBitsTypes](value T) (reverse T) {
-	for ; value != 0; value >>= 1 {
+	for ; value > 0; value >>= 1 {
 		reverse <<= 1        // left shift
 		reverse |= value & 1 // add next bit
 	}
