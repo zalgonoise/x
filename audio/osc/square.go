8e0dfaf4b62dfade393af1a4d6e80b7a759119b6
zalgonoise <mariozalgo@gmail.com>
2023-04-27 20:26:56 +0200

    audio/osc/square: compensate sampleRate / freq

--- audio/osc/square.go
+++ audio/osc/square.go
@@ -3,29 +3,38 @@ package osc
 // Square is an oscillator that writes a square wave of frequency `freq`, bit depth `depth`,
 // and sample rate `sampleRate`, into the buffer of type T `buffer`
 func Square[T BitDepths](buffer []T, freq, depth, sampleRate float64) {
-	halfPeriod := int(sampleRate) / (2 * int(freq))
-	sampleInt := T(2<<int16(depth-2)) - 1
+	var (
+		halfPeriod = int(sampleRate) / (2 * int(freq))
+		sampleInt  = T(2<<int16(depth-2)) - 1
+		cycle      = fullCycle(sampleRate, freq)
+	)
 
 	if len(buffer) > halfPeriod {
 		var wave = make([]T, halfPeriod)
-		square(wave, halfPeriod, sampleInt)
+		square(wave, halfPeriod, cycle, sampleInt)
 		for i := 0; i < len(buffer); i += len(wave) {
 			copy(buffer[i:], wave)
 		}
 		return
 	}
 
-	square(buffer, halfPeriod, sampleInt)
+	square(buffer, halfPeriod, cycle, sampleInt)
 }
 
-func square[T BitDepths](buffer []T, halfPeriod int, sampleInt T) {
+func square[T BitDepths](buffer []T, halfPeriod, cycle int, sampleInt T) {
 	quarterPeriod := halfPeriod / 2
 
 	for i := 0; i < len(buffer); i++ {
+		if i+1%cycle == 0 {
+			buffer[i] = buffer[i-1]
+			continue
+		}
+
 		if i%halfPeriod < quarterPeriod {
 			buffer[i] = sampleInt
 			continue
 		}
+
 		buffer[i] = -sampleInt
 	}
 }
