bdfacb38465d218a74dc20e1d5fd184aaf7fc22f
zalgonoise <mariozalgo@gmail.com>
2023-04-28 20:11:49 +0200

    audio/osc/sawtooth: updated fullCycle call; added freq drift detection to the buffer

--- audio/osc/sawtooth.go
+++ audio/osc/sawtooth.go
@@ -4,29 +4,37 @@ package osc
 // and sample rate `sampleRate`, into the buffer of type T `buffer`
 func Sawtooth[T BitDepths](buffer []T, freq, depth, sampleRate float64) {
 	var (
-		halfPeriod = int(sampleRate / freq)
-		increment  = 2.0 / float64(halfPeriod)
-		sampleInt  T
-		cycle      = fullCycle(sampleRate, freq)
+		halfPeriod, mul = fullCycle(sampleRate, freq)
+		gap             = halfPeriod * mul
+		increment       = 2.0 / float64(halfPeriod)
+		sampleInt       T
 	)
 
 	if len(buffer) > halfPeriod {
 		var wave = make([]T, halfPeriod)
-		sawtooth(wave, halfPeriod, cycle, sampleInt, increment, depth)
-		for i := 0; i < len(buffer); i += len(wave) {
+		sawtooth(wave, halfPeriod, gap, sampleInt, increment, depth)
+
+		for i, j := 0, 0; i < len(buffer); i, j = i+len(wave), j+1 {
 			copy(buffer[i:], wave)
+
+			next := i + len(wave)
+			if j+1%mul == 0 && next < len(buffer) {
+				buffer[next+1] = buffer[next]
+				i++
+			}
 		}
 		return
 	}
-	sawtooth(buffer, halfPeriod, cycle, sampleInt, increment, depth)
+
+	sawtooth(buffer, halfPeriod, gap, sampleInt, increment, depth)
 }
 
-func sawtooth[T BitDepths](buffer []T, halfPeriod, cycle int, sampleInt T, increment, depth float64) {
+func sawtooth[T BitDepths](buffer []T, halfPeriod, gap int, sampleInt T, increment, depth float64) {
 	var base T = ^(2 << int(depth-2)) + 2
 	inc := T(increment * float64(^base))
 
 	for i := 0; i < len(buffer); i++ {
-		if i+1%cycle == 0 {
+		if i+1%gap == 0 {
 			buffer[i] = sampleInt
 			continue
 		}
