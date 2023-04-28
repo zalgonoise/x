37b4236db279bbc463b69905d874afdd8046ab2b
zalgonoise <mariozalgo@gmail.com>
2023-04-28 20:11:43 +0200

    audio/osc/square: updated fullCycle call; added freq drift detection to the buffer

--- audio/osc/square.go
+++ audio/osc/square.go
@@ -4,32 +4,38 @@ package osc
 // and sample rate `sampleRate`, into the buffer of type T `buffer`
 func Square[T BitDepths](buffer []T, freq, depth, sampleRate float64) {
 	var (
-		halfPeriod = int(sampleRate) / (2 * int(freq))
-		sampleInt  = T(2<<int16(depth-2)) - 1
-		cycle      = fullCycle(sampleRate, freq)
+		halfPeriod, mul = fullCycle(sampleRate, 2*freq)
+		gap             = halfPeriod * mul
+		sampleInt       = T(2<<int16(depth-2)) - 1
 	)
 
 	if len(buffer) > halfPeriod {
 		var wave = make([]T, halfPeriod)
-		square(wave, halfPeriod, cycle, sampleInt)
-		for i := 0; i < len(buffer); i += len(wave) {
+		square(wave, halfPeriod, gap, sampleInt)
+
+		for i, j := 0, 0; i < len(buffer); i, j = i+len(wave), j+1 {
 			copy(buffer[i:], wave)
+
+			next := i + len(wave)
+			if j+1%mul == 0 && next < len(buffer) {
+				buffer[next+1] = buffer[next]
+				i++
+			}
 		}
 		return
 	}
 
-	square(buffer, halfPeriod, cycle, sampleInt)
+	square(buffer, halfPeriod, gap, sampleInt)
 }
 
-func square[T BitDepths](buffer []T, halfPeriod, cycle int, sampleInt T) {
+func square[T BitDepths](buffer []T, halfPeriod, gap int, sampleInt T) {
 	quarterPeriod := halfPeriod / 2
 
 	for i := 0; i < len(buffer); i++ {
-		if i+1%cycle == 0 {
+		if i+1%gap == 0 {
 			buffer[i] = buffer[i-1]
-			continue
+			i++
 		}
-
 		if i%halfPeriod < quarterPeriod {
 			buffer[i] = sampleInt
 			continue
