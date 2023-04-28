3f39bc9c2c45a824ce5a95bae5af5640d76a0997
zalgonoise <mariozalgo@gmail.com>
2023-04-28 20:11:58 +0200

    audio/osc/triangle: updated fullCycle call; added freq drift detection to the buffer

--- audio/osc/triangle.go
+++ audio/osc/triangle.go
@@ -4,25 +4,33 @@ package osc
 // and sample rate `sampleRate`, into the buffer of type T `buffer`
 func Triangle[T BitDepths](buffer []T, freq, depth, sampleRate float64) {
 	var (
-		halfPeriod   = int(sampleRate / freq)
-		increment    = 4.0 / float64(halfPeriod)
-		sampleInt  T = -(1 << int(depth-1))
-		cycle        = fullCycle(sampleRate, freq)
+		halfPeriod, mul   = fullCycle(sampleRate, freq)
+		gap               = halfPeriod * mul
+		increment         = 4.0 / float64(halfPeriod)
+		sampleInt       T = -(1 << int(depth-1))
 	)
 
 	if len(buffer) > halfPeriod {
 		var wave = make([]T, halfPeriod)
-		triangle(wave, halfPeriod, cycle, sampleInt, increment, depth)
-		for i := 0; i < len(buffer); i += len(wave) {
+		triangle(wave, halfPeriod, gap, sampleInt, increment, depth)
+
+		for i, j := 0, 0; i < len(buffer); i, j = i+len(wave), j+1 {
 			copy(buffer[i:], wave)
+
+			next := i + len(wave)
+			if j+1%mul == 0 && next < len(buffer) {
+				buffer[next+1] = buffer[next]
+				i++
+			}
 		}
+
 		return
 	}
 
-	triangle(buffer, halfPeriod, cycle, sampleInt, increment, depth)
+	triangle(buffer, halfPeriod, gap, sampleInt, increment, depth)
 }
 
-func triangle[T BitDepths](buffer []T, halfPeriod, cycle int, sampleInt T, increment, depth float64) {
+func triangle[T BitDepths](buffer []T, halfPeriod, gap int, sampleInt T, increment, depth float64) {
 	var (
 		swap          bool
 		stepValue     = T(increment * float64(int(2)<<int(depth-2)-1))
@@ -30,7 +38,7 @@ func triangle[T BitDepths](buffer []T, halfPeriod, cycle int, sampleInt T, incre
 	)
 
 	for i := 0; i < len(buffer); i++ {
-		if i+1%cycle == 0 {
+		if i+1%gap == 0 {
 			buffer[i] = sampleInt
 			continue
 		}
