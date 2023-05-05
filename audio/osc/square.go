649efb69a2c6c6208e82b543688f9e2c54d544c7
zalgonoise <zalgo.noise@gmail.com>
2023-05-05 19:57:54 +0200

    audio/wav/osc/square: refactored with 1Hz-mapping approach; updated signature

--- audio/osc/square.go
+++ audio/osc/square.go
@@ -2,45 +2,27 @@ package osc
 
 // Square is an oscillator that writes a square wave of frequency `freq`, bit depth `depth`,
 // and sample rate `sampleRate`, into the buffer of type T `buffer`
-func Square[T BitDepths](buffer []T, freq, depth, sampleRate float64) {
-	var (
-		halfPeriod, mul = fullCycle(sampleRate, freq)
-		gap             = halfPeriod * mul
-		sampleInt       = T(2<<int16(depth-2)) - 1
-	)
+func Square[T BitDepths](buffer []T, freq, depth, sampleRate int) {
+	var wave = buildFrom1Hz(len(buffer), sampleRate, freq, square1Hz)
 
-	if len(buffer) > halfPeriod {
-		var wave = make([]T, halfPeriod)
-		square(wave, halfPeriod, gap, sampleInt)
-
-		for i, j := 0, 0; i < len(buffer); i, j = i+len(wave), j+1 {
-			copy(buffer[i:], wave)
-
-			next := i + len(wave)
-			if j+1%mul == 0 && next < len(buffer) {
-				buffer[next+1] = buffer[next]
-				i++
-			}
-		}
-		return
+	for i := range buffer {
+		buffer[i] = T(wave[i] * float64(int(2)<<(depth-2)-1))
 	}
-
-	square(buffer, halfPeriod, gap, sampleInt)
 }
 
-func square[T BitDepths](buffer []T, halfPeriod, gap int, sampleInt T) {
-	quarterPeriod := halfPeriod / 2
+func square1Hz(sampleRate int) []float64 {
+	var (
+		buffer    = make([]float64, sampleRate)
+		halfCycle = sampleRate / 2
+	)
 
-	for i := 0; i < len(buffer); i++ {
-		if i+1%gap == 0 {
-			buffer[i] = buffer[i-1]
-			i++
-		}
-		if i%halfPeriod < quarterPeriod {
-			buffer[i] = sampleInt
-			continue
-		}
-
-		buffer[i] = -sampleInt
+	for i := 0; i < halfCycle; i++ {
+		buffer[i] = 1.0
 	}
+
+	for i := halfCycle; i < sampleRate; i++ {
+		buffer[i] = -1.0
+	}
+
+	return buffer
 }
