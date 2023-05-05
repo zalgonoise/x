b54a6c4ab519f7666005470a9af8626570319cb5
zalgonoise <zalgo.noise@gmail.com>
2023-05-05 19:57:20 +0200

    audio/wav/osc/sine: slight refactor; updated signature

--- audio/osc/sine.go
+++ audio/osc/sine.go
@@ -4,19 +4,22 @@ import "github.com/zalgonoise/x/audio/trig"
 
 // Sine is an oscillator that writes a sine wave of frequency `freq`, bit depth `depth`,
 // and sample rate `sampleRate`, into the buffer of type T `buffer`
-func Sine[T BitDepths](buffer []T, freq, depth, sampleRate float64) {
-	wave := sine(len(buffer), int(sampleRate), freq)
+func Sine[T BitDepths](buffer []T, freq, depth, sampleRate int) {
+	var wave = sine(len(buffer), sampleRate, freq)
 
 	for i := range buffer {
-		buffer[i] = T(wave[i] * float64(int(2)<<int(depth-1)/2-1))
+		buffer[i] = T(wave[i] * float64(int(2)<<(depth-1)/2-1))
 	}
 }
 
-func sine(size, sampleRate int, freq float64) []float64 {
-	var buffer = make([]float64, size)
+func sine(size, sampleRate, freq int) []float64 {
+	var (
+		buffer      = make([]float64, size)
+		tauOverFreq = tau * float64(freq)
+	)
 
 	for i := 0; i < size; i++ {
-		buffer[i] = trig.Sin(tau * freq * (float64(i) / float64(sampleRate)))
+		buffer[i] = trig.Sin(tauOverFreq * (float64(i) / float64(sampleRate)))
 	}
 
 	return buffer
