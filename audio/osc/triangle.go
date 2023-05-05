7223c23df4647958103c31495d0f7aa52b482c38
zalgonoise <zalgo.noise@gmail.com>
2023-05-05 19:58:01 +0200

    audio/wav/osc/triangle: refactored with 1Hz-mapping approach; updated signature

--- audio/osc/triangle.go
+++ audio/osc/triangle.go
@@ -2,32 +2,30 @@ package osc
 
 // Triangle is an oscillator that writes a triangle wave of frequency `freq`, bit depth `depth`,
 // and sample rate `sampleRate`, into the buffer of type T `buffer`
-func Triangle[T BitDepths](buffer []T, freq, depth, sampleRate float64) {
-	var wave = triangleWave(len(buffer), int(sampleRate), freq)
+func Triangle[T BitDepths](buffer []T, freq, depth, sampleRate int) {
+	var wave = buildFrom1Hz(len(buffer), sampleRate, freq, triangle1Hz)
 
 	for i := range buffer {
-		buffer[i] = T(wave[i] * float64(int(2)<<int(depth-2)-1))
+		buffer[i] = T(wave[i] * float64(int(2)<<(depth-2)-1))
 	}
 }
 
-func triangleWave(size, sampleRate int, freq float64) []float64 {
+func triangle1Hz(sampleRate int) []float64 {
 	var (
+		buffer    = make([]float64, sampleRate)
+		halfCycle = sampleRate / 2
+		stepValue = 2.0 / float64(sampleRate)
 		sample    = -1.0
-		buffer    = make([]float64, size)
-		cycle     = float64(sampleRate) / freq
-		halfCycle = cycle * 0.5
-		step      = 1 / halfCycle
 	)
 
-	for i := 0; i < size; i++ {
-		if i+1%int(halfCycle) == 0 {
-			step = -step
-		}
-
-		sample += step
+	for i := 0; i < halfCycle; i++ {
 		buffer[i] = sample
+		sample += stepValue
+	}
+
+	for i, j := halfCycle, halfCycle-1; i < sampleRate; i, j = i+1, j-1 {
+		buffer[i] = -buffer[j]
 	}
 
 	return buffer
-
 }
