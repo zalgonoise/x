d164d7bd748db48db6d0f0551f2da3dd551a8b5c
zalgonoise <zalgo.noise@gmail.com>
2023-05-06 19:09:08 +0200

    audio/osc/sine: added 1Hz func for sine wave, for consistency

--- audio/osc/sine.go
+++ audio/osc/sine.go
@@ -5,21 +5,35 @@ import "github.com/zalgonoise/x/audio/trig"
 // Sine is an oscillator that writes a sine wave of frequency `freq`, bit depth `depth`,
 // and sample rate `sampleRate`, into the buffer of type T `buffer`
 func Sine[T BitDepths](buffer []T, freq, depth, sampleRate int) {
-	var wave = sine(len(buffer), sampleRate, freq)
+	var wave = buildFrom1Hz(len(buffer), sampleRate, freq, sine1Hz)
 
 	for i := range buffer {
 		buffer[i] = T(wave[i] * float64(int(2)<<(depth-1)/2-1))
 	}
 }
 
-func sine(size, sampleRate, freq int) []float64 {
+func sine1Hz(sampleRate int) []float64 {
 	var (
-		buffer      = make([]float64, size)
-		tauOverFreq = tau * float64(freq)
+		buffer       = make([]float64, sampleRate)
+		halfCycle    = sampleRate / 2
+		quarterCycle = halfCycle / 2
 	)
 
-	for i := 0; i < size; i++ {
-		buffer[i] = trig.Sin(tauOverFreq * (float64(i) / float64(sampleRate)))
+	// gen first quadrant (Q1)
+	for i := 0; i < quarterCycle; i++ {
+		buffer[i] = trig.Sin(tau * float64(i) / float64(sampleRate))
+	}
+	buffer[quarterCycle] = 1.0
+
+	// Q2 is reverse of Q2
+	for i, j := quarterCycle+1, quarterCycle-1; i < halfCycle; i, j = i+1, j-1 {
+		buffer[i] = buffer[j]
+	}
+	buffer[halfCycle] = 0.0
+
+	// Q3+Q4 are negative reverse of Q2+Q1
+	for i, j := halfCycle+1, halfCycle-1; i < sampleRate; i, j = i+1, j-1 {
+		buffer[i] = -buffer[j]
 	}
 
 	return buffer
