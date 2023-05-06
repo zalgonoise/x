f7e4bbd0cb0ec0ce23c3d96126b2d339e53e0e7d
zalgonoise <zalgo.noise@gmail.com>
2023-05-06 18:53:28 +0200

    audio/osc/triangle: corrected and improved implementation

--- audio/osc/triangle.go
+++ audio/osc/triangle.go
@@ -12,19 +12,27 @@ func Triangle[T BitDepths](buffer []T, freq, depth, sampleRate int) {
 
 func triangle1Hz(sampleRate int) []float64 {
 	var (
-		buffer    = make([]float64, sampleRate)
-		halfCycle = sampleRate / 2
-		stepValue = 2.0 / float64(sampleRate)
-		sample    = -1.0
+		buffer       = make([]float64, sampleRate)
+		halfCycle    = sampleRate / 2
+		quarterCycle = halfCycle / 2
+		stepValue    = 1.0 / float64(quarterCycle) // from -1.0 to 0.0
 	)
 
-	for i := 0; i < halfCycle; i++ {
-		buffer[i] = sample
-		sample += stepValue
+	// gen first quadrant (Q1)
+	for i, j := 0, -1.0; i < quarterCycle; i, j = i+1, j+stepValue {
+		buffer[i] = j
 	}
+	buffer[quarterCycle] = 0
 
-	for i, j := halfCycle, halfCycle-1; i < sampleRate; i, j = i+1, j-1 {
+	// Q2 is negative reverse of Q2
+	for i, j := quarterCycle+1, quarterCycle-1; i < halfCycle; i, j = i+1, j-1 {
 		buffer[i] = -buffer[j]
+	}
+	buffer[halfCycle] = 1.0
+
+	// Q3+Q4 are reverse of Q2+Q1
+	for i, j := halfCycle+1, halfCycle-1; i < sampleRate; i, j = i+1, j-1 {
+		buffer[i] = buffer[j]
 	}
 
 	return buffer
