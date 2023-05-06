052193a2ce637c6d08afb7bc999c2199a1421c57
zalgonoise <zalgo.noise@gmail.com>
2023-05-06 18:54:00 +0200

    audio/osc/sawtooth: improved implementation; added up / downward sawtooth

--- audio/osc/sawtooth.go
+++ audio/osc/sawtooth.go
@@ -1,25 +1,61 @@
 package osc
 
-// Sawtooth is an oscillator that writes a sawtooth wave of frequency `freq`, bit depth `depth`,
+// SawtoothUp is an oscillator that writes a rising sawtooth wave of frequency `freq`, bit depth `depth`,
 // and sample rate `sampleRate`, into the buffer of type T `buffer`
-func Sawtooth[T BitDepths](buffer []T, freq, depth, sampleRate int) {
-	var wave = buildFrom1Hz(len(buffer), sampleRate, freq, sawtooth1Hz)
+func SawtoothUp[T BitDepths](buffer []T, freq, depth, sampleRate int) {
+	var wave = buildFrom1Hz(len(buffer), sampleRate, freq, sawtoothUp1Hz)
 
 	for i := range buffer {
 		buffer[i] = T(wave[i] * float64(int(2)<<(depth-2)-1))
 	}
 }
 
-func sawtooth1Hz(sampleRate int) []float64 {
+// SawtoothDown is an oscillator that writes a falling sawtooth wave of frequency `freq`, bit depth `depth`,
+// and sample rate `sampleRate`, into the buffer of type T `buffer`
+func SawtoothDown[T BitDepths](buffer []T, freq, depth, sampleRate int) {
+	var wave = buildFrom1Hz(len(buffer), sampleRate, freq, sawtoothDown1Hz)
+
+	for i := range buffer {
+		buffer[i] = T(wave[i] * float64(int(2)<<(depth-2)-1))
+	}
+}
+
+func sawtoothUp1Hz(sampleRate int) []float64 {
 	var (
 		buffer    = make([]float64, sampleRate)
-		stepValue = 2.0 / float64(sampleRate) // from -1.0 to +1.0
-		sample    = -1.0
+		halfCycle = sampleRate / 2
+		stepValue = 1.0 / float64(halfCycle) // from -1.0 to 0.0
 	)
 
-	for i := 0; i < sampleRate; i++ {
-		buffer[i] = sample
-		sample += stepValue
+	for i, j := 0, -1.0; i < halfCycle; i, j = i+1, j+stepValue {
+		buffer[i] = j
+	}
+	buffer[halfCycle] = 0
+
+	// Q3+Q4 are negative reverse of Q2+Q1
+	for i, j := halfCycle+1, halfCycle-1; i < sampleRate; i, j = i+1, j-1 {
+		buffer[i] = -buffer[j]
+	}
+
+	return buffer
+}
+
+func sawtoothDown1Hz(sampleRate int) []float64 {
+	var (
+		buffer    = make([]float64, sampleRate)
+		halfCycle = sampleRate / 2
+		stepValue = 1.0 / float64(halfCycle) // from -1.0 to 0.0
+	)
+
+	// gen first two quadrants (Q1+Q2)
+	for i, j := 0, 1.0; i < halfCycle; i, j = i+1, j-stepValue {
+		buffer[i] = j
+	}
+	buffer[halfCycle] = 0
+
+	// Q3+Q4 are negative reverse of Q2+Q1
+	for i, j := halfCycle+1, halfCycle-1; i < sampleRate; i, j = i+1, j-1 {
+		buffer[i] = -buffer[j]
 	}
 
 	return buffer
