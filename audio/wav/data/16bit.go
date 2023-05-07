2d769127822e1a3b383c110585394fc1c2ac9d58
zalgonoise <zalgo.noise@gmail.com>
2023-05-07 19:32:57 +0200

    audio/wav/data/16bit: replace implementation with 16bit as a converter

--- audio/wav/data/16bit.go
+++ audio/wav/data/16bit.go
@@ -1,10 +1,7 @@
 package data
 
 import (
-	"time"
 	"unsafe"
-
-	"github.com/zalgonoise/x/audio/osc"
 )
 
 const (
@@ -12,100 +9,40 @@ const (
 	// minInt16 float64 = ^1<<14 + 1
 )
 
-// Chunk16bit is a Chunk used for 16 bit-depth PCM buffers
-type Chunk16bit struct {
-	*ChunkHeader
-	Data  []int16
-	Depth uint16
-}
+var _ = Converter(Conv16Bit{})
 
-// Parse will consume the input byte slice `buf`, to extract the PCM audio buffer
-// from raw bytes
-func (d *Chunk16bit) Parse(buf []byte) {
-	if d.Data == nil {
-		d.Data = *(*[]int16)(unsafe.Pointer(&buf))
-		d.Data = d.Data[:len(buf)/2]
-		if d.Subchunk2Size == 0 {
-			d.Subchunk2Size = uint32(len(buf))
-		}
-		return
-	}
-	newData := *(*[]int16)(unsafe.Pointer(&buf))
-	d.Data = append(d.Data, newData[:len(buf)/2]...)
-}
+type Conv16Bit struct{}
 
-// ParseFloat will consume the input float64 slice `buf`, to extract the PCM audio buffer
-// from floating-point audio data
-func (d *Chunk16bit) ParseFloat(buf []float64) {
-	d.Data = conv(
-		buf, func(f float64) int16 {
-			return int16(f * maxInt16)
-		},
-	)
-}
+func (c Conv16Bit) Parse(buf []byte) []float64 {
+	data := *(*[]int16)(unsafe.Pointer(&buf))
 
-// Bytes will return a slice of bytes with the encoded PCM buffer
-func (d *Chunk16bit) Bytes() []byte {
-	data := make([]byte, len(d.Data)*2)
-	for i := range d.Data {
-		append2Bytes(i, data, *(*[2]byte)(unsafe.Pointer(&d.Data[i])))
-	}
-
-	return data
-}
-
-// Header returns the ChunkHeader of the Chunk
-func (d *Chunk16bit) Header() *ChunkHeader { return d.ChunkHeader }
-
-// BitDepth returns the bit depth of the Chunk
-func (d *Chunk16bit) BitDepth() uint16 { return d.Depth }
-
-// Reset clears the data stored in the Chunk
-func (d *Chunk16bit) Reset() {
-	d.Data = make([]int16, 0, dataChunkBaseLen)
-}
-
-// Value returns the PCM audio buffer from the Chunk, as a slice of int
-func (d *Chunk16bit) Value() []int { return to[int16, int](d.Data) }
-
-// Float returns the PCM audio buffer from the Chunk, as a slice of float64
-func (d *Chunk16bit) Float() []float64 {
 	return conv(
-		d.Data, func(v int16) float64 {
+		data[:len(buf)/2],
+		func(v int16) float64 {
 			return float64(v) / maxInt16
 		},
 	)
 }
 
-// Generate creates a wave of the given form, frequency and duration within this Chunk
-func (d *Chunk16bit) Generate(waveType osc.Type, freq, sampleRate int, dur time.Duration) {
-	buffer := make([]int16, int(float64(sampleRate)*float64(dur)/float64(time.Second)))
-	fn := formFunc16bit(waveType)
-	if fn == nil {
-		return
-	}
-	fn(buffer, freq, int(d.Depth), sampleRate)
+func (c Conv16Bit) Bytes(buf []float64) []byte {
+	value := conv(
+		buf, func(f float64) int16 {
+			return int16(f * maxInt16)
+		},
+	)
 
-	if d.Data == nil {
-		d.Data = buffer
-		return
+	data := make([]byte, len(value)*2)
+	for i := range value {
+		append2Bytes(i, data, *(*[2]byte)(unsafe.Pointer(&value[i])))
 	}
-	d.Data = append(d.Data, buffer...)
+
+	return data
 }
 
-func formFunc16bit(typ osc.Type) osc.Oscillator[int16] {
-	switch typ {
-	case osc.SineWave:
-		return osc.Sine[int16]
-	case osc.SquareWave:
-		return osc.Square[int16]
-	case osc.TriangleWave:
-		return osc.Triangle[int16]
-	case osc.SawtoothUpWave:
-		return osc.SawtoothUp[int16]
-	case osc.SawtoothDownWave:
-		return osc.SawtoothDown[int16]
-	default:
-		return nil
-	}
+func (c Conv16Bit) Value(buf []float64) []int {
+	return conv(
+		buf, func(f float64) int {
+			return int(f * maxInt16)
+		},
+	)
 }
