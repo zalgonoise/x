499ffe053e692e3f11c22a207a451b816193c5c2
zalgonoise <zalgo.noise@gmail.com>
2023-05-07 19:35:56 +0200

    audio/wav: data chunks are uniform, using the new constructor

--- audio/wav/data.go
+++ audio/wav/data.go
@@ -59,38 +59,11 @@ func NewChunk(bitDepth uint16, subchunk *data.ChunkHeader) Chunk {
 			ChunkHeader: subchunk,
 			Depth:       0,
 		}
-	case bitDepth8:
+	case bitDepth8, bitDepth16, bitDepth24, bitDepth32:
 		if subchunk == nil {
 			subchunk = data.NewDataHeader()
 		}
-		return &data.Chunk8bit{
-			ChunkHeader: subchunk,
-			Depth:       bitDepth8,
-		}
-	case bitDepth16:
-		if subchunk == nil {
-			subchunk = data.NewDataHeader()
-		}
-		return &data.Chunk16bit{
-			ChunkHeader: subchunk,
-			Depth:       bitDepth16,
-		}
-	case bitDepth24:
-		if subchunk == nil {
-			subchunk = data.NewDataHeader()
-		}
-		return &data.Chunk24bit{
-			ChunkHeader: subchunk,
-			Depth:       bitDepth24,
-		}
-	case bitDepth32:
-		if subchunk == nil {
-			subchunk = data.NewDataHeader()
-		}
-		return &data.Chunk32bit{
-			ChunkHeader: subchunk,
-			Depth:       bitDepth32,
-		}
+		return data.NewDataChunk(bitDepth, subchunk)
 	default:
 		return nil
 	}
