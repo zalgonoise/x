3cdf38c68b9657e2f37048d66874ef9b295da777
zalgonoise <mariozalgo@gmail.com>
2023-05-09 19:32:49 +0200

    audio/wav/data/8bit: documented code

--- audio/wav/data/8bit.go
+++ audio/wav/data/8bit.go
@@ -11,8 +11,10 @@ const (
 
 var _ = Converter(Conv8Bit{})
 
+// Conv8Bit is a 8bit audio Converter
 type Conv8Bit struct{}
 
+// Parse consumes the input audio buffer, returning its floating point audio representation
 func (c Conv8Bit) Parse(buf []byte) []float64 {
 	return conv(
 		*(*[]int8)(unsafe.Pointer(&buf)),
@@ -22,6 +24,7 @@ func (c Conv8Bit) Parse(buf []byte) []float64 {
 	)
 }
 
+// Bytes consumes the input floating point audio buffer, returning its byte representation
 func (c Conv8Bit) Bytes(buf []float64) []byte {
 	value := conv(
 		buf, func(f float64) int8 {
@@ -32,6 +35,7 @@ func (c Conv8Bit) Bytes(buf []float64) []byte {
 	return *(*[]byte)(unsafe.Pointer(&value))
 }
 
+// Value consumes the input floating point audio buffer, returning its PCM audio values as a slice of int
 func (c Conv8Bit) Value(buf []float64) []int {
 	return conv(
 		buf, func(f float64) int {
