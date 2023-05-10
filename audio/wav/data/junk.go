5ec9b77bbf9038023b66e72d51597e5d16646c3e
zalgonoise <mariozalgo@gmail.com>
2023-05-10 18:12:13 +0200

    audio/wav/data/junk: rename type to JunkChunk. Add NewJunkChunk func

--- audio/wav/data/junk.go
+++ audio/wav/data/junk.go
@@ -6,8 +6,8 @@ import (
 	"github.com/zalgonoise/x/audio/osc"
 )
 
-// ChunkJunk is a DataChunk used for storing "junk"-ID subchunk data
-type ChunkJunk struct {
+// JunkChunk is a DataChunk used for storing "junk"-ID subchunk data
+type JunkChunk struct {
 	*ChunkHeader
 	Data  []byte
 	Depth uint16
@@ -15,7 +15,7 @@ type ChunkJunk struct {
 
 // Parse will consume the input byte slice `buf`, to extract the PCM audio buffer
 // from raw bytes
-func (d *ChunkJunk) Parse(buf []byte) {
+func (d *JunkChunk) Parse(buf []byte) {
 	if d.Data == nil {
 		d.Data = buf
 		if d.Subchunk2Size == 0 {
@@ -28,38 +28,38 @@ func (d *ChunkJunk) Parse(buf []byte) {
 
 // ParseFloat will consume the input float64 slice `buf`, to extract the PCM audio buffer
 // from floating-point audio data
-func (d *ChunkJunk) ParseFloat(_ []float64) {}
+func (d *JunkChunk) ParseFloat(_ []float64) {}
 
 // Bytes will return a slice of bytes with the encoded PCM buffer
-func (d *ChunkJunk) Bytes() []byte {
+func (d *JunkChunk) Bytes() []byte {
 	return d.Data
 }
 
 // Header returns the ChunkHeader of the DataChunk
-func (d *ChunkJunk) Header() *ChunkHeader { return d.ChunkHeader }
+func (d *JunkChunk) Header() *ChunkHeader { return d.ChunkHeader }
 
 // BitDepth returns the bit depth of the DataChunk
-func (d *ChunkJunk) BitDepth() uint16 { return d.Depth }
+func (d *JunkChunk) BitDepth() uint16 { return d.Depth }
 
 // Reset clears the data stored in the DataChunk
-func (d *ChunkJunk) Reset() {
+func (d *JunkChunk) Reset() {
 	d.Data = make([]byte, 0, dataChunkBaseLen)
 }
 
 // Value returns the PCM audio buffer from the DataChunk, as a slice of int
-func (d *ChunkJunk) Value() []int { return to[byte, int](d.Data) }
+func (d *JunkChunk) Value() []int { return to[byte, int](d.Data) }
 
 // Float returns the PCM audio buffer from the DataChunk, as a slice of float64
-func (d *ChunkJunk) Float() []float64 {
+func (d *JunkChunk) Float() []float64 {
 	return nil
 }
 
 // Generate creates a wave of the given form, frequency and duration within this DataChunk
-func (d *ChunkJunk) Generate(_ osc.Type, _, _ int, _ time.Duration) {}
+func (d *JunkChunk) Generate(_ osc.Type, _, _ int, _ time.Duration) {}
 
 // SetBitDepth returns a new DataChunk with the input `bitDepth`'s converter, or
 // an error if invalid. The new DataChunk retains any PCM data it contains, as a copy.
-func (d *ChunkJunk) SetBitDepth(bitDepth uint16) (*DataChunk, error) {
+func (d *JunkChunk) SetBitDepth(bitDepth uint16) (*DataChunk, error) {
 	header := NewDataHeader()
 	header.Subchunk2Size = d.Subchunk2Size
 
@@ -72,4 +72,16 @@ func (d *ChunkJunk) SetBitDepth(bitDepth uint16) (*DataChunk, error) {
 	newChunk.Parse(d.Data)
 
 	return newChunk, nil
+}
+
+// NewJunkChunk creates a JunkChunk with the input `subchunk` ChunkHeader, or with a default one if nil
+func NewJunkChunk(subchunk *ChunkHeader) *JunkChunk {
+	if subchunk == nil {
+		subchunk = NewJunkHeader()
+	}
+
+	return &JunkChunk{
+		ChunkHeader: subchunk,
+		Depth:       0,
+	}
 }
