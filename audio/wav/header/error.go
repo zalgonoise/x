ac442edb1f1a216fb7f96c4ca3eb559b9e1aa488
zalgonoise <zalgo.noise@gmail.com>
2023-05-18 19:55:00 +0200

    audio/wav/header: removed unused errors; added ErrEmptyHeader

--- audio/wav/header/error.go
+++ audio/wav/header/error.go
@@ -9,23 +9,19 @@ const (
 
 	ErrEmpty   = errs.Kind("missing")
 	ErrInvalid = errs.Kind("invalid")
-	ErrShort   = errs.Kind("short")
 
 	ErrNumChannels = errs.Entity("number of channels")
 	ErrSampleRate  = errs.Entity("sample rate")
 	ErrBitDepth    = errs.Entity("bit depth")
 	ErrHeader      = errs.Entity("WAV header")
-	ErrDataBuffer  = errs.Entity("data buffer")
 	ErrAudioFormat = errs.Entity("audio format")
 )
 
 var (
+	ErrEmptyHeader        = errs.New(ErrDomain, ErrEmpty, ErrHeader)
 	ErrInvalidNumChannels = errs.New(ErrDomain, ErrInvalid, ErrNumChannels)
 	ErrInvalidSampleRate  = errs.New(ErrDomain, ErrInvalid, ErrSampleRate)
 	ErrInvalidBitDepth    = errs.New(ErrDomain, ErrInvalid, ErrBitDepth)
 	ErrInvalidHeader      = errs.New(ErrDomain, ErrInvalid, ErrHeader)
-	ErrShortDataBuffer    = errs.New(ErrDomain, ErrShort, ErrDataBuffer)
-	ErrMissingHeader      = errs.New(ErrDomain, ErrEmpty, ErrHeader)
-	ErrMissingDataBuffer  = errs.New(ErrDomain, ErrInvalid, ErrDataBuffer)
 	ErrInvalidAudioFormat = errs.New(ErrDomain, ErrInvalid, ErrAudioFormat)
 )
