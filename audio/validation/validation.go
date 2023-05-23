3694002df0d344befce8aa35dc22551aabdaf0aa
zalgonoise <zalgo.noise@gmail.com>
2023-05-23 20:16:19 +0200

    audio/validation: refactored validation implementation

--- audio/validation/validation.go
+++ audio/validation/validation.go
@@ -1,79 +1,72 @@
 package validation
 
-// Validator is a generic type that matches an input item of comparable type T with a preselected list of
-// T values that are considered valid.
+import "errors"
+
+// ProcessorFunc is a generic function type implemented by the caller of New or FailFast, which analyzes an item of
+// any type to validate it on some level; returning an error if invalid
+type ProcessorFunc[T any] func(item T) error
+
+// Validator is a generic data structure that handles multi-pass / multi-element validation of any given type, as
+// implemented by the caller.
 //
-// If there are zero matches, then its Validate function returns a (configurable) error. It returns nil on the
-// first match encountered
-type Validator[T comparable] struct {
-	Values []T
-	err    error
+// The Validator iterates through its configured ProcessorFunc over a given item, reporting back to the caller on any
+// validity errors raised
+type Validator[T any] struct {
+	failFast bool
+	fn       []ProcessorFunc[T]
 }
 
-// Validate matches the input item against the slice of T configured in this Validator, returning nil on the first
-// match, or the configured error if there are no matches
+// Validate iterates through the configured ProcessorFunc to validate the input `item` of type T.
+//
+// This execution exits on the first error encountered if the Validator was created using FailFast; otherwise it
+// will collect a list of errors found, joining them (with errors.Join) if more than one error is found.
 func (v *Validator[T]) Validate(item T) error {
-	for i := range v.Values {
-		if v.Values[i] == item {
-			return nil
+	errs := make([]error, 0, len(v.fn))
+
+	for i := range v.fn {
+		if err := v.fn[i](item); err != nil {
+			if v.failFast {
+				return err
+			}
+
+			errs = append(errs, err)
 		}
 	}
 
-	return v.err
+	switch len(errs) {
+	case 0:
+		return nil
+	case 1:
+		return errs[0]
+	default:
+		return errors.Join(errs...)
+	}
 }
 
-// New creates a Validator of type T, based on the given error and slice of values.
+// New creates a plain Validator for type T, configured with the input ProcessorFunc.
 //
-// While the function signature is variadic, it is mandatory that the slice of T `values` is not empty; as well as the
-// provided error which should not be nil
-func New[T comparable](err error, values ...T) *Validator[T] {
-	if err == nil || len(values) == 0 {
+// This call returns nil if no ProcessorFunc is provided.
+func New[T any](fn ...ProcessorFunc[T]) *Validator[T] {
+	if len(fn) == 0 {
 		return nil
 	}
 
 	return &Validator[T]{
-		Values: values,
-		err:    err,
+		fn: fn,
 	}
 }
 
-// DynamicValidator is a generic type that matches an input item of any type T with a preselected list of
-// T values that are considered valid. The comparison is done by a function that compares two T, returning a boolean
-// if they are a match.
+// FailFast creates a Validator for type T, configured with the input ProcessorFunc,
+// that exits on the first error raised instead of iterating through the entire set of ProcessorFunc.
 //
-// If there are zero matches, then its Validate function returns a (configurable) error. It returns nil on the
-// first match encountered
-type DynamicValidator[T any] struct {
-	Values []T
-	fn     func(T, T) bool
-	err    error
-}
-
-// Validate matches the input item against the slice of T configured in this DynamicValidator,
-// using the configured function `func(T, T) bool` to compare each configured value to the input,
-// returning nil on the first match, or the configured error if there are no matches
-func (v *DynamicValidator[T]) Validate(item T) error {
-	for i := range v.Values {
-		if v.fn(item, v.Values[i]) {
-			return nil
-		}
-	}
-
-	return v.err
-}
-
-// With creates a DynamicValidator of type T, based on the given error, comparison function and slice of values.
-//
-// While the function signature is variadic, it is mandatory that the slice of T `values` is not empty; as well as the
-// provided function and error which should not be nil
-func With[T any](err error, fn func(input, target T) bool, values ...T) *DynamicValidator[T] {
-	if err == nil || fn == nil || len(values) == 0 {
+// This call returns nil if no ProcessorFunc is provided.
+func FailFast[T any](fn ...ProcessorFunc[T]) *Validator[T] {
+	if len(fn) == 0 {
 		return nil
 	}
 
-	return &DynamicValidator[T]{
-		Values: values,
-		fn:     fn,
-		err:    err,
+	return &Validator[T]{
+		failFast: true,
+		fn:       fn,
 	}
 }
