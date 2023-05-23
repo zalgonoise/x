d3453033923cac3989dbc6891dcd250307ada16c
zalgonoise <zalgo.noise@gmail.com>
2023-05-23 20:16:30 +0200

    audio/wav/header: refactored validators

--- audio/wav/header/validate.go
+++ audio/wav/header/validate.go
@@ -6,102 +6,100 @@ import (
 	"github.com/zalgonoise/x/audio/validation"
 )
 
-type SampleRate uint32
-
 const (
-	SampleRate44100  SampleRate = 44100
-	SampleRate48000  SampleRate = 48000
-	SampleRate88200  SampleRate = 88200
-	SampleRate96000  SampleRate = 96000
-	SampleRate176400 SampleRate = 176400
-	SampleRate192000 SampleRate = 192000
+	SampleRate44100  uint32 = 44100
+	SampleRate48000  uint32 = 48000
+	SampleRate88200  uint32 = 88200
+	SampleRate96000  uint32 = 96000
+	SampleRate176400 uint32 = 176400
+	SampleRate192000 uint32 = 192000
 )
 
-type Channels uint16
-
 const (
-	ChannelsMono Channels = iota + 1
+	ChannelsMono uint16 = iota + 1
 	ChannelsStereo
 )
 
-type BitDepth uint16
-
 const (
-	BitDepth8 BitDepth = 8 * (iota + 1)
+	BitDepth8 uint16 = 8 * (iota + 1)
 	BitDepth16
 	BitDepth24
 	BitDepth32
 )
 
-type AudioFormat uint16
-
 const (
-	UnsetFormat AudioFormat = iota
+	UnsetFormat uint16 = iota
 	PCMFormat
 	_
 	FloatFormat
 )
 
-var (
-	sampleRateValidator = validation.New[SampleRate](
-		ErrInvalidSampleRate,
-		SampleRate44100,
-		SampleRate48000,
-		SampleRate88200,
-		SampleRate96000,
-		SampleRate176400,
-		SampleRate192000,
-	)
-
-	bitDepthValidator = validation.New[BitDepth](
-		ErrInvalidBitDepth,
-		BitDepth8,
-		BitDepth16,
-		BitDepth24,
-		BitDepth32,
-	)
-
-	channelsValidator = validation.New[Channels](
-		ErrInvalidNumChannels,
-		ChannelsMono,
-		ChannelsStereo,
-	)
-
-	audioFormatValidator = validation.New[AudioFormat](
-		ErrInvalidAudioFormat,
-		PCMFormat,   // PCM audio
-		FloatFormat, // IEEE floating-point 32-bit audio
-	)
+var headerValidator = validation.New[*Header](
+	validateChunkID,
+	validateFormat,
+	validateSampleRate,
+	validateBitDepth,
+	validateNumChannels,
+	validateAudioFormat,
 )
 
-func Validate(header *Header) error {
-	if header == nil {
-		return ErrEmptyHeader
-	}
-
-	if string(header.ChunkID[:]) != string(defaultChunkID[:]) {
-		return fmt.Errorf("%w: ChunkID %s", ErrInvalidHeader, string(header.ChunkID[:]))
-	}
-
-	if string(header.Format[:]) != string(defaultFormat[:]) {
-		return fmt.Errorf("%w: Format %s", ErrInvalidHeader, string(header.Format[:]))
-	}
-
-	if err := sampleRateValidator.Validate(SampleRate(header.SampleRate)); err != nil {
-		return fmt.Errorf("%w: SampleRate %d", err, header.SampleRate)
-	}
-
-	if err := bitDepthValidator.Validate(BitDepth(header.BitsPerSample)); err != nil {
-		return fmt.Errorf("%w: BitsPerSample %d", err, header.BitsPerSample)
-	}
-
-	if err := channelsValidator.Validate(Channels(header.NumChannels)); err != nil {
-		return fmt.Errorf("%w: NumChannels %d", err, header.NumChannels)
-	}
-
-	if err := audioFormatValidator.Validate(AudioFormat(header.AudioFormat)); err != nil {
-		return fmt.Errorf("%w: AudioFormat %d", err, header.AudioFormat)
+func validateChunkID(h *Header) error {
+	if string(h.ChunkID[:]) != string(defaultChunkID[:]) {
+		return fmt.Errorf("%w: ChunkID %s", ErrInvalidHeader, string(h.ChunkID[:]))
 	}
 
 	return nil
+}
+
+func validateFormat(h *Header) error {
+	if string(h.Format[:]) != string(defaultFormat[:]) {
+		return fmt.Errorf("%w: Format %s", ErrInvalidHeader, string(h.Format[:]))
+	}
+
+	return nil
+}
+
+func validateSampleRate(h *Header) error {
+	switch h.SampleRate {
+	case SampleRate44100, SampleRate48000, SampleRate88200, SampleRate96000, SampleRate176400, SampleRate192000:
+		return nil
+	default:
+		return fmt.Errorf("%w: SampleRate %d", ErrInvalidSampleRate, h.SampleRate)
+	}
+}
+
+func validateBitDepth(h *Header) error {
+	switch h.BitsPerSample {
+	case BitDepth8, BitDepth16, BitDepth24, BitDepth32:
+		return nil
+	default:
+		return fmt.Errorf("%w: BitsPerSample %d", ErrInvalidBitDepth, h.BitsPerSample)
+	}
+}
+
+func validateNumChannels(h *Header) error {
+	switch h.NumChannels {
+	case ChannelsMono, ChannelsStereo:
+		return nil
+	default:
+		return fmt.Errorf("%w: NumChannels %d", ErrInvalidNumChannels, h.NumChannels)
+	}
+}
+
+func validateAudioFormat(h *Header) error {
+	switch h.AudioFormat {
+	case PCMFormat, FloatFormat:
+		return nil
+	default:
+		return fmt.Errorf("%w: AudioFormat %d", ErrInvalidAudioFormat, h.AudioFormat)
+	}
+}
+
+// Validate verifies that the input Header `h` is not nil and that it is valid
+func Validate(h *Header) error {
+	if h == nil {
+		return ErrEmptyHeader
+	}
+
+	return headerValidator.Validate(h)
 }
