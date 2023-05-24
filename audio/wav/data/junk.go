1cb0b01a19a4440a47cd548ce08c2a0d3be4ef35
zalgonoise <zalgo.noise@gmail.com>
2023-05-24 19:54:46 +0200

    audio/wav/data: updated JunkChunk references with data/header

--- audio/wav/data/junk.go
+++ audio/wav/data/junk.go
@@ -4,13 +4,14 @@ import (
 	"time"
 
 	"github.com/zalgonoise/x/audio/osc"
+	"github.com/zalgonoise/x/audio/wav/data/header"
 )
 
 // JunkChunk is a DataChunk used for storing "junk"-ID subchunk data
 type JunkChunk struct {
-	*ChunkHeader
-	Data  []byte
-	Depth uint16
+	ChunkHeader *header.Header
+	Data        []byte
+	Depth       uint16
 }
 
 // Parse will consume the input byte slice `buf`, to extract the PCM audio buffer
@@ -18,8 +19,8 @@ type JunkChunk struct {
 func (d *JunkChunk) Parse(buf []byte) {
 	if d.Data == nil {
 		d.Data = buf
-		if d.Subchunk2Size == 0 {
-			d.Subchunk2Size = uint32(len(buf))
+		if d.ChunkHeader.Subchunk2Size == 0 {
+			d.ChunkHeader.Subchunk2Size = uint32(len(buf))
 		}
 		return
 	}
@@ -36,7 +37,7 @@ func (d *JunkChunk) Bytes() []byte {
 }
 
 // Header returns the ChunkHeader of the DataChunk
-func (d *JunkChunk) Header() *ChunkHeader { return d.ChunkHeader }
+func (d *JunkChunk) Header() *header.Header { return d.ChunkHeader }
 
 // BitDepth returns the bit depth of the DataChunk
 func (d *JunkChunk) BitDepth() uint16 { return d.Depth }
@@ -60,10 +61,10 @@ func (d *JunkChunk) Generate(_ osc.Type, _, _ int, _ time.Duration) {}
 // SetBitDepth returns a new DataChunk with the input `bitDepth`'s converter, or
 // an error if invalid. The new DataChunk retains any PCM data it contains, as a copy.
 func (d *JunkChunk) SetBitDepth(bitDepth uint16) (*DataChunk, error) {
-	header := NewDataHeader()
-	header.Subchunk2Size = d.Subchunk2Size
+	h := header.NewData()
+	h.Subchunk2Size = d.ChunkHeader.Subchunk2Size
 
-	newChunk := NewPCMDataChunk(bitDepth, header)
+	newChunk := NewPCMDataChunk(bitDepth, h)
 	if newChunk == nil {
 		return nil, ErrInvalidBitDepth
 	}
@@ -78,13 +79,13 @@ func (d *JunkChunk) SetBitDepth(bitDepth uint16) (*DataChunk, error) {
 func (d *JunkChunk) Apply(_ ...FilterFunc) {}
 
 // NewJunkChunk creates a JunkChunk with the input `subchunk` ChunkHeader, or with a default one if nil
-func NewJunkChunk(subchunk *ChunkHeader) *JunkChunk {
-	if subchunk == nil {
-		subchunk = NewJunkHeader()
+func NewJunkChunk(h *header.Header) *JunkChunk {
+	if h == nil {
+		h = header.NewJunk()
 	}
 
 	return &JunkChunk{
-		ChunkHeader: subchunk,
+		ChunkHeader: h,
 		Depth:       0,
 	}
 }
