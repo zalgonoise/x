0a4a42bda20c017a6d429d2a91999866c6a20167
zalgonoise <mariozalgo@gmail.com>
2023-05-30 13:21:10 +0200

    audio/wav/data/filters: adding Add and Sub funcs

---
+++ audio/wav/data/filters/signal.go
@@ -0,0 +1,21 @@
+package filters
+
+// Add sums two signals together, repeating the pattern in `signal` if shorter than the
+// audio buffer
+func Add(signal []float64) func([]float64) {
+	return func(buffer []float64) {
+		for i := range buffer {
+			buffer[i] = (buffer[i] + signal[i%len(signal)]) / 2
+		}
+	}
+}
+
+// Sub subtracts two signals together, repeating the pattern in `signal` if shorter than the
+// audio buffer
+func Sub(signal []float64) func([]float64) {
+	return func(buffer []float64) {
+		for i := range buffer {
+			buffer[i] = (buffer[i] - signal[i%len(signal)]) / 2
+		}
+	}
+}
