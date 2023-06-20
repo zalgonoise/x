57d8119131a30562f2ab819a3b819b30f82c4618
zalgonoise <mariozalgo@gmail.com>
2023-06-20 22:20:05 +0200

    ptr/value: added GetInterface and IsNil functions, as well as converted Go runtime / ABI data types for type-casting

---
+++ ptr/value.go
@@ -0,0 +1,102 @@
+package ptr
+
+import "unsafe"
+
+// ABIType is a representation of Go ABI's (general / generic) type definition, as per internal/abi/type.go
+//
+// https://github.com/golang/go/blob/master/src/internal/abi/type.go#L20
+type ABIType struct {
+	Size_       uintptr
+	PtrBytes    uintptr // number of (prefix) bytes in the type that can contain pointers
+	Hash        uint32  // hash of type; avoids computation in hash tables
+	TFlag       uint8   // extra type information flags
+	Align_      uint8   // alignment of variable with this type
+	FieldAlign_ uint8   // alignment of struct field with this type
+	Kind_       uint8   // enumeration for C
+	// function for comparing objects of this type
+	// (ptr to object A, ptr to object B) -> ==?
+	Equal func(unsafe.Pointer, unsafe.Pointer) bool
+	// GCData stores the GC type data for the garbage collector.
+	// If the KindGCProg bit is set in kind, GCData is a GC program.
+	// Otherwise it is a ptrmask bitmap. See mbitmap.go for details.
+	GCData    *byte
+	Str       int32 // string form
+	PtrToThis int32 // type for pointer to this type, may be zero
+}
+
+// ABIName is a representation of Go ABI's name type, as per internal/abi/type.go
+//
+// https://github.com/golang/go/blob/master/src/internal/abi/type.go#L589
+type ABIName struct {
+	Bytes *byte
+}
+
+// ABIImethod is a representation of Go ABI's interface method type, as per internal/abi/type.go
+//
+// https://github.com/golang/go/blob/master/src/internal/abi/type.go#L231
+type ABIImethod struct {
+	Name int32 // name of method
+	Typ  int32 // .(*FuncType) underneath
+}
+
+// ABIInterface is a representation of Go ABI's interface type, as per internal/abi/type.go
+//
+// https://github.com/golang/go/blob/e122ebabb657021964f2bdd31e683ddfa023fd0c/src/internal/abi/type.go#L415
+type ABIInterface struct {
+	ABIType
+	PkgPath ABIName      // import path
+	Methods []ABIImethod // sorted by hash
+}
+
+// Itable is a representation behind Go interface tables, as per runtime/runtime2.go
+//
+// https://github.com/golang/go/blob/master/src/runtime/runtime2.go#L951
+type Itable struct {
+	Inter ABIInterface
+	Type  ABIType
+	// Hash is a copy of Type.hash. Used for type switches.
+	Hash uint32
+	_    [4]byte
+	// Fun is variable sized. fun[0]==0 means _type does not implement inter.
+	Fun [1]uintptr
+}
+
+// Interface is a representation behind Go interfaces, as per runtime/runtime2.go
+//
+// https://github.com/golang/go/blob/master/src/runtime/runtime2.go#L204
+type Interface struct {
+	Itab  Itable
+	Value unsafe.Pointer
+}
+
+// GetInterface extracts the interface information from the input value. This input must be an interface, otherwise the
+// results from this function call are unexpected, considering that it uses an unsafe approach to extract the underlying
+// metadata and concrete type.
+func GetInterface[T any](value T) Interface {
+	return *(*Interface)(unsafe.Pointer(&value))
+}
+
+// IsNil returns a boolean if the supplied value is nil, inspected as an interface, using a
+// fast (and unsafe) approach, by casting the input type as a Go interface, to check if its underlying
+// concrete value is or isn't nil.
+//
+// This will also return true if the input value is an interface with a nil concrete type such as `fmt.Stringer(nil)`.
+// It is up to the caller to evaluate if this is in fact useful within the logic of their application
+//
+// For this reason, the input type is `any`, an alias for `interface{}`. By casting the input as in interface in the
+// function's parameters, we can use this pattern for many more data types besides interfaces.
+//
+// NOTE: this call will return invalid / incorrect results when it comes to slices. For that, please see the
+// Slice definition, as its structure positions how the second element in the type cast operation would actually point
+// to the slice's length; as such a nil slice would not present this second element as a zero-value uintptr (nil).
+// It is up to the caller to avoid passing (nil) slices to this function to avoid these false negatives
+func IsNil(value any) bool {
+	if value == nil {
+		return true
+	}
+
+	// fastest means of checking an interface's concrete type
+	ptr := *(*[2]uintptr)(unsafe.Pointer(&value))
+
+	return ptr[1] == 0
+}
---
+++ ptr/value_test.go
@@ -0,0 +1,150 @@
+package ptr
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+type _stringerImpl struct{ value string }
+
+func (s _stringerImpl) String() string {
+	return s.value
+}
+
+func BenchmarkIsNil(b *testing.B) {
+	b.Run("nil interface checking", func(b *testing.B) {
+		b.Run("StandardLibrary", func(b *testing.B) {
+			var (
+				ok    bool
+				iface fmt.Stringer
+			)
+
+			for i := 0; i < b.N; i++ {
+				ok = iface == nil
+			}
+			_ = ok
+		})
+
+		b.Run("PtrLibrary", func(b *testing.B) {
+			var (
+				ok    bool
+				iface fmt.Stringer
+			)
+
+			for i := 0; i < b.N; i++ {
+				ok = IsNil(iface)
+			}
+			_ = ok
+		})
+
+	})
+
+	b.Run("interface w/ nil value checking", func(b *testing.B) {
+		b.Run("StandardLibrary", func(b *testing.B) {
+			var (
+				ok    bool
+				iface = fmt.Stringer(nil)
+			)
+
+			for i := 0; i < b.N; i++ {
+				ok = iface == nil
+			}
+			_ = ok
+		})
+
+		b.Run("PtrLibrary", func(b *testing.B) {
+			var (
+				ok    bool
+				iface = fmt.Stringer(nil)
+			)
+
+			for i := 0; i < b.N; i++ {
+				ok = IsNil(iface)
+			}
+			_ = ok
+		})
+
+	})
+}
+
+func TestIsNil(t *testing.T) {
+	for _, testcase := range []struct {
+		name  string
+		value any
+		isNil bool
+	}{
+		{
+			name:  "WithValue/String",
+			value: "test",
+		},
+		{
+			name:  "WithValue/Float32",
+			value: (float32)(1.5),
+		},
+		{
+			name:  "WithValue/map[string]string",
+			value: map[string]string{},
+		},
+		{
+			name:  "Nil/map[string]string",
+			value: (map[string]string)(nil),
+			isNil: true,
+		},
+		{
+			name:  "WithValue/chan string",
+			value: make(chan string),
+		},
+		{
+			name:  "Nil/chan string",
+			value: (chan string)(nil),
+			isNil: true,
+		},
+		{
+			name:  "WithValue/slice string",
+			value: make([]string, 0),
+		},
+		// test fails for slices since they are composed
+		// with a different structure
+		//{
+		//	name:  "Nil/slice string",
+		//	value: ([]string)(nil),
+		//	isNil: true,
+		//},
+		{
+			name:  "WithValue/custom type",
+			value: struct{ name string }{},
+		},
+		{
+			name:  "WithValue/custom type pointer",
+			value: &struct{ name string }{},
+		},
+		{
+			name:  "Nil/custom type pointer",
+			value: (*_stringerImpl)(nil),
+			isNil: true,
+		},
+		{
+			name:  "WithValue/interface",
+			value: fmt.Stringer(_stringerImpl{value: "OK"}),
+		},
+		{
+			name:  "Nil/interface",
+			value: fmt.Stringer(nil),
+			isNil: true,
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			require.Equal(t, testcase.isNil, IsNil(testcase.value))
+		})
+	}
+}
+
+func TestGetInterface(t *testing.T) {
+	t.Run("fmt.Stringer", func(t *testing.T) {
+		// use iface below as a breakpoint to explore the underlying data types
+		iface := GetInterface(fmt.Stringer(_stringerImpl{value: "OK"}))
+
+		require.NotEmpty(t, iface.Itab.Hash)
+	})
+}
