a38a55833e6f106de78008ec7d774e15cfc839a3
zalgonoise <mariozalgo@gmail.com>
2023-06-25 15:34:37 +0200

    audio/wav/data/conv: renamed type: ConvFloat to Float32

--- audio/wav/data/conv/float32.go
+++ audio/wav/data/conv/float32.go
@@ -4,11 +4,11 @@ import (
 	"unsafe"
 )
 
-// ConvFloat is a 32bit IEEE Floating Point audio Converter
-type ConvFloat struct{}
+// Float32 is a 32bit IEEE Floating Point audio Converter
+type Float32 struct{}
 
 // Parse consumes the input audio buffer, returning its floating point audio representation
-func (ConvFloat) Parse(buf []byte) []float64 {
+func (Float32) Parse(buf []byte) []float64 {
 	data := *(*[]uint32)(unsafe.Pointer(&buf))
 
 	return convert(
@@ -19,7 +19,7 @@ func (ConvFloat) Parse(buf []byte) []float64 {
 }
 
 // Bytes consumes the input floating point audio buffer, returning its byte representation
-func (ConvFloat) Bytes(buf []float64) []byte {
+func (Float32) Bytes(buf []float64) []byte {
 	value := convert(
 		buf, func(f float64) float32 {
 			return float32(f)
@@ -39,7 +39,7 @@ func (ConvFloat) Bytes(buf []float64) []byte {
 }
 
 // Value consumes the input floating point audio buffer, returning its PCM audio values as a slice of int
-func (ConvFloat) Value(buf []float64) []int {
+func (Float32) Value(buf []float64) []int {
 	return convert(
 		buf, func(f float64) int {
 			return int(f * maxInt32)
