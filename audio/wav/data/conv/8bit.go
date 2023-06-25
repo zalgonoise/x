c806b4d7e60a9cb261ed24fe8310acfc77186655
zalgonoise <mariozalgo@gmail.com>
2023-06-25 15:14:12 +0200

    audio/wav/data/conv: moved 8bit converter to conv package

--- audio/wav/data/conv/8bit.go
+++ audio/wav/data/conv/8bit.go
@@ -1,4 +1,4 @@
-package data
+package conv
 
 import (
 	"unsafe"
@@ -9,14 +9,12 @@ const (
 	// minInt8 float64 = ^1<<6 + 1
 )
 
-var _ Converter = Conv8Bit{}
-
 // Conv8Bit is a 8bit audio Converter
 type Conv8Bit struct{}
 
 // Parse consumes the input audio buffer, returning its floating point audio representation
 func (Conv8Bit) Parse(buf []byte) []float64 {
-	return conv(
+	return convert(
 		*(*[]int8)(unsafe.Pointer(&buf)),
 		func(v int8) float64 {
 			return float64(v) / maxInt8
@@ -26,7 +24,7 @@ func (Conv8Bit) Parse(buf []byte) []float64 {
 
 // Bytes consumes the input floating point audio buffer, returning its byte representation
 func (Conv8Bit) Bytes(buf []float64) []byte {
-	value := conv(
+	value := convert(
 		buf, func(f float64) int8 {
 			return int8(f * maxInt8)
 		},
@@ -37,7 +35,7 @@ func (Conv8Bit) Bytes(buf []float64) []byte {
 
 // Value consumes the input floating point audio buffer, returning its PCM audio values as a slice of int
 func (Conv8Bit) Value(buf []float64) []int {
-	return conv(
+	return convert(
 		buf, func(f float64) int {
 			return int(f * maxInt8)
 		},
---
+++ audio/wav/data/conv/8bit_test.go
@@ -0,0 +1,99 @@
+package conv_test
+
+import (
+	"github.com/zalgonoise/x/audio/wav/data"
+	"github.com/zalgonoise/x/audio/wav/data/internal/testdata"
+	"reflect"
+	"testing"
+
+	"github.com/zalgonoise/x/audio/wav/data/header"
+)
+
+const (
+	bitDepth8  uint16 = 8
+	bitDepth16 uint16 = 16
+	bitDepth24 uint16 = 24
+	bitDepth32 uint16 = 32
+)
+
+func BenchmarkChunk8bit(b *testing.B) {
+	b.Run(
+		"Parse", func(b *testing.B) {
+			b.Run(
+				"NewBuffer", func(b *testing.B) {
+					h, err := header.From(testdata.Test8bitHeader)
+					if err != nil {
+						b.Error(err)
+						return
+					}
+
+					var chunk *data.DataChunk
+					b.ResetTimer()
+					for i := 0; i < b.N; i++ {
+						chunk = data.NewPCMDataChunk(bitDepth8, h)
+						chunk.Parse(testdata.Test8bitPCM)
+					}
+					_ = chunk
+				},
+			)
+			b.Run(
+				"Append", func(b *testing.B) {
+					h, err := header.From(testdata.Test8bitHeader)
+					if err != nil {
+						b.Error(err)
+						return
+					}
+
+					var chunk = data.NewPCMDataChunk(bitDepth8, h)
+					chunk.Parse(testdata.Test8bitPCM)
+					b.ResetTimer()
+					for i := 0; i < b.N; i++ {
+						chunk.Parse(testdata.Test8bitPCM)
+					}
+					_ = chunk
+				},
+			)
+		},
+	)
+	b.Run(
+		"Bytes", func(b *testing.B) {
+			h, err := header.From(testdata.Test8bitHeader)
+			if err != nil {
+				b.Error(err)
+				return
+			}
+
+			var (
+				chunk = data.NewPCMDataChunk(bitDepth8, h)
+				buf   []byte
+			)
+			chunk.Parse(testdata.Test8bitPCM)
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				buf = chunk.Bytes()
+			}
+			_ = buf
+		},
+	)
+}
+
+func Test8bitHeader(t *testing.T) {
+	h, err := header.From(testdata.Test8bitHeader)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	chunk := &data.DataChunk{
+		ChunkHeader: h,
+		Depth:       8, // set by NewChunk()
+	}
+
+	if output := chunk.Header(); !reflect.DeepEqual(*h, *output) {
+		t.Errorf("output mismatch error: wanted %+v ; got %+v", *h, *output)
+	}
+
+	if bitDepth := chunk.BitDepth(); bitDepth != chunk.Depth {
+		t.Errorf("bit depth mismatch error: wanted %v ; got %v", chunk.Depth, bitDepth)
+	}
+}
