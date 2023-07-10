a878f7ad71b1986eadf5db58ca2629df984eb22e
zalgonoise <zalgo.noise@gmail.com>
2023-07-10 20:58:02 +0200

    audio/cmd/gsp/client: refactored HTTP client logic

--- audio/cmd/gsp/client/client.go
+++ audio/cmd/gsp/client/client.go
@@ -4,54 +4,24 @@ import (
 	"context"
 	"net/http"
 	"time"
+
+	"github.com/zalgonoise/x/audio/errs"
 )
-
-type err string
-
-func (e err) Error() string { return (string)(e) }
 
 const (
-	ErrEmptyURL err = "gsp/client: empty URL"
+	domain   = errs.Domain("gsp/http_client")
+	ErrEmpty = errs.Kind("empty")
+	ErrURL   = errs.Entity("URL")
 )
 
-type streamClient struct {
-	client *http.Client
-	req    *http.Request
-	ctx    context.Context
-}
+var ErrEmptyURL = errs.New(domain, ErrEmpty, ErrURL)
 
-// Doer interface describes an object with a http.Client with a http.Request
-// and a context.Context.
-//
-// The exposed methods allow issuing the http.Request, as well as one to
-// retrieve its set context.Context
-type Doer interface {
-	// Do issues the http.Request in the Doer, returning a http.Response and an error
-	Do() (*http.Response, error)
-	// Context returns the set context.Context
-	Context() context.Context
-}
-
-// Do issues the http.Request in the Doer, returning a http.Response and an error
-func (c *streamClient) Do() (*http.Response, error) {
-	if c.client == nil {
-		return http.DefaultClient.Do(c.req)
-	}
-	return c.client.Do(c.req)
-}
-
-// Context returns the set context.Context
-func (c *streamClient) Context() context.Context {
-	return c.ctx
-}
+const defaultTimeout = 30 * time.Second
 
 // New creates a basic Doer interface, based on the input URL `url` and timeout `timeout`
 //
 // This Doer is based on the default http.Client from Go's standard library
-func New(url string, timeout *time.Duration) (Doer, context.CancelFunc, error) {
-	if url == "" {
-		return nil, nil, ErrEmptyURL
-	}
+func New(url string, timeout *time.Duration) (*http.Response, context.CancelFunc, error) {
 	if len(url) > 0 {
 		if url[0] == '"' {
 			url = url[1:]
@@ -60,6 +30,11 @@ func New(url string, timeout *time.Duration) (Doer, context.CancelFunc, error) {
 			url = url[:len(url)-1]
 		}
 	}
+
+	if url == "" {
+		return nil, nil, ErrEmptyURL
+	}
+
 	ctx, cancel := context.WithCancel(context.Background())
 	if timeout != nil {
 		ctx, cancel = context.WithTimeout(ctx, *timeout)
@@ -68,24 +43,21 @@ func New(url string, timeout *time.Duration) (Doer, context.CancelFunc, error) {
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
 		cancel()
+
 		return nil, nil, err
 	}
+
 	req.Header.Set("Content-Type", "audio/wav")
 
-	doer := new(streamClient)
-	doer.req = req
-	doer.ctx = ctx
-	return doer, cancel, nil
-}
+	res, err := (&http.Client{
+		Timeout: defaultTimeout,
+	}).Do(req)
 
-// WithClient is just like New, but it is configured with the input
-// http.Client `client`, instead
-func WithClient(url string, timeout *time.Duration, client *http.Client) (Doer, context.CancelFunc, error) {
-	doer, cancel, err := New(url, timeout)
 	if err != nil {
+		cancel()
+
 		return nil, nil, err
 	}
 
-	doer.(*streamClient).client = client
-	return doer, cancel, err
+	return res, cancel, nil
 }
