0360e6708f208a2229c97e64810800b51552fbe1
zalgonoise <zalgo.noise@gmail.com>
2023-07-13 21:17:47 +0200

    audio/cmd/audio/stream: added log writer implementation

---
+++ audio/cmd/audio/stream/log.go
@@ -0,0 +1,44 @@
+package stream
+
+import (
+	"github.com/zalgonoise/attr"
+	"github.com/zalgonoise/logx"
+)
+
+const defaultMessage = "new value registered"
+
+type LogWriter struct {
+	msg    string
+	logger logx.Logger
+}
+
+func (w LogWriter) SetPeakValues(data []float64) (err error) {
+	for i := range data {
+		if err = w.SetPeakValue(data[i]); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (w LogWriter) SetPeakValue(data float64) (err error) {
+	w.logger.Info(w.msg, attr.Float("value", data))
+
+	return nil
+}
+
+func (w LogWriter) Close() error {
+	return nil
+}
+
+func NewLogWriter(message string, logger logx.Logger) LogWriter {
+	if message == "" {
+		message = defaultMessage
+	}
+
+	return LogWriter{
+		msg:    message,
+		logger: logger,
+	}
+}
