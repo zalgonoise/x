1f48881953d31476413e9c254a81d5464ba77f5f
zalgonoise <zalgo.noise@gmail.com>
2023-07-15 16:18:15 +0200

    audio/cmd/audio/http: added backoff logic to HTTP request

--- audio/cmd/audio/http/client.go
+++ audio/cmd/audio/http/client.go
@@ -2,26 +2,41 @@ package http
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/zalgonoise/attr"
+	"github.com/zalgonoise/logx"
 
 	"github.com/zalgonoise/x/audio/errs"
 )
 
 const (
-	domain   = errs.Domain("audio/http")
-	ErrEmpty = errs.Kind("empty")
-	ErrURL   = errs.Entity("URL")
+	domain = errs.Domain("audio/http")
+
+	ErrEmpty     = errs.Kind("empty")
+	ErrExhausted = errs.Kind("exhausted")
+
+	ErrURL     = errs.Entity("URL")
+	ErrBackoff = errs.Entity("backoff")
 )
 
-var ErrEmptyURL = errs.New(domain, ErrEmpty, ErrURL)
+var (
+	ErrEmptyURL         = errs.New(domain, ErrEmpty, ErrURL)
+	ErrExhaustedBackoff = errs.New(domain, ErrExhausted, ErrBackoff)
+)
 
-const defaultTimeout = 30 * time.Second
+const (
+	defaultTimeout = 30 * time.Second
+	minBackoff     = 125 * time.Millisecond
+	maxBackoff     = 10 * time.Second
+)
 
 // New issues an HTTP GET request based on the input URL `url` and timeout `timeout`
 //
 // The body of the response is then used as an audio stream
-func New(url string, timeout time.Duration) (*http.Response, context.CancelFunc, error) {
+func New(logger logx.Logger, url string, timeout time.Duration) (*http.Response, context.CancelFunc, error) {
 	if len(url) > 0 {
 		if url[0] == '"' {
 			url = url[1:]
@@ -35,6 +50,39 @@ func New(url string, timeout time.Duration) (*http.Response, context.CancelFunc,
 		return nil, nil, ErrEmptyURL
 	}
 
+	res, done, err := doWithBackoff(logger, url, timeout)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return res, done, nil
+}
+
+func doWithBackoff(logger logx.Logger, url string, timeout time.Duration) (*http.Response, context.CancelFunc, error) {
+	backoff := minBackoff
+
+	for {
+		res, done, err := do(url, timeout)
+		if err == nil {
+			return res, done, nil
+		}
+
+		logger.Warn("request has failed",
+			attr.String("error", err.Error()),
+			attr.String("backoff", backoff.String()),
+		)
+
+		time.Sleep(backoff)
+
+		backoff = backoff * 2
+
+		if backoff > maxBackoff {
+			return res, done, fmt.Errorf("%w: %w", ErrExhaustedBackoff, err)
+		}
+	}
+}
+
+func do(url string, timeout time.Duration) (*http.Response, context.CancelFunc, error) {
 	ctx, cancel := context.WithCancel(context.Background())
 	if timeout > 0 {
 		ctx, cancel = context.WithTimeout(ctx, timeout)
