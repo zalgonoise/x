af9eabf57c3f13e4249495a18dfe57a33ec6cb4b
zalgonoise <zalgo.noise@gmail.com>
2023-07-17 18:29:57 +0200

    audio/wav/cmd/audio/stream: refactored processor functions configuration

--- audio/cmd/audio/stream/stream.go
+++ audio/cmd/audio/stream/stream.go
@@ -14,15 +14,26 @@ import (
 
 	"github.com/zalgonoise/x/audio/cmd/audio/config"
 	"github.com/zalgonoise/x/audio/cmd/audio/http"
+	"github.com/zalgonoise/x/audio/errs"
 	"github.com/zalgonoise/x/audio/fft"
 	"github.com/zalgonoise/x/audio/fft/window"
 	"github.com/zalgonoise/x/audio/wav"
 )
 
+const (
+	streamDomain = errs.Domain("audio/stream")
+
+	ErrInvalid = errs.Kind("invalid")
+
+	ErrMode = errs.Entity("operation mode")
+)
+
+var ErrInvalidMode = errs.New(streamDomain, ErrInvalid, ErrMode)
+
 type Reporter interface {
 	SetPeakValue(data float64) (err error)
 	SetPeakValues(data []float64) (err error)
-	SetPeakFreq(frequency int, magnitude float64) (err error)
+	SetPeakFreq(frequency int) (err error)
 	io.Closer
 }
 
@@ -118,49 +129,65 @@ func New(cfg *config.Config) (*Stream, error) {
 		s.logger = logger
 	}
 
+	procFns := make([]func([]float64) error, 0)
+
 	switch cfg.Mode {
 	case config.Monitor:
-		s.proc = func(data []float64) error {
-			var max float64
-
-			for i := range data {
-				if data[i] > max {
-					max = data[i]
-				}
-			}
-
-			return s.out.SetPeakValue(max)
-		}
+		procFns = append(procFns, newMontiorFunc(s))
 	case config.Analyze:
-		bs := fft.Block1024
-		windowBlock := window.New(window.Blackman, int(bs))
-
-		s.proc = func(data []float64) error {
-			for i := 0; i+int(bs) < len(data); i += int(bs) {
-				spectrum := fft.Apply(
-					int(s.stream.Wav.Header.SampleRate),
-					data[i:i+int(bs)],
-					windowBlock,
-				)
-
-				var max float64
-				var freq int
-
-				for idx := range spectrum {
-					if max < spectrum[idx].Mag {
-						max = spectrum[idx].Mag
-						freq = spectrum[idx].Freq
-					}
-				}
-
-				if err := s.out.SetPeakFreq(freq, max); err != nil {
-					return err
-				}
-			}
-
-			return nil
-		}
+		procFns = append(procFns, newAnalyzeFunc(s))
+	case config.Combined:
+		procFns = append(procFns, newMontiorFunc(s), newAnalyzeFunc(s))
+	default:
+		return nil, ErrInvalidMode
 	}
 
+	s.proc = wav.MultiProc(false, procFns...)
+
 	return s, nil
+}
+
+func newMontiorFunc(s *Stream) func([]float64) error {
+	return func(data []float64) error {
+		var max float64
+
+		for i := range data {
+			if data[i] > max {
+				max = data[i]
+			}
+		}
+
+		return s.out.SetPeakValue(max)
+	}
+}
+
+func newAnalyzeFunc(s *Stream) func([]float64) error {
+	bs := fft.Block1024
+	windowBlock := window.New(window.Blackman, int(bs))
+
+	return func(data []float64) error {
+		for i := 0; i+int(bs) < len(data); i += int(bs) {
+			spectrum := fft.Apply(
+				int(s.stream.Wav.Header.SampleRate),
+				data[i:i+int(bs)],
+				windowBlock,
+			)
+
+			var max float64
+			var freq int
+
+			for idx := range spectrum {
+				if max < spectrum[idx].Mag {
+					max = spectrum[idx].Mag
+					freq = spectrum[idx].Freq
+				}
+			}
+
+			if err := s.out.SetPeakFreq(freq); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
 }
