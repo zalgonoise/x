631a3e6a323def1acdb68105ab67d0357f0b9180
zalgonoise <zalgo.noise@gmail.com>
2023-07-21 19:23:28 +0200

    audio/wav/cmd/audio/config: refactored flags config

--- audio/cmd/audio/config/flags.go
+++ audio/cmd/audio/config/flags.go
@@ -5,31 +5,26 @@ import (
 	"time"
 )
 
-// NewConfig creates a new Config by reading the input flags to the application startup
-//
-// It returns a new Config and an error, which is a call to the Validate(Config) function
-func NewConfig() (*Config, error) {
+// FromFlags loads a Config from the input command-line flags, in the app's runtime
+func FromFlags() (*Config, error) {
 	url := flag.String("url", "", "the URL for the WAV audio stream")
-	mode := flag.String("mode", "monitor", "defines the operation mode")
-	out := flag.String("to", "logger", "defines the output mode [logger, file, prometheus]")
+	mode := flag.String("mode", "", "defines the operation mode")
+	out := flag.String("to", "", "defines the output mode [logger, file, prometheus]")
 	path := flag.String("o", "", "defines the path to the output (if a file, or a port / address for Prometheus)")
 	exit := flag.Int("exit", 0, "sets a custom exit code for when the app exits")
-	timeout := flag.String("dur", "30s", "sets the duration of the recording or analysis")
+	timeout := flag.String("dur", "", "sets the duration of the recording or analysis")
 
 	flag.Parse()
 
-	var dur time.Duration
-	var err error
-
-	dur, err = time.ParseDuration(*timeout)
-	if err != nil || dur < 0 {
-		dur = 0
+	var duration time.Duration
+	if dur, err := time.ParseDuration(*timeout); err == nil && dur > 0 {
+		duration = dur
 	}
 
 	config := &Config{
 		Mode:       OpMode(*mode),
 		URL:        *url,
-		Duration:   dur,
+		Duration:   duration,
 		Output:     Output(*out),
 		OutputPath: *path,
 		ExitCode:   *exit,
