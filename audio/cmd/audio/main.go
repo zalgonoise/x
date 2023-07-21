52fbdf3c808fcb0d152874370a1a03cb36787013
zalgonoise <zalgo.noise@gmail.com>
2023-07-21 19:25:27 +0200

    audio/wav/cmd/audio: load config with defaults

--- audio/cmd/audio/main.go
+++ audio/cmd/audio/main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/zalgonoise/attr"
 	"github.com/zalgonoise/logx"
-	"github.com/zalgonoise/logx/handlers/texth"
 
 	"github.com/zalgonoise/x/audio/cmd/audio/config"
 	"github.com/zalgonoise/x/audio/cmd/audio/stream"
@@ -15,7 +14,7 @@ import (
 func main() {
 	err, code := run()
 	if err != nil {
-		logx.New(texth.New(os.Stderr)).Error(
+		logx.Error(
 			"audio/gsp: runtime error",
 			attr.String("error", err.Error()),
 		)
@@ -25,7 +24,7 @@ func main() {
 }
 
 func run() (error, int) {
-	cfg, err := config.NewConfig()
+	cfg, err := config.WithDefaults()
 	if err != nil {
 		return err, 1
 	}
