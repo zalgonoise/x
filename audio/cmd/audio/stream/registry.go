c9d6cf72828fc34ba14eac87175fdd3ca622c276
zalgonoise <zalgo.noise@gmail.com>
2023-07-21 19:21:44 +0200

    audio/wav/cmd/audio/stream: documented MaxRegistry

--- audio/cmd/audio/stream/registry.go
+++ audio/cmd/audio/stream/registry.go
@@ -1,17 +1,28 @@
 package stream
 
+// LessFunc is a generic, injectable function that determines how to
+// compare two values of any type, returning a boolean on whether
+// item `i` is less than item `j`
+type LessFunc[T any] func(i, j T) bool
+
+// MaxRegistry stores the maximum value for a series of writes, as
+// determined by the configured sort.LessFunc provided
 type MaxRegistry[T any] struct {
 	max T
 
-	lessFn func(i, j T) bool
+	lessFn LessFunc[T]
 }
 
+// Register stores the input value as the max value in the MaxRegistry,
+// if it is in fact greater than the stored one
 func (r *MaxRegistry[T]) Register(value T) {
 	if r.lessFn(r.max, value) {
 		r.max = value
 	}
 }
 
+// Flush returns the max value in the MaxRegistry, while resetting the one
+// it has stored
 func (r *MaxRegistry[T]) Flush() T {
 	max := r.max
 	r.max = *new(T)
@@ -19,7 +30,8 @@ func (r *MaxRegistry[T]) Flush() T {
 	return max
 }
 
-func NewMaxRegistry[T any](lessFn func(i, j T) bool) *MaxRegistry[T] {
+// NewMaxRegistry creates a MaxRegistry from the input LessFunc
+func NewMaxRegistry[T any](lessFn LessFunc[T]) *MaxRegistry[T] {
 	if lessFn == nil {
 		return nil
 	}
