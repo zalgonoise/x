23456b4f47f529c742931ee5be1ddfda505dfefb
zalgonoise <mariozalgo@gmail.com>
2023-07-28 02:59:08 +0200

    audio/cmd/audio/stream: added BucketRegistry type

--- audio/cmd/audio/stream/registry.go
+++ audio/cmd/audio/stream/registry.go
@@ -1,9 +1,20 @@
 package stream
 
+import (
+	"errors"
+
+	"golang.org/x/exp/maps"
+)
+
 // LessFunc is a generic, injectable function that determines how to
 // compare two values of any type, returning a boolean on whether
 // item `i` is less than item `j`
 type LessFunc[T any] func(i, j T) bool
+
+type Registry[T any, F any] interface {
+	Register(T)
+	Flush() F
+}
 
 // MaxRegistry stores the maximum value for a series of writes, as
 // determined by the configured sort.LessFunc provided
@@ -40,4 +51,67 @@ func NewMaxRegistry[T any](lessFn LessFunc[T]) *MaxRegistry[T] {
 		max:    *new(T),
 		lessFn: lessFn,
 	}
+}
+
+type BucketRegistry[T any, F ~map[string]T] struct {
+	max map[string]T
+
+	bucket bucket[T]
+	less   LessFunc[T]
+}
+
+func (r BucketRegistry[T, F]) Register(value T) {
+	if label := r.bucket.Get(value); label != "" {
+		if max, ok := r.max[label]; ok && r.less(max, value) {
+			r.max[label] = value
+		}
+	}
+}
+
+func (r BucketRegistry[T, F]) Flush() F {
+	values := make(map[string]T, len(r.max))
+	maps.Copy(values, r.max)
+
+	for k := range r.max {
+		r.max[k] = *new(T)
+	}
+
+	return values
+}
+
+func NewBucketRegistry[T any, F ~map[string]T](
+	values []T, labels []string, lessFunc LessFunc[T],
+) (r BucketRegistry[T, F], err error) {
+	switch {
+	case len(labels) == 0 && len(values) == 0, len(labels) > 0 && len(values) == 0:
+		//labels = frequencyLabels
+		//values = make([]T, len(frequencyValues))
+		//for i := range values {
+		//	values[i] = T(frequencyValues[i])
+		//}
+		return r, errors.New("invalid set of labels and values")
+
+	case len(labels) == 0 && len(values) > 0:
+		//labels = make([]string, len(values))
+		//for i := range labels {
+		//	labels[i] = fmt.Sprintf("%d", int(values[i]))
+		//}
+		return r, errors.New("invalid set of labels and values")
+	}
+
+	max := make(map[string]T, len(labels))
+	for i := range labels {
+		max[labels[i]] = *new(T)
+	}
+
+	b := newBucket[T](values, labels, lessFunc)
+	if b == nil {
+		return r, errors.New("failed to create bucket")
+	}
+
+	return BucketRegistry[T, F]{
+		max:    max,
+		bucket: *b,
+		less:   lessFunc,
+	}, nil
 }
