5c61f17a953a5a7d6027b2972249676de6adb391
zalgonoise <mariozalgo@gmail.com>
2023-07-31 01:11:09 +0200

    logbuf: initial commit for config

---
+++ logbuf/config.go
@@ -0,0 +1,6 @@
+package logbuf
+
+// BufferedHandlerConfig describes the configuration values for a BufferedHandler
+//
+// TODO: extend data structure with config values as needed
+type BufferedHandlerConfig struct{}
