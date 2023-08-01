b894a50f4f9a45f42648e8a92b8e3ef34a25708c
zalgonoise <mariozalgo@gmail.com>
2023-08-01 19:39:52 +0200

    logbuf/database: add migrations logic

--- logbuf/database/database.go
+++ logbuf/database/database.go
@@ -1,15 +1,26 @@
 package database
 
 import (
+	"context"
 	"database/sql"
+	"embed"
+	"errors"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	_ "modernc.org/sqlite"
 )
 
+//go:embed migration/*
+var migrations embed.FS
+
+type Logger interface {
+	InfoContext(ctx context.Context, msg string, args ...any)
+}
+
 // Connect creates a new instance of a SQLite database from the input URI. This URI can be empty (or ":memory:", if the
 // caller wishes to run the database in-memory.
-//
-// TODO: implement database schema; implement database migration logic
 func Connect(uri string) (*sql.DB, error) {
 	if uri == "" {
 		uri = ":memory:"
@@ -21,4 +32,62 @@ func Connect(uri string) (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+// Migrate updates an instance of a database to the latest schema and its modifications, by running chronologically
+// ordered migrations
+func Migrate(ctx context.Context, db *sql.DB, logger Logger) error {
+	conn, err := db.Conn(ctx)
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
+	sources, err := iofs.New(migrations, "migration")
+	if err != nil {
+		return err
+	}
+
+	driver, err := sqlite3.WithInstance(db, &sqlite3.Config{
+		DatabaseName: "logs_buffer",
+	})
+	if err != nil {
+		return err
+	}
+
+	migrator, err := migrate.NewWithInstance("iofs", sources, "traces", driver)
+	if err != nil {
+		return err
+	}
+
+	defer migrator.Close()
+
+	currentVersion, dirty, err := migrator.Version()
+	if errors.Is(err, migrate.ErrNilVersion) {
+		currentVersion = 0
+	} else if err != nil {
+		return err
+	}
+
+	logger.InfoContext(ctx, "before migration",
+		"version", currentVersion,
+		"dirty", dirty,
+	)
+
+	if err = migrator.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return err
+	}
+
+	currentVersion, dirty, err = migrator.Version()
+	if err != nil {
+		return err
+	}
+
+	logger.InfoContext(ctx, "after migration",
+		"version", currentVersion,
+		"dirty", dirty,
+	)
+
+	return nil
 }
