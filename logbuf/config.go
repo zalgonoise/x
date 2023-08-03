42fce61b661320a8dd831a5b50698aebf1a0fc79
zalgonoise <zalgo.noise@gmail.com>
2023-08-03 18:31:04 +0200

    logbuf/config: updated configuration data structure

--- logbuf/config.go
+++ logbuf/config.go
@@ -1,6 +1,14 @@
 package logbuf
 
+import (
+	"time"
+)
+
 // BufferedHandlerConfig describes the configuration values for a BufferedHandler
 //
-// TODO: extend data structure with config values as needed
-type BufferedHandlerConfig struct{}
+// TODO: extend and document data structure with config values as needed; add envars for fluidity
+type BufferedHandlerConfig struct {
+	DeletionThresh time.Duration
+	FlushFrequency time.Duration
+	FlushLevel     int
+}
