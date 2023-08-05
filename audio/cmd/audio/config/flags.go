679429ec5b392beff611a09b6c497c86fdc411ff
zalgonoise <zalgo.noise@gmail.com>
2023-08-05 17:11:35 +0200

    audio/cmd/audio/config: add support for number of spectrum buckets as a CLI flag

--- audio/cmd/audio/config/flags.go
+++ audio/cmd/audio/config/flags.go
@@ -9,12 +9,13 @@ var emptyFlagsConfig = Config{}
 
 // FromFlags loads a Config from the input command-line flags, in the app's runtime
 func FromFlags() *Config {
-	url := flag.String("url", "", "the URL for the WAV audio stream")
 	mode := flag.String("mode", "", "defines the operation mode")
+	url := flag.String("url", "", "the URL for the WAV audio stream")
+	timeout := flag.String("dur", "", "sets the duration of the recording or analysis")
 	out := flag.String("to", "", "defines the output mode [logger, file, prometheus]")
 	path := flag.String("o", "", "defines the path to the output (if a file, or a port / address for Prometheus)")
+	numBuckets := flag.Int("num-buckets", 0, "defines the number of buckets to separate frequencies in a spectrum analysis")
 	exit := flag.Int("exit", 0, "sets a custom exit code for when the app exits")
-	timeout := flag.String("dur", "", "sets the duration of the recording or analysis")
 
 	flag.Parse()
 
@@ -24,12 +25,13 @@ func FromFlags() *Config {
 	}
 
 	config := &Config{
-		Mode:       OpMode(*mode),
-		URL:        *url,
-		Duration:   duration,
-		Output:     Output(*out),
-		OutputPath: *path,
-		ExitCode:   *exit,
+		Mode:               OpMode(*mode),
+		URL:                *url,
+		Duration:           duration,
+		Output:             Output(*out),
+		OutputPath:         *path,
+		NumSpectrumBuckets: *numBuckets,
+		ExitCode:           *exit,
 	}
 
 	if *config == emptyFlagsConfig {
