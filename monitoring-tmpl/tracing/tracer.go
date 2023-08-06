7b7c98aa58db33c61844c55ee9fe457fb264e327
zalgonoise <zalgo.noise@gmail.com>
2023-08-06 20:29:58 +0200

    exemplars/tracing: added OTEL tracer implementation

---
+++ monitoring-tmpl/tracing/tracer.go
@@ -0,0 +1,41 @@
+package tracing
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
+	"go.opentelemetry.io/otel/trace"
+)
+
+const ServiceName = "observability-with-exemplars"
+
+func Tracer() trace.Tracer {
+	return otel.GetTracerProvider().Tracer(ServiceName)
+}
+
+type ShutdownFunc func(err context.Context) error
+
+func Init(ctx context.Context, traceExporter sdktrace.SpanExporter) (ShutdownFunc, error) {
+	res, err := resource.New(ctx,
+		resource.WithAttributes(semconv.ServiceName(ServiceName)),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+
+	otel.SetTracerProvider(tracerProvider)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	return tracerProvider.Shutdown, nil
+}
