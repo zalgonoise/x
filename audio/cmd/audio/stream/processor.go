d78751daf30cfcd0eaf864b45f2129788ab75c3f
zalgonoise <mariozalgo@gmail.com>
2023-08-16 02:53:05 +0200

    audio/cmd/audio/stream/processor: documented types

--- audio/cmd/audio/stream/processor.go
+++ audio/cmd/audio/stream/processor.go
@@ -24,12 +24,22 @@ type Exporter interface {
 	Shutdown(ctx context.Context) (err error)
 }
 
+// ProcessorConfig enumerates general options that may or may not be used by a processor
 type ProcessorConfig struct {
-	Size       int
+	// Size limits a certain size, in the context of the processor
+	Size int
+	// SampleRate denotes the input signal's sample rate
 	SampleRate int
 }
 
+// ProcessorFunc is a type of function that creates other ProcessFunc. The resulting ProcessFunc
+// will pipe the (processed) data to the input Exporter, using the ProcessorConfig if it needs so
 type ProcessorFunc func(exporter Exporter, config *ProcessorConfig) ProcessFunc
+
+// ProcessFunc is a function used by gbuf.RingFilter which is called on each pass through read data, as floating-point
+// audio. It is always the responsibility of the caller or user to create any processing for that data
+//
+// An error can be returned from this function, indicating to the gbuf.RingFilter that reading should stop.
 type ProcessFunc func(data []float64) error
 
 const (
@@ -44,6 +54,18 @@ var (
 	ErrEmptyExporter      = errs.New(processorDomain, ErrEmpty, ErrExporter)
 )
 
+// PCMProcessor is a Processor type that processes an io.Reader as PCM audio
+//
+// Multiple ProcessorFunc are accepted, as to shape the ProcessFunc on both what to do with the audio stream
+// and where to send it.
+//
+// An Exporter is also a part of the Processor, as one is directly linked to the other. The exported types
+// are to allow direct interaction with the Exporter interface within the PCMProcessor, as part of the logic of the
+// application.
+//
+// Finally, a ProcessorConfig can be added, which is passed into all input ProcessorFunc.
+//
+// TODO: replace Stream implementation with this type
 type PCMProcessor struct {
 	Exporter      Exporter
 	processorFunc func([]float64) error
@@ -52,6 +74,12 @@ type PCMProcessor struct {
 	err    error
 }
 
+// Process consumes the input io.Reader as a WAV buffer, while also moving the
+// processed data into the exporter.
+//
+// Process is a blocking operation but should be executed in a goroutine. If an operation fails
+// during this call, Process and the underlying audio stream read are halted, and its internal error value
+// (retrievable with Err()) is populated.
 func (p *PCMProcessor) Process(ctx context.Context, reader io.Reader) {
 	var (
 		signalCh = make(chan os.Signal, 1)
@@ -95,9 +123,15 @@ func (p *PCMProcessor) Process(ctx context.Context, reader io.Reader) {
 	}
 }
 
+// Shutdown gracefully stops the processor, returning any stored error if it exists
 func (p *PCMProcessor) Shutdown(_ context.Context) error {
 	p.cancel()
 
+	return p.err
+}
+
+// Err returns any internal error in the PCMProcessor, raised while reading the audio stream
+func (p *PCMProcessor) Err() error {
 	return p.err
 }
 
@@ -124,6 +158,7 @@ func NewPCMProcessor(
 	}, nil
 }
 
+// ProcessPeaks is a ProcessorFunc that processes the signal to extract the peak value (in intensity) in the signal
 func ProcessPeaks(exporter Exporter, _ *ProcessorConfig) ProcessFunc {
 	return func(data []float64) error {
 		var maximum float64
@@ -138,6 +173,8 @@ func ProcessPeaks(exporter Exporter, _ *ProcessorConfig) ProcessFunc {
 	}
 }
 
+// ProcessSpectrum is a ProcessorFunc that processes the signal to extract the peak frequency (in magnitude, relative to
+// the other analyzed frequencies) in the signal
 func ProcessSpectrum(exporter Exporter, config *ProcessorConfig) ProcessFunc {
 	bs := fft.NearestBlock(config.Size)
 	windowBlock := window.New(window.Blackman, int(bs))
