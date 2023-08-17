ee78da668833e91cfd6f35f9e31dd76cb63380dc
zalgonoise <mariozalgo@gmail.com>
2023-08-17 22:21:10 +0200

    audio/cmd/audio: adding a runV2 function temporarily to improve SDK's design

--- audio/cmd/audio/main.go
+++ audio/cmd/audio/main.go
@@ -4,13 +4,14 @@ import (
 	"context"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/zalgonoise/x/audio/cmd/audio/config"
 	"github.com/zalgonoise/x/audio/cmd/audio/stream"
 )
 
 func main() {
-	err, code := run()
+	err, code := runV2()
 	if err != nil {
 		slog.Error(
 			"audio/gsp: runtime error",
@@ -45,4 +46,47 @@ func run() (error, int) {
 	}
 
 	return nil, 0
+}
+
+func runV2() (error, int) {
+	cfg, err := config.WithDefaults()
+	if err != nil {
+		return err, 1
+	}
+
+	consumer, err := stream.NewHTTPConsumer(cfg.URL, cfg.Duration)
+	if err != nil {
+		return err, 1
+	}
+
+	exporter := stream.NewLogExporter(os.Stderr)
+
+	processor, err := stream.NewPCMProcessor(
+		exporter,
+		&stream.ProcessorConfig{Size: 64},
+		stream.ProcessPeaks, stream.ProcessSpectrum,
+	)
+	if err != nil {
+		return err, 1
+	}
+
+	ctx := context.Background()
+	reader, err := consumer.Consume(ctx)
+	if err != nil {
+		return err, 1
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+
+	go processor.Process(ctx, reader)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err(), 0
+		case err := <-processor.Err():
+			return err, 1
+		}
+	}
 }
