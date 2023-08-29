92e138ecbe5eae495d3b22466b2b3b9063ecf381
zalgonoise <mariozalgo@gmail.com>
2023-08-29 17:35:19 +0200

    audio/sdk/audio: Aggregator is now composed of Extractor, Registerer and Compactor types

--- audio/sdk/audio/aggregator.go
+++ audio/sdk/audio/aggregator.go
@@ -1,27 +1,75 @@
 package audio
 
 import (
+	"context"
+	"io"
 	"slices"
+	"sync"
+	"sync/atomic"
+	"time"
 
+	"github.com/zalgonoise/gbuf"
 	"github.com/zalgonoise/x/audio/fft"
 	"github.com/zalgonoise/x/audio/fft/window"
 	"github.com/zalgonoise/x/audio/wav/header"
+	"github.com/zalgonoise/x/cfg"
 )
 
-// Aggregator is a generic function type that serves as an audio processor function,
+type Aggregator interface {
+	Aggregate(h *header.Header, data []float64) error
+}
+
+type Registerer[T any] interface {
+	Register(T) error
+	Load() (T, error)
+}
+
+type Registry[T any] func(T) error
+
+func (r Registry[T]) Register(value T) error {
+	return r(value)
+}
+
+type aggregator[T any] struct {
+	extractor  Extractor[T]
+	registerer Registerer[T]
+}
+
+func (a aggregator[T]) Aggregate(h *header.Header, data []float64) error {
+	return a.registerer.Register(a.extractor.Extract(h, data))
+}
+
+func NewAggregator[T any](extractor Extractor[T], registerer Registerer[T]) Aggregator {
+	return aggregator[T]{
+		extractor:  extractor,
+		registerer: registerer,
+	}
+}
+
+type Compactor[T any] func([]T) (T, error)
+
+// Extraction is a generic function type that serves as an audio processor function,
 // but returns any type desired, as appropriate to the analysis, processing, recording, whatever it may be.
 //
-// It is of the responsibility of the Exporter to position the configured Aggregator to actually export the
+// It is of the responsibility of the Exporter to position the configured Extractor to actually export the
 // aggregations.
 //
-// The sole responsibility of an Aggregator is to convert raw audio (as chunks of float64 values) into anything
-// meaningful, that is exported / handled separately. Not all Exporter will need one or more Aggregator, however
+// The sole responsibility of an Extractor is to convert raw audio (as chunks of float64 values) into anything
+// meaningful, that is exported / handled separately. Not all Exporter will need one or more Extractor, however
 // these are supposed to be perceived as preset building blocks to work with the incoming audio chunks.
-type Aggregator[T any] func(*header.Header, []float64) T
+type Extraction[T any] func(*header.Header, []float64) T
+
+func (a Extraction[T]) Extract(h *header.Header, data []float64) T {
+	return a(h, data)
+}
+
+type Extractor[T any] interface {
+	Extract(*header.Header, []float64) T
+}
 
 // MaxPeak returns a float64 Aggregator that calculates the maximum peak value in an audio signal
-func MaxPeak() Aggregator[float64] {
-	return func(_ *header.Header, data []float64) (maximum float64) {
+func MaxPeak() Extractor[float64] {
+	return Extraction[float64](func(_ *header.Header, data []float64) (maximum float64) {
 		for i := range data {
 			if data[i] > maximum {
 				maximum = data[i]
@@ -29,29 +77,29 @@ func MaxPeak() Aggregator[float64] {
 		}
 
 		return maximum
-	}
+	})
 }
 
 // AveragePeak returns a float64 Aggregator that calculates the average peak value in an audio signal
-func AveragePeak() Aggregator[float64] {
-	return func(_ *header.Header, data []float64) (average float64) {
+func AveragePeak() Extractor[float64] {
+	return Extraction[float64](func(_ *header.Header, data []float64) (average float64) {
 		for i := range data {
 			average += data[i]
 		}
 
 		return average / float64(len(data))
-	}
+	})
 }
 
 // MaxSpectrum returns a []fft.FrequencyPower Aggregator that calculates the maximum spectrum values in an audio signal
-func MaxSpectrum(size int) Aggregator[[]fft.FrequencyPower] {
+func MaxSpectrum(size int) Extractor[[]fft.FrequencyPower] {
 	if size < 8 {
 		size = 64
 	}
 
 	sampleRate := 44100
 
-	return func(h *header.Header, data []float64) []fft.FrequencyPower {
+	return Extraction[[]fft.FrequencyPower](func(h *header.Header, data []float64) []fft.FrequencyPower {
 		if h != nil {
 			sampleRate = int(h.SampleRate)
 		}
@@ -83,5 +131,163 @@ func MaxSpectrum(size int) Aggregator[[]fft.FrequencyPower] {
 		}
 
 		return maximum
+	})
+}
+
+type BatchConfig struct {
+	flushFrequency time.Duration
+	maxBatchSize   int
+}
+
+func WithBatchSize(size int) cfg.Option[BatchConfig] {
+	return cfg.Register(func(config BatchConfig) BatchConfig {
+		config.maxBatchSize = size
+
+		return config
+	})
+}
+
+func WithFlushFrequency(dur time.Duration) cfg.Option[BatchConfig] {
+	return cfg.Register(func(config BatchConfig) BatchConfig {
+		config.flushFrequency = dur
+
+		return config
+	})
+}
+
+type unitRegistry[T any] struct {
+	value T
+	mu    *sync.Mutex
+	isSet *atomic.Bool
+}
+
+func (r *unitRegistry[T]) Register(value T) error {
+	r.mu.Lock()
+	r.value = value
+	r.isSet.Store(true)
+	r.mu.Unlock()
+
+	return nil
+}
+
+func (r *unitRegistry[T]) Load() (T, error) {
+	r.mu.Lock()
+	value := r.value
+	isSet := r.isSet.Load()
+	r.isSet.Store(false)
+	r.mu.Unlock()
+
+	if !isSet {
+		return *new(T), io.EOF
 	}
+
+	return value, nil
+}
+
+func NewRegistry[T any]() Registerer[T] {
+	return &unitRegistry[T]{
+		mu:    &sync.Mutex{},
+		isSet: &atomic.Bool{},
+	}
+}
+
+type batchRegistry[T any] struct {
+	buffer    *gbuf.RingBuffer[T]
+	reg       Registerer[T]
+	compactor Compactor[T]
+
+	config BatchConfig
+
+	errCh  chan error
+	cancel context.CancelFunc
+}
+
+func (r batchRegistry[T]) Register(value T) error {
+	return r.buffer.WriteItem(value)
+}
+
+func (r batchRegistry[T]) Load() (T, error) {
+	if r.compactor != nil {
+		return r.compactor(r.buffer.Value())
+	}
+
+	return r.buffer.ReadItem()
+}
+
+func (r batchRegistry[T]) Shutdown(_ context.Context) error {
+	r.cancel()
+
+	return nil
+}
+
+func (r batchRegistry[T]) Err() <-chan error {
+	return r.errCh
+}
+
+func (r batchRegistry[T]) flush() error {
+	value := r.buffer.Value()
+
+	if r.compactor != nil {
+		v, err := r.compactor(value)
+		if err != nil {
+			return err
+		}
+
+		return r.reg.Register(v)
+	}
+
+	for i := range value {
+		if err := r.reg.Register(value[i]); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func BatchRegistry[T any](reg Registerer[T], compactor Compactor[T], options ...cfg.Option[BatchConfig]) Registerer[T] {
+	config := cfg.New[BatchConfig[T]](options...)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	batchReg := batchRegistry[T]{
+		buffer:    gbuf.NewRingBuffer[T](config.maxBatchSize),
+		reg:       reg,
+		config:    config,
+		compactor: compactor,
+		errCh:     make(chan error),
+		cancel:    cancel,
+	}
+
+	go func() {
+		ticker := time.NewTicker(config.flushFrequency)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				if err := batchReg.flush(); err != nil {
+					batchReg.errCh <- err
+				}
+
+				return
+			case <-ticker.C:
+				if err := batchReg.flush(); err != nil {
+					batchReg.errCh <- err
+
+					return
+				}
+			default:
+				if config.maxBatchSize > 0 && batchReg.buffer.Len() > config.maxBatchSize {
+					if err := batchReg.flush(); err != nil {
+						batchReg.errCh <- err
+
+						return
+					}
+				}
+			}
+		}
+	}()
+
+	return batchReg
 }
