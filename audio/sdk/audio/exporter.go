4646432063262ae690106a7e6099653537e3ccf8
zalgonoise <mariozalgo@gmail.com>
2023-08-30 14:33:35 +0200

    audio/sdk/audio: modified Aggregator as Collector, documented types

--- audio/sdk/audio/exporter.go
+++ audio/sdk/audio/exporter.go
@@ -7,7 +7,7 @@ import "github.com/zalgonoise/x/audio/wav/header"
 // It is of the implementer's responsibility to comply with any requirements needed to export the audio,
 // which is out-of-scope for this package.
 //
-// Exporter will periodically receive data either from the Processor or its Aggregator functions. The data
+// Exporter will periodically receive data either from the Processor or its Collector functions. The data
 // is ready to be pushed to its destination by the time it is passed to the Exporter and there shouldn't be any
 // further processing.
 //
@@ -19,12 +19,12 @@ import "github.com/zalgonoise/x/audio/wav/header"
 type Exporter interface {
 	// Export consumes the audio data chunks from the Processor, preparing them to be pushed to their destination.
 	//
-	// The Exporter may have a set of Aggregator configured -- in that case it send the audio data it receives to all
-	// Aggregator. It is also the responsibility of the implementation of Exporter to properly send those aggregations
+	// The Exporter may have a set of Collector configured -- in that case it send the audio data it receives to all
+	// Collector. It is also the responsibility of the implementation of Exporter to properly send those aggregations
 	// or batches to their correct destination once they're done being collected, or on a frequent flush.
 	//
 	// The returned error from an Export call is related to an error raised when pushing the values or items to the
-	// target, or from any errors raised by the configured Aggregator types.
+	// target, or from any errors raised by the configured Collector types.
 	Export(h *header.Header, data []float64) error
 	// StreamCloser defines common methods when interacting with a streaming module, targeting actions to either flush
 	// the module or to shut it down gracefully.
