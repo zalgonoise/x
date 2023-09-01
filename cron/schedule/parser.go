a97ea4386462be0ca9e321cbfbeaa6a7828f973f
zalgonoise <mariozalgo@gmail.com>
2023-09-01 19:26:18 +0200

    cron/schedule: implemented '@' parser; improved '*' parser

--- cron/schedule/parser.go
+++ cron/schedule/parser.go
@@ -7,9 +7,7 @@ import (
 func initParse(t *parse.Tree[token, byte]) parse.ParseFn[token, byte] {
 	switch t.Peek().Type {
 	case tokenAt:
-		t.Node(t.Next())
-
-		return parseAlphanum
+		return parseAt
 	case tokenStar:
 		return parseStar
 	case tokenAlphanum:
@@ -21,15 +19,35 @@ func initParse(t *parse.Tree[token, byte]) parse.ParseFn[token, byte] {
 	}
 }
 
+func parseAt(t *parse.Tree[token, byte]) parse.ParseFn[token, byte] {
+	t.Node(t.Next())
+
+	switch t.Peek().Type {
+	case tokenAlphanum:
+		return parseAlphanum
+	default:
+		item := t.Next()
+		item.Type = tokenError
+		t.Set(t.Parent())
+
+		return initParse
+	}
+}
+
 func parseStar(t *parse.Tree[token, byte]) parse.ParseFn[token, byte] {
 	t.Node(t.Next())
-	t.Set(t.Parent())
 
 	switch t.Peek().Type {
 	case tokenSpace:
+		t.Set(t.Parent())
 		t.Next()
+
 		return initParse
+	case tokenSlash:
+		return parseAlphanum
 	default:
+		t.Set(t.Parent())
+
 		return nil
 	}
 }
