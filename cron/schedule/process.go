dc4839d3c5a12cd06df5c0aa9b42fcb3ffbe44f7
zalgonoise <mariozalgo@gmail.com>
2023-09-03 20:27:37 +0200

    cron/schedule: refactor processor to build a cronSchedule type

--- cron/schedule/process.go
+++ cron/schedule/process.go
@@ -1,95 +1,323 @@
 package schedule
 
 import (
+	"slices"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/zalgonoise/parse"
 )
 
-func process(t *parse.Tree[token, byte]) (string, error) {
-	if err := validate(t); err != nil {
-		return "", err
+func process(t *parse.Tree[token, byte]) (c cronSchedule, err error) {
+	if err = validate(t); err != nil {
+		return c, err
 	}
 
-	sb := new(strings.Builder)
 	nodes := t.List()
 
-	for i := range nodes {
-		switch nodes[i].Type {
-		case tokenStar:
-			processStar(sb, nodes[i])
-		case tokenAlphanum:
-			processAlphanum(sb, nodes[i])
-		case tokenComma:
-			sb.WriteString("and: ")
-			sb.Write(nodes[i].Value)
-			sb.WriteString(" ;")
-		case tokenDash:
-			sb.WriteString("to: ")
-			sb.Write(nodes[i].Value)
-			sb.WriteString(" ;")
-		case tokenSlash:
-			sb.WriteString("by: ")
-			sb.Write(nodes[i].Value)
-			sb.WriteString(" ;")
-		case tokenAt:
-			processException(sb, nodes[i])
-		default:
-			break
-		}
+	switch len(nodes) {
+	case 1:
+		return buildException(nodes[0], time.Local), nil
+	case 5:
+		return cronSchedule{
+			Loc:      time.Local,
+			min:      buildMinutes(nodes[0]),
+			hour:     buildHours(nodes[1]),
+			dayMonth: buildMonthDays(nodes[2]),
+			month:    buildMonths(nodes[3]),
+			dayWeek:  buildWeekdays(nodes[4]),
+		}, nil
+	default:
+		return c, ErrInvalidNumNodes
 	}
-
-	return sb.String(), nil
 }
 
-func processAlphanum(sb *strings.Builder, n *parse.Node[token, byte]) {
-	sb.WriteString("alphanum: ")
-	sb.Write(n.Value)
-
-	for i := 0; i < len(n.Edges); i++ {
-		switch n.Edges[i].Type {
-		case tokenComma:
-			sb.WriteString(" and ")
-		case tokenDash:
-			sb.WriteString(" to ")
-		case tokenSlash:
-			sb.WriteString(" by ")
-		}
-
-		if len(n.Edges[i].Edges) > 0 {
-			sb.Write(n.Edges[i].Edges[0].Value)
-		}
+func buildMinutes(node *parse.Node[token, byte]) resolver {
+	switch node.Type {
+	case tokenStar:
+		return processStar(node, 0, 59)
+	case tokenAlphanum:
+		return processAlphanum(node, 0, 59, nil)
+	default:
+		return everytime{}
 	}
-
-	sb.WriteString(" ; ")
 }
 
-func processStar(sb *strings.Builder, n *parse.Node[token, byte]) {
-	sb.WriteString("everything: ")
-	sb.Write(n.Value)
+func buildHours(node *parse.Node[token, byte]) resolver {
+	switch node.Type {
+	case tokenStar:
+		return processStar(node, 0, 23)
+	case tokenAlphanum:
+		return processAlphanum(node, 0, 23, nil)
+	default:
+		return everytime{}
+	}
+}
 
-	for i := 0; i < len(n.Edges); i++ {
-		if n.Edges[i].Type == tokenSlash {
-			sb.WriteString(" by ")
-			if len(n.Edges[i].Edges) > 0 {
-				sb.Write(n.Edges[i].Edges[0].Value)
+func buildMonthDays(node *parse.Node[token, byte]) resolver {
+	switch node.Type {
+	case tokenStar:
+		return processStar(node, 1, 31)
+	case tokenAlphanum:
+		return processAlphanum(node, 1, 31, nil)
+	default:
+		return everytime{}
+	}
+}
+
+func buildMonths(node *parse.Node[token, byte]) resolver {
+	switch node.Type {
+	case tokenStar:
+		return processStar(node, 1, 12)
+	case tokenAlphanum:
+		return processAlphanum(node, 1, 12, monthsList)
+	default:
+		return everytime{}
+	}
+}
+
+func buildWeekdays(node *parse.Node[token, byte]) resolver {
+	switch node.Type {
+	case tokenStar:
+		return processStar(node, 0, 7)
+	case tokenAlphanum:
+		return processAlphanum(node, 0, 7, weekdaysList)
+	default:
+		return everytime{}
+	}
+}
+
+func defaultSchedule(loc *time.Location) cronSchedule {
+	return cronSchedule{
+		Loc: loc,
+		min: fixedSchedule{
+			maximum: 59,
+			at:      0,
+		},
+		hour:     everytime{},
+		dayMonth: everytime{},
+		month:    everytime{},
+		dayWeek:  everytime{},
+	}
+}
+
+func buildException(node *parse.Node[token, byte], loc *time.Location) cronSchedule {
+	if node.Type != tokenAt {
+		return defaultSchedule(loc)
+	}
+
+	if value, ok := getValueFromSymbol(node.Edges[0], 0, 6, exceptionsList); ok {
+		switch value {
+		// TODO: implement reboot (case 0:)
+		case 1:
+			return defaultSchedule(loc)
+		case 2:
+			return cronSchedule{
+				Loc: loc,
+				min: fixedSchedule{
+					maximum: 59,
+					at:      0,
+				},
+				hour: fixedSchedule{
+					maximum: 23,
+					at:      0,
+				},
+				dayMonth: everytime{},
+				month:    everytime{},
+				dayWeek:  everytime{},
+			}
+		case 3:
+			return cronSchedule{
+				Loc: loc,
+				min: fixedSchedule{
+					maximum: 59,
+					at:      0,
+				},
+				hour: fixedSchedule{
+					maximum: 23,
+					at:      0,
+				},
+				dayMonth: everytime{},
+				month:    everytime{},
+				dayWeek: fixedSchedule{
+					maximum: 6,
+					at:      0,
+				},
+			}
+		case 4:
+			return cronSchedule{
+				Loc: loc,
+				min: fixedSchedule{
+					maximum: 59,
+					at:      0,
+				},
+				hour: fixedSchedule{
+					maximum: 23,
+					at:      0,
+				},
+				dayMonth: fixedSchedule{
+					maximum: 31,
+					at:      1,
+				},
+				month:   everytime{},
+				dayWeek: everytime{},
+			}
+		case 5, 6:
+			return cronSchedule{
+				Loc: loc,
+				min: fixedSchedule{
+					maximum: 59,
+					at:      0,
+				},
+				hour: fixedSchedule{
+					maximum: 23,
+					at:      0,
+				},
+				dayMonth: fixedSchedule{
+					maximum: 31,
+					at:      1,
+				},
+				month: fixedSchedule{
+					maximum: 12,
+					at:      1,
+				},
+				dayWeek: everytime{},
 			}
 		}
 	}
 
-	sb.WriteString(" ; ")
+	return defaultSchedule(loc)
 }
 
-func processException(sb *strings.Builder, n *parse.Node[token, byte]) {
-	sb.WriteString("exception: ")
-	sb.Write(n.Value)
+func getValueFromSymbol(
+	symbol *parse.Node[token, byte], minimum, maximum int, valueList []string,
+) (int, bool) {
+	if len(symbol.Edges) == 1 {
+		value := symbol.Edges[0].Value
 
-	for i := 0; i < len(n.Edges); i++ {
-		if n.Edges[i].Type == tokenAlphanum {
-			sb.WriteString(" freq: ")
-			sb.Write(n.Edges[i].Value)
+		// try to use the value as a number
+		if len(value) > 0 && value[0] > '0' && value[0] < '9' {
+			num, err := strconv.Atoi(string(value))
+			if err == nil {
+				return num, true
+			}
+
+			return -1, false
+		}
+
+		// fallback to using it as a string
+		v := strings.ToUpper(string(value))
+		num := -1
+
+		for idx := range valueList {
+			if v == valueList[idx] {
+				num = idx
+			}
+		}
+
+		if num > -1 && num >= minimum && num <= maximum {
+			return num, true
 		}
 	}
 
-	sb.WriteString(" ; ")
+	return -1, false
+}
+
+func processAlphanum(n *parse.Node[token, byte], minimum, maximum int, valueList []string) resolver {
+	atValue, err := strconv.Atoi(string(n.Value))
+	if err != nil {
+		return everytime{}
+	}
+
+	if atValue < minimum {
+		atValue = minimum
+	}
+
+	switch len(n.Edges) {
+	case 0:
+		return fixedSchedule{
+			maximum: maximum,
+			at:      atValue,
+		}
+	default:
+		stepValues := make([]int, 0, len(n.Edges)*2)
+		every := -1
+		rangeEnd := -1
+
+		for i := range n.Edges {
+			switch n.Edges[i].Type {
+			case tokenComma:
+				if value, ok := getValueFromSymbol(n.Edges[i], minimum, maximum, valueList); ok {
+					stepValues = append(stepValues, value)
+				}
+
+			case tokenDash:
+				if value, ok := getValueFromSymbol(n.Edges[i], minimum, maximum, valueList); ok {
+					rangeEnd = value
+				}
+
+			case tokenSlash:
+				if value, ok := getValueFromSymbol(n.Edges[i], minimum, maximum, valueList); ok {
+					every = value
+				}
+			}
+		}
+
+		// handle step values only
+		if every == -1 && rangeEnd == -1 && len(stepValues) > 0 {
+			return stepSchedule{
+				maximum: maximum,
+				steps:   stepValues,
+			}
+		}
+
+		// handle range only
+		if every == -1 && rangeEnd > 0 && len(stepValues) == 0 {
+			return rangeSchedule{
+				maximum: maximum,
+				from:    atValue,
+				to:      rangeEnd,
+			}
+		}
+
+		// set frequency if unset
+		if every < 0 {
+			every = 1
+		}
+
+		// set end if unset
+		if rangeEnd < 0 {
+			rangeEnd = maximum
+		}
+
+		stepValues = append(stepValues, newValueRange(atValue, rangeEnd, every)...)
+
+		// sort and remove duplicates
+		slices.Sort(stepValues)
+		slices.Compact(stepValues)
+
+		return stepSchedule{
+			maximum: maximum,
+			steps:   stepValues,
+		}
+	}
+}
+
+func processStar(n *parse.Node[token, byte], minimum, maximum int) resolver {
+	switch len(n.Edges) {
+	case 1:
+		if n.Edges[0].Type == tokenSlash && len(n.Edges[0].Edges) == 1 {
+			stepValue, err := strconv.Atoi(string(n.Edges[0].Edges[0].Value))
+			if err != nil {
+				return everytime{}
+			}
+
+			return newStepSchedule(minimum, maximum, maximum, stepValue)
+		}
+
+		fallthrough
+	default:
+		return everytime{}
+	}
 }
