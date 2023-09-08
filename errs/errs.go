07f2d3b5851d9390be12481c432aa148a76ec6ec
zalgonoise <mariozalgo@gmail.com>
2023-09-08 02:48:56 +0200

    errs: improved implementation; added tests

--- errs/errs.go
+++ errs/errs.go
@@ -6,6 +6,13 @@ import (
 	"strings"
 )
 
+const (
+	defaultFormat           = "%s %s"
+	wrappedFormat           = "%s %s: %s"
+	defaultFormatWithDomain = "%s: %s %s"
+	wrappedFormatWithDomain = "%s: %s %s: %s"
+)
+
 type Domain string
 
 func (e Domain) Error() string { return (string)(e) }
@@ -19,40 +26,150 @@ type Entity string
 func (e Entity) Error() string { return (string)(e) }
 
 type Error struct {
-	domain Domain
 	kind   Kind
 	entity Entity
-	error  string
+
+	format string
+	inner  error
 }
 
 func (e Error) Error() string {
-	return e.error
+	format := e.format
+
+	if e.inner == nil {
+		if format == "" {
+			format = defaultFormat
+		}
+
+		return fmt.Sprintf(format, e.kind, e.entity)
+	}
+
+	if format == "" {
+		format = wrappedFormat
+	}
+
+	return fmt.Sprintf(format, e.kind, e.entity, e.inner.Error())
 }
 
 func (e Error) Unwrap() error {
-	return errors.Join(e.domain, e.kind, e.entity)
+	return e.inner
+}
+
+type ErrorWithDomain struct {
+	err Error
+
+	domain Domain
+}
+
+func (e ErrorWithDomain) Error() string {
+	format := e.err.format
+
+	if e.err.inner == nil {
+		if format == "" {
+			format = defaultFormatWithDomain
+		}
+
+		return fmt.Sprintf(format, e.domain, e.err.kind, e.err.entity)
+	}
+
+	if format == "" {
+		format = wrappedFormatWithDomain
+	}
+
+	return fmt.Sprintf(format, e.domain, e.err.kind, e.err.entity, e.err.inner.Error())
+}
+
+func (e ErrorWithDomain) Unwrap() error {
+	return e.err.inner
 }
 
 func New(domain Domain, kind Kind, entity Entity, args ...any) error {
-	sb := new(strings.Builder)
-
-	if domain != "" {
-		sb.WriteString((string)(domain))
-		sb.WriteString(": ")
+	switch {
+	case kind == "" && entity == "":
+		return nil
+	case kind == "" && entity != "":
+		return entity
+	case kind != "" && entity == "":
+		return kind
 	}
 
-	sb.WriteString((string)(kind))
-	sb.WriteByte(' ')
-	sb.WriteString((string)(entity))
-
-	if len(args) > 0 {
-		_, _ = fmt.Fprint(sb, args)
-	}
-
-	return Error{
-		domain: domain,
+	return WithDomain(domain, Error{
 		kind:   kind,
 		entity: entity,
-		error:  sb.String(),
+		inner:  joinArgs(args...),
+	})
+}
+
+func Format(domain Domain, kind Kind, entity Entity, format string, args ...any) error {
+	err := New(domain, kind, entity, args...)
+
+	if format == "" {
+		return err
 	}
+
+	switch e := err.(type) {
+	case Error:
+		e.format = format
+
+		return e
+	case ErrorWithDomain:
+		e.err.format = format
+
+		return e
+	default:
+		return err
+	}
+}
+
+func WithDomain(domain Domain, err Error) error {
+	if domain == "" {
+		return err
+	}
+
+	return ErrorWithDomain{
+		err:    err,
+		domain: domain,
+	}
+}
+
+func joinArgs(args ...any) error {
+	if len(args) == 0 {
+		return nil
+	}
+
+	format := &strings.Builder{}
+	errs := make([]any, 0, len(args))
+
+	for i := range args {
+		switch v := args[i].(type) {
+		case nil:
+			continue
+
+		case ErrorWithDomain:
+			format.WriteString("%w")
+			if i < len(args)-1 {
+				format.WriteString(": ")
+			}
+
+			errs = append(errs, errors.New(fmt.Sprintf(defaultFormatWithDomain, v.domain, v.err.kind, v.err.entity)))
+
+		case error:
+			format.WriteString("%w")
+			if i < len(args)-1 {
+				format.WriteString(": ")
+			}
+
+			errs = append(errs, v)
+
+		default:
+			format.WriteString("%w")
+			if i < len(args)-1 {
+				format.WriteString(": ")
+			}
+
+			errs = append(errs, errors.New(fmt.Sprint(v)))
+		}
+	}
+
+	return fmt.Errorf(format.String(), errs...)
 }
