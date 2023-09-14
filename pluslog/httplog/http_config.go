1ce8ff85cb74b0d82e28144c90a3897d29fce232
Zalgo <mariozalgo@gmail.com>
2023-09-14 22:14:50 +0200

    pluslog/httplog: refactored HTTPLogger's encoder and options

---
+++ pluslog/httplog/http_config.go
@@ -0,0 +1,48 @@
+package httplog
+
+import (
+	"log/slog"
+	"time"
+
+	"github.com/zalgonoise/x/cfg"
+)
+
+type Config struct {
+	encoder Encoder
+	level   slog.Leveler
+	timeout time.Duration
+}
+
+func WithEncoder(enc Encoder) cfg.Option[Config] {
+	return cfg.Register(func(config Config) Config {
+		config.encoder = enc
+
+		return config
+	})
+}
+
+func JSON(indent bool) cfg.Option[Config] {
+	return cfg.Register(func(config Config) Config {
+		config.encoder = MarshalJSON{
+			indent: indent,
+		}
+
+		return config
+	})
+}
+
+func WithLevel(level slog.Leveler) cfg.Option[Config] {
+	return cfg.Register(func(config Config) Config {
+		config.level = level
+
+		return config
+	})
+}
+
+func WithTimeout(dur time.Duration) cfg.Option[Config] {
+	return cfg.Register(func(config Config) Config {
+		config.timeout = dur
+
+		return config
+	})
+}
