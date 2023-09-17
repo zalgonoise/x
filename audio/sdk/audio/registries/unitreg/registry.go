591b2489fd1decdaa22daf4c7bab98b90c24cf07
zalgonoise <mariozalgo@gmail.com>
2023-09-17 20:52:57 +0200

    audio/sdk/audio/registries/unitreg: added unit Registry logic

---
+++ audio/sdk/audio/registries/unitreg/registry.go
@@ -0,0 +1,31 @@
+package unitreg
+
+import (
+	"context"
+
+	"github.com/zalgonoise/x/audio/sdk/audio"
+)
+
+type unitRegistry[T any] struct {
+	ch chan T
+}
+
+func (r *unitRegistry[T]) Register(value T) error {
+	r.ch <- value
+
+	return nil
+}
+
+func (r *unitRegistry[T]) Load() <-chan T {
+	return r.ch
+}
+
+func (r *unitRegistry[T]) Shutdown(context.Context) error {
+	close(r.ch)
+
+	return nil
+}
+
+func New[T any]() audio.Registerer[T] {
+	return &unitRegistry[T]{}
+}
