3cc28f42973f50bcb491238de48553f131297db6
zalgonoise <mariozalgo@gmail.com>
2023-09-19 21:22:23 +0200

    audio/sdk/audio: add NoOpRegisterer

--- audio/sdk/audio/registerer.go
+++ audio/sdk/audio/registerer.go
@@ -11,3 +11,18 @@ type Registerer[T any] interface {
 type Loader[T any] interface {
 	Load() <-chan T
 }
+
+type noOpRegisterer[T any] struct{}
+
+func (noOpRegisterer[T]) Register(T) error { return nil }
+func (noOpRegisterer[T]) Load() <-chan T   { return nil }
+
+// NoOpLoader returns a no-op Registerer, scoped as a Loader interface
+func NoOpLoader[T any]() Loader[T] {
+	return noOpRegisterer[T]{}
+}
+
+// NoOpRegisterer returns a no-op Registerer
+func NoOpRegisterer[T any]() Registerer[T] {
+	return noOpRegisterer[T]{}
+}
