f55105ac60789b9207106f8c855268c630adfcee
zalgonoise <mariozalgo@gmail.com>
2023-09-21 21:03:25 +0200

    audio/sdk/audio: documented consumer.go

--- audio/sdk/audio/consumer.go
+++ audio/sdk/audio/consumer.go
@@ -42,8 +42,15 @@ type Consumer interface {
 
 type noOpConsumer struct{}
 
+// Consume implements the Consumer interface.
+//
+// This is a no-op call and the returned values are both always nil.
 func (noOpConsumer) Consume(context.Context) (reader io.Reader, err error) { return nil, nil }
-func (noOpConsumer) Shutdown(context.Context) error                        { return nil }
+
+// Shutdown implements the Consumer and Closer interfaces.
+//
+// This is a no-op call and the returned error is always nil.
+func (noOpConsumer) Shutdown(context.Context) error { return nil }
 
 // NoOpConsumer returns a no-op Consumer
 func NoOpConsumer() Consumer {
