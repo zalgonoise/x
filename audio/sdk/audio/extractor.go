0df537a08c5c48517d4915da50a6282b9ae9c223
zalgonoise <mariozalgo@gmail.com>
2023-09-21 21:04:06 +0200

    audio/sdk/audio: documented extractor.go

--- audio/sdk/audio/extractor.go
+++ audio/sdk/audio/extractor.go
@@ -6,7 +6,19 @@ import (
 
 // Extractor is a generic interface for a type that implements the Extract method, which can return a value from
 // parsing an audio chunk.
+//
+// It should be perceived as the map process in a Map-Reduce strategy.
+//
+// Its Extract method is intended to be executed multiple times on each audio chunk received from a stream, and it is
+// configured with a Collector in mind.
 type Extractor[T any] interface {
+	// Extract will analyze the audio chunk (as a slice of float64 values), optionally referring to the audio header's
+	// metadata, to extract a value of a given type.
+	//
+	// It should be perceived as the map process in a Map-Reduce strategy.
+	//
+	// Extract method is intended to be executed multiple times on each audio chunk received from a stream, and it is
+	// configured with a Collector in mind.
 	Extract(*header.Header, []float64) T
 }
 
@@ -21,6 +33,9 @@ type Extractor[T any] interface {
 // these are supposed to be perceived as preset building blocks to work with the incoming audio chunks.
 type Extraction[T any] func(*header.Header, []float64) T
 
+// Extract implements the Extractor interface.
+//
+// It will call itself as a function, using the input parameters as its arguments.
 func (e Extraction[T]) Extract(h *header.Header, data []float64) T {
 	return e(h, data)
 }
