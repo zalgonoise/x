2c3ee7283ec9b958080a9d2ca8f51f203ee11e54
zalgonoise <mariozalgo@gmail.com>
2023-09-22 19:23:47 +0200

    audio/sdk/audio: documented registry.go; included Closer interface in Registry interface

--- audio/sdk/audio/registry.go
+++ audio/sdk/audio/registry.go
@@ -1,11 +1,21 @@
 package audio
 
+import "context"
+
 // Registry is a generic type that registers and loads values on a specific type context.
 //
 // Registries are responsible for handling aggregations and compacting values into one, when Load is called
 type Registry[T any] interface {
+	// Register stores the input data in the Registry, returning an error if raised
 	Register(T) error
+	// Loader implements the Load method, which returns a receive-only channel of items of a given type, usually as
+	// part of a Registry features.
+	//
+	// It is the responsibility of the Loader or the Registry to feed items into the returned channel for consumers to
+	// use accordingly, according to the Loader or Registry strategy implemented by the type.
 	Loader[T]
+	// Closer implements the Shutdown method, allowing a Registry to gracefully shutdown.
+	Closer
 }
 
 // Loader is a generic type that only emits items of a given type from a channel. This channel is accessible via
@@ -29,24 +39,29 @@ type Loader[T any] interface {
 // A Compactor is an optional, configurable component within a Registry, if applicable
 type Compactor[T any] func([]T) (T, error)
 
-type noOpRegisterer[T any] struct{}
+type noOpRegistry[T any] struct{}
 
 // Register implements the Registry interface.
 //
 // This is a no-op call and the returned error is always nil.
-func (noOpRegisterer[T]) Register(T) error { return nil }
+func (noOpRegistry[T]) Register(T) error { return nil }
 
 // Load implements the Registry interface.
 //
 // This is a no-op call and the returned channel is always nil.
-func (noOpRegisterer[T]) Load() <-chan T { return nil }
+func (noOpRegistry[T]) Load() <-chan T { return nil }
+
+// Shutdown implements the Registry and Closer interfaces.
+//
+// This is a no-op call and the returned error is always nil.
+func (noOpRegistry[T]) Shutdown(context.Context) error { return nil }
 
 // NoOpLoader returns a no-op Registry, scoped as a Loader interface
 func NoOpLoader[T any]() Loader[T] {
-	return noOpRegisterer[T]{}
+	return noOpRegistry[T]{}
 }
 
 // NoOpRegisterer returns a no-op Registry
 func NoOpRegisterer[T any]() Registry[T] {
-	return noOpRegisterer[T]{}
+	return noOpRegistry[T]{}
 }
