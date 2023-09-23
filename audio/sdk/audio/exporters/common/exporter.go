e1d50e51cb2d1bca12a52980071074decb8931ce
zalgonoise <mariozalgo@gmail.com>
2023-09-23 21:04:26 +0200

    audio/sdk/audio/exporters/common: added a common exporter solution to benchmark later on, using DI to emit the captured values

---
+++ audio/sdk/audio/exporters/common/exporter.go
@@ -0,0 +1,151 @@
+package common
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+
+	"github.com/zalgonoise/x/audio/errs"
+	"github.com/zalgonoise/x/audio/fft"
+	"github.com/zalgonoise/x/audio/sdk/audio"
+	"github.com/zalgonoise/x/audio/sdk/audio/extractors"
+	"github.com/zalgonoise/x/audio/sdk/audio/registries/batchreg"
+	"github.com/zalgonoise/x/audio/sdk/audio/registries/unitreg"
+	"github.com/zalgonoise/x/audio/wav/header"
+	"github.com/zalgonoise/x/cfg"
+)
+
+const (
+	errDomain = errs.Domain("x/audio/sdk/audio/exporters/common")
+
+	ErrNil = errs.Kind("nil")
+
+	ErrEmitter = errs.Entity("emitter")
+)
+
+var ErrNilEmitter = errs.New(errDomain, ErrNil, ErrEmitter)
+
+type Emitter interface {
+	EmitPeaks(float64)
+	EmitSpectrum([]fft.FrequencyPower)
+}
+
+type commonExporter struct {
+	config Config
+
+	peaks    audio.Collector[float64]
+	spectrum audio.Collector[[]fft.FrequencyPower]
+
+	emitter Emitter
+
+	logger *slog.Logger
+
+	cancel context.CancelFunc
+}
+
+func (e commonExporter) Export(h *header.Header, data []float64) error {
+	return errors.Join(
+		e.peaks.Collect(h, data),
+		e.spectrum.Collect(h, data),
+	)
+}
+
+func (e commonExporter) ForceFlush() error {
+	return errors.Join(
+		e.peaks.ForceFlush(),
+		e.spectrum.ForceFlush(),
+	)
+}
+
+func (e commonExporter) Shutdown(ctx context.Context) error {
+	e.logger.InfoContext(ctx, "log exporter shutting down")
+	e.cancel()
+
+	return errors.Join(
+		e.peaks.Shutdown(ctx),
+		e.spectrum.Shutdown(ctx),
+	)
+}
+
+func (e commonExporter) export(ctx context.Context) {
+	peaksValues := e.peaks.Load()
+	spectrumValues := e.spectrum.Load()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case v, ok := <-peaksValues:
+			if !ok {
+				return
+			}
+
+			e.emitter.EmitPeaks(v)
+		case v, ok := <-spectrumValues:
+			if !ok {
+				return
+			}
+
+			e.emitter.EmitSpectrum(v)
+		}
+	}
+}
+
+func ToEmitter(emitter Emitter, options ...cfg.Option[Config]) (audio.Exporter, error) {
+	if emitter == nil {
+		return audio.NoOpExporter(), ErrNilEmitter
+	}
+
+	config := cfg.Set[Config](defaultConfig, options...)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	exporter := commonExporter{
+		config:   config,
+		peaks:    newPeaksCollector(config),
+		spectrum: newSpectrumCollector(config),
+		emitter:  emitter,
+		logger:   slog.New(config.logHandler),
+		cancel:   cancel,
+	}
+
+	go exporter.export(ctx)
+
+	return exporter, nil
+}
+
+func newPeaksCollector(config Config) audio.Collector[float64] {
+	if !config.withPeaks {
+		return audio.NoOpCollector[float64]()
+	}
+
+	if !config.batchedPeaks {
+		return audio.NewCollector[float64](
+			extractors.MaxPeak(),
+			unitreg.New[float64](0),
+		)
+	}
+
+	return audio.NewCollector[float64](
+		extractors.MaxPeak(),
+		batchreg.New[float64](config.batchedPeaksOptions...),
+	)
+}
+
+func newSpectrumCollector(config Config) audio.Collector[[]fft.FrequencyPower] {
+	if !config.withSpectrum {
+		return audio.NoOpCollector[[]fft.FrequencyPower]()
+	}
+
+	if !config.batchedPeaks {
+		return audio.NewCollector[[]fft.FrequencyPower](
+			extractors.MaxSpectrum(config.spectrumBlockSize),
+			unitreg.New[[]fft.FrequencyPower](0),
+		)
+	}
+
+	return audio.NewCollector[[]fft.FrequencyPower](
+		extractors.MaxSpectrum(config.spectrumBlockSize),
+		batchreg.New[[]fft.FrequencyPower](config.batchedSpectrumOptions...),
+	)
+}
