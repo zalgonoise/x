bb3872602d8a0e86b990da21c3ab23c5998ca49c
zalgonoise <mariozalgo@gmail.com>
2023-09-23 15:38:08 +0200

    audio/sdk/audio/compactors: documented compactors. Added AbsMax compactor and Number type constraint

--- audio/sdk/audio/compactors/common.go
+++ audio/sdk/audio/compactors/common.go
@@ -20,6 +20,16 @@ var (
 	ErrEmptyValueSet = errs.New(errDomain, ErrEmpty, ErrValueSet)
 )
 
+type Number interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~float32 | ~float64
+}
+
+// Max finds the biggest (ordered) value in a slice of a given type, with a bigger-than approach,
+// meaning that it will work for positive integer and float values.
+//
+// To include negative values, an AbsMax approach would be required.
 func Max[T cmp.Ordered](values []T) (T, error) {
 	if len(values) == 0 {
 		return *new(T), ErrEmptyValueSet
@@ -36,6 +46,49 @@ func Max[T cmp.Ordered](values []T) (T, error) {
 	return maximum, nil
 }
 
+// AbsMax finds the absolute maximum Number value in the input slice.
+//
+// Using and absolute-approach, the function will return the biggest value in the slice, regardless if
+// it is positive or negative. For example, calling AbsMax on the following slice would return the
+// negative value of -1.9:  []float64{0.1, 0.9, -0.3, -1.9}.
+//
+// AbsMax may be slower than Max, and also it will not be compatible with its support for uintptr and string types.
+func AbsMax[T Number](values []T) (T, error) {
+	if len(values) == 0 {
+		return *new(T), ErrEmptyValueSet
+	}
+
+	var zero T
+	idx := 0
+	maximum := values[0]
+
+	for i := 0; i < len(values); i++ {
+		if values[i] >= zero {
+			if values[i] > maximum {
+				maximum = values[i]
+				idx = i
+			}
+
+			continue
+		}
+
+		absValue := -values[i]
+		if absValue > maximum {
+			maximum = absValue
+			idx = i
+		}
+	}
+
+	return values[idx], nil
+}
+
+// MaxSpectra reduces a matrix of frequencies (several registries of sets of frequencies), into a single (ordered) set.
+//
+// Its strategy involves sorting each set in the matrix to present the strongest magnitude frequencies as the first
+// element, and collects these into a new slice (of the same capacity as the matrix).
+//
+// Finally, it sorts the final slice once again, putting the strongest magnitude frequencies at the beginning of the
+// slice, so consumers can consume it up straight away.
 func MaxSpectra(data [][]fft.FrequencyPower) ([]fft.FrequencyPower, error) {
 	if len(data) == 0 {
 		return nil, nil
