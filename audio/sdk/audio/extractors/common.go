f8bf8c7af363496dec072bc06f3f73a421d3c0db
zalgonoise <mariozalgo@gmail.com>
2023-09-23 21:03:25 +0200

    audio/sdk/audio/extractors: simplified MaxSpectrum extractor. Added Spectrum extractor (with injectable Compactor)

--- audio/sdk/audio/extractors/common.go
+++ audio/sdk/audio/extractors/common.go
@@ -1,6 +1,7 @@
 package extractors
 
 import (
+	"cmp"
 	"slices"
 
 	"github.com/zalgonoise/x/audio/fft"
@@ -33,7 +34,7 @@ func AveragePeak() audio.Extractor[float64] {
 	})
 }
 
-// MaxSpectrum returns a []fft.FrequencyPower Collector that calculates the maximum spectrum values in an audio signal
+// MaxSpectrum returns a []fft.FrequencyPower Collector that calculates the spectrum values in an audio signal
 func MaxSpectrum(size int) audio.Extractor[[]fft.FrequencyPower] {
 	if size < 8 {
 		size = 64
@@ -59,19 +60,54 @@ func MaxSpectrum(size int) audio.Extractor[[]fft.FrequencyPower] {
 			)
 
 			slices.SortFunc(spectrum, func(a, b fft.FrequencyPower) int {
-				switch {
-				case a.Mag > b.Mag:
-					return -1
-				case a.Mag < b.Mag:
-					return 1
-				default:
-					return 0
-				}
+				return cmp.Compare(b.Mag, a.Mag)
 			})
 
 			maximum = append(maximum, spectrum[0])
 		}
 
 		return maximum
+	})
+}
+
+// Spectrum returns a []fft.FrequencyPower Collector that calculates the full spectrum values in an audio signal
+// with a given Compactor as reducer / filter
+func Spectrum(size int, compactor audio.Compactor[[]fft.FrequencyPower]) audio.Extractor[[]fft.FrequencyPower] {
+	if size < 8 {
+		size = 64
+	}
+
+	sampleRate := 44100
+
+	return audio.Extraction[[]fft.FrequencyPower](func(h *header.Header, data []float64) []fft.FrequencyPower {
+		if h != nil {
+			sampleRate = int(h.SampleRate)
+		}
+
+		bs := fft.NearestBlock(size)
+		windowBlock := window.New(window.Blackman, int(bs))
+
+		spectra := make([][]fft.FrequencyPower, 0, len(data)/int(bs))
+
+		for i := 0; i+int(bs) < len(data); i += int(bs) {
+			spectrum := fft.Apply(
+				sampleRate,
+				data[i:i+int(bs)],
+				windowBlock,
+			)
+
+			if len(spectrum) == 0 {
+				continue
+			}
+
+			spectra = append(spectra, spectrum)
+		}
+
+		compact, err := compactor(spectra)
+		if err != nil {
+			return spectra[0]
+		}
+
+		return compact
 	})
 }
