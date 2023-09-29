953f3726e4e92345196c5c605266c31fe009debe
zalgonoise <mariozalgo@gmail.com>
2023-09-29 19:30:43 +0200

    audio/sdk/audio/processors: updated PCM implmentation to use a base processor

--- audio/sdk/audio/processors/pcm.go
+++ audio/sdk/audio/processors/pcm.go
@@ -1,76 +1,11 @@
 package processors
 
 import (
-	"context"
-	"fmt"
-	"io"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/zalgonoise/x/audio/errs"
 	"github.com/zalgonoise/x/audio/sdk/audio"
 	"github.com/zalgonoise/x/audio/sdk/audio/exporters"
 	"github.com/zalgonoise/x/audio/wav"
 	"github.com/zalgonoise/x/audio/wav/header"
 )
-
-const (
-	errDomain = errs.Domain("x/audio/sdk/processors/pcm")
-
-	ErrHalt = errs.Kind("process stopped")
-
-	ErrSignal = errs.Entity("with OS signal")
-)
-
-var ErrHaltSignal = errs.New(errDomain, ErrHalt, ErrSignal)
-
-type pcm struct {
-	exporter audio.Exporter
-
-	errCh  chan error
-	cancel context.CancelFunc
-	stream *wav.Stream
-}
-
-func (p *pcm) Process(ctx context.Context, reader io.Reader) {
-	signalCh := make(chan os.Signal, 1)
-	signal.Notify(signalCh, os.Interrupt, os.Kill, syscall.SIGTERM)
-	defer close(signalCh)
-
-	ctx, p.cancel = context.WithCancel(ctx)
-
-	go p.stream.Stream(ctx, reader, p.errCh)
-
-	for {
-		select {
-		case <-ctx.Done():
-			p.cancel()
-
-			return
-		case sig := <-signalCh:
-			p.errCh <- fmt.Errorf("%w: %s", ErrHaltSignal, sig.String())
-			p.cancel()
-
-			return
-		}
-	}
-}
-
-func (p *pcm) Err() <-chan error {
-	return p.errCh
-}
-
-func (p *pcm) ForceFlush() error {
-	return p.exporter.ForceFlush()
-}
-
-func (p *pcm) Shutdown(ctx context.Context) error {
-	defer close(p.errCh)
-	p.cancel()
-
-	return p.exporter.Shutdown(ctx)
-}
 
 func PCM(e ...audio.Exporter) audio.Processor {
 	if len(e) == 0 {
@@ -79,11 +14,10 @@ func PCM(e ...audio.Exporter) audio.Processor {
 
 	exporter := exporters.Multi(e...)
 
-	return &pcm{
-		exporter: exporter,
-		errCh:    make(chan error),
-		stream: wav.NewStream(nil, func(h *header.Header, data []float64) error {
+	return NewProcessor(
+		wav.NewStream(nil, func(h *header.Header, data []float64) error {
 			return exporter.Export(h, data)
 		}),
-	}
+		exporter,
+	)
 }
