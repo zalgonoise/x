f000e59445c8d6f51fee2dba16c43407e1811676
zalgonoise <mariozalgo@gmail.com>
2023-09-30 16:44:54 +0200

    audio/sdk/audio/exporters: moving exporter base implementation to sdk/audio package

--- audio/sdk/audio/exporters/exporter.go
+++ audio/sdk/audio/exporters/exporter.go
@@ -1,111 +1,25 @@
 package exporters
 
 import (
-	"context"
-	"errors"
-	"log/slog"
-
-	"github.com/zalgonoise/x/audio/errs"
 	"github.com/zalgonoise/x/audio/fft"
 	"github.com/zalgonoise/x/audio/sdk/audio"
 	"github.com/zalgonoise/x/audio/sdk/audio/extractors"
 	"github.com/zalgonoise/x/audio/sdk/audio/registries/batchreg"
 	"github.com/zalgonoise/x/audio/sdk/audio/registries/unitreg"
-	"github.com/zalgonoise/x/audio/wav/header"
 	"github.com/zalgonoise/x/cfg"
 )
 
-const (
-	errDomain = errs.Domain("x/audio/sdk/audio/exporters")
-
-	ErrNil = errs.Kind("nil")
-
-	ErrEmitter = errs.Entity("emitter")
-)
-
-var ErrNilEmitter = errs.New(errDomain, ErrNil, ErrEmitter)
-
-type exporter struct {
-	config Config
-
-	peaks    audio.Collector[float64]
-	spectrum audio.Collector[[]fft.FrequencyPower]
-
-	emitter audio.Emitter
-
-	logger *slog.Logger
-
-	cancel context.CancelFunc
-}
-
-func (e exporter) Export(h audio.Header, data []float64) error {
-	return errors.Join(
-		e.peaks.Collect(h, data),
-		e.spectrum.Collect(h, data),
-	)
-}
-
-func (e exporter) ForceFlush() error {
-	return errors.Join(
-		e.peaks.ForceFlush(),
-		e.spectrum.ForceFlush(),
-	)
-}
-
-func (e exporter) Shutdown(ctx context.Context) error {
-	e.logger.InfoContext(ctx, "exporter shutting down")
-	e.cancel()
-
-	return errors.Join(
-		e.peaks.Shutdown(ctx),
-		e.spectrum.Shutdown(ctx),
-		e.emitter.Shutdown(ctx),
-	)
-}
-
-func (e exporter) export(ctx context.Context) {
-	peaksValues := e.peaks.Load()
-	spectrumValues := e.spectrum.Load()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case v, ok := <-peaksValues:
-			if !ok {
-				return
-			}
-
-			e.emitter.EmitPeaks(v)
-		case v, ok := <-spectrumValues:
-			if !ok {
-				return
-			}
-
-			e.emitter.EmitSpectrum(v)
-		}
-	}
-}
-
 func NewExporter(emitter audio.Emitter, options ...cfg.Option[Config]) (audio.Exporter, error) {
-	if emitter == nil {
-		return audio.NoOpExporter[*header.Header](), ErrNilEmitter
-	}
-
 	config := cfg.Set[Config](DefaultConfig, options...)
 
-	ctx, cancel := context.WithCancel(context.Background())
-
-	e := exporter{
-		config:   config,
-		peaks:    newPeaksCollector(config),
-		spectrum: newSpectrumCollector(config),
-		emitter:  emitter,
-		logger:   slog.New(config.LogHandler),
-		cancel:   cancel,
+	e, err := audio.NewExporter(emitter, newPeaksCollector(config), newSpectrumCollector(config))
+	if err != nil {
+		return audio.NoOpExporter(), err
 	}
 
-	go e.export(ctx)
+	if config.LogHandler != nil {
+		e = audio.ExporterWithLogs(e, config.LogHandler)
+	}
 
 	return e, nil
 }
