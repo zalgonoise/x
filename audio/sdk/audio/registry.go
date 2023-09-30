e9378ce3dac0fea027819334816f71c81bf5b047
zalgonoise <mariozalgo@gmail.com>
2023-09-30 16:52:31 +0200

    audio/sdk/audio: registry: trimming down file contents; moving no-op logic to its own file

--- audio/sdk/audio/registry.go
+++ audio/sdk/audio/registry.go
@@ -1,7 +1,5 @@
 package audio
 
-import "context"
-
 // Registry is a generic type that registers and loads values on a specific type context.
 //
 // Registries are responsible for handling aggregations and compacting values into one, when Load is called
@@ -38,30 +36,3 @@ type Loader[T any] interface {
 //
 // A Compactor is an optional, configurable component within a Registry, if applicable
 type Compactor[T any] func([]T) (T, error)
-
-type noOpRegistry[T any] struct{}
-
-// Register implements the Registry interface.
-//
-// This is a no-op call and the returned error is always nil.
-func (noOpRegistry[T]) Register(T) error { return nil }
-
-// Load implements the Registry interface.
-//
-// This is a no-op call and the returned channel is always nil.
-func (noOpRegistry[T]) Load() <-chan T { return nil }
-
-// Shutdown implements the Registry and Closer interfaces.
-//
-// This is a no-op call and the returned error is always nil.
-func (noOpRegistry[T]) Shutdown(context.Context) error { return nil }
-
-// NoOpLoader returns a no-op Registry, scoped as a Loader interface
-func NoOpLoader[T any]() Loader[T] {
-	return noOpRegistry[T]{}
-}
-
-// NoOpRegisterer returns a no-op Registry
-func NoOpRegisterer[T any]() Registry[T] {
-	return noOpRegistry[T]{}
-}
