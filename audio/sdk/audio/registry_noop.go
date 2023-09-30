0995666741016f492842d04694c90e73a8235fad
zalgonoise <mariozalgo@gmail.com>
2023-09-30 16:52:36 +0200

    audio/sdk/audio: registry: trimming down file contents; moving no-op logic to its own file

---
+++ audio/sdk/audio/registry_noop.go
@@ -0,0 +1,30 @@
+package audio
+
+import "context"
+
+type noOpRegistry[T any] struct{}
+
+// Register implements the Registry interface.
+//
+// This is a no-op call and the returned error is always nil.
+func (noOpRegistry[T]) Register(T) error { return nil }
+
+// Load implements the Registry interface.
+//
+// This is a no-op call and the returned channel is always nil.
+func (noOpRegistry[T]) Load() <-chan T { return nil }
+
+// Shutdown implements the Registry and Closer interfaces.
+//
+// This is a no-op call and the returned error is always nil.
+func (noOpRegistry[T]) Shutdown(context.Context) error { return nil }
+
+// NoOpLoader returns a no-op Registry, scoped as a Loader interface
+func NoOpLoader[T any]() Loader[T] {
+	return noOpRegistry[T]{}
+}
+
+// NoOpRegisterer returns a no-op Registry
+func NoOpRegisterer[T any]() Registry[T] {
+	return noOpRegistry[T]{}
+}
