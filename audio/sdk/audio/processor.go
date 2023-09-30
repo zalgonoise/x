9cc2b14796bf1f0c135380b8580f3ca6044ccbda
zalgonoise <mariozalgo@gmail.com>
2023-09-30 16:51:05 +0200

    audio/sdk/audio: processor: trimming down file contents; moving no-op logic to its own file

--- audio/sdk/audio/processor.go
+++ audio/sdk/audio/processor.go
@@ -40,30 +40,3 @@ type Processor interface {
 	// the module or to shut it down gracefully.
 	StreamCloser
 }
-
-type noOpProcessor struct{}
-
-// Process implements the Processor interface.
-//
-// This is a no-op call.
-func (noOpProcessor) Process(context.Context, io.Reader) {}
-
-// Err implements the Processor interface.
-//
-// This is a no-op call and the returned channel is always nil
-func (noOpProcessor) Err() <-chan error { return nil }
-
-// ForceFlush implements the Processor and StreamCloser interfaces.
-//
-// This is a no-op call and the returned error is always nil
-func (noOpProcessor) ForceFlush() error { return nil }
-
-// Shutdown implements the Processor, Closer and StreamCloser interfaces.
-//
-// This is a no-op call and the returned error is always nil
-func (noOpProcessor) Shutdown(context.Context) error { return nil }
-
-// NoOpProcessor returns a no-op Processor
-func NoOpProcessor() Processor {
-	return noOpProcessor{}
-}
