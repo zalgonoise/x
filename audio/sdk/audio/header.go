f77a8c95b480907907e2d2e88ec50b9c7b601410
zalgonoise <mariozalgo@gmail.com>
2023-09-30 16:48:26 +0200

    audio/sdk/audio: header: trimming down file contents; moving no-op logic to its own file

--- audio/sdk/audio/header.go
+++ audio/sdk/audio/header.go
@@ -1,10 +1,5 @@
 package audio
 
-import (
-	"context"
-	"io"
-)
-
 // Header describes types considered headers on different audio encodings.
 //
 // Methods implemented by a Header return data that is common between all
@@ -12,33 +7,4 @@ import (
 type Header interface {
 	// GetSampleRate returns the sample rate for this audio signal.
 	GetSampleRate() int
-}
-
-type noOpHeader struct{}
-
-// GetSampleRate implements the Header interface
-//
-// This is a no-op call and the returned sample rate is always zero
-func (noOpHeader) GetSampleRate() int { return 0 }
-
-// NoOpHeader returns a no-op Header
-func NoOpHeader() Header {
-	return noOpHeader{}
-}
-
-// Streamer describes types that are able to stream an audio signal
-type Streamer interface {
-	Stream(ctx context.Context, r io.Reader, errCh chan<- error)
-}
-
-type noOpStreamer struct{}
-
-// Stream implements the Streamer interface
-//
-// This is a no-op call and has no effect.
-func (noOpStreamer) Stream(context.Context, io.Reader, chan<- error) {}
-
-// NoOpStreamer returns a no-op Streamer
-func NoOpStreamer() Streamer {
-	return noOpStreamer{}
 }
