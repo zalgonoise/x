bf61388802522d339fc8a7b10a945c4010e1f305
zalgonoise <mariozalgo@gmail.com>
2023-09-30 16:43:43 +0200

    audio/sdk/audio: collector: trimming down file contents; moving no-op and base to their own file

---
+++ audio/sdk/audio/collector_noop.go
@@ -0,0 +1,30 @@
+package audio
+
+import "context"
+
+type noOpCollector[T any] struct{}
+
+// Collect implements the Collector interface
+//
+// This is a no-op call and the returned error is always nil
+func (noOpCollector[T]) Collect(Header, []float64) error { return nil }
+
+// Load implements the Collector and Loader interfaces
+//
+// This is a no-op call and the returned error is always nil
+func (noOpCollector[T]) Load() <-chan T { return nil }
+
+// ForceFlush implements the Collector and StreamCloser interfaces
+//
+// This is a no-op call and the returned error is always nil
+func (noOpCollector[T]) ForceFlush() error { return nil }
+
+// Shutdown implements the Collector, Closer and StreamCloser interfaces
+//
+// This is a no-op call and the returned error is always nil
+func (noOpCollector[T]) Shutdown(context.Context) error { return nil }
+
+// NoOpCollector returns a no-op Collector for a given type
+func NoOpCollector[T any]() Collector[T] {
+	return noOpCollector[T]{}
+}
