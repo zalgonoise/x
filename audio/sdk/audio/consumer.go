7c289b301cd3583ed0002fc67a5811811230222c
zalgonoise <mariozalgo@gmail.com>
2023-09-30 16:44:00 +0200

    audio/sdk/audio: consumer: trimming down file contents; moving no-op to its own file

--- audio/sdk/audio/consumer.go
+++ audio/sdk/audio/consumer.go
@@ -5,26 +5,26 @@ import (
 	"io"
 )
 
-// Consumer is responsible for consuming, extracting, reading an audio source and returning a byte stream to read it,
+// Consumer is responsible for consuming, extracting, reading an audio source and returning a byte streamer to read it,
 // as an io.Reader.
 //
 // Implementations should be able to create and return the io.Reader in a non-blocking way, to be read by a Processor.
-// Ideally, the implementations of Consumer will not read an entire audio stream before returning the io.Reader.
+// Ideally, the implementations of Consumer will not read an entire audio streamer before returning the io.Reader.
 //
-// Be whatever the audio source it may be, the byte stream should be valid audio encoded in a supported format. This is
-// the sole responsibility of the Consumer -- anything regarding the actual content of the byte stream would fall on a
+// Be whatever the audio source it may be, the byte streamer should be valid audio encoded in a supported format. This is
+// the sole responsibility of the Consumer -- anything regarding the actual content of the byte streamer would fall on a
 // Processor's scope.
 type Consumer interface {
-	// Consume interacts with the audio source to extract its audio content or stream as an io.Reader.
+	// Consume interacts with the audio source to extract its audio content or streamer as an io.Reader.
 	//
-	// Implementers of the Consumer must return a byte stream that can be decoded with a supported encoding format.
+	// Implementers of the Consumer must return a byte streamer that can be decoded with a supported encoding format.
 	//
-	// The origin of the byte stream can be anything, such an os.File, http.Response.Body, bytes.Buffer, etc. The origin
+	// The origin of the byte streamer can be anything, such an os.File, http.Response.Body, bytes.Buffer, etc. The origin
 	// of the target audio and any requirements to Consume it are of the Consumers responsibility, and are prepared
 	// and / or verified by the time a Consumer is created.
 	//
 	// The error returned by a Consume call must point to any issues raised in the process of preparing or extracting the
-	// returned io.Reader. It must not be related to any of the content of the byte stream in the io.Reader.
+	// returned io.Reader. It must not be related to any of the content of the byte streamer in the io.Reader.
 	Consume(ctx context.Context) (reader io.Reader, err error)
 	// Shutdown gracefully shuts down the Consumer.
 	//
@@ -38,21 +38,4 @@ type Consumer interface {
 	// The returned error points to any issue or issues raised during this process. If possible, the shutdown process
 	// should still continue and close the Consumer on this call.
 	Shutdown(ctx context.Context) error
-}
-
-type noOpConsumer struct{}
-
-// Consume implements the Consumer interface.
-//
-// This is a no-op call and the returned values are both always nil.
-func (noOpConsumer) Consume(context.Context) (reader io.Reader, err error) { return nil, nil }
-
-// Shutdown implements the Consumer and Closer interfaces.
-//
-// This is a no-op call and the returned error is always nil.
-func (noOpConsumer) Shutdown(context.Context) error { return nil }
-
-// NoOpConsumer returns a no-op Consumer
-func NoOpConsumer() Consumer {
-	return noOpConsumer{}
 }
