3ed569b466ac846f4eeb6eaf29e21794130a5459
zalgonoise <mariozalgo@gmail.com>
2023-09-30 16:48:16 +0200

    audio/sdk/audio: extractor: trimming down file contents; moving no-op logic to its own file

---
+++ audio/sdk/audio/extractor_noop.go
@@ -0,0 +1,9 @@
+package audio
+
+// NoOpExtractor returns an Extractor for a given type, that does not perform any operations on the input values,
+// and only returns zero values for a given type
+func NoOpExtractor[T any]() Extractor[T] {
+	return Extraction[T](func(header Header, float64s []float64) T {
+		return *new(T)
+	})
+}
