d9da848f5c6697564b5dbde5812653f3c84e97a1
zalgonoise <mariozalgo@gmail.com>
2023-09-30 16:45:17 +0200

    audio/sdk/audio: exporter: trimming down file contents; moving no-op to its own file

--- audio/sdk/audio/exporter.go
+++ audio/sdk/audio/exporter.go
@@ -1,9 +1,5 @@
 package audio
 
-import (
-	"context"
-)
-
 // Exporter is responsible for pushing the processed data into a certain destination.
 //
 // It is of the implementer's responsibility to comply with any requirements needed to export the audio,
@@ -31,26 +27,4 @@ type Exporter interface {
 	// StreamCloser defines common methods when interacting with a streaming module, targeting actions to either flush
 	// the module or to shut it down gracefully.
 	StreamCloser
-}
-
-type noOpExporter struct{}
-
-// Export implements the Exporter interface.
-//
-// This is a no-op call and the returned error is always nil.
-func (noOpExporter) Export(Header, []float64) error { return nil }
-
-// ForceFlush implements the Exporter and StreamCloser interfaces.
-//
-// This is a no-op call and the returned error is always nil.
-func (noOpExporter) ForceFlush() error { return nil }
-
-// Shutdown implements the Exporter, Closer and StreamCloser interfaces.
-//
-// This is a no-op call and the returned error is always nil.
-func (noOpExporter) Shutdown(context.Context) error { return nil }
-
-// NoOpExporter returns a no-op Exporter
-func NoOpExporter() Exporter {
-	return noOpExporter{}
 }
