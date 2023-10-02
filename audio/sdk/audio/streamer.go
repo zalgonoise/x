eb9933425a9a6eb29d327294c7492946a4c017e6
zalgonoise <mariozalgo@gmail.com>
2023-10-02 17:47:42 +0200

    audio/sdk/audio: streamer: improved code docs

--- audio/sdk/audio/streamer.go
+++ audio/sdk/audio/streamer.go
@@ -6,6 +6,12 @@ import (
 )
 
 // Streamer describes types that are able to streamer an audio signal
+//
+// The Streamer interface is implemented by audio encoder packages as a means of reading
+// from an audio source and converting its signal as audio float values.
+//
+// Streamer types should define Stream as a blocking call to be issued asynchronously (in a goroutine),
+// therefore its io.Reader and error channel parameters.
 type Streamer interface {
 	Stream(ctx context.Context, r io.Reader, errCh chan<- error)
 }
@@ -27,7 +33,8 @@ type streamExporter struct {
 // It is the responsibility of the caller to configure these accordingly, considering that certain (if not most)
 // implementations should require a connection or configuration between the two components.
 //
-// The resulting type is a simple struct embedding both interfaces, allowing access to all StreamExporter methods.
+// The resulting type is a simple struct embedding both interfaces, allowing access to all StreamExporter methods
+// with no alterations or method overloading involved.
 func NewStreamExporter(streamer Streamer, exporter Exporter) StreamExporter {
 	switch {
 	case streamer == nil && exporter == nil:
