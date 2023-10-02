2e461665b14926d5236cf87db531f53f5928a5d9
zalgonoise <mariozalgo@gmail.com>
2023-10-02 17:47:57 +0200

    audio/sdk/audio: processor: documented base processor

--- audio/sdk/audio/processor_base.go
+++ audio/sdk/audio/processor_base.go
@@ -17,6 +17,7 @@ const (
 	ErrSignal = errs.Entity("with OS signal")
 )
 
+// ErrHaltSignal is a sentinel error for when an OS signal is emitted, to halt or stop the application's runtime
 var ErrHaltSignal = errs.New(errDomain, ErrHalt, ErrSignal)
 
 type processor struct {
@@ -26,6 +27,13 @@ type processor struct {
 	cancel context.CancelFunc
 }
 
+// Process implements the Processor interface.
+//
+// It reads the byte stream from the input io.Reader using the configured StreamExporter, which
+// is both converting the bytes data into floating point audio; processing it, and finally exporting
+// it to its configured outputs.
+//
+// Process should be called in a goroutine, as a blocking call that is supposed to be issued asynchronously.
 func (p *processor) Process(ctx context.Context, reader io.Reader) {
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt, os.Kill, syscall.SIGTERM)
@@ -50,14 +58,26 @@ func (p *processor) Process(ctx context.Context, reader io.Reader) {
 	}
 }
 
+// Err implements the Processor interface.
+//
+// It returns a receiving channel for errors, that allows the caller of a Process method to listen for any raised
+// errors
 func (p *processor) Err() <-chan error {
 	return p.errCh
 }
 
+// ForceFlush implements the Processor and StreamCloser interfaces.
+//
+// It allows direct access to the configured Exporter's (or, StreamExporter's) ForceFlush method, for example,
+// when its exporter contains a batched Registry that buffers items in chunks.
 func (p *processor) ForceFlush() error {
 	return p.streamExporter.ForceFlush()
 }
 
+// Shutdown implements the Processor and StreamCloser interfaces.
+//
+// It allows a graceful shutdown of the Processor. It starts by stopping its runtime, and then gracefully shutting down
+// the configured Exporter (or, StreamExporter).
 func (p *processor) Shutdown(ctx context.Context) error {
 	defer close(p.errCh)
 	p.cancel()
@@ -65,6 +85,11 @@ func (p *processor) Shutdown(ctx context.Context) error {
 	return p.streamExporter.Shutdown(ctx)
 }
 
+// NewProcessor creates a Processor from a StreamExporter.
+//
+// The input StreamExporter should be already set-up and ready to be used, since the Processor will simply
+// call on its available methods. It is the responsibility of the caller to set it up accordingly, especially when
+// both Streamer and Exporter types are connected in any way.
 func NewProcessor(streamExporter StreamExporter) Processor {
 	if streamExporter == nil {
 		return NoOpProcessor()
