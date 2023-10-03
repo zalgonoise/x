b4c7a6e04d8a245af79926158a2bc72a04b808d9
zalgonoise <mariozalgo@gmail.com>
2023-10-03 18:50:55 +0200

    audio/sdk/audio: exporter: documented base exporter

--- audio/sdk/audio/exporter_base.go
+++ audio/sdk/audio/exporter_base.go
@@ -13,10 +13,14 @@ const (
 
 	ErrNil = errs.Kind("nil")
 
-	ErrEmitter = errs.Entity("emitter")
+	ErrEmitter    = errs.Entity("emitter")
+	ErrCollectors = errs.Entity("collectors")
 )
 
-var ErrNilEmitter = errs.New(errDomain, ErrNil, ErrEmitter)
+var (
+	ErrNilEmitter    = errs.New(errDomain, ErrNil, ErrEmitter)
+	ErrNilCollectors = errs.New(errDomain, ErrNil, ErrCollectors)
+)
 
 type exporter struct {
 	peaks    Collector[float64]
@@ -27,6 +31,15 @@ type exporter struct {
 	cancel context.CancelFunc
 }
 
+// Export implements the Exporter interface.
+//
+// It consumes the audio data chunks from the Processor, as the signal is streamed from a Process call.
+//
+// It will use the configured Collector types to extract meaningful data from the signal (peaks and spectrum data),
+// as a series of steps in a Collector pipeline, usually composed of an Extractor, a Registry and optionally
+// a Compactor.
+//
+// The returned error is a wrapped error of both peaks and spectrum Collector Collect method call, if raised.
 func (e exporter) Export(h Header, data []float64) error {
 	return errors.Join(
 		e.peaks.Collect(h, data),
@@ -34,6 +47,11 @@ func (e exporter) Export(h Header, data []float64) error {
 	)
 }
 
+// ForceFlush implements the Exporter and StreamCloser interfaces.
+//
+// It will call on the peaks and spectrum Collector ForceFlush method if their Registry has it.
+//
+// The returned error is a wrapped error of both peaks and spectrum Collector ForceFlush method call, if raised.
 func (e exporter) ForceFlush() error {
 	return errors.Join(
 		e.peaks.ForceFlush(),
@@ -41,6 +59,13 @@ func (e exporter) ForceFlush() error {
 	)
 }
 
+// Shutdown implements the Exporter and StreamCloser interfaces.
+//
+// It will stop the running goroutine which listens to the Registry's incoming values. Then, it will call on the
+// peaks and spectrum Collector Shutdown method if their Extractor has it, as well as their Registry's Shutdown method.
+// Lastly, its Emitter is gracefully shut down via its Shutdown method as well.
+//
+// The returned error is a wrapped error of both peaks and spectrum Collector ForceFlush method call, if raised.
 func (e exporter) Shutdown(ctx context.Context) error {
 	e.cancel()
 
@@ -75,18 +100,33 @@ func (e exporter) export(ctx context.Context) {
 	}
 }
 
+// NewExporter creates an audio Exporter based on the input Emitter (that publishes the values somewhere), and
+// the input peaks and spectrum Collector, which will extract, process and / or buffer values from the incoming
+// audio signal.
+//
+// The input Emitter and both Collector types need to be configured on their own, if they require any custom
+// configuration, preparation or connection between each other. For example, the caller may want to create a
+// Map-Reduce-like pipeline -- which is possible by combining the right Extractor, and a batch Registry with a
+// configured Compactor. While this covers the Collector; the Emitter should exclusively be concerned with publishing
+// these values to the appropriate destination and / or backend.
+//
+// A nil Emitter results in a no-op Exporter with an ErrNilEmitter error. Also, if both peaks and spectrum Collector
+// types are nil, a no-op Exporter and ErrNilCollectors error are returned. Otherwise, the Exporter is created,
+// using a no-op Collector where the provided one is nil.
+//
+// The created Exporter launches a goroutine to listen on both Collector types' value channel, that is controlled via
+// a context.Context. Its done signal is sent on the Exporter's Shutdown method call.
 func NewExporter(
 	emitter Emitter, peaks Collector[float64], spectrum Collector[[]fft.FrequencyPower],
 ) (Exporter, error) {
-	if emitter == nil {
+	switch {
+	case emitter == nil:
 		return NoOpExporter(), ErrNilEmitter
-	}
-
-	if peaks == nil {
+	case peaks == nil && spectrum == nil:
+		return NoOpExporter(), ErrNilCollectors
+	case peaks == nil:
 		peaks = NoOpCollector[float64]()
-	}
-
-	if spectrum == nil {
+	case spectrum == nil:
 		spectrum = NoOpCollector[[]fft.FrequencyPower]()
 	}
 
