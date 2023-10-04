6957d132b7375dabf8f44289e04329fadc9937d7
zalgonoise <mariozalgo@gmail.com>
2023-10-04 17:46:15 +0200

    audio/sdk/audio: emitter: documented emitter no-op implementation

--- audio/sdk/audio/emitter_noop.go
+++ audio/sdk/audio/emitter_noop.go
@@ -8,10 +8,22 @@ import (
 
 type noOpEmitter struct{}
 
-func (noOpEmitter) EmitPeaks(float64)                 {}
-func (noOpEmitter) EmitSpectrum([]fft.FrequencyPower) {}
-func (noOpEmitter) Shutdown(context.Context) error    { return nil }
+// EmitPeaks implements the Emitter interface.
+//
+// This is a no-op call, and has no effect.
+func (noOpEmitter) EmitPeaks(float64) {}
 
+// EmitSpectrum implements the Emitter interface.
+//
+// This is a no-op call, and has no effect.
+func (noOpEmitter) EmitSpectrum([]fft.FrequencyPower) {}
+
+// Shutdown implements the Emitter and Closer interfaces.
+//
+// This is a no-op call, and the returned error is always nil.
+func (noOpEmitter) Shutdown(context.Context) error { return nil }
+
+// NoOpEmitter returns a no-op Emitter.
 func NoOpEmitter() Emitter {
 	return noOpEmitter{}
 }
