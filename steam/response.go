69fb98f5a24b2ac2b3314e20c41fa1693f128cf9
zalgonoise <mariozalgo@gmail.com>
2023-10-11 18:04:01 +0200

    steam: response: fixed response extraction and data structure access

--- steam/response.go
+++ steam/response.go
@@ -1,11 +1,9 @@
 package steam
 
 import (
-	"bytes"
 	"encoding/json"
 
 	"github.com/zalgonoise/x/errs"
-	pb "github.com/zalgonoise/x/steam/pb/proto/steam/store/v1"
 )
 
 const (
@@ -34,6 +32,15 @@ type FilterResult[T any] struct {
 	Data    map[string]T `json:"data"`
 }
 
+// Get decodes a JSON response of an HTTP GET call to the appdetails endpoint.
+//
+// If the filter string is unset, it returns a map[string]*pb.Data, linking appids
+// to pb.Data objects.
+//
+// If a filter is provided, the returned value will be a map[string]*T.
+//
+// The underlying call is the following:
+// GET https://store.steampowered.com/api/appdetails/?appids={comma_separated_app_ids}&cc={country_code}
 func Get[T any](data []byte, filter string) (map[string]*T, error) {
 	if filter == "" {
 		return getAll[T](data)
@@ -42,38 +49,8 @@ func Get[T any](data []byte, filter string) (map[string]*T, error) {
 	return getFilter[T](data, filter)
 }
 
-// UnmarshalJSON decodes a JSON response of an HTTP GET call to the appdetails endpoint
-// in the Steam store, as a map[string]pb.Data, linking appids to pb.Data objects.
-//
-// The underlying call is the following:
-// GET https://store.steampowered.com/api/appdetails/?appids={comma_separated_app_ids}&cc={country_code}
-func UnmarshalJSON(data []byte) (map[string]*pb.Data, error) {
-	if len(data) == 0 {
-		return nil, ErrEmptyData
-	}
-
-	return Get[pb.Data](data, "")
-}
-
-func addWrapper(data []byte) []byte {
-	if len(data) < 5 {
-		return data
-	}
-
-	if bytes.Equal(data[:5], []byte(headerMatcher)) {
-		return data
-	}
-
-	buf := make([]byte, len(data)+len(wrapperHead)+len(wrapperTail))
-	n := copy(buf, wrapperHead)
-	n += copy(buf[n:], data)
-	copy(buf[n:], wrapperTail)
-
-	return buf
-}
-
 func getAll[T any](data []byte) (map[string]*T, error) {
-	response := &map[string]Result[T]{}
+	response := &map[string]Result[*T]{}
 
 	if err := json.Unmarshal(data, response); err != nil {
 		return nil, err
@@ -81,47 +58,27 @@ func getAll[T any](data []byte) (map[string]*T, error) {
 
 	output := make(map[string]*T, len(*response))
 
-	for id, priceData := range *response {
-		res := new(T)
-
-		buf, err := json.Marshal(priceData)
-		if err != nil {
-			return nil, err
-		}
-
-		if err = json.Unmarshal(buf, res); err != nil {
-			return nil, err
-		}
-
-		output[id] = res
+	for id, objData := range *response {
+		output[id] = objData.Data
 	}
 
 	return output, nil
 }
 
 func getFilter[T any](data []byte, filter string) (map[string]*T, error) {
-	response := &map[string]FilterResult[T]{}
+	response := &map[string]FilterResult[*T]{}
 
 	if err := json.Unmarshal(data, response); err != nil {
-		return nil, err
+		// some objects are not encapsulated like the others. If it fails to unmarshal into a map,
+		// try to unmarshal into an object of the input type.
+		return getAll[T](data)
 	}
 
 	output := make(map[string]*T, len(*response))
 
 	for id, filteredDataRaw := range *response {
 		if filteredData, ok := filteredDataRaw.Data[filter]; ok {
-			res := new(T)
-
-			buf, err := json.Marshal(filteredData)
-			if err != nil {
-				return nil, err
-			}
-
-			if err = json.Unmarshal(buf, res); err != nil {
-				return nil, err
-			}
-
-			output[id] = res
+			output[id] = filteredData
 		}
 	}
 
