e74c9e3614d78ab95e9b2e9dbef6e628f916df89
zalgonoise <mariozalgo@gmail.com>
2023-10-12 17:31:26 +0200

    steam/cmd/steam/alert: added remaining alert operation logic, with support for logger (CLI), slack and discord (webhooks)

--- steam/cmd/steam/alert/alert.go
+++ steam/cmd/steam/alert/alert.go
@@ -7,30 +7,37 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
+	"strings"
+
+	discordwh "github.com/zalgonoise/x/discord/webhook"
+	slackwh "github.com/zalgonoise/x/slack/webhook"
 
 	"github.com/zalgonoise/x/steam"
 	"github.com/zalgonoise/x/steam/cmd/steam/query"
+	"github.com/zalgonoise/x/steam/pb/proto/steam/store/v1"
 )
 
 const (
-	priceFilter = "price_overview"
+	priceFilter    = "price_overview"
+	productBaseURL = "https://store.steampowered.com/app"
 )
 
 var (
 	errEmptyID          = errors.New("empty app ID")
 	errEmptyPlatform    = errors.New("empty platform")
+	errInvalidPlatform  = errors.New("invalid platform")
 	errEmptyURL         = errors.New("empty webhook URL")
 	errEmptyTargetPrice = errors.New("empty target price")
 )
 
 func Exec(ctx context.Context, logger *slog.Logger, args []string) (error, int) {
-	fs := flag.NewFlagSet("store", flag.ExitOnError)
+	fs := flag.NewFlagSet("alert", flag.ExitOnError)
 
 	ids := fs.String("ids", "", "comma-separated list of app ID values")
 	country := fs.String("country", "", "country code (2-character-long)")
-	platform := fs.String("platform", "", "target platform where to post")
-	url := fs.String("url", "", "webhook target URL")
-	target := fs.Int("target_discount", 50, "target discount percent")
+	platform := fs.String("platform", "logger", "target platform where to post (logger; slack)")
+	url := fs.String("url", "", "webhook target URL (platform: slack)")
+	targetDiscount := fs.Int("target_discount", 50, "target discount percent")
 
 	if err := fs.Parse(args); err != nil {
 		return err, 1
@@ -40,15 +47,15 @@ func Exec(ctx context.Context, logger *slog.Logger, args []string) (error, int) 
 		return errEmptyID, 1
 	}
 
-	if *target == 0 {
+	if *targetDiscount == 0 {
 		return errEmptyTargetPrice, 1
 	}
 
 	if *platform == "" {
-		return errEmptyPlatform, 1
+		*platform = "logger"
 	}
 
-	if *url == "" {
+	if *url == "" && *platform != "logger" {
 		return errEmptyURL, 1
 	}
 
@@ -73,14 +80,67 @@ func Exec(ctx context.Context, logger *slog.Logger, args []string) (error, int) 
 
 	// eval against target
 	for appID, data := range priceOverview {
-		if discount := data.DiscountPercent; int(discount) >= *target {
+		if discount := data.DiscountPercent; int(discount) <= *targetDiscount {
 			continue
 		}
 
 		// exec webhook if current price is lower or equal to discount ratio
-		_ = appID
-		_ = data
+		if err = sendMessage(ctx, logger, *platform, *url, appID, data); err != nil {
+			return err, 1
+		}
 	}
 
 	return nil, 0
+}
+
+func sendMessage(
+	ctx context.Context, logger *slog.Logger,
+	platform, url, appID string, data *store.PriceOverview,
+) error {
+	switch strings.ToLower(platform) {
+	case "logger":
+		logger.InfoContext(ctx, textAlert(appID, data))
+
+		return nil
+	case "slack":
+		w, err := slackwh.New(url, slackwh.WithLogger(logger))
+		if err != nil {
+			return err
+		}
+
+		if _, err = w.Execute(ctx, textAlert(appID, data)); err != nil {
+			return err
+		}
+
+		return nil
+	case "discord":
+		w, err := discordwh.New(url, discordwh.WithLogger(logger))
+		if err != nil {
+			return err
+		}
+		if _, err = w.Execute(ctx, textAlert(appID, data)); err != nil {
+			return err
+		}
+
+		return nil
+	default:
+		return fmt.Errorf("%w: %s", errInvalidPlatform, platform)
+	}
+}
+
+func textAlert(appID string, data *store.PriceOverview) string {
+	return fmt.Sprintf(
+		`Woah! The app %s is currently with a %d percent discount!!
+
+	Initial price: %s
+	Current price: %s
+
+Check it out at: %s/%s`,
+		appID,
+		data.DiscountPercent,
+		data.InitialFormatted,
+		data.FinalFormatted,
+		productBaseURL,
+		appID,
+	)
 }
