0f5c0a43aa2fc2b2f03f3c6621b0da8f7924f4bc
zalgonoise <mariozalgo@gmail.com>
2023-10-15 18:22:05 +0200

    steam: added app list logic

--- steam/app_list.go
+++ steam/app_list.go
@@ -1,15 +1,60 @@
 package steam
 
 import (
+	"context"
 	_ "embed"
 	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	appsListURL  = "https://api.steampowered.com/ISteamApps/GetAppList/v0001/"
+	baseFilePath = "internal/app_list/applist.json"
 )
 
 // fetch app_list manually and embed it in the binary, as it's not reasonable
 // to download a 10MB file on each execution just for querying for an app ID
 //
-//go:embed internal/app_list/applist_*.json
+//go:embed internal/app_list/applist.json
 var rawAppList []byte
+
+func GetAppsList() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, appsListURL, http.NoBody)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Accept", "application/json")
+
+	res, err := (&http.Client{
+		Transport: http.DefaultTransport,
+		Timeout:   time.Minute,
+	}).Do(req)
+
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	f, err := os.Create(baseFilePath)
+
+	if err != nil {
+		return err
+	}
+
+	if _, err = io.Copy(f, res.Body); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func LoadAppsList() ([]App, error) {
 	list := &AppsListHTTPResponse{}
