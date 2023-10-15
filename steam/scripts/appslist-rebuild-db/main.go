3f1b507aec5f480abb3de16b7f6a1dd42f00c08e
zalgonoise <mariozalgo@gmail.com>
2023-10-15 18:24:20 +0200

    steam/scripts/appslist-rebuild-db: added apps list rebuild DB script

---
+++ steam/scripts/appslist-rebuild-db/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/zalgonoise/x/steam/apps"
+)
+
+const topLevelURI = "internal/app_list/applist.db"
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+	ctx := context.Background()
+
+	logger.InfoContext(ctx, "rebuilding local apps list database")
+
+	if _, err := apps.NewRepository(apps.WithURI(topLevelURI)); err != nil {
+		logger.ErrorContext(ctx, "failed to setup database", slog.String("error", err.Error()))
+
+		return
+	}
+
+	logger.InfoContext(ctx, "database setup successfully")
+}
