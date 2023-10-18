88149748583f8259a313d2ca8ab0eebcb93d8844
zalgonoise <mariozalgo@gmail.com>
2023-10-19 00:45:24 +0200

    audio/encoding/wav/data: updated references

--- audio/encoding/wav/data/validate.go
+++ audio/encoding/wav/data/validate.go
@@ -26,7 +26,7 @@ var (
 	ErrShortBuffer           = errs.WithDomain(ErrDomain, ErrShort, ErrBuffer)
 )
 
-var headerValidator = validation.Register[*Header](validateHeaderSubChunkID)
+var headerValidator = validation.New(validateHeaderSubChunkID)
 
 func validateHeaderSubChunkID(h *Header) error {
 	switch string(h.Subchunk2ID[:]) {
