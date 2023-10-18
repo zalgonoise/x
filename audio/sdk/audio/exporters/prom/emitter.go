ad9f8fd7afa642596e314fab50be77259ce2aa93
zalgonoise <mariozalgo@gmail.com>
2023-10-18 23:04:01 +0200

    audio/sdk/audio/exporters/prom: updated references

--- audio/sdk/audio/exporters/prom/emitter.go
+++ audio/sdk/audio/exporters/prom/emitter.go
@@ -11,10 +11,10 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/zalgonoise/x/audio/encoding/wav/header"
 	"github.com/zalgonoise/x/audio/fft"
 	"github.com/zalgonoise/x/audio/sdk/audio"
 	"github.com/zalgonoise/x/audio/sdk/audio/exporters"
-	"github.com/zalgonoise/x/audio/wav/header"
 	"github.com/zalgonoise/x/cfg"
 )
 
