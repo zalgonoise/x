e2da2ba1d5f3f8479d6f6300394b67681f8ec6e9
zalgonoise <mariozalgo@gmail.com>
2023-10-18 23:27:24 +0200

    audio/encoding/wav: moved stream processor logic into its own file

--- audio/encoding/wav/stream.go
+++ audio/encoding/wav/stream.go
@@ -3,69 +3,12 @@ package wav
 import (
 	"bytes"
 	"context"
-	"errors"
 	"io"
 	"time"
 
 	dataheader "github.com/zalgonoise/x/audio/encoding/wav/data/header"
 	"github.com/zalgonoise/x/audio/encoding/wav/header"
 )
-
-// ProcessFunc describes a function that processes a portion of the audio buffer
-// as it is read and decoded from the incoming byte stream
-type ProcessFunc func(header *header.Header, data []float64) error
-
-// MultiProc merges multiple processor functions for floating point audio data, with
-// or without a fail-fast strategy
-func MultiProc(failFast bool, fns ...ProcessFunc) ProcessFunc {
-	switch len(fns) {
-	case 0:
-		return nil
-	case 1:
-		return fns[0]
-	}
-
-	if failFast {
-		return func(h *header.Header, data []float64) error {
-			for i := range fns {
-				if err := fns[i](h, data); err != nil {
-					return err
-				}
-			}
-
-			return nil
-		}
-	}
-
-	return func(h *header.Header, data []float64) error {
-		var errs = make([]error, 0, len(fns))
-		for i := range fns {
-			if err := fns[i](h, data); err != nil {
-				errs = append(errs, err)
-			}
-		}
-
-		switch len(errs) {
-		case 0:
-			return nil
-		case 1:
-			return errs[0]
-		default:
-			return errors.Join(errs...)
-		}
-	}
-}
-
-func ErrorPipe(fn ProcessFunc, errs chan<- error) ProcessFunc {
-	return func(header *header.Header, data []float64) error {
-		err := fn(header, data)
-		if err != nil {
-			errs <- err
-		}
-
-		return err
-	}
-}
 
 // ByteRate calculates the byte rate of a certain signal from its header metadata
 func ByteRate(sampleRate uint32, bitDepth, numChannels uint16) uint32 {
---
+++ audio/encoding/wav/stream_processor.go
@@ -0,0 +1,63 @@
+package wav
+
+import (
+	"errors"
+
+	"github.com/zalgonoise/x/audio/encoding/wav/header"
+)
+
+// ProcessFunc describes a function that processes a portion of the audio buffer
+// as it is read and decoded from the incoming byte stream
+type ProcessFunc func(header *header.Header, data []float64) error
+
+// MultiProc merges multiple processor functions for floating point audio data, with
+// or without a fail-fast strategy
+func MultiProc(failFast bool, fns ...ProcessFunc) ProcessFunc {
+	switch len(fns) {
+	case 0:
+		return nil
+	case 1:
+		return fns[0]
+	}
+
+	if failFast {
+		return func(h *header.Header, data []float64) error {
+			for i := range fns {
+				if err := fns[i](h, data); err != nil {
+					return err
+				}
+			}
+
+			return nil
+		}
+	}
+
+	return func(h *header.Header, data []float64) error {
+		var errs = make([]error, 0, len(fns))
+		for i := range fns {
+			if err := fns[i](h, data); err != nil {
+				errs = append(errs, err)
+			}
+		}
+
+		switch len(errs) {
+		case 0:
+			return nil
+		case 1:
+			return errs[0]
+		default:
+			return errors.Join(errs...)
+		}
+	}
+}
+
+func ErrorPipe(fn ProcessFunc, errs chan<- error) ProcessFunc {
+	return func(header *header.Header, data []float64) error {
+		err := fn(header, data)
+		if err != nil {
+			errs <- err
+		}
+
+		return err
+	}
+}
