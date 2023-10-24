c912c0f3c9d0bcbbf820287e36327fa426a94104
zalgonoise <mariozalgo@gmail.com>
2023-10-24 21:26:31 +0200

    steam/apps: updated indexer spawning to run an initial load if in-memory or if it's a new DB file

--- steam/apps/indexer.go
+++ steam/apps/indexer.go
@@ -1,28 +1,61 @@
 package apps
 
 import (
+	"errors"
 	"log/slog"
+	"os"
 
 	"github.com/zalgonoise/x/fts"
 	"github.com/zalgonoise/x/ptr"
 	"github.com/zalgonoise/x/steam"
 )
 
+const inMemory = ":memory:"
+
 func NewIndexer(uri string, logger *slog.Logger) (fts.Indexer[int64, string], error) {
+	var (
+		attr []fts.Attribute[int64, string]
+		err  error
+	)
+
+	// perform an initial load if in-memory, or if (database) file does not exist
+	switch uri {
+	case "", inMemory:
+		if attr, err = loadApps(); err != nil {
+			return nil, err
+		}
+	default:
+		_, err = os.Stat(uri)
+
+		if err == nil {
+			break
+		}
+
+		if errors.Is(err, os.ErrNotExist) {
+			if attr, err = loadApps(); err != nil {
+				return nil, err
+			}
+
+			break
+		}
+
+		return nil, err
+	}
+
+	indexer, err := fts.New(
+		attr,
+		fts.WithURI(uri),
+		fts.WithLogger(logger),
+	)
+
+	return indexer, err
+}
+
+func loadApps() ([]fts.Attribute[int64, string], error) {
 	appsList, err := steam.LoadAppsList()
 	if err != nil {
 		return nil, err
 	}
 
-	index, err := fts.New(
-		ptr.Cast[[]fts.Attribute[int64, string]](appsList),
-		fts.WithURI(uri),
-		fts.WithLogger(logger),
-	)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return index, nil
+	return ptr.Cast[[]fts.Attribute[int64, string]](appsList), nil
 }
