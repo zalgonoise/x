de3924f4e46cdcc54594ab89605cdd8f808c78fd
zalgonoise <mariozalgo@gmail.com>
2023-10-28 18:24:11 +0200

    pluslog/httplog: fixed errs package reference

--- pluslog/httplog/http.go
+++ pluslog/httplog/http.go
@@ -24,7 +24,7 @@ const (
 )
 
 var (
-	ErrFailedHTTPRequest = errs.New(errDomain, ErrFailed, ErrHTTPRequest)
+	ErrFailedHTTPRequest = errs.WithDomain(errDomain, ErrFailed, ErrHTTPRequest)
 )
 
 type Encoder interface {
