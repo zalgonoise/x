2a8fbad99bac9b116199244406318007a1ac42a2
zalgonoise <mariozalgo@gmail.com>
2023-10-31 23:53:26 +0100

    cron/log: improved new logger creation

--- cron/log/logger.go
+++ cron/log/logger.go
@@ -7,50 +7,29 @@ import (
 	"github.com/zalgonoise/cfg"
 )
 
-func New(options ...cfg.Option[Config]) *slog.Logger {
+func New(h slog.Handler, options ...cfg.Option[Config]) *slog.Logger {
 	config := cfg.New(options...)
 
-	handler := newHandler(config)
-
-	if config.withTraceID {
-		handler = NewSpanContextHandler(handler, config.withSpanID)
+	if h == nil {
+		h = newHandler(config)
 	}
 
-	return slog.New(handler)
+	if config.withTraceID {
+		h = NewSpanContextHandler(h, config.withSpanID)
+	}
+
+	return slog.New(h)
 }
 
 func newHandler(config Config) slog.Handler {
-	if config.handler != nil && config.handler != slog.Handler(nil) {
-		return config.handler
-	}
-
-	addSource := true
-	if config.noSource {
-		addSource = false
-	}
-
-	level := config.level
-	if level == nil {
-		level = slog.LevelInfo
-	}
-
-	var replaceAttrs func(groups []string, a slog.Attr) slog.Attr
-	if config.replaceAttrs != nil {
-		replaceAttrs = config.replaceAttrs
-	}
-
 	switch config.format {
 	case formatText:
 		return slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-			AddSource:   addSource,
-			Level:       level,
-			ReplaceAttr: replaceAttrs,
+			AddSource: config.source,
 		})
 	default:
 		return slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
-			AddSource:   addSource,
-			Level:       level,
-			ReplaceAttr: replaceAttrs,
+			AddSource: config.source,
 		})
 	}
 }
