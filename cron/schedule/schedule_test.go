2403952d7093954788caa6290c980a9a84338e39
zalgonoise <mariozalgo@gmail.com>
2023-10-31 23:56:58 +0100

    cron/schedule: added (some, not all) tests

---
+++ cron/schedule/schedule_test.go
@@ -0,0 +1,50 @@
+package schedule
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/zalgonoise/x/is"
+)
+
+func TestCronSchedule_Next(t *testing.T) {
+	for _, testcase := range []struct {
+		name       string
+		cronString string
+		sched      Scheduler
+		input      time.Time
+		wants      time.Time
+	}{
+		{
+			name:       "Success/OneHour",
+			cronString: "0 * * * *",
+
+			input: time.Date(2023, 10, 30, 10, 12, 43, 0, time.UTC),
+			wants: time.Date(2023, 10, 30, 11, 0, 0, 0, time.UTC),
+		},
+		{
+			name:       "Success/OneDay/WithDayChange",
+			cronString: "0 0 * * *",
+			input:      time.Date(2023, 10, 30, 22, 12, 43, 0, time.UTC),
+			wants:      time.Date(2023, 10, 31, 00, 0, 0, 0, time.UTC),
+		},
+		{
+			name:       "Success/WithWeekday/NoWeekends",
+			cronString: "0 0 * * 1-5",
+			input:      time.Date(2023, 10, 30, 22, 12, 43, 0, time.UTC),
+			wants:      time.Date(2023, 10, 31, 00, 0, 0, 0, time.UTC),
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			sched, err := New(
+				WithSchedule(testcase.cronString), WithLocation(time.UTC),
+			)
+			is.Empty(t, err)
+
+			next := sched.Next(context.Background(), testcase.input)
+
+			is.Equal(t, testcase.wants, next)
+		})
+	}
+}
