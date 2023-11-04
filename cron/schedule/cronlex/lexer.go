8949418a6ac3bce3ca4960eb5183b6d6e6a6d3bd
zalgonoise <mariozalgo@gmail.com>
2023-11-04 18:53:13 +0100

    cron/schedule/cronlex: fixed lexer character acceptance logic

--- cron/schedule/cronlex/lexer.go
+++ cron/schedule/cronlex/lexer.go
@@ -1,11 +1,14 @@
 package cronlex
 
-import "github.com/zalgonoise/lex"
+import (
+	"github.com/zalgonoise/lex"
+)
 
 func StateFunc(l lex.Lexer[Token, byte]) lex.StateFn[Token, byte] {
 	switch l.Next() {
 	case '@':
 		l.Emit(TokenAt)
+
 		return stateException
 	case '-':
 		l.Emit(TokenDash)
@@ -28,6 +31,8 @@ func StateFunc(l lex.Lexer[Token, byte]) lex.StateFn[Token, byte] {
 
 		return StateFunc
 	case 0:
+		l.Emit(TokenEOF)
+
 		return nil
 	default:
 		return stateAlphanumeric
@@ -35,54 +40,39 @@ func StateFunc(l lex.Lexer[Token, byte]) lex.StateFn[Token, byte] {
 }
 
 func stateAlphanumeric(l lex.Lexer[Token, byte]) lex.StateFn[Token, byte] {
-	l.AcceptRun(func(item byte) bool {
-		switch {
-		case item >= '0' && item <= '9', item >= 'A' && item <= 'Z', item >= 'a' && item <= 'z':
-			return true
-		default:
-			return false
+	l.Backup() // undo l.Next() for the l.AcceptRun call
+
+	for {
+		if item := l.Cur(); (item >= '0' && item <= '9') || (item >= 'A' && item <= 'Z') || (item >= 'a' && item <= 'z') {
+			l.Next()
+
+			continue
 		}
-	})
+		break
+	}
 
 	if l.Width() > 0 {
-		// advance cursor on last item
-		if item := l.Peek(); item == 0 {
-			l.Next()
-		}
-
 		l.Emit(TokenAlphaNum)
 	}
 
-	switch l.Next() {
-	case '-', ',', '/', '*', ' ':
-		l.Prev()
-
-		return StateFunc
-	default:
-		l.Emit(TokenEOF)
-		return nil
-	}
+	return StateFunc
 }
 
 func stateException(l lex.Lexer[Token, byte]) lex.StateFn[Token, byte] {
-	l.AcceptRun(func(item byte) bool {
-		switch {
-		case item >= 'A' && item <= 'Z', item >= 'a' && item <= 'z':
-			return true
-		default:
-			return false
+	l.Backup() // undo l.Next() for the l.AcceptRun call
+
+	for {
+		if item := l.Cur(); (item >= 'A' && item <= 'Z') || (item >= 'a' && item <= 'z') {
+			l.Next()
+
+			continue
 		}
-	})
+		break
+	}
 
 	if l.Width() > 0 {
-		// advance cursor on last item
-		if item := l.Peek(); item == 0 {
-			l.Next()
-		}
-
 		l.Emit(TokenAlphaNum)
 	}
 
-	l.Emit(TokenEOF)
-	return nil
+	return StateFunc
 }
