841f6c9fba815372cb0be4ad3647553f1253c8c6
zalgonoise <mariozalgo@gmail.com>
2023-11-05 12:35:49 +0100

    cron/schedule: boosted test coverage

--- cron/schedule/schedule_test.go
+++ cron/schedule/schedule_test.go
@@ -2,11 +2,13 @@ package schedule
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
 	"github.com/zalgonoise/x/cron/log"
 	"github.com/zalgonoise/x/cron/metrics"
+	"github.com/zalgonoise/x/cron/schedule/cronlex"
 	"github.com/zalgonoise/x/is"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -18,7 +20,15 @@ func TestCronSchedule_Next(t *testing.T) {
 		sched      Scheduler
 		input      time.Time
 		wants      time.Time
+		err        error
 	}{
+		{
+			name:       "Success/EveryMinute",
+			cronString: "* * * * *",
+
+			input: time.Date(2023, 10, 30, 10, 12, 43, 0, time.UTC),
+			wants: time.Date(2023, 10, 30, 10, 13, 0, 0, time.UTC),
+		},
 		{
 			name:       "Success/OneHour",
 			cronString: "0 * * * *",
@@ -68,6 +78,20 @@ func TestCronSchedule_Next(t *testing.T) {
 			input:      time.Date(2023, 10, 30, 22, 12, 43, 0, time.UTC),
 			wants:      time.Date(2023, 10, 31, 00, 0, 0, 0, time.UTC),
 		},
+		{
+			name:       "Success/EveryMinuteFromZeroToFive",
+			cronString: "0-5 * * * *",
+
+			input: time.Date(2023, 10, 30, 10, 12, 43, 0, time.UTC),
+			wants: time.Date(2023, 10, 30, 11, 0, 0, 0, time.UTC),
+		},
+		{
+			name:       "Success/InvalidCronString",
+			cronString: "*",
+
+			input: time.Date(2023, 10, 30, 10, 12, 43, 0, time.UTC),
+			err:   cronlex.ErrInvalidNodeType,
+		},
 	} {
 		t.Run(testcase.name, func(t *testing.T) {
 			sched, err := New(
@@ -77,6 +101,12 @@ func TestCronSchedule_Next(t *testing.T) {
 				WithMetrics(metrics.NoOp()),
 				WithTrace(trace.NewNoopTracerProvider().Tracer("test")),
 			)
+			if testcase.err != nil {
+				is.True(t, errors.Is(err, testcase.err))
+
+				return
+			}
+
 			is.Empty(t, err)
 
 			next := sched.Next(context.Background(), testcase.input)
@@ -84,4 +114,10 @@ func TestCronSchedule_Next(t *testing.T) {
 			is.Equal(t, testcase.wants, next)
 		})
 	}
+}
+
+func TestNoOp(t *testing.T) {
+	noop := NoOp()
+
+	is.Equal(t, time.Time{}, noop.Next(context.Background(), time.Now()))
 }
