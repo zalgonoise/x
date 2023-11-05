34c7de5e06fbb6351dc03f9e191d723ca6e8b95d
zalgonoise <mariozalgo@gmail.com>
2023-11-05 14:32:50 +0100

    cron/schedule: removed From completely (just create a new Schedule please)

--- cron/schedule/scheduler_config.go
+++ cron/schedule/scheduler_config.go
@@ -5,8 +5,6 @@ import (
 	"time"
 
 	"github.com/zalgonoise/cfg"
-	"github.com/zalgonoise/x/cron/schedule/cronlex"
-	"github.com/zalgonoise/x/cron/schedule/resolve"
 	"github.com/zalgonoise/x/errs"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -100,90 +98,4 @@ func WithTrace(tracer trace.Tracer) cfg.Option[SchedulerConfig] {
 
 		return config
 	})
-}
-
-var defaultSchedule = cronlex.Schedule{
-	Min:      resolve.Everytime{},
-	Hour:     resolve.Everytime{},
-	DayMonth: resolve.Everytime{},
-	Month:    resolve.Everytime{},
-	DayWeek:  resolve.Everytime{},
-}
-
-func From(s Scheduler, options ...cfg.Option[SchedulerConfig]) (Scheduler, error) {
-	if len(options) == 0 {
-		return s, nil
-	}
-
-	var (
-		config    = cfg.New(options...)
-		cronSched cronlex.Schedule
-		loc       *time.Location
-		tracer    trace.Tracer
-		m         Metrics
-		logger    *slog.Logger
-	)
-
-	if sched, ok := (s).(withTrace); ok {
-		s = sched.s
-		tracer = sched.tracer
-	}
-
-	if sched, ok := (s).(withLogs); ok {
-		s = sched.s
-		logger = sched.logger
-	}
-
-	if sched, ok := (s).(withMetrics); ok {
-		s = sched.s
-		m = sched.m
-	}
-
-	sched, ok := (s).(CronSchedule)
-	if !ok {
-		return s, ErrInvalidScheduler
-	}
-
-	loc = sched.Loc
-	cronSched = sched.Schedule
-
-	cron, err := newScheduler(config)
-	if err != nil {
-		return noOpScheduler{}, err
-	}
-
-	if cron.(CronSchedule).Schedule == defaultSchedule && cronSched != defaultSchedule {
-		c := cron.(CronSchedule)
-		c.Schedule = cronSched
-		cron = c
-	}
-
-	if cron.(CronSchedule).Loc == time.Local && loc != time.Local {
-		c := cron.(CronSchedule)
-		c.Loc = loc
-		cron = c
-	}
-
-	switch {
-	case config.metrics != nil:
-		cron = schedulerWithMetrics(cron, config.metrics)
-	case m != nil:
-		cron = schedulerWithMetrics(cron, m)
-	}
-
-	switch {
-	case config.handler != nil:
-		cron = schedulerWithLogs(cron, config.handler)
-	case logger != nil:
-		cron = schedulerWithLogs(cron, logger.Handler())
-	}
-
-	switch {
-	case config.handler != nil:
-		cron = schedulerWithTrace(cron, config.tracer)
-	case tracer != nil:
-		cron = schedulerWithTrace(cron, tracer)
-	}
-
-	return cron, nil
 }
