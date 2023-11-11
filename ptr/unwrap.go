99862a8fd0dba881726d9a1fc93e1a2921a8213d
zalgonoise <mariozalgo@gmail.com>
2023-11-11 16:13:17 +0100

    ptr/unwrap: added decorator / type unwrapping function

---
+++ ptr/unwrap.go
@@ -0,0 +1,42 @@
+package ptr
+
+import (
+	"errors"
+	"unsafe"
+)
+
+var ErrMaxDepth = errors.New("maximum depth reached")
+
+// Unwrap traverses nested data structures to find a given type, that is wrapped as the first
+// element of other (decorator) types.
+//
+// For instance, if a given service exposes a withLogs, withMetrics, and / or withTrace types,
+// with a struct format that places the service as the first element and the other component as the second,
+// this function will be able to extract that value in search for a given type T.
+//
+// Unwrap checks if the input value is of type T, and returns it if so. If it is not, then it uses
+// an unsafe approach casting the input value as a struct containing an element of type T (as the first field).
+// This retrieved element is then passed back into a private unwrap function that keeps looking for the type T
+// until the maximum depth of 64 is reached.
+//
+// This technique allows to blindly extract a type from multiple nested decorators; if you're positive that the
+// fields are there and ordered in the expected way.
+func Unwrap[T any](value any) (T, error) {
+	return unwrap[T](value, 0)
+}
+
+func unwrap[T any](value any, iter int) (T, error) {
+	if iter >= maxDepth {
+		return *new(T), ErrMaxDepth
+	}
+
+	switch v := value.(type) {
+	case T:
+		return v, nil
+	default:
+		cast := (*struct{ x T })(unsafe.Pointer(&value))
+
+		iter++
+		return unwrap[T](cast.x, iter)
+	}
+}
---
+++ ptr/unwrap_test.go
@@ -0,0 +1,80 @@
+package ptr_test
+
+import (
+	"errors"
+	"io"
+	"log/slog"
+	"reflect"
+	"testing"
+
+	"github.com/zalgonoise/x/ptr"
+)
+
+type testDoer interface {
+	Do() error
+}
+
+type testMetrics interface{}
+
+type testService struct{}
+
+func (s testService) Do() error { return nil }
+
+type testServiceWithLogs struct {
+	s      testDoer
+	logger *slog.Logger
+}
+
+func (s testServiceWithLogs) Do() error { return nil }
+
+type testServiceWithMetrics struct {
+	s testDoer
+	m testMetrics
+}
+
+func (s testServiceWithMetrics) Do() error { return nil }
+
+func TestUnwrap(t *testing.T) {
+	for _, testcase := range []struct {
+		name  string
+		input testDoer
+		wants testDoer
+		err   error
+	}{
+		{
+			name:  "TestService",
+			input: testService{},
+			wants: testService{},
+		},
+		{
+			name: "WrappedOnce",
+			input: testServiceWithLogs{
+				s:      testService{},
+				logger: slog.New(slog.NewJSONHandler(io.Discard, nil)),
+			},
+			wants: testService{},
+		},
+		{
+			name: "WrappedTwice",
+			input: testServiceWithMetrics{
+				s: testServiceWithLogs{
+					s:      testService{},
+					logger: slog.New(slog.NewJSONHandler(io.Discard, nil)),
+				},
+				m: struct{}{},
+			},
+			wants: testService{},
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			doer, err := ptr.Unwrap[testService](testcase.input)
+			if !errors.Is(err, testcase.err) {
+				t.Errorf("unexpected error: wanted %v ; got %v", testcase.err, err)
+			}
+
+			if !reflect.DeepEqual(testcase.wants, doer) {
+				t.Errorf("output mismatch error: wanted %v ; got %v", testcase.wants, doer)
+			}
+		})
+	}
+}
