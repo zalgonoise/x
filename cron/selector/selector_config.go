49adb3f9d11435f3eb407ce2945f54774efeca82
zalgonoise <mariozalgo@gmail.com>
2023-11-18 17:23:59 +0100

    cron/selector/config: improved WithExecutors option; added code docs

--- cron/selector/selector_config.go
+++ cron/selector/selector_config.go
@@ -16,24 +16,38 @@ type Config struct {
 	tracer  trace.Tracer
 }
 
+// WithExecutors configures the Selector with the input executor.Executor(s).
+//
+// This call returns a cfg.NoOp cfg.Option if the input set of executor.Executor is empty, or contains
+// only nil and / or no-op executor.Executor.
 func WithExecutors(executors ...executor.Executor) cfg.Option[Config] {
-	if len(executors) == 0 {
+	execs := make([]executor.Executor, 0, len(executors))
+	for i := range executors {
+		if executors[i] == nil || executors[i] == executor.NoOp() {
+			continue
+		}
+
+		execs = append(execs, executors[i])
+	}
+
+	if len(execs) == 0 {
 		return cfg.NoOp[Config]{}
 	}
 
 	return cfg.Register(func(config Config) Config {
 		if len(config.exec) == 0 {
-			config.exec = executors
+			config.exec = execs
 
 			return config
 		}
 
-		config.exec = append(config.exec, executors...)
+		config.exec = append(config.exec, execs...)
 
 		return config
 	})
 }
 
+// WithMetrics decorates the Selector with the input metrics registry.
 func WithMetrics(m Metrics) cfg.Option[Config] {
 	if m == nil {
 		return cfg.NoOp[Config]{}
@@ -46,6 +60,7 @@ func WithMetrics(m Metrics) cfg.Option[Config] {
 	})
 }
 
+// WithLogger decorates the Selector with the input logger.
 func WithLogger(logger *slog.Logger) cfg.Option[Config] {
 	if logger == nil {
 		return cfg.NoOp[Config]{}
@@ -58,6 +73,7 @@ func WithLogger(logger *slog.Logger) cfg.Option[Config] {
 	})
 }
 
+// WithLogHandler decorates the Selector with logging using the input log handler.
 func WithLogHandler(handler slog.Handler) cfg.Option[Config] {
 	if handler == nil {
 		return cfg.NoOp[Config]{}
@@ -70,6 +86,7 @@ func WithLogHandler(handler slog.Handler) cfg.Option[Config] {
 	})
 }
 
+// WithTrace decorates the Selector with the input trace.Tracer.
 func WithTrace(tracer trace.Tracer) cfg.Option[Config] {
 	if tracer == nil {
 		return cfg.NoOp[Config]{}
