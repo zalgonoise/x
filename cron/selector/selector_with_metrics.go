525c1ef0dc1a7aa8a91143b5c115b694eb32dd01
zalgonoise <mariozalgo@gmail.com>
2023-11-18 17:24:32 +0100

    cron/selector/with_metrics: improved logic; exported decorator function; added code docs

--- cron/selector/selector_with_metrics.go
+++ cron/selector/selector_with_metrics.go
@@ -2,6 +2,8 @@ package selector
 
 import (
 	"context"
+
+	"github.com/zalgonoise/x/cron/metrics"
 )
 
 type Metrics interface {
@@ -14,6 +16,16 @@ type withMetrics struct {
 	m Metrics
 }
 
+// Next picks up the following scheduled job to execute from its configured (set of) executor.Executor, and
+// calls its Exec method.
+//
+// This call also imposes a minimum step duration of 50ms, to ensure that early-runs are not executed twice due to the
+// nature of using clocks in Go. This sleep is deferred to come in after the actual execution of the job.
+//
+// The Selector allows multiple executor.Executor to be configured, and multiple executor.Executor can share similar
+// execution times. If that is the case, the executor is launched in an executor.Multi call.
+//
+// The error returned from a Next call is the error raised by the executor.Executor's Exec call.
 func (s withMetrics) Next(ctx context.Context) error {
 	s.m.IncSelectorSelectCalls()
 
@@ -26,19 +38,28 @@ func (s withMetrics) Next(ctx context.Context) error {
 	return nil
 }
 
-func selectorWithMetrics(s Selector, m Metrics) Selector {
-	if s == nil {
+// AddMetrics decorates the input Selector with metrics, using the input Metrics interface.
+//
+// If the input Selector is nil or a no-op Selector, a no-op Selector is returned. If the input Metrics is nil or if it
+// is a no-op Metrics interface, then the input Selector is returned as-is.
+//
+// If the input Selector is already a Selector with metrics, then this Selector with metrics is returned with the new
+// Metrics interface configured in place of the former.
+//
+// Otherwise, the Selector is decorated with metrics within a custom type that implements Selector.
+func AddMetrics(s Selector, m Metrics) Selector {
+	if s == nil || s == NoOp() {
 		return noOpSelector{}
 	}
 
-	if m == nil {
+	if m == nil || m == metrics.NoOp() {
 		return s
 	}
 
-	if metrics, ok := s.(withMetrics); ok {
-		metrics.m = m
+	if metric, ok := s.(withMetrics); ok {
+		metric.m = m
 
-		return metrics
+		return metric
 	}
 
 	return withMetrics{
