2cf8a5a16fb116ce3254a0568ce0e868a570782f
zalgonoise <mariozalgo@gmail.com>
2023-11-20 17:55:19 +0100

    cron/executor: documented Multi; updated local variable definition to be the actual Executor instead of its index

--- cron/executor/multi.go
+++ cron/executor/multi.go
@@ -6,6 +6,13 @@ import (
 	"sync"
 )
 
+// Multi calls the Executor.Exec on a set of Executor that are meant to be executed at the same time (e.g. when
+// scheduled for the same time).
+//
+// This call will execute the input (set of) action(s) in parallel, collecting any error(s) that the Executor(s) raises.
+//
+// The returned error is a joined error, for any failing executions. The executions are synchronized in a
+// sync.WaitGroup, and are bound to the input context.Context's lifetime.
 func Multi(ctx context.Context, execs ...Executor) error {
 	errs := make([]error, 0, len(execs))
 
@@ -13,13 +20,13 @@ func Multi(ctx context.Context, execs ...Executor) error {
 	wg := &sync.WaitGroup{}
 
 	for i := range execs {
-		i := i
+		e := execs[i]
 
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 
-			if err := execs[i].Exec(ctx); err != nil {
+			if err := e.Exec(ctx); err != nil {
 				mu.Lock()
 				errs = append(errs, err)
 				mu.Unlock()
