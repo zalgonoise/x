3ac8e08505787afc89a1f5a78a4744492af60973
zalgonoise <mariozalgo@gmail.com>
2023-11-20 17:53:37 +0100

    cron/scheduler/config: documented types; removed unused error types; renamed SchedulerConfig to Config; improved logic and checks

--- cron/schedule/scheduler_config.go
+++ cron/schedule/scheduler_config.go
@@ -5,21 +5,10 @@ import (
 	"time"
 
 	"github.com/zalgonoise/cfg"
-	"github.com/zalgonoise/x/errs"
 	"go.opentelemetry.io/otel/trace"
 )
 
-const (
-	errDomain = errs.Domain("x/cron/schedule")
-
-	ErrInvalid = errs.Kind("invalid")
-
-	ErrScheduler = errs.Entity("scheduler")
-)
-
-var ErrInvalidScheduler = errs.WithDomain(errDomain, ErrInvalid, ErrScheduler)
-
-type SchedulerConfig struct {
+type Config struct {
 	cronString string
 	loc        *time.Location
 
@@ -28,72 +17,82 @@ type SchedulerConfig struct {
 	tracer  trace.Tracer
 }
 
-func WithSchedule(cronString string) cfg.Option[SchedulerConfig] {
+// WithSchedule configures the Scheduler with the input cron string.
+//
+// This call returns a cfg.NoOp cfg.Option if the input cron string is empty.
+func WithSchedule(cronString string) cfg.Option[Config] {
 	if cronString == "" {
-		return cfg.NoOp[SchedulerConfig]{}
+		return cfg.NoOp[Config]{}
 	}
 
-	return cfg.Register(func(config SchedulerConfig) SchedulerConfig {
+	return cfg.Register(func(config Config) Config {
 		config.cronString = cronString
 
 		return config
 	})
 }
 
-func WithLocation(loc *time.Location) cfg.Option[SchedulerConfig] {
+// WithLocation configures the Scheduler with the input time.Location.
+//
+// This call returns a cfg.NoOp cfg.Option if the input time.Location is nil.
+func WithLocation(loc *time.Location) cfg.Option[Config] {
 	if loc == nil {
-		return cfg.NoOp[SchedulerConfig]{}
+		return cfg.NoOp[Config]{}
 	}
 
-	return cfg.Register(func(config SchedulerConfig) SchedulerConfig {
+	return cfg.Register(func(config Config) Config {
 		config.loc = loc
 
 		return config
 	})
 }
 
-func WithMetrics(m Metrics) cfg.Option[SchedulerConfig] {
+// WithMetrics decorates the Scheduler with the input metrics registry.
+func WithMetrics(m Metrics) cfg.Option[Config] {
 	if m == nil {
-		return cfg.NoOp[SchedulerConfig]{}
+		return cfg.NoOp[Config]{}
 	}
 
-	return cfg.Register(func(config SchedulerConfig) SchedulerConfig {
+	return cfg.Register(func(config Config) Config {
 		config.metrics = m
 
 		return config
 	})
 }
 
-func WithLogger(logger *slog.Logger) cfg.Option[SchedulerConfig] {
+// WithLogger decorates the Scheduler with the input logger.
+func WithLogger(logger *slog.Logger) cfg.Option[Config] {
 	if logger == nil {
-		return cfg.NoOp[SchedulerConfig]{}
+		return cfg.NoOp[Config]{}
 	}
 
-	return cfg.Register(func(config SchedulerConfig) SchedulerConfig {
+	return cfg.Register(func(config Config) Config {
 		config.handler = logger.Handler()
 
 		return config
 	})
 }
 
-func WithLogHandler(handler slog.Handler) cfg.Option[SchedulerConfig] {
+// WithLogHandler decorates the Scheduler with logging using the input log handler.
+func WithLogHandler(handler slog.Handler) cfg.Option[Config] {
 	if handler == nil {
-		return cfg.NoOp[SchedulerConfig]{}
+		return cfg.NoOp[Config]{}
 	}
 
-	return cfg.Register(func(config SchedulerConfig) SchedulerConfig {
+	return cfg.Register(func(config Config) Config {
 		config.handler = handler
 
 		return config
 	})
 }
 
-func WithTrace(tracer trace.Tracer) cfg.Option[SchedulerConfig] {
+// WithTrace decorates the Scheduler with the input trace.Tracer.
+func WithTrace(tracer trace.Tracer) cfg.Option[Config] {
 	if tracer == nil {
-		return cfg.NoOp[SchedulerConfig]{}
+		return cfg.NoOp[Config]{}
 	}
 
-	return cfg.Register(func(config SchedulerConfig) SchedulerConfig {
+	return cfg.Register(func(config Config) Config {
 		config.tracer = tracer
 
 		return config
