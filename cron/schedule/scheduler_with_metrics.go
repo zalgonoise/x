b2ba13095c5b735773c27b4667bcc4761e253526
zalgonoise <mariozalgo@gmail.com>
2023-11-20 17:54:18 +0100

    cron/scheduler/with_metrics: documented types; exported decorator function

--- cron/schedule/scheduler_with_metrics.go
+++ cron/schedule/scheduler_with_metrics.go
@@ -3,9 +3,13 @@ package schedule
 import (
 	"context"
 	"time"
+
+	"github.com/zalgonoise/x/cron/metrics"
 )
 
+// Metrics describes the actions that register Scheduler-related metrics.
 type Metrics interface {
+	// IncSchedulerNextCalls increases the count of Next calls, by the Scheduler.
 	IncSchedulerNextCalls()
 }
 
@@ -14,25 +18,35 @@ type withMetrics struct {
 	m Metrics
 }
 
+// Next calculates and returns the following scheduled time, from the input time.Time.
 func (s withMetrics) Next(ctx context.Context, now time.Time) time.Time {
 	s.m.IncSchedulerNextCalls()
 
 	return s.s.Next(ctx, now)
 }
 
-func schedulerWithMetrics(s Scheduler, m Metrics) Scheduler {
-	if s == nil {
-		return noOpScheduler{}
+// AddMetrics decorates the input Scheduler with metrics, using the input Metrics interface.
+//
+// If the input Scheduler is nil or a no-op Scheduler, a no-op Scheduler is returned. If the input Metrics is nil or if
+// it is a no-op Metrics interface, then the input Scheduler is returned as-is.
+//
+// If the input Scheduler is already a Scheduler with metrics, then this Scheduler with metrics is returned with the new
+// Metrics interface configured in place of the former.
+//
+// Otherwise, the Scheduler is decorated with metrics within a custom type that implements Scheduler.
+func AddMetrics(s Scheduler, m Metrics) Scheduler {
+	if s == nil || s == NoOp() {
+		return NoOp()
 	}
 
-	if m == nil {
+	if m == nil || m == metrics.NoOp() {
 		return s
 	}
 
-	if metrics, ok := s.(withMetrics); ok {
-		metrics.m = m
+	if metric, ok := s.(withMetrics); ok {
+		metric.m = m
 
-		return metrics
+		return metric
 	}
 
 	return withMetrics{
