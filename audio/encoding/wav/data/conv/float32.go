a9ebd4fb84ea1100f46b5e2ad78e496fce57ed64
zalgonoise <mariozalgo@gmail.com>
2023-11-26 18:54:56 +0100

    audio/encoding/wav/data/conv/float32: linter compliance fixes

--- audio/encoding/wav/data/conv/float32.go
+++ audio/encoding/wav/data/conv/float32.go
@@ -4,21 +4,23 @@ import (
 	"unsafe"
 )
 
-// Float32 is a 32bit IEEE Floating Point audio Converter
+const sizeFloat32 = 4
+
+// Float32 is a 32bit IEEE Floating Point audio Converter.
 type Float32 struct{}
 
-// Parse consumes the input audio buffer, returning its floating point audio representation
+// Parse consumes the input audio buffer, returning its floating point audio representation.
 func (Float32) Parse(buf []byte) []float64 {
 	data := *(*[]uint32)(unsafe.Pointer(&buf))
 
 	return convert(
-		data[:len(buf)/4], func(v uint32) float64 {
+		data[:len(buf)/sizeFloat32], func(v uint32) float64 {
 			return float64(*(*float32)(unsafe.Pointer(&v)))
 		},
 	)
 }
 
-// Bytes consumes the input floating point audio buffer, returning its byte representation
+// Bytes consumes the input floating point audio buffer, returning its byte representation.
 func (Float32) Bytes(buf []float64) []byte {
 	value := convert(
 		buf, func(f float64) float32 {
@@ -28,17 +30,16 @@ func (Float32) Bytes(buf []float64) []byte {
 
 	uintValue := *(*[]uint32)(unsafe.Pointer(&value))
 
-	data := make([]byte, len(uintValue)*4)
+	data := make([]byte, len(uintValue)*sizeFloat32)
 
 	for i := range value {
-		append4Bytes(i, data, *(*[4]byte)(unsafe.Pointer(&uintValue[i])))
+		append4Bytes(i, data, *(*[sizeFloat32]byte)(unsafe.Pointer(&uintValue[i])))
 	}
 
 	return data
-
 }
 
-// Value consumes the input floating point audio buffer, returning its PCM audio values as a slice of int
+// Value consumes the input floating point audio buffer, returning its PCM audio values as a slice of int.
 func (Float32) Value(buf []float64) []int {
 	return convert(
 		buf, func(f float64) int {
