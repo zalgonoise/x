9def4e6ae9615b16ed609d738c83b3d6c075777e
zalgonoise <mariozalgo@gmail.com>
2023-11-26 19:00:26 +0100

    audio/fft/window: linter compliance fixes

--- audio/fft/window/window.go
+++ audio/fft/window/window.go
@@ -5,44 +5,50 @@ import (
 )
 
 const (
-	tau    = math.Pi * 2
-	twoTau = math.Pi * 4
+	minBlockSize = 4
+	tau          = math.Pi * 2
+	twoTau       = math.Pi * 4
 )
 
-// WindowFunc is a type of function that generates a Window based on an input size
-type WindowFunc func(int) Window
+// GeneratorFunc is a type of function that generates a Window based on an input size.
+type GeneratorFunc func(int) Window
 
-// Generate calls the WindowFunc itself, with the input int `size`
-func (w WindowFunc) Generate(size int) Window {
+// Generate calls the GeneratorFunc itself, with the input int `size`.
+func (w GeneratorFunc) Generate(size int) Window {
 	return w(size)
 }
 
 // Window is a custom type for a slice of float64, that repesents a Window that shapes
-// input signal
+// input signal.
 type Window []float64
 
 // Apply will go through the input slice of float64 values, and apply the window to it by
-// multiplying the value by the ratio in the respective element in the Window
+// multiplying the value by the ratio in the respective element in the Window.
 func (w Window) Apply(value []float64) {
 	for idx := range value {
 		value[idx] *= w[idx]
 	}
 }
 
+//nolint:gochecknoglobals // immutable slice that contains supported block sizes
 var blockSizes = []int{8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192}
 
-// New creates a new Window from the input WindowFunc and size
+// New creates a new Window from the input GeneratorFunc and size
 //
 // The size should be a power of two supported in the fft package's BlockSize.
 //
 // In case the input size `int` is not a power of two, it will fallback to the next
 // available block size, with a series of bit-shifting operations on the input size
 // to derive the closest element in the (private) blockSizes slice of ints.
-func New(w WindowFunc, size int) Window {
-	blockSize := size >> 4
-	var count int
-	for i := blockSize; i > 0; i = i >> 1 {
+func New(w GeneratorFunc, size int) Window {
+	var (
+		count     int
+		blockSize = size >> minBlockSize
+	)
+
+	for i := blockSize; i > 0; i >>= 1 {
 		count++
 	}
+
 	return w(blockSizes[count])
 }
--- audio/fft/window/window_test.go
+++ audio/fft/window/window_test.go
@@ -12,7 +12,7 @@ func BenchmarkHamming(b *testing.B) {
 	)
 
 	b.Run("Library", func(b *testing.B) {
-		var hamming = func(L int) []float64 {
+		hamming := func(L int) []float64 {
 			r := make([]float64, L)
 
 			if L == 1 {
@@ -20,6 +20,7 @@ func BenchmarkHamming(b *testing.B) {
 			} else {
 				N := L - 1
 				coef := math.Pi * 2 / float64(N)
+
 				for n := 0; n <= N; n++ {
 					r[n] = 0.54 - 0.46*math.Cos(coef*float64(n))
 				}
@@ -29,26 +30,31 @@ func BenchmarkHamming(b *testing.B) {
 		}
 
 		b.ResetTimer()
+
 		for i := 0; i < b.N; i++ {
 			outA = hamming(1024)
 		}
-		_ = outA
 
+		_ = outA
 	})
 
 	b.Run("Self", func(b *testing.B) {
 		b.ResetTimer()
+
 		for i := 0; i < b.N; i++ {
 			outB = newHamming(1024)
 		}
+
 		_ = outB
 	})
 
 	b.Run("Compare", func(b *testing.B) {
 		if len(outA) != len(outB) {
 			b.Errorf("length mismatch error: wanted %d ; got %d", len(outA), len(outB))
+
 			return
 		}
+
 		for i := range outA {
 			if outA[i] != outB[i] {
 				b.Errorf("output mismatch error: A: %v ; B: %v", outA[i], outB[i])
@@ -64,7 +70,7 @@ func BenchmarkHann(b *testing.B) {
 	)
 
 	b.Run("Library", func(b *testing.B) {
-		var hann = func(L int) []float64 {
+		hann := func(L int) []float64 {
 			r := make([]float64, L)
 
 			if L == 1 {
@@ -72,6 +78,7 @@ func BenchmarkHann(b *testing.B) {
 			} else {
 				N := L - 1
 				coef := 2 * math.Pi / float64(N)
+
 				for n := 0; n <= N; n++ {
 					r[n] = 0.5 * (1 - math.Cos(coef*float64(n)))
 				}
@@ -81,24 +88,28 @@ func BenchmarkHann(b *testing.B) {
 		}
 
 		b.ResetTimer()
+
 		for i := 0; i < b.N; i++ {
 			outA = hann(1024)
 		}
-		_ = outA
 
+		_ = outA
 	})
 
 	b.Run("Self", func(b *testing.B) {
 		b.ResetTimer()
+
 		for i := 0; i < b.N; i++ {
 			outB = newHann(1024)
 		}
+
 		_ = outB
 	})
 
 	b.Run("Compare", func(b *testing.B) {
 		if len(outA) != len(outB) {
 			b.Errorf("length mismatch error: wanted %d ; got %d", len(outA), len(outB))
+
 			return
 		}
 		for i := range outA {
@@ -116,7 +127,7 @@ func BenchmarkBlackman(b *testing.B) {
 	)
 
 	b.Run("Library", func(b *testing.B) {
-		var blackman = func(L int) []float64 {
+		blackman := func(L int) []float64 {
 			r := make([]float64, L)
 			if L == 1 {
 				r[0] = 1
@@ -124,35 +135,43 @@ func BenchmarkBlackman(b *testing.B) {
 				N := L - 1
 				for n := 0; n <= N; n++ {
 					const term0 = 0.42
+
 					term1 := -0.5 * math.Cos(2*math.Pi*float64(n)/float64(N))
 					term2 := 0.08 * math.Cos(4*math.Pi*float64(n)/float64(N))
+
 					r[n] = term0 + term1 + term2
 				}
 			}
+
 			return r
 		}
 
 		b.ResetTimer()
+
 		for i := 0; i < b.N; i++ {
 			outA = blackman(1024)
 		}
-		_ = outA
 
+		_ = outA
 	})
 
 	b.Run("Self", func(b *testing.B) {
 		b.ResetTimer()
+
 		for i := 0; i < b.N; i++ {
 			outB = newBlackman(1024)
 		}
+
 		_ = outB
 	})
 
 	b.Run("Compare", func(b *testing.B) {
 		if len(outA) != len(outB) {
 			b.Errorf("length mismatch error: wanted %d ; got %d", len(outA), len(outB))
+
 			return
 		}
+
 		for i := range outA {
 			if outA[i] != outB[i] {
 				b.Errorf("output mismatch error: A: %v ; B: %v", outA[i], outB[i])
