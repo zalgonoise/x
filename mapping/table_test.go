411a02463cde97743267e49b38772c23f2c74bc1
zalgonoise <mariozalgo@gmail.com>
2023-12-30 14:45:00 +0100

    mapping: added tests

---
+++ mapping/table_test.go
@@ -0,0 +1,113 @@
+package mapping
+
+import "testing"
+
+func TestTable(t *testing.T) {
+	key1 := "alpha"
+	value1 := "A-alpha"
+
+	key2 := "beta"
+	value2 := "B-beta"
+
+	key3 := "gamma"
+
+	zero := "zero"
+
+	m := map[string]*string{
+		key1: &value1,
+		key2: &value2,
+	}
+
+	for _, testcase := range []struct {
+		name  string
+		m     map[string]*string
+		zero  *string
+		input string
+		wants *string
+		ok    bool
+	}{
+		{
+			name:  "WithValue/NilZero",
+			m:     m,
+			zero:  nil,
+			input: key1,
+			ok:    true,
+			wants: &value1,
+		},
+		{
+			name:  "WithValue/SetZero",
+			m:     m,
+			zero:  &zero,
+			input: key2,
+			ok:    true,
+			wants: &value2,
+		},
+		{
+			name:  "WithoutValue/NilZero",
+			m:     m,
+			zero:  nil,
+			input: key3,
+			ok:    false,
+			wants: nil,
+		},
+		{
+			name:  "WithoutValue/SetZero",
+			m:     m,
+			zero:  &zero,
+			input: key3,
+			ok:    false,
+			wants: &zero,
+		},
+		{
+			name:  "EmptyKey/NilZero",
+			m:     m,
+			zero:  nil,
+			input: "",
+			ok:    false,
+			wants: nil,
+		},
+		{
+			name:  "EmptyKey/SetZero",
+			m:     m,
+			zero:  &zero,
+			input: "",
+			ok:    false,
+			wants: &zero,
+		},
+		{
+			name:  "EmptyMap/NilZero",
+			m:     map[string]*string{},
+			zero:  nil,
+			input: "",
+			ok:    false,
+			wants: nil,
+		},
+		{
+			name:  "EmptyMap/SetZero",
+			m:     map[string]*string{},
+			zero:  &zero,
+			input: "",
+			ok:    false,
+			wants: &zero,
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			table := New(testcase.m, testcase.zero)
+
+			result, ok := table.Get(testcase.input)
+			isEqual(t, testcase.ok, ok)
+			isEqual(t, testcase.wants, result)
+		})
+	}
+}
+
+func isEqual[T comparable](t *testing.T, wants, got T) {
+	if got != wants {
+		t.Errorf("output mismatch error: wanted %v ; got %v", wants, got)
+		t.Fail()
+
+		return
+	}
+
+	t.Logf("output matched expected value: %v", wants)
+}
