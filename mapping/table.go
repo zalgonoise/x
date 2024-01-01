bb1fa1b95806e4e5b7fe4af9dfd85a7db723637b
zalgonoise <mariozalgo@gmail.com>
2024-01-01 13:06:01 +0100

    mapping/table: added NewTable function to provide access to the concrete type

--- mapping/table.go
+++ mapping/table.go
@@ -1,5 +1,9 @@
 package mapping
 
+import (
+	"github.com/zalgonoise/cfg"
+)
+
 type Table[K comparable, T any] struct {
 	zero   T
 	values map[K]T
@@ -18,4 +22,13 @@ func (t Table[K, T]) Get(key K) (T, bool) {
 	}
 
 	return value, true
+}
+
+func NewTable[K comparable, T any](values map[K]T, opts ...cfg.Option[Config[K, T]]) Table[K, T] {
+	config := cfg.New(opts...)
+
+	return Table[K, T]{
+		zero:   config.zero,
+		values: values,
+	}
 }
