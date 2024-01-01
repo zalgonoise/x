a9ed57f8d03ddfe756ae135afdd1869b68e4fe1e
zalgonoise <mariozalgo@gmail.com>
2024-01-01 13:04:42 +0100

    mapping/index: added NewIndex function to provide access to the Index concrete type

--- mapping/index.go
+++ mapping/index.go
@@ -1,5 +1,11 @@
 package mapping
 
+import (
+	"slices"
+
+	"github.com/zalgonoise/cfg"
+)
+
 type Index[K comparable, T any] struct {
 	Keys []K
 
@@ -20,4 +26,24 @@ func (i Index[K, T]) Get(key K) (T, bool) {
 	}
 
 	return value, true
+}
+
+func NewIndex[K comparable, T any](values map[K]T, opts ...cfg.Option[Config[K, T]]) Index[K, T] {
+	config := cfg.New(opts...)
+
+	idx := make([]K, 0, len(values))
+	for key := range values {
+		idx = append(idx, key)
+	}
+
+	if config.cmpFunc != nil {
+		slices.SortFunc(idx, config.cmpFunc)
+	}
+
+	return Index[K, T]{
+		Keys: idx,
+
+		zero:   config.zero,
+		values: values,
+	}
 }
