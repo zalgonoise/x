afe1834a06d8b935063a001c940f5dd868a503d9
zalgonoise <mariozalgo@gmail.com>
2024-01-02 15:09:47 +0100

    mapping/table: implemented Set method; documented types

--- mapping/table.go
+++ mapping/table.go
@@ -4,12 +4,16 @@ import (
 	"github.com/zalgonoise/cfg"
 )
 
+// Table is a simple Field type, that provides control over the returned zero value when accessing keys in a map.
 type Table[K comparable, T any] struct {
 	zero   T
 	values map[K]T
 }
 
-func (t Table[K, T]) Get(key K) (T, bool) {
+// Get fetches the value in a mapping Field for a given key. If the value does not exist, the Field's
+// configured zero value is returned. A boolean value is also returned to highlight whether accessing the key was
+// successful or not.
+func (t *Table[K, T]) Get(key K) (T, bool) {
 	var zero K
 
 	if key == zero || len(t.values) == 0 {
@@ -24,10 +28,20 @@ func (t Table[K, T]) Get(key K) (T, bool) {
 	return value, true
 }
 
-func NewTable[K comparable, T any](values map[K]T, opts ...cfg.Option[Config[K, T]]) Table[K, T] {
+// Set replaces the value of a certain key in the map, or it adds it if it does not exist. The returned boolean value
+// represents whether the key is new in the mapping Field or not.
+func (t *Table[K, T]) Set(key K, value T) bool {
+	_, ok := t.values[key]
+
+	t.values[key] = value
+
+	return !ok
+}
+
+func NewTable[K comparable, T any](values map[K]T, opts ...cfg.Option[Config[K, T]]) *Table[K, T] {
 	config := cfg.New(opts...)
 
-	return Table[K, T]{
+	return &Table[K, T]{
 		zero:   config.zero,
 		values: values,
 	}
