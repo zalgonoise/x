4da9ce3c3ae7f3f61b2b2f4fee2d08189db6594c
zalgonoise <mariozalgo@gmail.com>
2024-01-08 15:06:52 +0100

    grid/queue: queue logic with generic numeric priority element

--- grid/queue.go
+++ grid/queue.go
@@ -1,31 +1,31 @@
 package grid
 
-type QueueItem[T any] struct {
+type QueueItem[T any, I Integer] struct {
 	Item     T
-	Priority int
+	Priority I
 }
 
-type PriorityQueue[T any] struct {
-	q []QueueItem[T]
+type PriorityQueue[T any, I Integer] struct {
+	q []QueueItem[T, I]
 }
 
-func (q *PriorityQueue[T]) Len() int {
+func (q *PriorityQueue[T, I]) Len() int {
 	return len(q.q)
 }
 
-func (q *PriorityQueue[T]) Less(i, j int) bool {
+func (q *PriorityQueue[T, I]) Less(i, j int) bool {
 	return q.q[i].Priority < q.q[j].Priority
 }
 
-func (q *PriorityQueue[T]) Swap(i, j int) {
+func (q *PriorityQueue[T, I]) Swap(i, j int) {
 	q.q[i], q.q[j] = q.q[j], q.q[i]
 }
 
-func (q *PriorityQueue[T]) Push(x QueueItem[T]) {
+func (q *PriorityQueue[T, I]) Push(x QueueItem[T, I]) {
 	q.q = append(q.q, x)
 }
 
-func (q *PriorityQueue[T]) Pop() QueueItem[T] {
+func (q *PriorityQueue[T, I]) Pop() QueueItem[T, I] {
 	old := q.q
 	n := len(old)
 
