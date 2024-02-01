e329e6a31a22f7e8c482c12523e84634363a0fbc
zalgonoise <mariozalgo@gmail.com>
2024-02-01 19:29:27 +0100

    authz/tools.go: adding tools for go.mod versioning

---
+++ authz/tools.go
@@ -0,0 +1,13 @@
+package tools
+
+import (
+	_ "github.com/bufbuild/buf/cmd/buf"
+	_ "github.com/bufbuild/buf/cmd/protoc-gen-buf-breaking"
+	_ "github.com/bufbuild/buf/cmd/protoc-gen-buf-lint"
+	_ "github.com/envoyproxy/protoc-gen-validate"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "github.com/vektra/mockery/v2"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
