3124f04eb143f91a7581ba701c1473c1f240b85c
zalgonoise <mariozalgo@gmail.com>
2024-02-10 19:24:46 +0100

    authz/ca/template/config: optional config for certificate templates

---
+++ authz/ca/template_config.go
@@ -0,0 +1,82 @@
+package ca
+
+import (
+	"crypto/ecdsa"
+	"crypto/x509/pkix"
+	"math/big"
+
+	"github.com/zalgonoise/cfg"
+)
+
+func newDefaultTemplate() Template {
+	return Template{
+		DurMonth:  defaultDurMonths,
+		SerialExp: defaultExp,
+		SerialSub: defaultSub,
+	}
+}
+
+func WithName(name pkix.Name) cfg.Option[Template] {
+	if name.CommonName == "" {
+		return cfg.NoOp[Template]{}
+	}
+
+	return cfg.Register[Template](func(template Template) Template {
+		template.Name = name
+
+		return template
+	})
+}
+
+func WithDurMonth(durMonth int) cfg.Option[Template] {
+	if durMonth == 0 {
+		durMonth = defaultDurMonths
+	}
+
+	return cfg.Register[Template](func(template Template) Template {
+		template.DurMonth = durMonth
+
+		return template
+	})
+}
+
+func WithPrivateKey(key *ecdsa.PrivateKey) cfg.Option[Template] {
+	if key == nil {
+		return cfg.NoOp[Template]{}
+	}
+
+	return cfg.Register[Template](func(template Template) Template {
+		template.PrivateKey = key
+
+		return template
+	})
+}
+
+func WithSerial(i *big.Int) cfg.Option[Template] {
+	if i == nil {
+		return cfg.NoOp[Template]{}
+	}
+
+	return cfg.Register[Template](func(template Template) Template {
+		template.Serial = i
+
+		return template
+	})
+}
+
+func WithNewSerial(exp, sub int64) cfg.Option[Template] {
+	if exp == 0 {
+		exp = defaultExp
+	}
+
+	if sub == 0 {
+		sub = defaultSub
+	}
+
+	return cfg.Register[Template](func(template Template) Template {
+		template.SerialExp = exp
+		template.SerialSub = sub
+
+		return template
+	})
+}
