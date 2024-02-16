9d92da09ed9146ea5cec0f5b93f7ec06d005fdf3
zalgonoise <mariozalgo@gmail.com>
2024-02-16 15:16:07 +0100

    authz/grpcserver/test: added tests

---
+++ authz/grpcserver/server_test.go
@@ -0,0 +1,56 @@
+package grpcserver
+
+import (
+	"context"
+	"net"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/zalgonoise/x/authz/grpcserver/mocks"
+)
+
+func TestServer_Serve(t *testing.T) {
+	s := Server{server: grpc.NewServer()}
+
+	testListener, err := net.Listen("tcp", "127.0.0.1:0")
+	require.NoError(t, err)
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		err := s.Serve(testListener)
+		require.NoError(t, err)
+	}()
+
+	conn, err := grpc.Dial(testListener.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	for state := conn.GetState(); state != connectivity.Ready; state = conn.GetState() {
+		require.True(t, conn.WaitForStateChange(ctx, state), "timeout waiting for conn to get ready")
+	}
+
+	s.Shutdown(context.Background())
+	wg.Wait()
+}
+
+func TestNewServer(t *testing.T) {
+	metrics := mocks.NewMetrics(t)
+
+	metrics.EXPECT().RegisterCollector(mock.Anything).Return()
+
+	s := NewServer(metrics, nil, nil)
+	require.NotNil(t, s.server)
+}
