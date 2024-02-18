c0aae32745f1da6a657535c00f6f73a9548c3440
zalgonoise <mariozalgo@gmail.com>
2024-02-18 15:38:44 +0100

    authz/database: include expiry date field

--- authz/database/database.go
+++ authz/database/database.go
@@ -33,7 +33,8 @@ CREATE TABLE services
     id           INTEGER PRIMARY KEY NOT NULL,
     name         TEXT                NOT NULL UNIQUE,
     pub_key      BLOB                NOT NULL,
-    cert         BLOB                NULL
+    cert         BLOB                NULL,
+    expiry 			 DATETIME 					 NULL
 );
 `
 )
