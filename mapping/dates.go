933dd0d2401e2c5a146ba574f59a0bbb74f684b6
zalgonoise <mariozalgo@gmail.com>
2024-03-01 01:00:33 +0100

    mapping/dates: cleanup

--- mapping/dates.go
+++ mapping/dates.go
@@ -17,16 +17,15 @@ var (
 	errTimeSplitFailed       = errors.New("failed to split time intervals")
 )
 
+type Timeframe[K comparable, T any] struct {
+	noOverlap bool
+
+	Index *Index[Interval, map[K]T]
+}
+
 type Interval struct {
 	From time.Time
 	To   time.Time
-}
-
-type Timeframe[K comparable, T any] struct {
-	noOverlap bool
-	//maxGap    time.Duration
-
-	Index *Index[Interval, map[K]T]
 }
 
 type IntervalSet struct {
@@ -34,6 +33,18 @@ type IntervalSet struct {
 	next bool
 	i    Interval
 }
+
+// SeqKV describes a sequence of iterable items, which takes a yield func which will be used
+// to perform a certain operation on each yielded item throughout the iteration
+//
+// ref: https://github.com/golang/go/issues/61899
+type SeqKV[T, K any] func(yield func(T, K) bool) bool
+
+// Seq describes a sequence of iterable items, which takes a yield func which will be used
+// to perform a certain operation on each yielded item throughout the iteration
+//
+// ref: https://github.com/golang/go/issues/61899
+type Seq[T any] func(yield func(T) bool) bool
 
 // Add joins the Interval i and its values to the Timeframe t, while ordering its
 // previously inserted Interval(s) in the process.
@@ -50,177 +61,19 @@ func (t *Timeframe[K, T]) Add(i Interval, values map[K]T) error {
 		return nil
 	}
 
-	// TODO: what if we're adding an interval way back in the past?
-	//
-	// needs to scan the entire indexed keys to match any overlaps within the
-	// Interval i's From and To values
-	//
-	// TestTimeframe --> interleaved/recursive: reproduces this event
-	//
-	// this would become more and more expensive the more passes we do through the data
-	// so organizing the intervals separately seems best. implemented within this function given the current logic,
-	// and certain keys and values get lost in the end. That is not ideal.
-	//
-	// for that we can split the intervals like it's done here (as it works) but in a new Index,
-	// and collecting the data into a set, concatenating it in the end.
-	last := t.Index.Keys[len(t.Index.Keys)-1]
-	lastVal := t.Index.values[last]
-
-	// iterative process to join intervals if needed; should cycle through
-	// all occurrences where current interval overlaps
-	sets, err := t.split(last, i)
-	if err != nil {
-		return err
-	}
-
-	switch {
-	case len(sets) == 1 && sets[0].cur && sets[0].next:
-		t.Index.values[last] = coalesce(lastVal, values)
-	// keep last as-is
-	case len(sets) == 2 && sets[0].i == last:
+	if _, ok := t.Index.Get(i); ok {
 		t.Index.Set(i, func(old map[K]T) (map[K]T, bool) {
-			return values, true
+			return coalesce(old, values), true
 		})
-	default:
-		t.Index.Keys = t.Index.Keys[:len(t.Index.Keys)-1]
-		delete(t.Index.values, last)
 
-		for idx := range sets {
-			switch {
-			case sets[idx].cur && sets[idx].next:
-				valuesCopy := make(map[K]T, len(values))
-				maps.Copy(valuesCopy, values)
-
-				t.Index.Set(sets[idx].i, func(old map[K]T) (map[K]T, bool) {
-					return coalesce(valuesCopy, lastVal), true
-				})
-			case sets[idx].cur && !sets[idx].next:
-				t.Index.Set(sets[idx].i, func(old map[K]T) (map[K]T, bool) {
-					return lastVal, true
-				})
-			case !sets[idx].cur && sets[idx].next:
-				t.Index.Set(sets[idx].i, func(old map[K]T) (map[K]T, bool) {
-					return values, true
-				})
-			}
-		}
+		return nil
 	}
+
+	t.Index.Set(i, func(old map[K]T) (map[K]T, bool) {
+		return values, true
+	})
 
 	return nil
-}
-
-// TODO: needs rework with a different strategy, in order to flatten any type of Interval combination:
-//  1. iterate through all Interval values, and create an ordered slice of their From time.Time values, along with the
-//     index to the entry they are pointing to (that is starting)
-//  2. iterate through all Interval values, and create an ordered slice of their To time.Time values, along with the
-//     index to the entry they are pointing to (that is ending)
-//     -- this can be combined with step 1, where the indices are shared between starting and ending slices
-//  3. iterate through all From time.Time values, and find the following To time.Time value. This is the first reference
-//     point to that particular interval
-//     Then, iterate through the next From time.Time values to find another From value before the next To. This allows
-//     splitting Intervals according to their content; where the involved indices to the data can be referenced and
-//     aggregated if applicable.
-//  4. with the final Intervals and data indices, rebuild or initialize a new *Timeframe type with the flattened
-//     Intervals, and the aggregated data that they should have respectively.
-//     ---
-//     Ideally, this should be done separately from the Add method, to avoid multiple iterations on the underlying
-//     Timeframe Interval values. The All and Append methods can stay, however, with a similar sequence / iter approach
-//
-// split takes two Interval values, returning a slice of IntervalSet and an error.
-//
-// This method provides context to the caller on how many data points and their respective intervals of time and data,
-// when combining Timeframe Interval values.
-func (t *Timeframe[K, T]) split(cur, next Interval) ([]IntervalSet, error) {
-	switch {
-	// next is after
-	case cur.To.Before(next.From):
-		//if next.From.Sub(cur.To) > t.maxGap {
-		//	return nil, errGapBetweenTimeframes
-		//}
-
-		return []IntervalSet{{cur: true, i: cur}, {next: true, i: next}}, nil
-
-	// cur is after
-	case cur.From.After(next.To):
-		//if cur.From.Sub(next.To) > t.maxGap {
-		//	return nil, errGapBetweenTimeframes
-		//}
-
-		return []IntervalSet{{next: true, i: next}, {cur: true, i: cur}}, nil
-
-	// overlapping start
-	case cur.From.Equal(next.From):
-		if t.noOverlap {
-			return nil, errOverlappingTimeframes
-		}
-
-		switch cur.To.Compare(next.To) {
-		case -1: // before
-			return []IntervalSet{
-				{cur: true, next: true, i: Interval{From: cur.From, To: cur.To}},
-				{next: true, i: Interval{From: cur.To, To: next.To}},
-			}, nil
-		case 1: // after
-			return []IntervalSet{
-				{cur: true, next: true, i: Interval{From: cur.From, To: next.To}},
-				{cur: true, i: Interval{From: next.To, To: cur.To}},
-			}, nil
-		case 0: // equal
-			return []IntervalSet{
-				{cur: true, next: true, i: cur},
-			}, nil
-		}
-
-	// overlap: portion of end
-	case next.From.After(cur.From):
-		if t.noOverlap {
-			return nil, errOverlappingTimeframes
-		}
-
-		switch next.To.Compare(cur.To) {
-		case 1: // after; next goes beyond cur
-			return []IntervalSet{
-				{cur: true, i: Interval{From: cur.From, To: next.From}},
-				{cur: true, next: true, i: Interval{From: next.From, To: cur.To}},
-				{next: true, i: Interval{From: cur.To, To: next.To}},
-			}, nil
-		case -1: // before; next is within cur
-			return []IntervalSet{
-				{cur: true, i: Interval{From: cur.From, To: next.From}},
-				{cur: true, next: true, i: Interval{From: next.From, To: next.To}},
-				{next: true, i: Interval{From: next.To, To: cur.To}},
-			}, nil
-		case 0:
-			return []IntervalSet{
-				{cur: true, i: Interval{From: cur.From, To: next.From}},
-				{cur: true, next: true, i: Interval{From: next.From, To: next.To}},
-			}, nil
-		}
-
-	// overlap: portion of start
-	case next.From.Before(cur.From):
-		switch next.To.Compare(cur.To) {
-		case 1:
-			return []IntervalSet{
-				{next: true, i: Interval{From: next.From, To: cur.From}},
-				{cur: true, next: true, i: Interval{From: cur.From, To: cur.To}},
-				{next: true, i: Interval{From: cur.To, To: next.To}},
-			}, nil
-		case -1:
-			return []IntervalSet{
-				{next: true, i: Interval{From: next.From, To: cur.From}},
-				{cur: true, next: true, i: Interval{From: cur.From, To: next.To}},
-				{cur: true, i: Interval{From: next.To, To: cur.To}},
-			}, nil
-		case 0:
-			return []IntervalSet{
-				{next: true, i: Interval{From: next.From, To: cur.From}},
-				{cur: true, next: true, i: Interval{From: cur.From, To: cur.To}},
-			}, nil
-		}
-	}
-
-	return nil, errTimeSplitFailed
 }
 
 func (t *Timeframe[K, T]) Organize() (*Timeframe[K, T], error) {
@@ -301,18 +154,6 @@ func (t *Timeframe[K, T]) Organize() (*Timeframe[K, T], error) {
 
 	return tf, nil
 }
-
-// SeqKV describes a sequence of iterable items, which takes a yield func which will be used
-// to perform a certain operation on each yielded item throughout the iteration
-//
-// ref: https://github.com/golang/go/issues/61899
-type SeqKV[T, K any] func(yield func(T, K) bool) bool
-
-// Seq describes a sequence of iterable items, which takes a yield func which will be used
-// to perform a certain operation on each yielded item throughout the iteration
-//
-// ref: https://github.com/golang/go/issues/61899
-type Seq[T any] func(yield func(T) bool) bool
 
 // All returns an iterator over the values in the Timeframe,
 // through the indexed Interval values ordered by their From time.Time value.
