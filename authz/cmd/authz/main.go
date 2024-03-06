1e2708c3fbe706206b37fd878b6f35a18dd42417
zalgonoise <mariozalgo@gmail.com>
2024-03-06 13:24:51 +0100

    authz/cmd/authz: segregated executable commands in different files; added authz target

--- authz/cmd/authz/main.go
+++ authz/cmd/authz/main.go
@@ -10,38 +10,31 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/zalgonoise/x/authz/config"
 	"github.com/zalgonoise/x/authz/keygen"
 	"github.com/zalgonoise/x/authz/log"
 	"github.com/zalgonoise/x/authz/metrics"
 	pb "github.com/zalgonoise/x/authz/pb/authz/v1"
-	"github.com/zalgonoise/x/authz/tracing"
 	"github.com/zalgonoise/x/cli"
-	"go.opentelemetry.io/otel/trace/noop"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
-	"github.com/zalgonoise/x/authz/ca"
-	"github.com/zalgonoise/x/authz/database"
 	"github.com/zalgonoise/x/authz/grpcserver"
 	"github.com/zalgonoise/x/authz/httpserver"
-	"github.com/zalgonoise/x/authz/repository"
 )
 
-var modes = []string{"ca"}
+var modes = []string{"ca", "authz"}
 
 func main() {
 	logger := log.New("debug")
 	runner := cli.NewRunner("authz",
 		cli.WithOneOf(modes...),
 		cli.WithExecutors(map[string]cli.Executor{
-			"ca": cli.Executable(ExecCertificateAuthority),
+			"ca":    cli.Executable(ExecCertificateAuthority),
+			"authz": cli.Executable(ExecAuthz),
 		}),
 	)
 
@@ -58,163 +51,12 @@ const (
 	defaultDB    = "local/ca.db"
 	defaultKey   = "local/key.pem"
 
-	defaultHTTPPort = 8080
-	defaultGRPCPort = 8081
-
 	shutdownTimeout = time.Minute
 )
 
 var (
-	ErrIsDir          = errors.New("path cannot be a directory")
-	ErrNotDir         = errors.New("path must be a directory")
-	ErrFailedCreateDB = errors.New("failed to create database file")
-	ErrInvalidKey     = errors.New("invalid ECDSA private key")
+	ErrNotDir = errors.New("path must be a directory")
 )
-
-func ExecCertificateAuthority(ctx context.Context, logger *slog.Logger, args []string) (int, error) {
-	conf, err := config.New(args)
-	if err != nil {
-		return 1, err
-	}
-
-	logger.DebugContext(ctx, "preparing tracer")
-
-	var (
-		tracer      = noop.NewTracerProvider().Tracer(tracing.ServiceNameCA)
-		tracingDone = func(context.Context) error { return nil }
-	)
-
-	if conf.Tracer.URI != "" {
-		exporter, err := tracing.GRPCExporter(ctx, conf.Tracer.URI,
-			tracing.WithCredentials(conf.Tracer.Username, conf.Tracer.Password),
-			tracing.WithTimeout(conf.Tracer.ConnTimeout),
-		)
-
-		if err != nil {
-			return 1, err
-		}
-
-		tracingDone, err = tracing.Init(ctx, tracing.ServiceNameCA, exporter)
-		if err != nil {
-			return 1, err
-		}
-
-		tracer = tracing.Tracer(tracing.ServiceNameCA)
-	}
-
-	logger.DebugContext(ctx, "preparing database")
-
-	if conf.Database.URI == "" {
-		if err := checkOrCreateDir(defaultLocal); err != nil {
-			return 1, err
-		}
-		conf.Database.URI = defaultDB
-	}
-
-	db, err := database.Open(conf.Database.URI)
-	if err != nil {
-		return 1, err
-	}
-
-	if err = database.Migrate(ctx, db, database.CAService, logger); err != nil {
-		return 1, err
-	}
-
-	logger.DebugContext(ctx, "database is ready")
-	logger.DebugContext(ctx, "preparing private key")
-
-	if conf.PrivateKey == "" {
-		if err = checkOrCreateDir(defaultLocal); err != nil {
-			return 1, err
-		}
-
-		conf.PrivateKey = defaultKey
-	}
-
-	key, err := openOrCreateKey(conf.PrivateKey)
-	if err != nil {
-		return 1, err
-	}
-
-	logger.DebugContext(ctx, "private key is ready")
-	logger.DebugContext(ctx, "preparing CA service")
-
-	m := metrics.NewMetrics()
-
-	repo, err := repository.NewServices(db,
-		repository.WithLogger(logger),
-		repository.WithMetrics(m),
-		repository.WithTrace(tracer),
-		repository.WithCleanupTimeout(conf.Database.CleanupTimeout),
-		repository.WithCleanupSchedule(conf.Database.CleanupSchedule),
-	)
-	if err != nil {
-		return 1, err
-	}
-
-	caService, err := ca.NewCertificateAuthority(
-		repo,
-		key,
-		ca.WithLogger(logger),
-		ca.WithMetrics(m),
-		ca.WithTracer(tracer),
-		ca.WithTemplate(keygen.WithDurMonth(conf.CA.CertDurMonths)),
-	)
-
-	logger.DebugContext(ctx, "CA service is ready")
-	logger.DebugContext(ctx, "preparing HTTP server")
-
-	httpServer, err := httpserver.NewServer(fmt.Sprintf(":%d", conf.HTTPPort))
-	if err != nil {
-		return 1, err
-	}
-
-	logger.DebugContext(ctx, "HTTP server is ready", slog.Int("port", conf.HTTPPort))
-	logger.DebugContext(ctx, "preparing gRPC server")
-
-	grpcServer, err := runGRPCServer(ctx, conf.GRPCPort, caService, nil, httpServer, logger, m)
-	if err != nil {
-		return 1, err
-	}
-
-	logger.DebugContext(ctx, "gRPC server is ready", slog.Int("port", conf.GRPCPort))
-	logger.DebugContext(ctx, "setting up metrics handler")
-
-	if err = registerMetrics(m, httpServer); err != nil {
-		return 1, err
-	}
-
-	logger.DebugContext(ctx, "metrics handler is ready", slog.String("endpoint", "/metrics"))
-	logger.DebugContext(ctx, "serving requests")
-
-	go runHTTPServer(ctx, conf.HTTPPort, httpServer, logger)
-
-	signalChannel := make(chan os.Signal, 1)
-	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
-
-	<-signalChannel
-
-	logger.InfoContext(ctx, "shutting down", slog.Duration("timeout", shutdownTimeout))
-
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
-	defer cancel()
-
-	if err = httpServer.Shutdown(shutdownCtx); err != nil {
-		return 1, err
-	}
-
-	grpcServer.Shutdown(shutdownCtx)
-
-	if err = tracingDone(shutdownCtx); err != nil {
-		return 1, err
-	}
-
-	if err = repo.Close(); err != nil {
-		return 1, err
-	}
-
-	return 0, nil
-}
 
 func runGRPCServer(
 	ctx context.Context,
