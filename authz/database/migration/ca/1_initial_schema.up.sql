c78a5f5e8c7a2c0b5cc0226074cfeb060d9cdac9
zalgonoise <mariozalgo@gmail.com>
2024-03-10 12:43:31 +0100

    authz/database/migrations/ca: remove go-migrate files

--- authz/database/migration/ca/1_initial_schema.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE services
-(
-    id           INTEGER PRIMARY KEY NOT NULL,
-    name         TEXT                NOT NULL UNIQUE,
-    pub_key      BLOB                NOT NULL,
-    cert         BLOB                NULL
-);
