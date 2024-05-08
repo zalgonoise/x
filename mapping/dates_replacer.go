3ff2c010e30ba28e906942ab18cacd27a612068c
zalgonoise <mariozalgo@gmail.com>
2024-05-08 02:21:23 +0200

    mapping/dates/replacer: added a TimeframeReplacer type that respects order and replaces conflicts in intervals with the most recent interval

---
+++ mapping/dates_replacer.go
@@ -0,0 +1,84 @@
+package mapping
+
+// TimeframeReplacer stores values in intervals of time, as a 2D-map of Interval to a map of types K and T; by handling
+// conflicts as replacement-updates.
+type TimeframeReplacer[K comparable, T any] struct {
+	Keys   []Interval
+	Values map[Interval]map[K]T
+}
+
+// NewTimeframeReplacer creates a TimeframeReplacer of types K and T, with an 2D-map of Interval's to a map of types
+// K and T.
+func NewTimeframeReplacer[K comparable, T any]() *TimeframeReplacer[K, T] {
+	return &TimeframeReplacer[K, T]{
+		Keys:   make([]Interval, 0, minAlloc),
+		Values: make(map[Interval]map[K]T, minAlloc),
+	}
+}
+
+// Add joins the Interval i and its values to the TimeframeReplacer t, while ordering its
+// previously inserted Interval(s) in the process.
+func (t *TimeframeReplacer[K, T]) Add(i Interval, values map[K]T) bool {
+	if _, ok := t.Values[i]; ok {
+		t.Values[i] = values
+
+		return false
+	}
+
+	t.Keys = append(t.Keys, i)
+	t.Values[i] = values
+
+	return true
+}
+
+// Append iterates through the input SeqKV and adds all intervals and respective values
+// to the TimeframeReplacer t.
+func (t *TimeframeReplacer[K, T]) Append(seq SeqKV[Interval, map[K]T]) error {
+	if !seq(t.Add) {
+		return ErrAppendFailed
+	}
+
+	return nil
+}
+
+// All returns an iterator over the values in the TimeframeReplacer,
+// through the indexed Interval values ordered by their From time.Time value.
+func (t *TimeframeReplacer[K, T]) All() SeqKV[Interval, map[K]T] {
+	return func(yield func(Interval, map[K]T) bool) bool {
+		for i := range t.Keys {
+			values, ok := t.Values[t.Keys[i]]
+			if !ok {
+				continue
+			}
+
+			if !yield(t.Keys[i], values) {
+				return false
+			}
+		}
+
+		return true
+	}
+}
+
+// Organize returns a new TimeframeMap with organized Interval(s) and respective values. It is the result of
+// calling Flatten on TimeframeMap.All, and appending the resulting sequence to a new instance of TimeframeMap.
+func (t *TimeframeReplacer[K, T]) Organize() (*TimeframeMap[K, T], error) {
+	seq, err := Replace(t.All())
+	if err != nil {
+		return nil, err
+	}
+
+	tf := NewTimeframeMap[K, T]()
+	if err = tf.Append(seq); err != nil {
+		return nil, err
+	}
+
+	return tf, nil
+}
+
+// Merge joins the intervals and respective values of the TimeframeMap tf into the TimeframeMap t,
+// by extracting a SeqKV of the same items from tf using TimeframeMap.All, and adding them into
+// Timeframe t using TimeframeMap.Append.
+func (t *TimeframeReplacer[K, T]) Merge(tf *Timeframe[K, T]) (err error) {
+	return t.Append(tf.All())
+}
