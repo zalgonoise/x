04d9912b55a2cb6deacac78ccbaf4473844b6bed
zalgonoise <mariozalgo@gmail.com>
2024-05-10 19:10:10 +0200

    mapping/dates/range: add a simpler Replace implementation without a map

---
+++ mapping/dates_range.go
@@ -0,0 +1,75 @@
+package mapping
+
+type TimeframeRange[T any] struct {
+	Keys   []Interval
+	Values map[Interval]T
+}
+
+// NewTimeframeRange creates a TimeframeRange of type T, with an 2D-map of Interval's to type T.
+func NewTimeframeRange[T any]() *TimeframeRange[T] {
+	return &TimeframeRange[T]{
+		Keys:   make([]Interval, 0, minAlloc),
+		Values: make(map[Interval]T, minAlloc),
+	}
+}
+
+// Add joins the Interval i and its values to the TimeframeRange t, while ordering its
+// previously inserted Interval(s) in the process.
+func (t *TimeframeRange[T]) Add(i Interval, value T) bool {
+	if _, ok := t.Values[i]; ok {
+		t.Values[i] = value
+
+		return false
+	}
+
+	t.Keys = append(t.Keys, i)
+	t.Values[i] = value
+
+	return true
+}
+
+// Append iterates through the input SeqKV and adds all intervals and respective values
+// to the TimeframeRange t.
+func (t *TimeframeRange[T]) Append(seq SeqKV[Interval, T]) error {
+	if !seq(t.Add) {
+		return ErrAppendFailed
+	}
+
+	return nil
+}
+
+// All returns an iterator over the values in the TimeframeRange,
+// through the indexed Interval values ordered by their From time.Time value.
+func (t *TimeframeRange[T]) All() SeqKV[Interval, T] {
+	return func(yield func(Interval, T) bool) bool {
+		for i := range t.Keys {
+			values, ok := t.Values[t.Keys[i]]
+			if !ok {
+				continue
+			}
+
+			if !yield(t.Keys[i], values) {
+				return false
+			}
+		}
+
+		return true
+	}
+}
+
+// Organize returns a new TimeframeRange with organized Interval(s) and respective values. It is the result of
+// calling Replace on TimeframeRange.All, and appending the resulting sequence to a new instance of
+// TimeframeRange.
+func (t *TimeframeRange[T]) Organize() (*TimeframeRange[T], error) {
+	seq, err := Replace(t.All())
+	if err != nil {
+		return nil, err
+	}
+
+	tf := NewTimeframeRange[T]()
+	if err = tf.Append(seq); err != nil {
+		return nil, err
+	}
+
+	return tf, nil
+}
---
+++ mapping/dates_range_test.go
@@ -0,0 +1,277 @@
+package mapping_test
+
+import (
+	"slices"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/zalgonoise/x/mapping"
+)
+
+func TestTimeframeRange(t *testing.T) {
+	interval1 := mapping.Interval{
+		From: time.Date(2024, 1, 10, 12, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 18, 0, 0, 0, time.UTC),
+	}
+
+	interval2 := mapping.Interval{
+		From: time.Date(2024, 1, 10, 18, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 21, 0, 0, 0, time.UTC),
+	}
+
+	interval3 := mapping.Interval{
+		From: time.Date(2024, 1, 10, 12, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 21, 0, 0, 0, time.UTC),
+	}
+
+	interval4 := mapping.Interval{
+		From: time.Date(2024, 1, 10, 12, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 14, 0, 0, 0, time.UTC),
+	}
+
+	i4split := mapping.Interval{
+		From: time.Date(2024, 1, 10, 14, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 18, 0, 0, 0, time.UTC),
+	}
+
+	interval5 := mapping.Interval{
+		From: time.Date(2024, 1, 10, 14, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 17, 0, 0, 0, time.UTC),
+	}
+
+	i5split1 := mapping.Interval{
+		From: time.Date(2024, 1, 10, 12, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 14, 0, 0, 0, time.UTC),
+	}
+
+	i5split2 := mapping.Interval{
+		From: time.Date(2024, 1, 10, 17, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 18, 0, 0, 0, time.UTC),
+	}
+
+	interval6 := mapping.Interval{
+		From: time.Date(2024, 1, 10, 14, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 23, 0, 0, 0, time.UTC),
+	}
+
+	i6split := mapping.Interval{
+		From: time.Date(2024, 1, 10, 12, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 14, 0, 0, 0, time.UTC),
+	}
+
+	interval7 := mapping.Interval{
+		From: time.Date(2024, 1, 10, 14, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 18, 0, 0, 0, time.UTC),
+	}
+
+	i7split := mapping.Interval{
+		From: time.Date(2024, 1, 10, 12, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 14, 0, 0, 0, time.UTC),
+	}
+
+	interval8 := mapping.Interval{
+		From: time.Date(2024, 1, 10, 10, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 20, 0, 0, 0, time.UTC),
+	}
+
+	interval9 := mapping.Interval{
+		From: time.Date(2024, 1, 10, 10, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 14, 0, 0, 0, time.UTC),
+	}
+
+	i9split := mapping.Interval{
+		From: time.Date(2024, 1, 10, 14, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 18, 0, 0, 0, time.UTC),
+	}
+
+	interval10 := mapping.Interval{
+		From: time.Date(2024, 1, 10, 10, 0, 0, 0, time.UTC),
+		To:   time.Date(2024, 1, 10, 18, 0, 0, 0, time.UTC),
+	}
+
+	blob1 := blob{
+		user: user{0, "user"},
+		data: data{63},
+	}
+
+	blob2 := blob{
+		user: user{0, "user_x"},
+		data: data{64},
+	}
+
+	for _, testcase := range []struct {
+		name  string
+		sets  []dataSet
+		wants []dataSet
+	}{
+		{
+			name:  "OneBlob",
+			sets:  []dataSet{{interval: interval1, blob: blob1}},
+			wants: []dataSet{{interval: interval1, blob: blob1}},
+		},
+		{
+			name: "TwoBlobs/Separate/NextIsAfter",
+			sets: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval2, blob: blob2},
+			},
+			wants: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval2, blob: blob2},
+			},
+		},
+		{
+			name: "TwoBlobs/Separate/NextIsBefore",
+			sets: []dataSet{
+				{interval: interval2, blob: blob2},
+				{interval: interval1, blob: blob1},
+			},
+			wants: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval2, blob: blob2},
+			},
+		},
+		{
+			name: "TwoBlobs/MatchingStart/NextOverlapsCurrent",
+			sets: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval3, blob: blob2},
+			},
+			wants: []dataSet{
+				{interval: interval3, blob: blob2},
+			},
+		},
+		{
+			name: "TwoBlobs/MatchingStart/NextOverlapsCurrent",
+			sets: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval3, blob: blob2},
+			},
+			wants: []dataSet{
+				{interval: interval3, blob: blob2},
+			},
+		},
+		{
+			name: "TwoBlobs/MatchingStart/NextWithinCurrent",
+			sets: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval4, blob: blob2},
+			},
+			wants: []dataSet{
+				{interval: interval4, blob: blob2},
+				{interval: i4split, blob: blob1},
+			},
+		},
+		{
+			name: "TwoBlobs/MatchingStart/NextIsSameRange",
+			sets: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval1, blob: blob2},
+			},
+			wants: []dataSet{
+				{interval: interval1, blob: blob2},
+			},
+		},
+		{
+			name: "TwoBlobs/OverlappingMiddle/NextWithinCurrent",
+			sets: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval5, blob: blob2},
+			},
+			wants: []dataSet{
+				{interval: i5split1, blob: blob1},
+				{interval: interval5, blob: blob2},
+				{interval: i5split2, blob: blob1},
+			},
+		},
+		{
+			name: "TwoBlobs/OverlappingEnd/NextGoesBeyondCurrent",
+			sets: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval6, blob: blob2},
+			},
+			wants: []dataSet{
+				{interval: i6split, blob: blob1},
+				{interval: interval6, blob: blob2},
+			},
+		},
+		{
+			name: "TwoBlobs/OverlappingEnd/NextMatchesEnds",
+			sets: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval7, blob: blob2},
+			},
+			wants: []dataSet{
+				{interval: i7split, blob: blob1},
+				{interval: interval7, blob: blob2},
+			},
+		},
+		{
+			name: "TwoBlobs/OverlappingStart/NextCoversCurrent",
+			sets: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval8, blob: blob2},
+			},
+			wants: []dataSet{
+				{interval: interval8, blob: blob2},
+			},
+		},
+		{
+			name: "TwoBlobs/OverlappingStart/PortionOfStart",
+			sets: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval9, blob: blob2},
+			},
+			wants: []dataSet{
+				{interval: interval9, blob: blob2},
+				{interval: i9split, blob: blob1},
+			},
+		},
+		{
+			name: "TwoBlobs/OverlappingStart/MatchingEnds",
+			sets: []dataSet{
+				{interval: interval1, blob: blob1},
+				{interval: interval10, blob: blob2},
+			},
+			wants: []dataSet{
+				{interval: interval10, blob: blob2},
+			},
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			tf := mapping.NewTimeframeRange[blob]()
+
+			for i := range testcase.sets {
+				_ = tf.Add(testcase.sets[i].interval, testcase.sets[i].blob)
+			}
+
+			newTF, err := tf.Organize()
+			require.NoError(t, err)
+
+			seq := newTF.All()
+
+			require.True(t, seq(verifySeq(testcase.wants)))
+		})
+	}
+}
+
+func verifySeq(wants []dataSet) func(interval mapping.Interval, value blob) bool {
+	return func(interval mapping.Interval, value blob) bool {
+		var zero blob
+
+		if value == zero {
+			return false
+		}
+
+		idx := slices.IndexFunc(wants, func(set dataSet) bool {
+			return set.interval == interval
+		})
+
+		if idx < 0 {
+			return false
+		}
+
+		return value == wants[idx].blob
+	}
+}
