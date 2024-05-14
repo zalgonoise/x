83e64dba4e743a0f980e7f7c039e17996fc11c2b
zalgonoise <mariozalgo@gmail.com>
2024-05-14 19:03:04 +0200

    mapping/dates/set: added TimeframeSet type

---
+++ mapping/dates_set.go
@@ -0,0 +1,82 @@
+package mapping
+
+import "slices"
+
+type TimeframeSet[T any] struct {
+	buffer []DataInterval[T]
+	//mergeFunc func(a, b T) T
+}
+
+// NewTimeframeSet creates a NewTimeframeSet of type T, with an 2D-map of Interval's to type T.
+func NewTimeframeSet[T any]() *TimeframeSet[T] {
+	return &TimeframeSet[T]{
+		buffer: make([]DataInterval[T], 0, minAlloc),
+	}
+}
+
+func (t *TimeframeSet[T]) init() *TimeframeSet[T] {
+	if t == nil {
+		return NewTimeframeSet[T]()
+	}
+
+	if t.buffer == nil {
+		t.buffer = make([]DataInterval[T], 0, minAlloc)
+	}
+
+	return t
+}
+
+// Add joins the Interval i and its values to the TimeframeSet t, while ordering its
+// previously inserted Interval(s) in the process.
+func (t *TimeframeSet[T]) Add(i Interval, value T) bool {
+	t.buffer = append(t.buffer, DataInterval[T]{Interval: i, Data: value})
+
+	return true
+}
+
+// Append iterates through the input SeqKV and adds all intervals and respective values
+// to the TimeframeSet t.
+func (t *TimeframeSet[T]) Append(seq SeqKV[Interval, T]) bool {
+	if !seq(t.Add) {
+		return false
+	}
+
+	return true
+}
+
+// All returns an iterator over the values in the TimeframeSet,
+// through the indexed Interval values ordered by their From time.Time value.
+func (t *TimeframeSet[T]) All() SeqKV[Interval, T] {
+	return func(yield func(Interval, T) bool) bool {
+		for i := range t.buffer {
+			if !yield(t.buffer[i].Interval, t.buffer[i].Data) {
+				return false
+			}
+		}
+
+		return true
+	}
+}
+
+// Organize returns a new TimeframeSet with organized Interval(s) and respective values. It is the result of
+// calling the input ReducerFunc (like Flatten or Replace) on TimeframeSet.All, and appending the resulting sequence
+// to a new instance of TimeframeSet.
+func (t *TimeframeSet[T]) Organize(reducer ReducerFunc[T]) (*TimeframeSet[T], error) {
+	seq, err := reducer(t.All())
+	if err != nil {
+		return nil, err
+	}
+
+	buffer := make([]DataInterval[T], 0, len(t.buffer))
+	seq(func(interval Interval, data T) bool {
+		buffer = append(buffer, DataInterval[T]{Interval: interval, Data: data})
+
+		return true
+	})
+
+	slices.SortFunc(buffer, func(a, b DataInterval[T]) int {
+		return a.Interval.From.Compare(b.Interval.From)
+	})
+
+	return &TimeframeSet[T]{buffer: buffer}, nil
+}
