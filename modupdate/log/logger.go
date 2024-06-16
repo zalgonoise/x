129b2d84bedeef187d4ec40fa2ce08b10622c5c5
zalgonoise <mariozalgo@gmail.com>
2024-06-17 01:32:30 +0200

    modupdate: log: added log package

---
+++ modupdate/log/logger.go
@@ -0,0 +1,77 @@
+package log
+
+import (
+	"context"
+	"log/slog"
+	"os"
+)
+
+func New(level string) *slog.Logger {
+	var logLevel slog.Level
+
+	err := logLevel.UnmarshalText([]byte(level))
+	if err != nil {
+		logLevel = slog.LevelInfo
+	}
+
+	logger := slog.New(
+		NewSpanContextHandler(
+			slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+				AddSource: true,
+				Level:     logLevel,
+			}),
+			true,
+		),
+	)
+
+	if err != nil {
+		logger.WarnContext(context.Background(), "invalid log level string",
+			slog.String("input_level", level),
+			slog.String("error", err.Error()),
+		)
+	}
+
+	return logger
+}
+
+func From(level string, handler slog.Handler) *slog.Logger {
+	var logLevel slog.Level
+
+	err := logLevel.UnmarshalText([]byte(level))
+	if err != nil {
+		logLevel = slog.LevelInfo
+	}
+
+	logger := slog.New(NewSpanContextHandler(handler, true))
+
+	if err != nil {
+		logger.WarnContext(context.Background(), "invalid log level string",
+			slog.String("input_level", level),
+			slog.String("error", err.Error()),
+		)
+	}
+
+	return logger
+}
+
+func NoOp() *slog.Logger {
+	return slog.New(noOpHandler{})
+}
+
+type noOpHandler struct{}
+
+func (noOpHandler) Enabled(context.Context, slog.Level) bool {
+	return false
+}
+
+func (noOpHandler) Handle(context.Context, slog.Record) error {
+	return nil
+}
+
+func (h noOpHandler) WithAttrs([]slog.Attr) slog.Handler {
+	return h
+}
+
+func (h noOpHandler) WithGroup(string) slog.Handler {
+	return h
+}
