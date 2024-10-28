3c92479fcbf0ba3ea5224ef5001cc54ec81774f6
zalgonoise <mariozalgo@gmail.com>
2024-10-29 00:26:55 +0100

    randoom/items: added ID to items

--- randoom/items/items.go
+++ randoom/items/items.go
@@ -1,6 +1,7 @@
 package items
 
 type Item struct {
+	ID      int64   `json:"id" yaml:"id"`
 	Content string  `json:"content" yaml:"content"`
 	Count   uint64  `json:"count" yaml:"count"`
 	Ratio   float32 `json:"ratio" yaml:"ratio"`
