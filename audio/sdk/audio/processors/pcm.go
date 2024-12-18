d120e168aa461a3822619f88639aa8187ca5e592
zalgonoise <mariozalgo@gmail.com>
2024-12-18 19:37:30 +0100

    audio: audio/sdk/audio/processors: added added observability dependencies to PCM processor's constructor

--- audio/sdk/audio/processors/pcm.go
+++ audio/sdk/audio/processors/pcm.go
@@ -2,12 +2,17 @@ package processors
 
 import (
 	"github.com/zalgonoise/cfg"
+	"go.opentelemetry.io/otel/trace"
+	"log/slog"
 
 	"github.com/zalgonoise/x/audio/encoding/wav"
 	"github.com/zalgonoise/x/audio/sdk/audio"
 )
 
-func PCM(exporters []audio.Exporter, opts ...cfg.Option[wav.Config]) audio.Processor {
+func PCM(
+	exporters []audio.Exporter, streamOptions []cfg.Option[wav.Config],
+	logger *slog.Logger, metrics audio.ProcessorMetrics, tracer trace.Tracer,
+) audio.Processor {
 	if len(exporters) == 0 {
 		return audio.NoOpProcessor()
 	}
@@ -16,8 +21,9 @@ func PCM(exporters []audio.Exporter, opts ...cfg.Option[wav.Config]) audio.Proce
 
 	return audio.NewProcessor(
 		audio.NewStreamExporter(
-			wav.NewStreamContext(exporter.Export, opts...),
+			wav.NewStreamContext(exporter.Export, streamOptions...),
 			exporter,
 		),
+		logger, metrics, tracer,
 	)
 }
