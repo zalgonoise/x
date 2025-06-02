e21ab0fbb71894e42eb5deb70e0d88f198d731f6
zalgonoise <mariozalgo@gmail.com>
2025-06-02 12:51:48 +0200

    collide/tracks: added GetCollisions and GetOpenTracks implementation and tests

--- collide/tracks/tracks.go
+++ collide/tracks/tracks.go
@@ -1,15 +1,23 @@
 package tracks
 
 import (
+	"errors"
+	"fmt"
+	"slices"
+
 	"gopkg.in/yaml.v3"
+)
+
+var (
+	ErrNotFound = errors.New("track not found")
 )
 
 type Track struct {
 	ID           uint8
-	Name         string  `yaml:"name"`
-	District     string  `yaml:"district"`
-	IsDriftTrack bool    `yaml:"is_drift_track"`
-	CollidesWith []uint8 `yaml:"collides_with"`
+	Name         string   `yaml:"name"`
+	District     string   `yaml:"district"`
+	IsDriftTrack bool     `yaml:"is_drift_track"`
+	CollidesWith []string `yaml:"collides_with"`
 }
 
 type TrackList struct {
@@ -22,4 +30,34 @@ func (t *TrackList) Read(b []byte) (n int, err error) {
 	}
 
 	return len(b), nil
+}
+
+func GetCollisions(list *TrackList, track string) ([]string, error) {
+	i := slices.IndexFunc(list.Tracks, func(t Track) bool {
+		return t.Name == track
+	})
+
+	if i < 0 {
+		return nil, fmt.Errorf("%w: %s", ErrNotFound, track)
+	}
+
+	return list.Tracks[i].CollidesWith, nil
+}
+
+func GetOpenTracks(list *TrackList, track string) ([]string, error) {
+	i := slices.IndexFunc(list.Tracks, func(t Track) bool {
+		return t.Name == track
+	})
+
+	if i < 0 {
+		return nil, fmt.Errorf("%w: %s", ErrNotFound, track)
+	}
+
+	return slices.Collect(func(yield func(t string) bool) {
+		for idx := range list.Tracks {
+			if i != idx && !slices.Contains(list.Tracks[i].CollidesWith, list.Tracks[idx].Name) {
+				yield(list.Tracks[idx].Name)
+			}
+		}
+	}), nil
 }
--- collide/tracks/tracks_test.go
+++ collide/tracks/tracks_test.go
@@ -7,8 +7,52 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-//go:embed tracks.yaml
-var trackBytes []byte
+var (
+	//go:embed tracks.yaml
+	trackBytes []byte
+
+	testList = &TrackList{
+		Tracks: []Track{
+			{
+				ID:           0,
+				Name:         "Construction",
+				District:     "Financial",
+				IsDriftTrack: true,
+				CollidesWith: []string{
+					"Construction Rev",
+					"Church Of Pinkies",
+					"Eva's LAT",
+					"Fuckin' W",
+				},
+			},
+			{
+				ID:           1,
+				Name:         "Construction Rev",
+				District:     "Financial",
+				IsDriftTrack: true,
+				CollidesWith: []string{
+					"Construction",
+					"Church Of Pinkies",
+					"Eva's LAT",
+					"Fuckin' W",
+				},
+			},
+			{
+				ID:           2,
+				Name:         "Xmas Bash",
+				District:     "Financial",
+				IsDriftTrack: true,
+				CollidesWith: []string{
+					"Tunnel of Death",
+					"LAT",
+					"Eva's LAT",
+					"Mikro Tournament",
+					"Fuckin' W",
+				},
+			},
+		},
+	}
+)
 
 func TestTrackList_Read(t *testing.T) {
 	for _, testcase := range []struct {
@@ -33,6 +77,83 @@ func TestTrackList_Read(t *testing.T) {
 
 			require.NoError(t, err)
 			t.Log(trackList)
+		})
+	}
+}
+
+func TestGetCollisions(t *testing.T) {
+	for _, testcase := range []struct {
+		name  string
+		list  *TrackList
+		track string
+		wants []string
+		err   error
+	}{
+		{
+			name:  "Success",
+			list:  testList,
+			track: "Construction",
+			wants: []string{
+				"Construction Rev",
+				"Church Of Pinkies",
+				"Eva's LAT",
+				"Fuckin' W",
+			},
+		},
+		{
+			name:  "Fail/ErrNotFound",
+			list:  testList,
+			track: "Beacon Mess",
+			err:   ErrNotFound,
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			tracks, err := GetCollisions(testcase.list, testcase.track)
+			if err != nil {
+				require.ErrorIs(t, err, testcase.err)
+
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, testcase.wants, tracks)
+		})
+	}
+}
+
+func TestGetOpenTracks(t *testing.T) {
+	for _, testcase := range []struct {
+		name  string
+		list  *TrackList
+		track string
+		wants []string
+		err   error
+	}{
+		{
+			name:  "Success",
+			list:  testList,
+			track: "Construction",
+			wants: []string{
+				"Xmas Bash",
+			},
+		},
+		{
+			name:  "Fail/ErrNotFound",
+			list:  testList,
+			track: "Beacon Mess",
+			err:   ErrNotFound,
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			tracks, err := GetOpenTracks(testcase.list, testcase.track)
+			if err != nil {
+				require.ErrorIs(t, err, testcase.err)
+
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, testcase.wants, tracks)
 		})
 	}
 }
