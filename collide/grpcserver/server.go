f35c9f916f4922538132574f93fe1e9fd64bb499
zalgonoise <mariozalgo@gmail.com>
2025-06-18 19:04:51 +0200

    collide/grpcserver: added gRPC server logic

---
+++ collide/grpcserver/server.go
@@ -0,0 +1,76 @@
+package grpcserver
+
+import (
+	"context"
+	"net"
+
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-middleware/providers/prometheus"
+	"github.com/prometheus/client_golang/prometheus"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	pb "github.com/zalgonoise/x/collide/pkg/api/pb/collide/v1"
+)
+
+type Server struct {
+	server        *grpc.Server
+	serverMetrics *grpc_prometheus.ServerMetrics
+}
+
+type Metrics interface {
+	RegisterCollector(collector prometheus.Collector)
+}
+
+func NewServer(
+	metrics Metrics,
+	unaryInterceptors []grpc.UnaryServerInterceptor,
+	streamInterceptors []grpc.StreamServerInterceptor,
+) *Server {
+	serverMetrics := grpc_prometheus.NewServerMetrics(grpc_prometheus.WithServerHandlingTimeHistogram())
+
+	s := grpc.NewServer(
+		grpc.StatsHandler(otelgrpc.NewServerHandler(
+			otelgrpc.WithMessageEvents(otelgrpc.SentEvents, otelgrpc.ReceivedEvents),
+		)),
+		grpc.ChainUnaryInterceptor(
+			append([]grpc.UnaryServerInterceptor{serverMetrics.UnaryServerInterceptor()}, unaryInterceptors...)...,
+		),
+		grpc.ChainStreamInterceptor(
+			append([]grpc.StreamServerInterceptor{serverMetrics.StreamServerInterceptor()}, streamInterceptors...)...,
+		),
+	)
+
+	reflection.Register(s)
+	metrics.RegisterCollector(serverMetrics)
+
+	return &Server{
+		server:        s,
+		serverMetrics: serverMetrics,
+	}
+}
+
+func (s *Server) Serve(l net.Listener) error {
+	s.serverMetrics.InitializeMetrics(s.server)
+
+	return s.server.Serve(l)
+}
+
+func (s *Server) Shutdown(ctx context.Context) {
+	shutdownSuccess := make(chan struct{})
+
+	go func() {
+		s.server.GracefulStop()
+		close(shutdownSuccess)
+	}()
+
+	select {
+	case <-ctx.Done():
+		s.server.Stop()
+	case <-shutdownSuccess:
+	}
+}
+
+func (s *Server) RegisterCollideServer(backend pb.CollideServiceServer) {
+	pb.RegisterCollideServiceServer(s.server, backend)
+}
