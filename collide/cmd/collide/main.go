08474fca1de5418d479a4c03b32fe458a21f4b83
zalgonoise <mariozalgo@gmail.com>
2025-06-18 19:05:28 +0200

    collide/cmd/collide: base scaffolding for app's entrypoint

--- collide/cmd/collide/main.go
+++ collide/cmd/collide/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"context"
+	"github.com/zalgonoise/x/cli"
+	"github.com/zalgonoise/x/collide/log"
+	"log/slog"
+	"os"
+)
 
+var modes = []string{"ca", "authz"}
+
+func main() {
+	logger := log.New("debug")
+
+	runner := cli.NewRunner("collide",
+		cli.WithOneOf(modes...),
+		cli.WithExecutors(map[string]cli.Executor{
+			"serve": cli.Executable(ExecServe),
+		}),
+	)
+
+	code, err := runner.Run(logger)
+	if err != nil {
+		logger.ErrorContext(context.Background(), "runtime error", slog.String("error", err.Error()))
+	}
+
+	os.Exit(code)
+}
+
+func ExecServe(ctx context.Context, logger *slog.Logger, args []string) (int, error) {
+	return 0, nil
 }
