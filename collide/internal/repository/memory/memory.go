939de2f09c1783231fe6d14f5f70c0ee7580a060
zalgonoise <mariozalgo@gmail.com>
2025-07-04 17:05:34 +0200

    collide/internal/repository/memory: instrumented ListDistricts; refactored constructor

--- collide/internal/repository/memory/memory.go
+++ collide/internal/repository/memory/memory.go
@@ -4,8 +4,14 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/zalgonoise/x/collide/internal/tracks"
+	"go.opentelemetry.io/otel/attribute"
+	otelcodes "go.opentelemetry.io/otel/codes"
+	"log/slog"
 	"slices"
+
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/zalgonoise/x/collide/internal/tracks"
 )
 
 var (
@@ -16,36 +22,57 @@ var (
 
 type Repository struct {
 	tracks *tracks.TrackList
+
+	logger *slog.Logger
+	tracer trace.Tracer
 }
 
-func New(tracks *tracks.TrackList) *Repository {
-	return &Repository{tracks: tracks}
+func New(logger *slog.Logger, tracer trace.Tracer) *Repository {
+	return &Repository{logger: logger, tracer: tracer}
 }
 
-func FromBytes(buf []byte) (*Repository, error) {
-	list := &tracks.TrackList{}
+func (r *Repository) FromBytes(buf []byte) error {
+	r.tracks = &tracks.TrackList{}
 
-	if _, err := list.Read(buf); err != nil {
-		return nil, err
+	if _, err := r.tracks.Read(buf); err != nil {
+		return err
 	}
 
-	return &Repository{tracks: list}, nil
+	return nil
 }
 
-func (r Repository) ListDistricts(ctx context.Context) ([]string, error) {
+func (r *Repository) ListDistricts(ctx context.Context) ([]string, error) {
+	ctx, span := r.tracer.Start(ctx, "Repository.ListDistricts", trace.WithAttributes(
+		attribute.Int("track_count", len(r.tracks.Tracks))))
+	defer span.End()
+
 	districts, err := tracks.GetDistricts(r.tracks)
 	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(otelcodes.Error, err.Error())
+		span.AddEvent("listing districts", trace.WithAttributes(
+			attribute.String("error", err.Error())))
+
 		return nil, err
 	}
 
 	if len(districts) == 0 {
+		span.RecordError(ErrNoDistricts)
+		span.SetStatus(otelcodes.Error, ErrNoDistricts.Error())
+		span.AddEvent("listing districts", trace.WithAttributes(
+			attribute.String("error", ErrNoDistricts.Error())))
+
 		return nil, ErrNoDistricts
 	}
+
+	span.AddEvent("listed districts successfully", trace.WithAttributes(
+		attribute.Int("district_count", len(districts))))
+	span.SetStatus(otelcodes.Ok, "")
 
 	return districts, nil
 }
 
-func (r Repository) ListAllTracksByDistrict(ctx context.Context, district string) ([]string, error) {
+func (r *Repository) ListAllTracksByDistrict(ctx context.Context, district string) ([]string, error) {
 	t, err := tracks.GetTracksByDistrict(r.tracks, district, false)
 	if err != nil {
 		return nil, err
@@ -67,7 +94,7 @@ func (r Repository) ListAllTracksByDistrict(ctx context.Context, district string
 	return tracks.GetNamesFromIDs(r.tracks, t)
 }
 
-func (r Repository) ListDriftTracksByDistrict(ctx context.Context, district string) ([]string, error) {
+func (r *Repository) ListDriftTracksByDistrict(ctx context.Context, district string) ([]string, error) {
 	t, err := tracks.GetTracksByDistrict(r.tracks, district, true)
 	if err != nil {
 		return nil, err
@@ -89,7 +116,7 @@ func (r Repository) ListDriftTracksByDistrict(ctx context.Context, district stri
 	return tracks.GetNamesFromIDs(r.tracks, t)
 }
 
-func (r Repository) GetAlternativesByDistrictAndTrack(ctx context.Context, district, track string) ([]string, error) {
+func (r *Repository) GetAlternativesByDistrictAndTrack(ctx context.Context, district, track string) ([]string, error) {
 	trackID, err := tracks.GetIDFromName(r.tracks, track)
 	if err != nil {
 		return nil, err
@@ -116,7 +143,7 @@ func (r Repository) GetAlternativesByDistrictAndTrack(ctx context.Context, distr
 	return tracks.GetNamesFromIDs(r.tracks, t)
 }
 
-func (r Repository) GetCollisionsByDistrictAndTrack(ctx context.Context, district, track string) ([]string, error) {
+func (r *Repository) GetCollisionsByDistrictAndTrack(ctx context.Context, district, track string) ([]string, error) {
 	trackID, err := tracks.GetIDFromName(r.tracks, track)
 	if err != nil {
 		return nil, err
