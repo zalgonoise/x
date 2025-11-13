67f3690e1ac7c8fe3127a7bc794c317c6c3e8a40
zalgonoise <mariozalgo@gmail.com>
2025-11-13 04:13:51 +0100

    tempvar: chance: add variables by chance

---
+++ tempvar/chance.go
@@ -0,0 +1,58 @@
+package tempvar
+
+import (
+	"encoding/binary"
+	"errors"
+	"math/rand/v2"
+	"time"
+)
+
+var (
+	ErrMaxMustNotBeZero  = errors.New("max must not be zero")
+	ErrThresholdOverflow = errors.New("threshold overflow")
+)
+
+type Chance[T any] struct {
+	rng    *rand.Rand
+	max    uint
+	thresh uint
+	value  *T
+}
+
+func NewChance[T any](value *T, max uint, thresh uint) (*Chance[T], error) {
+	if max == 0 {
+		return nil, ErrMaxMustNotBeZero
+	}
+
+	if thresh >= max {
+		return nil, ErrThresholdOverflow
+	}
+
+	return &Chance[T]{
+		rng:    setupRNG(),
+		max:    max,
+		thresh: thresh,
+		value:  value,
+	}, nil
+}
+
+func (c *Chance[T]) Value() *T {
+	if c.thresh > c.rng.UintN(c.max) {
+		return nil
+	}
+
+	return c.value
+}
+
+func setupRNG() *rand.Rand {
+	b := make([]byte, 32)
+
+	now := uint64(time.Now().UnixMilli())
+
+	binary.NativeEndian.PutUint64(b, now)
+	binary.NativeEndian.PutUint64(b, now)
+	binary.NativeEndian.PutUint64(b, now)
+	binary.NativeEndian.PutUint64(b, now)
+
+	return rand.New(rand.NewChaCha8([32]byte(b)))
+}
---
+++ tempvar/chance_test.go
@@ -0,0 +1,65 @@
+package tempvar
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/zalgonoise/x/is"
+)
+
+func TestChance(t *testing.T) {
+	type user struct {
+		name string
+		id   int
+	}
+
+	u := user{
+		name: "Gopher",
+		id:   1,
+	}
+
+	for _, testcase := range []struct {
+		name   string
+		max    uint
+		thresh uint
+		err    error
+		wants  *user
+	}{
+		{
+			name:   "Success/MostLikelyPass",
+			max:    1_000_000_000,
+			thresh: 1,
+			wants:  &u,
+		},
+		{
+			name:   "Success/MostLikelyFail",
+			max:    1_000_000_000,
+			thresh: 999_999_999,
+		},
+		{
+			name:   "Fail/MaxIsZero",
+			max:    0,
+			thresh: 1,
+			err:    ErrMaxMustNotBeZero,
+		},
+		{
+			name:   "Fail/ThresholdOverflow",
+			max:    1,
+			thresh: 1,
+			err:    ErrThresholdOverflow,
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			v, err := NewChance(&u, testcase.max, testcase.thresh)
+			if err != nil {
+				is.True(t, errors.Is(err, testcase.err))
+
+				return
+			}
+
+			is.NilError(t, err)
+			is.True(t, v != nil)
+			is.EqualValue(t, v.Value(), testcase.wants)
+		})
+	}
+}
